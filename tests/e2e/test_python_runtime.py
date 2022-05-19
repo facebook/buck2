9da6a01902ffa91ce4b88f8c3edf5cf3b72ecbf4
Alexander Mols <asm@fb.com>
2022-05-19 08:08:07 -0700

    Allow users to decorate the python main module
    Summary:
    It's common for Python developer tools to decorate the main module at
    runtime.  E.g. calling `python -m <tool> -m <original args>`. This is tricky to
    do with buck's inplace par files, because `sys.path` gets set up via a startup
    script that is executed through `-c ...`.
    
    Add support for decorating the main module in the inplace par. When the
    `PAR_MAIN_OVERRIDE` environment variable is set, run the main module from the
    environment variable instead. Set the `PAR_MAIN_ORIGINAL` environment variable
    to the original main module for the decorating module to pick up.
    
    This approach will allow us to ship single file decorating modules to enable
    the use of Python developer tools within our repositories.
    
    This is a port of D28350583 for Buck1.
    
    Reviewed By: RuijieC-dev
    
    Differential Revision: D36483019
    
    fbshipit-source-id: 04c346b14800fc97b30b62aa285d3f48d3ce754c

--- tests/e2e/test_python_runtime.py
+++ tests/e2e/test_python_runtime.py
@@ -47,3 +47,46 @@ async def test_python_par_supports_pdb_debugging(buck: Buck) -> None:
 
         # pdb hit a breakpoint
         assert b"Breakpoint 1 at" in stdout
+
+
+@buck_test(inplace=True)
+async def test_python_par_decorating_main(buck: Buck) -> None:
+    args = [
+        "fbcode//buck2/tests/targets/rules/python/decorate_main:decorate_main",
+        "--show-full-output",
+    ]
+    if sys.platform == "darwin":
+        args.append("@mode/mac")
+    result = await buck.build(*args)
+    output_dict = result.get_target_to_build_output()
+    for _target, output in output_dict.items():
+        gen_folder = output[: output.find("/gen/") + 4]
+        symlink = Path(
+            gen_folder,
+            # v1:   buck2/tests/targets/rules/python/decorate_main/decorate_main.par
+            "fbcode/buck2/tests/targets/rules/python/decorate_main/decorate_main.par",
+        )
+        assert os.path.exists(symlink)
+
+        # Run the resulting binary without decorating main
+        stdout = await run(symlink, env={})
+        assert b"Hello from original main!" in stdout
+
+        # Run the resulting binary and decorate main
+        stdout = await run(symlink, env={"PAR_MAIN_OVERRIDE": "main_wrapper"})
+        assert b"Hello from main wrapper for original_main!" in stdout
+
+
+# Run the binary. Returns stdout.
+async def run(binary: Path, env) -> bytes:
+    proc = await asyncio.create_subprocess_exec(
+        binary,
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        env=env,
+    )
+
+    stdout, _stderr = await proc.communicate()
+
+    return stdout
