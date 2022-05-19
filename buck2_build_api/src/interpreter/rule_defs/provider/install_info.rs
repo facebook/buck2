e4d62beb4b4365143ad6616d1cefc72c380190f7
Neil Mitchell <ndmitchell@fb.com>
2022-05-19 05:39:36 -0700

    Delete copy/pasted tests
    Summary: These aren't about InstallInfo at all
    
    Reviewed By: krallin
    
    Differential Revision: D36510392
    
    fbshipit-source-id: 29db81fdbbf741fb0dcb012bfd63f25db2008f45

--- buck2_build_api/src/interpreter/rule_defs/provider/install_info.rs
+++ buck2_build_api/src/interpreter/rule_defs/provider/install_info.rs
@@ -79,83 +79,10 @@ mod tests {
     use buck2_core::result::SharedResult;
     use indoc::indoc;
 
-    use crate::interpreter::{
-        rule_defs::{
-            artifact::testing::artifactory, cmd_args::tester::command_line_stringifier,
-            provider::tester::collection_creator,
-        },
-        testing::{run_starlark_bzl_test_expecting_error, Tester},
-    };
+    use crate::interpreter::{rule_defs::provider::tester::collection_creator, testing::Tester};
 
     #[test]
-    fn run_info_stringifies() -> SharedResult<()> {
-        let mut tester = Tester::new()?;
-        tester.set_additional_globals(Arc::new(|globals| {
-            command_line_stringifier(globals);
-            artifactory(globals);
-        }));
-        let content = indoc!(
-            r#"
-            a = source_artifact("foo", "bar.o")
-            b = source_artifact("foo/bar", "baz.o")
-            c = source_artifact("bar", "baz.o")
-            f1 ={}
-            f2={"foo": a}
-            f3={"c": c,  "b":b}
-
-            ri1=RunInfo()
-            ri2=RunInfo(args=["foo.o"])
-            ri3=RunInfo(args=["bar/foo.o","-some_option"])
-
-            frozen_ii1=InstallInfo(installer=ri1, files={})
-            frozen_ii2=InstallInfo(installer=ri2,files=f2)
-            frozen_ii3=InstallInfo(installer=ri3,files=f3)
-
-            def test():
-                ri=RunInfo()
-                assert_eq("RunInfo(args=cmd_args())", repr(frozen_ii1.installer))
-                assert_eq({}, frozen_ii1.files)
-                assert_eq([], get_args(frozen_ii1.installer))
-                assert_eq(["foo.o"], get_args(frozen_ii2.installer))
-                assert_eq(["foo"], frozen_ii2.files.keys())
-
-                assert_eq(["bar/foo.o", "-some_option"], get_args(frozen_ii3.installer))
-                assert_eq("<source bar/baz.o>",repr(frozen_ii3.files["c"]))
-            "#
-        );
-        tester.run_starlark_bzl_test(content)
-    }
-
-    #[test]
-    fn run_info_validates_types() {
-        let content_bad_args1 = indoc!(
-            r#"
-            def test():
-                df = DefaultInfo()
-                InstallInfo(installer=df,files={})
-            "#
-        );
-        run_starlark_bzl_test_expecting_error(content_bad_args1, "Type of parameter");
-
-        let content_bad_args2 = indoc!(
-            r#"
-            def test():
-                InstallInfo(installer=RunInfo(),files=[])
-            "#
-        );
-        run_starlark_bzl_test_expecting_error(content_bad_args2, "Type of parameter");
-
-        let content_bad_args3 = indoc!(
-            r#"
-            def test():
-                InstallInfo(installer=RunInfo(),files={"foo":"foo.o"})
-            "#
-        );
-        run_starlark_bzl_test_expecting_error(content_bad_args3, "Type of parameter");
-    }
-
-    #[test]
-    fn test_info_works_as_provider_key() -> SharedResult<()> {
+    fn install_info_works_as_provider_key() -> SharedResult<()> {
         let mut tester = Tester::new()?;
         tester.set_additional_globals(Arc::new(collection_creator));
 
