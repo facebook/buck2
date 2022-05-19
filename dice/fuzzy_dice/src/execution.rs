9abc2de18cf67397b6974c41d4b430f1a5910cd8
Jasper Chapman-Black <jaspercb@fb.com>
2022-05-19 14:52:55 -0700

    Add answer-checking functionality to fuzzer
    Summary:
    This is complicated slightly by the fact that sometimes panicking is the correct behavior for DICE - say we set InjectedKey1 at v1, set InjectedKey2 at v2, and then try to read InjectedKey2 at v1: that should panic, so we need to check that and catch it.
    
    The implementation of this is naive, since it doesn't re-use shared values computed at the same version. In practice I think this is fine because most fuzz test cases are computing values at wildly different versions, and also are qutie small.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D36359914
    
    fbshipit-source-id: 26e36b6157f35706951aea85e467f1406141c195

--- dice/fuzzy_dice/src/execution.rs
+++ dice/fuzzy_dice/src/execution.rs
@@ -16,11 +16,102 @@ use std::{
 
 use crossbeam::queue::SegQueue;
 use dice::{cycles::DetectCycles, Dice, DiceTransaction};
+use futures::FutureExt;
 use gazebo::prelude::*;
 use quickcheck::{Arbitrary, Gen};
 use serde::{Deserialize, Serialize};
 
 use crate::computation::{ComputationStep, Expr, FuzzEquations, FuzzMath, FuzzState, Unit, Var};
+
+/// For a given DiceExecutionOrder that includes some Queries, computes the
+/// expected result of each of those queries.
+/// A result of None indicates the computation is expected to fail or panic.
+struct MathAnswerKey {
+    values_by_ctx_id: HashMap<usize, HashMap<Var, Option<bool>>>,
+}
+
+impl MathAnswerKey {
+    pub fn new(order: &DiceExecutionOrder) -> Self {
+        // Pre-process the execution order to see which computations will be requested.
+        let queried_vars_by_ctx_id = Self::queried_vars_by_ctx_id(order);
+        // Maintain the current set of exprs.
+        let mut exprs = HashMap::new();
+        let mut values_by_ctx_id: HashMap<usize, HashMap<Var, Option<bool>>> = HashMap::new();
+        for op in order.init_vars.iter().chain(order.timeline.iter()) {
+            match op {
+                Operation::SetValue {
+                    new_ctx_id,
+                    var,
+                    expr,
+                } => {
+                    exprs.insert(*var, expr.clone());
+                    if let Some(queried_vars) = queried_vars_by_ctx_id.get(new_ctx_id) {
+                        values_by_ctx_id.insert(
+                            *new_ctx_id,
+                            queried_vars
+                                .iter()
+                                .map(|v| (*v, Self::eval(*v, &exprs)))
+                                .collect(),
+                        );
+                    }
+                }
+                Operation::Query { .. } => (),
+                Operation::EnqueueStep(..) => (),
+            }
+        }
+        Self { values_by_ctx_id }
+    }
+
+    pub fn value_of_at_ctx(&self, ctx_id: usize, var: Var) -> Option<bool> {
+        self.values_by_ctx_id
+            .get(&ctx_id)
+            .and_then(|map| map.get(&var))
+            .copied()
+            .unwrap_or(None)
+    }
+
+    fn queried_vars_by_ctx_id(order: &DiceExecutionOrder) -> HashMap<usize, Vec<Var>> {
+        let mut queried_vars_by_ctx_id: HashMap<usize, Vec<Var>> = HashMap::new();
+        for op in order.timeline.iter() {
+            if let Operation::Query { ctx_id, var } = op {
+                queried_vars_by_ctx_id
+                    .entry(*ctx_id)
+                    .or_insert_with(std::vec::Vec::new)
+                    .push(*var);
+            }
+        }
+        queried_vars_by_ctx_id
+    }
+
+    fn resolve_unit(unit: &Unit, exprs: &HashMap<Var, Expr>) -> Option<bool> {
+        match unit {
+            Unit::Variable(var) => Self::eval(*var, exprs),
+            Unit::Literal(lit) => Some(*lit),
+        }
+    }
+
+    fn eval(var: Var, exprs: &HashMap<Var, Expr>) -> Option<bool> {
+        exprs.get(&var).and_then(|expr| match expr {
+            Expr::Unit(unit) => Self::resolve_unit(unit, exprs),
+            Expr::Cond {
+                test,
+                then,
+                otherwise,
+            } => {
+                if Self::resolve_unit(test, exprs)? {
+                    Self::resolve_unit(then, exprs)
+                } else {
+                    Self::resolve_unit(otherwise, exprs)
+                }
+            }
+            Expr::Xor(vars) => vars
+                .iter()
+                .map(|x| Self::resolve_unit(x, exprs))
+                .collect::<Option<Vec<_>>>()
+                .map(|bools| bools.into_iter().reduce(|x, y| x ^ y).unwrap_or(false)),
+        })
+    }
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum Operation {
@@ -73,18 +164,23 @@ impl DiceExecutionOrder {
     /// Since shrinking a big testcase takes multiple consecutive failures,
     /// without multiple samples, shrinking will usually stop early.
     pub async fn execute(&self, options: &DiceExecutionOrderOptions) -> anyhow::Result<()> {
+        let answer_key = MathAnswerKey::new(self);
         let ntimes = if self.is_shrinking {
             Self::NSAMPLES_SHRINKING
         } else {
             Self::NSAMPLES_SEARCHING
         };
         for _ in 0..ntimes {
-            self.execute_once(options).await?;
+            self.execute_once(&answer_key, options).await?;
         }
         Ok(())
     }
 
-    async fn execute_once(&self, options: &DiceExecutionOrderOptions) -> anyhow::Result<()> {
+    async fn execute_once(
+        &self,
+        answer_key: &MathAnswerKey,
+        options: &DiceExecutionOrderOptions,
+    ) -> anyhow::Result<()> {
         let dice = Dice::builder().build(DetectCycles::Disabled);
         let mut dice_ctxs: HashMap<usize, DiceTransaction> = HashMap::new();
         let state = {
@@ -102,7 +198,32 @@ impl DiceExecutionOrder {
             match &op {
                 Operation::Query { ctx_id, var } => {
                     if let Some(ctx) = dice_ctxs.get(ctx_id) {
-                        ctx.eval(state.dupe(), *var).await;
+                        let expected = answer_key.value_of_at_ctx(*ctx_id, *var);
+                        match expected {
+                            Some(expected_result) => {
+                                let result = ctx.eval(state.dupe(), *var).await;
+                                Self::maybe_dump_dice(options, &dice)
+                                    .expect("couldn't dump DICE to stderr");
+                                assert_eq!(
+                                    expected_result, result,
+                                    "incremental computation gave wrong answer for {:?}",
+                                    &op
+                                );
+                            }
+                            None => {
+                                if let Ok(actual_value) = std::panic::AssertUnwindSafe(async {
+                                    ctx.eval(state.dupe(), *var).await
+                                })
+                                .catch_unwind()
+                                .await
+                                {
+                                    panic!(
+                                        "expected computing {:?} to panic, but didn't panic, got {}",
+                                        &op, actual_value
+                                    );
+                                }
+                            }
+                        }
                     }
                 }
                 Operation::SetValue {
@@ -113,6 +234,7 @@ impl DiceExecutionOrder {
                     let ctx = dice.ctx();
                     ctx.set_equation(*var, expr.clone());
                     dice_ctxs.insert(*new_ctx_id, ctx.commit());
+                    Self::maybe_dump_dice(options, &dice).expect("couldn't dump DICE to stderr");
                 }
                 Operation::EnqueueStep(var, steps) => {
                     let queue = state.steps.get(var).unwrap();
@@ -121,7 +243,6 @@ impl DiceExecutionOrder {
                     }
                 }
             }
-            Self::maybe_dump_dice(options, &dice).expect("couldn't dump DICE to stderr");
         }
         Ok(())
     }
@@ -261,5 +382,50 @@ impl Iterator for DiceExecutionOrderShrinker {
             ]
             .concat(),
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn answer_key() {
+        let order = DiceExecutionOrder {
+            init_vars: vec![],
+            timeline: vec![
+                Operation::SetValue {
+                    new_ctx_id: 1,
+                    var: Var(1),
+                    expr: Expr::Unit(Unit::Literal(true)),
+                },
+                Operation::SetValue {
+                    new_ctx_id: 2,
+                    var: Var(1),
+                    expr: Expr::Unit(Unit::Literal(false)),
+                },
+                Operation::Query {
+                    ctx_id: 1,
+                    var: Var(1),
+                },
+                Operation::Query {
+                    ctx_id: 2,
+                    var: Var(1),
+                },
+                Operation::Query {
+                    ctx_id: 1,
+                    var: Var(2),
+                },
+            ],
+            is_shrinking: false,
+        };
+        let answer_key = MathAnswerKey::new(&order);
+        // Two present and well-behaved query results.
+        assert_eq!(Some(true), answer_key.value_of_at_ctx(1, Var(1)));
+        assert_eq!(Some(false), answer_key.value_of_at_ctx(2, Var(1)));
+        // A query of an injected key that hasn't been set yet should be None.
+        assert_eq!(None, answer_key.value_of_at_ctx(1, Var(2)));
+        // A never-queried result should be None.
+        assert_eq!(None, answer_key.value_of_at_ctx(2, Var(2)));
     }
 }
