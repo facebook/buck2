bf1121b9dbd9b578a8b907bdae4966c79c8f5b3f
Junpeng Qiu <qjp@fb.com>
2022-06-01 17:57:32 -0700

    Bootstrap intellij project generation
    Summary: Added a simple IJ project generation logic & tests
    
    Reviewed By: bobyangyf
    
    Differential Revision: D36639012
    
    fbshipit-source-id: b48c15c40f993f3dcb9125d47e78a0b90358255d

---
+++ tests/e2e/intellij_project/test_intellij_project.py
@@ -0,0 +1,33 @@
+import json
+from pathlib import Path
+
+from xplat.build_infra.buck_e2e.api.buck import Buck
+from xplat.build_infra.buck_e2e.buck_workspace import buck_test
+
+
+@buck_test(inplace=True)
+async def test_generate_intellij_project(buck: Buck) -> None:
+    bxl_label = (
+        "fbcode//buck2/prelude/intellij_project/main.bxl:generate_intellij_project"
+    )
+    result = await buck.bxl(
+        bxl_label,
+        "--show-all-outputs",
+        "--show-all-outputs-format",
+        "full_json",
+    )
+
+    outputs = json.loads(result.stdout)
+    output_file = outputs[bxl_label][0]
+    assert (
+        Path(output_file).read_text()
+        == """\
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="ProjectModuleManager">
+    <modules>
+    </modules>
+  </component>
+</project>
+"""
+    )
