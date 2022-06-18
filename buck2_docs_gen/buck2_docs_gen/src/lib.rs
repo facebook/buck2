6fb05fa8340c70f264607a5eafbcd05ce34e85de
Bob Yang <bobyf@fb.com>
2022-06-17 21:28:01 -0700

    Add inventory to be exposed via derive_refs
    Reviewed By: stepancheg
    
    Differential Revision: D37129739
    
    fbshipit-source-id: b6308abb81043cb336ff468943cf0de2c028ea2d

--- buck2_docs_gen/buck2_docs_gen/src/lib.rs
+++ buck2_docs_gen/buck2_docs_gen/src/lib.rs
@@ -24,3 +24,12 @@ impl StarlarkObject {
 }
 
 inventory::collect!(StarlarkObject);
+
+/// __derive_refs allows us to reference other crates in buck2_docs_gen_derive without users
+/// needing to be aware of those dependencies. We make them public here and then can reference
+/// them like `buck2_docs_gen::__derive_refs::foo`.
+#[doc(hidden)]
+pub mod __derive_refs {
+    pub use inventory;
+    pub use starlark;
+}
