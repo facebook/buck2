26a500813266d30d307503aa0c2961da1b4ee425
Bob Yang <bobyf@fb.com>
2022-06-17 21:28:01 -0700

    Add StarlarkObject to hold documentation objects
    Reviewed By: stepancheg
    
    Differential Revision: D37129743
    
    fbshipit-source-id: 12005695d3695516ece9e4b59995d2f3d43fa4a6

--- buck2_docs_gen/buck2_docs_gen/src/lib.rs
+++ buck2_docs_gen/buck2_docs_gen/src/lib.rs
@@ -1,2 +1,17 @@
 //!
 //! Documentation generation utilities
+
+use starlark::values::docs::DocItem;
+
+/// A starlark object to be documented
+pub struct StarlarkObject {
+    pub name: &'static str,
+    pub module: Box<dyn Fn() -> DocItem>,
+}
+
+impl StarlarkObject {
+    /// Gets the docitem to this starlark object
+    pub fn get_docs(&self) -> DocItem {
+        (self.module)()
+    }
+}
