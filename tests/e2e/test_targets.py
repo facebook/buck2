7e87c7df85451ac13e94714533a5b1d9906674aa
Ben Foxman <benfoxman@fb.com>
2022-06-27 15:06:25 -0700

    Adding e2e test cases for unconfigured hashing
    Summary: Now that we have unconfigured target hashing implemented, we write the equivalent e2e test cases that configured_hashing had, as well as a test that the hashes differ between the two graph_types.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D37424034
    
    fbshipit-source-id: b1e31fc1538202678b9b44e0d959564f374b99a8

--- tests/e2e/test_targets.py
+++ tests/e2e/test_targets.py
@@ -29,7 +29,7 @@ BAR_UNMODIFIED = ["1", "2", "3", "4", "5"]
         ("7", BAR_UNMODIFIED, "bar.txt"),
     ],
 )
-async def test_target_hashing(
+async def test_configured_target_hashing(
     buck: Buck, rule: str, unmodified: list, src_changed: str
 ) -> None:
     target = "fbcode//buck2/tests/targets/target_hashing:rule{}".format(rule)
@@ -45,6 +45,60 @@ async def test_target_hashing(
     modified_result = await buck.targets(
         target,
         "--show-target-hash",
+        "--json",
+        "--target-hash-file-mode",
+        "PATHS_ONLY",
+        "--target-hash-modified-paths",
+        "buck2/tests/targets/target_hashing/{}".format(src_changed),
+    )
+    output = json.loads(result.stdout)
+    modified_output = json.loads(modified_result.stdout)
+
+    # Hash should change iff the target is the modified target or depends on the modified target
+    if rule in unmodified:
+        assert output[0]["$target_hash"] == modified_output[0]["$target_hash"]
+    else:
+        assert output[0]["$target_hash"] != modified_output[0]["$target_hash"]
+
+
+# TODO: Make this test isolated, i.e. inplace=False.
+@buck_test(inplace=True)
+@pytest.mark.parametrize(
+    "rule, unmodified, src_changed",
+    [
+        ("1", FOO_UNMODIFIED, "foo.txt"),
+        ("2", FOO_UNMODIFIED, "foo.txt"),
+        ("3", FOO_UNMODIFIED, "foo.txt"),
+        ("4", FOO_UNMODIFIED, "foo.txt"),
+        ("5", FOO_UNMODIFIED, "foo.txt"),
+        ("6", FOO_UNMODIFIED, "foo.txt"),
+        ("7", FOO_UNMODIFIED, "foo.txt"),
+        ("1", BAR_UNMODIFIED, "bar.txt"),
+        ("2", BAR_UNMODIFIED, "bar.txt"),
+        ("3", BAR_UNMODIFIED, "bar.txt"),
+        ("4", BAR_UNMODIFIED, "bar.txt"),
+        ("5", BAR_UNMODIFIED, "bar.txt"),
+        ("6", BAR_UNMODIFIED, "bar.txt"),
+        ("7", BAR_UNMODIFIED, "bar.txt"),
+    ],
+)
+async def test_unconfigured_target_hashing(
+    buck: Buck, rule: str, unmodified: list, src_changed: str
+) -> None:
+
+    target = "fbcode//buck2/tests/targets/target_hashing:rule{}".format(rule)
+    result = await buck.targets(
+        target,
+        "--show-unconfigured-target-hash",
+        "--json",
+        "--target-hash-file-mode",
+        "PATHS_ONLY",
+    )
+
+    # Modify a target
+    modified_result = await buck.targets(
+        target,
+        "--show-unconfigured-target-hash",
         "--json",
         "--target-hash-file-mode",
         "PATHS_ONLY",
