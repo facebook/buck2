41b67a10f6866ad2cae6d5869dfb894a492645b0
Ian Childs <ianc@fb.com>
2022-07-13 14:41:08 -0700

    Sort libraries into project libraries and module libraries
    Summary: Intellij project generation has the concept of "project libraries" and "module libraries". This looks through all the module dep to find which libraries should be considered as each, and then write the project libraries as before, but now writes the module libraries as part of the module definition.
    
    Reviewed By: milend
    
    Differential Revision: D37786331
    
    fbshipit-source-id: 60ae537e2d9a8491d2de9de3ae9ccdb2b44c1024

--- tests/e2e/intellij_project/test_intellij_project.py
+++ tests/e2e/intellij_project/test_intellij_project.py
@@ -50,22 +50,10 @@ async def test_generate_intellij_project(buck: Buck) -> None:
 """
     )
 
-    assert (
-        (
-            output_dir
-            / "libraries/__fbandroid_buck2_tests_good_sample_intellij_project_prebuilt_jar_no_sources_prebuilt_no_sources__.xml"
-        ).read_text()
-        == """\
-<component name="libraryTable">
-  <library name="fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources:prebuilt_no_sources">
-    <CLASSES>
-      <root url="jar://$PROJECT_DIR$/fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources/prebuilt_no_sources.jar!/" />
-    </CLASSES>
-    <JAVADOC />
-  </library>
-</component>
-"""
-    )
+    assert not (
+        output_dir
+        / "libraries/__fbandroid_buck2_tests_good_sample_intellij_project_prebuilt_jar_no_sources_prebuilt_no_sources__.xml"
+    ).exists()
 
     assert (
         (
@@ -76,6 +64,14 @@ async def test_generate_intellij_project(buck: Buck) -> None:
 <?xml version="1.0" encoding="UTF-8"?>
 <module type="JAVA_MODULE" version="4">
   <component name="NewModuleRootManager" inherit-compiler-output="false">
+    <orderEntry type="module-library" scope="COMPILE">
+      <library name="fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources:prebuilt_no_sources">
+        <CLASSES>
+          <root url="jar://$PROJECT_DIR$/fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources/prebuilt_no_sources.jar!/" />
+        </CLASSES>
+        <JAVADOC />
+      </library>
+    </orderEntry>
   </component>
 </project>
 """
