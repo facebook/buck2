eb8529efae4bae1c05f8b646c6ed6f5672626fb6
Ian Childs <ianc@fb.com>
2022-07-29 04:25:13 -0700

    Support for android_prebuilt_aar
    Summary: These are also represented as an `IjLibrary`. The `.aar` is unzipped into various parts - right now we just access the `.jar` that is unzipped and constructed in the rule, and that is our `binary_jar`.
    
    Reviewed By: cute-jumper
    
    Differential Revision: D38150289
    
    fbshipit-source-id: 13e8be16fe3e7e58c049e675bf2f98d03c86e346

--- tests/e2e/intellij_project/test_intellij_project.py
+++ tests/e2e/intellij_project/test_intellij_project.py
@@ -1,4 +1,5 @@
 import json
+import re
 from pathlib import Path
 
 from xplat.build_infra.buck_e2e.api.buck import Buck
@@ -56,7 +57,7 @@ async def test_generate_intellij_project(buck: Buck) -> None:
         modules_dir
         / "fbandroid_buck2_tests_good_sample_intellij_project_android_main.json"
     ).open("r") as json_file:
-        assert json.load(json_file) == {
+        assert _sanitize_buck_out_paths(json.load(json_file)) == {
             "dependencies": {
                 "LIBRARY": [
                     {
@@ -72,13 +73,23 @@ async def test_generate_intellij_project(buck: Buck) -> None:
                     {
                         "library": {
                             "binaryJars": [
+                                "buck-out-path",
+                            ],
+                            "name": "fbsource//fbandroid/buck2/tests/good/sample_intellij_project/android_prebuilt_aar:aar",
+                            "sourceJars": [],
+                            "type": "DEFAULT",
+                        }
+                    },
+                    {
+                        "library": {
+                            "binaryJars": [
                                 "fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources/prebuilt_no_sources.jar"
                             ],
                             "name": "fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources:prebuilt_no_sources",
                             "sourceJars": [],
                             "type": "DEFAULT",
                         }
-                    }
+                    },
                 ],
             }
         }
@@ -122,3 +133,16 @@ async def test_generate_sample_project(buck: Buck) -> None:
         "--mode",
         "foo",
     )
+
+
+def _sanitize_buck_out_paths(data):
+    if type(data) == dict:
+        return {key: _sanitize_buck_out_paths(value) for key, value in data.items()}
+    elif type(data) == list:
+        return [_sanitize_buck_out_paths(x) for x in data]
+    elif type(data) == str:
+        return re.sub(r"buck-out/[^\"]*", "buck-out-path", data)
+    else:
+        raise Exception(
+            "Unknown data type for _sanitize_buck_out_paths: {}".format(type(data))
+        )
