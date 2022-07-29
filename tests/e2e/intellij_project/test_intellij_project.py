9e86c2cda20d80ee10ae26dc1d823899c879ac96
Ian Childs <ianc@fb.com>
2022-07-29 02:12:31 -0700

    Write modules as JSON, in the same format as D38224361
    Summary: ^, see summary from previous diff for motivation.
    
    Reviewed By: cute-jumper
    
    Differential Revision: D38240158
    
    fbshipit-source-id: fc9b3d1071be0f49696858fc9f9d2aec3f9f5add

--- tests/e2e/intellij_project/test_intellij_project.py
+++ tests/e2e/intellij_project/test_intellij_project.py
@@ -52,52 +52,63 @@ async def test_generate_intellij_project(buck: Buck) -> None:
     modules_dir = output_dir / "modules"
     assert len(list(modules_dir.iterdir())) == 2
 
-    assert (
-        (
-            modules_dir
-            / "fbandroid_buck2_tests_good_sample_intellij_project_android_main.iml"
-        ).read_text()
-        == """\
-<?xml version="1.0" encoding="UTF-8"?>
-<module type="JAVA_MODULE" version="4">
-  <component name="NewModuleRootManager" inherit-compiler-output="false">
-    <orderEntry type="module" module-name="fbandroid_buck2_tests_good_sample_intellij_project_java_single_lib" scope="COMPILE" />
-    <orderEntry type="module-library" scope="COMPILE">
-      <library name="fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources:prebuilt_no_sources">
-        <CLASSES>
-          <root url="jar://$PROJECT_DIR$/fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources/prebuilt_no_sources.jar!/" />
-        </CLASSES>
-        <JAVADOC />
-      </library>
-    </orderEntry>
-    <orderEntry type="library" name="fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar:prebuilt" scope="COMPILE" level="project" />
-  </component>
-</project>
-"""
-    )
+    with (
+        modules_dir
+        / "fbandroid_buck2_tests_good_sample_intellij_project_android_main.json"
+    ).open("r") as json_file:
+        assert json.load(json_file) == {
+            "dependencies": {
+                "LIBRARY": [
+                    {
+                        "name": "fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar:prebuilt"
+                    }
+                ],
+                "MODULE": [
+                    {
+                        "name": "fbandroid_buck2_tests_good_sample_intellij_project_java_single_lib"
+                    }
+                ],
+                "MODULE-LIBRARY": [
+                    {
+                        "library": {
+                            "binaryJars": [
+                                "fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources/prebuilt_no_sources.jar"
+                            ],
+                            "name": "fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar_no_sources:prebuilt_no_sources",
+                            "sourceJars": [],
+                            "type": "DEFAULT",
+                        }
+                    }
+                ],
+            }
+        }
 
-    assert (
-        (
-            modules_dir
-            / "fbandroid_buck2_tests_good_sample_intellij_project_java_single_lib.iml"
-        ).read_text()
-        == """\
-<?xml version="1.0" encoding="UTF-8"?>
-<module type="JAVA_MODULE" version="4">
-  <component name="NewModuleRootManager" inherit-compiler-output="false">
-    <orderEntry type="module-library" scope="COMPILE">
-      <library name="fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar:prebuilt_in_same_package">
-        <CLASSES>
-          <root url="jar://$PROJECT_DIR$/fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar/another_prebuilt.jar!/" />
-        </CLASSES>
-        <JAVADOC />
-      </library>
-    </orderEntry>
-    <orderEntry type="library" name="fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar:prebuilt" scope="COMPILE" level="project" />
-  </component>
-</project>
-"""
-    )
+    with (
+        modules_dir
+        / "fbandroid_buck2_tests_good_sample_intellij_project_java_single_lib.json"
+    ).open("r") as json_file:
+        assert json.load(json_file) == {
+            "dependencies": {
+                "LIBRARY": [
+                    {
+                        "name": "fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar:prebuilt"
+                    }
+                ],
+                "MODULE": [],
+                "MODULE-LIBRARY": [
+                    {
+                        "library": {
+                            "binaryJars": [
+                                "fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar/another_prebuilt.jar"
+                            ],
+                            "name": "fbsource//fbandroid/buck2/tests/good/sample_intellij_project/prebuilt_jar:prebuilt_in_same_package",
+                            "sourceJars": [],
+                            "type": "DEFAULT",
+                        }
+                    }
+                ],
+            }
+        }
 
 
 @buck_test(inplace=True)
