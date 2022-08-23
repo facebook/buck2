864cae42fc861f4761d06dc5914d3e7860402198
Princy Joy <princy@fb.com>
2022-08-23 12:18:10 -0700

    Update testcase to verify XML files instead of JSON
    Summary:
    - Removed json files and added their corresponding xml/iml files.
    - Added the buck run command to call the external script.
    - Minor refactorings and changes in files.
    
    Reviewed By: cute-jumper
    
    Differential Revision: D38878949
    
    fbshipit-source-id: b91c43c5dc5c8b44a832e3cfb20ca81564d8d912

--- tests/e2e/intellij_project/test_intellij_project.py
+++ tests/e2e/intellij_project/test_intellij_project.py
@@ -1,54 +1,71 @@
-import json
+import difflib
 import re
+import tempfile
 from pathlib import Path
 
 from xplat.build_infra.buck_e2e.api.buck import Buck
 from xplat.build_infra.buck_e2e.buck_workspace import buck_test
 
-bxl_label = "fbcode//buck2/prelude/intellij_project/main.bxl:generate_intellij_project"
-expected_dir_relative_path = "buck2/tests/e2e/intellij_project/testdata"
+BXL_LABEL = "fbcode//buck2/prelude/intellij_project/main.bxl:generate_intellij_project"
+EXPECTED_DIR_RELATIVE_PATH = "buck2/tests/e2e/intellij_project/testdata"
+
+# TODO: Find a more general pattern: This regex work only if target is in fbandroid.
+# Or replace with just general re.compile(r"\b[0-9a-f]{16}\b") if the path doesn't
+# need sanitizing for other segments.
+BUCK_OUT_HASH_PATTERN = re.compile(r"buck-out/(.*)/fbandroid")
+BUCK_OUT_HASH_REPLACE_TXT = "buck-out/buck-out-hash/fbandroid"
 
 
 @buck_test(inplace=True)
-async def test_generate_intellij_project(buck: Buck) -> None:
-    expected_dir = buck.cwd / expected_dir_relative_path / "apk" / ".idea"
+async def test_android_apk(buck: Buck) -> None:
+    expected_dir_name = "apk"
+    test_target = "fbsource//fbandroid/buck2/tests/good/sample_intellij_project/apk:apk"
+    await run_and_verify_project(buck, expected_dir_name, test_target)
+
+
+async def run_and_verify_project(
+    buck: Buck, expected_dir_name: str, test_target: str
+) -> None:
+    expected_dir_path = (
+        buck.cwd / EXPECTED_DIR_RELATIVE_PATH / expected_dir_name / ".idea"
+    )
     result = await buck.bxl(
-        bxl_label,
+        BXL_LABEL,
         "--",
         "--targets",
-        "fbsource//fbandroid/buck2/tests/good/sample_intellij_project/apk:apk",
+        test_target,
     )
     output_dir = Path(result.stdout.strip())
-    assert verify_expected_files(expected_dir, output_dir)
 
-
-def _sanitize_buck_out_paths(data):
-    if type(data) == dict:
-        return {key: _sanitize_buck_out_paths(value) for key, value in data.items()}
-    elif type(data) == list:
-        return [_sanitize_buck_out_paths(x) for x in data]
-    elif type(data) == str:
-        return re.sub(r"buck-out/[^\"]*", "buck-out-path", data)
-    else:
-        raise Exception(
-            "Unknown data type for _sanitize_buck_out_paths: {}".format(type(data))
+    with tempfile.TemporaryDirectory() as xml_output_temp_dir:
+        await buck.run(
+            "fbcode//buck2/prelude/intellij_project/tools/project_writer:project_writer",
+            "--",
+            "--input_path",
+            output_dir.as_posix(),
+            "--output_path",
+            xml_output_temp_dir,
         )
+        verify_expected_files(expected_dir_path, Path(xml_output_temp_dir))
 
 
-def verify_expected_files(expected_dir: Path, output_dir: Path) -> bool:
+def verify_expected_files(expected_dir: Path, output_dir: Path) -> None:
     for cur_file in expected_dir.rglob("*.expected"):
         relative_path = cur_file.relative_to(expected_dir).parent
         generated_file = output_dir / relative_path / cur_file.stem
+        assert generated_file.is_file(), "File does not exist:{}".format(generated_file)
 
-        if generated_file.suffix == ".json":
-            with cur_file.open("r") as cur_json_file, generated_file.open(
-                "r"
-            ) as gen_json_file:
-                expected = json.load(cur_json_file)
-                actual = _sanitize_buck_out_paths(json.load(gen_json_file))
-                if actual != expected:
-                    return False
-        else:
-            if cur_file.read_text() != generated_file.read_text():
-                return False
-    return True
+        sanitized_gen_file_content = BUCK_OUT_HASH_PATTERN.sub(
+            BUCK_OUT_HASH_REPLACE_TXT, generated_file.read_text()
+        )
+
+        is_match = cur_file.read_text() == sanitized_gen_file_content
+        # TODO: remove diff report for buckout paths
+        diff = difflib.context_diff(
+            open(cur_file).readlines(), open(generated_file).readlines(), n=0
+        )
+        assert (
+            is_match
+        ), "Generated file:{} does not match expected file at: {}\n{}".format(
+            generated_file, cur_file, "".join(diff)
+        )
