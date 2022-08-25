a106a1b33a1b5b2f98b9f607f01c6c09ab8ce66f
Wendy Yu <wendyy@fb.com>
2022-08-25 12:05:41 -0700

    Change CellResolver key Value to be an Option<CellResolver>
    Summary: Need this to implement corner case in `reuse-current-config` when there is no previous config to use. Previously, the behavior would be to [panic](https://www.internalfb.com/code/fbsource/[a171a3900c5ae7eff111480c5dc048f1372256e6]/fbcode/buck2/dice/dice/src/injected.rs?lines=45-51) if we tried to retrieve a nonexistent CellResolverKey from DICE (since CellResolverKey [implements InjectedKey](https://www.internalfb.com/code/fbsource/[a41862cda8ac6fb40f46bef6b973baf6114447ba]/fbcode/buck2/buck2_common/src/dice/cells.rs?lines=30))
    
    Reviewed By: bobyangyf
    
    Differential Revision: D38974664
    
    fbshipit-source-id: e57087bf61c43134ca94b6b44a3acb81ba3f91da

--- buck2_common/src/dice/cells.rs
+++ buck2_common/src/dice/cells.rs
@@ -15,12 +15,23 @@ use derive_more::Display;
 use dice::DiceComputations;
 use dice::InjectedKey;
 use gazebo::prelude::*;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum CellResolverKeyError {
+    #[error("No cell resolver key was injected into the DICE cache")]
+    NoCellResolverKeyFound,
+}
 
 #[async_trait]
 pub trait HasCellResolver {
     async fn get_cell_resolver(&self) -> anyhow::Result<CellResolver>;
 
+    async fn try_get_cell_resolver(&self) -> anyhow::Result<Option<CellResolver>>;
+
     fn set_cell_resolver(&self, cell_resolver: CellResolver) -> anyhow::Result<()>;
+
+    fn set_none_cell_resolver(&self) -> anyhow::Result<()>;
 }
 
 #[derive(Clone, Dupe, Display, Debug, Eq, Hash, PartialEq)]
@@ -28,20 +39,34 @@ pub trait HasCellResolver {
 struct CellResolverKey;
 
 impl InjectedKey for CellResolverKey {
-    type Value = CellResolver;
+    type Value = Option<CellResolver>;
 
     fn compare(x: &Self::Value, y: &Self::Value) -> bool {
-        x == y
+        match (x, y) {
+            (Some(x), Some(y)) => x == y,
+            (None, None) => true,
+            (_, _) => false,
+        }
     }
 }
 
 #[async_trait]
 impl HasCellResolver for DiceComputations {
     async fn get_cell_resolver(&self) -> anyhow::Result<CellResolver> {
+        self.compute(&CellResolverKey)
+            .await?
+            .ok_or_else(|| CellResolverKeyError::NoCellResolverKeyFound.into())
+    }
+
+    async fn try_get_cell_resolver(&self) -> anyhow::Result<Option<CellResolver>> {
         Ok(self.compute(&CellResolverKey).await?)
     }
 
     fn set_cell_resolver(&self, cell_resolver: CellResolver) -> anyhow::Result<()> {
-        Ok(self.changed_to(vec![(CellResolverKey, cell_resolver)])?)
+        Ok(self.changed_to(vec![(CellResolverKey, Some(cell_resolver))])?)
+    }
+
+    fn set_none_cell_resolver(&self) -> anyhow::Result<()> {
+        Ok(self.changed_to(vec![(CellResolverKey, None)])?)
     }
 }
