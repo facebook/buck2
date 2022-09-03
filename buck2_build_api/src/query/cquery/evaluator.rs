bf1a46100882d918d6897769ce1d9f5335727614
Stiopa Koltsov <nga@fb.com>
2022-09-03 04:33:30 -0700

    Move universe construction
    Reviewed By: ndmitchell
    
    Differential Revision: D39232971
    
    fbshipit-source-id: e7d00b2e9c74e875c38916e58363f6656c0f7942

--- buck2_build_api/src/query/cquery/evaluator.rs
+++ buck2_build_api/src/query/cquery/evaluator.rs
@@ -8,24 +8,17 @@
  */
 
 //! Implementation of the cli and query_* attr query language.
-use std::collections::BTreeMap;
-use std::collections::BTreeSet;
+
 use std::sync::Arc;
 
-use async_trait::async_trait;
 use buck2_common::result::ToSharedResultExt;
 use buck2_core::fs::project::ProjectRelativePath;
 use buck2_core::fs::project::ProjectRoot;
-use buck2_core::package::Package;
 use buck2_core::target::TargetLabel;
-use buck2_core::target::TargetName;
 use buck2_node::nodes::configured::ConfiguredTargetNode;
 use buck2_query::query::environment::QueryEnvironment;
-use buck2_query::query::syntax::simple::eval::label_indexed::LabelIndexed;
 use buck2_query::query::syntax::simple::eval::values::QueryEvaluationResult;
 use buck2_query::query::syntax::simple::functions::DefaultQueryFunctionsModule;
-use buck2_query::query::traversal::AsyncTraversalDelegate;
-use buck2_query::query::traversal::ChildVisitor;
 use dice::DiceComputations;
 use futures::stream::FuturesUnordered;
 use futures::StreamExt;
@@ -109,50 +102,9 @@ async fn resolve_literals_in_universe<L: AsRef<str>, U: AsRef<str>>(
     // we can get errors for packages or targets that don't exist or fail to load.
     let refs: Vec<_> = universe.map(|v| v.as_ref());
     let universe_resolved = env.eval_literals(&refs).await?;
-    // To support package/recursive patterns, we hold the map by package. To support a
-    // single target name having multiple instances in the universe, we map them to a list of nodes.
-    struct Delegate {
-        targets:
-            BTreeMap<Package, BTreeMap<TargetName, BTreeSet<LabelIndexed<ConfiguredTargetNode>>>>,
-    }
 
-    #[async_trait]
-    impl AsyncTraversalDelegate<ConfiguredTargetNode> for Delegate {
-        fn visit(&mut self, target: ConfiguredTargetNode) -> anyhow::Result<()> {
-            let label = target.name();
-            let package_targets: &mut _ = self
-                .targets
-                .entry(label.pkg().dupe())
-                .or_insert_with(BTreeMap::new);
+    let universe = CqueryUniverse::build(&universe_resolved, env).await?;
 
-            let nodes: &mut _ = match package_targets.get_mut(label.name()) {
-                Some(v) => v,
-                None => package_targets
-                    .entry(label.name().dupe())
-                    .or_insert_with(BTreeSet::new),
-            };
-
-            nodes.insert(LabelIndexed(target));
-
-            Ok(())
-        }
-
-        async fn for_each_child(
-            &mut self,
-            target: &ConfiguredTargetNode,
-            func: &mut dyn ChildVisitor<ConfiguredTargetNode>,
-        ) -> anyhow::Result<()> {
-            for dep in target.deps() {
-                func.visit(dep.name().dupe())?;
-            }
-            Ok(())
-        }
-    }
-    let mut delegate = Delegate {
-        targets: BTreeMap::new(),
-    };
-    env.dfs_postorder(&universe_resolved, &mut delegate).await?;
-    let universe = CqueryUniverse::new(delegate.targets);
     // capture a reference so the ref can be moved into the future below.
     let universe = &universe;
 
--- buck2_build_api/src/query/cquery/universe.rs
+++ buck2_build_api/src/query/cquery/universe.rs
@@ -10,15 +10,22 @@
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 
+use async_trait::async_trait;
 use buck2_common::pattern::resolve::ResolvedPattern;
 use buck2_core::package::Package;
 use buck2_core::pattern::PackageSpec;
 use buck2_core::target::TargetName;
 use buck2_node::nodes::configured::ConfiguredTargetNode;
+use buck2_query::query::environment::QueryEnvironment;
 use buck2_query::query::syntax::simple::eval::label_indexed::LabelIndexed;
 use buck2_query::query::syntax::simple::eval::set::TargetSet;
+use buck2_query::query::traversal::AsyncTraversalDelegate;
+use buck2_query::query::traversal::ChildVisitor;
 use gazebo::dupe::Dupe;
 
+/// Subset of targets `cquery` command works with.
+///
+/// Targets are resolved in the universe, and file owners are also resolved in the universe.
 pub(crate) struct CqueryUniverse {
     targets: BTreeMap<Package, BTreeMap<TargetName, BTreeSet<LabelIndexed<ConfiguredTargetNode>>>>,
 }
@@ -31,6 +38,58 @@ impl CqueryUniverse {
         >,
     ) -> CqueryUniverse {
         CqueryUniverse { targets }
+    }
+
+    pub(crate) async fn build(
+        universe: &TargetSet<ConfiguredTargetNode>,
+        env: &dyn QueryEnvironment<Target = ConfiguredTargetNode>,
+    ) -> anyhow::Result<CqueryUniverse> {
+        // To support package/recursive patterns, we hold the map by package. To support a
+        // single target name having multiple instances in the universe, we map them to a list of nodes.
+        struct Delegate {
+            targets: BTreeMap<
+                Package,
+                BTreeMap<TargetName, BTreeSet<LabelIndexed<ConfiguredTargetNode>>>,
+            >,
+        }
+
+        #[async_trait]
+        impl AsyncTraversalDelegate<ConfiguredTargetNode> for Delegate {
+            fn visit(&mut self, target: ConfiguredTargetNode) -> anyhow::Result<()> {
+                let label = target.name();
+                let package_targets: &mut _ = self
+                    .targets
+                    .entry(label.pkg().dupe())
+                    .or_insert_with(BTreeMap::new);
+
+                let nodes: &mut _ = match package_targets.get_mut(label.name()) {
+                    Some(v) => v,
+                    None => package_targets
+                        .entry(label.name().dupe())
+                        .or_insert_with(BTreeSet::new),
+                };
+
+                nodes.insert(LabelIndexed(target));
+
+                Ok(())
+            }
+
+            async fn for_each_child(
+                &mut self,
+                target: &ConfiguredTargetNode,
+                func: &mut dyn ChildVisitor<ConfiguredTargetNode>,
+            ) -> anyhow::Result<()> {
+                for dep in target.deps() {
+                    func.visit(dep.name().dupe())?;
+                }
+                Ok(())
+            }
+        }
+        let mut delegate = Delegate {
+            targets: BTreeMap::new(),
+        };
+        env.dfs_postorder(universe, &mut delegate).await?;
+        Ok(CqueryUniverse::new(delegate.targets))
     }
 
     pub(crate) fn get(
