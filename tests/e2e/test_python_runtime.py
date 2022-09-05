0d2498a46e8a7c93a4a00ede3f50c9b5dc528bd8
Ruslan Sayfutdinov <sayfutdinov@fb.com>
2022-09-05 03:01:10 -0700

    fix python runtime tests on Windows
    Summary: Some python tests are now passing.
    
    Reviewed By: krallin
    
    Differential Revision: D39020570
    
    fbshipit-source-id: 0297db656daa1a7ff2fec61a66b5db563f5e3b29

--- tests/e2e/test_python_runtime.py
+++ tests/e2e/test_python_runtime.py
@@ -1,11 +1,17 @@
 import asyncio
 import asyncio.subprocess
 import os
+import platform
 import sys
 from pathlib import Path
+from typing import Dict, Optional
 
 from xplat.build_infra.buck_e2e.api.buck import Buck
 from xplat.build_infra.buck_e2e.buck_workspace import buck_test
+
+
+def extract_gen_folder(output: str) -> str:
+    return output[: output.find("{0}gen{0}".format(os.path.sep)) + 4]
 
 
 @buck_test(inplace=True)
@@ -16,31 +22,35 @@ async def test_python_par_supports_pdb_debugging(buck: Buck) -> None:
     ]
     if sys.platform == "darwin":
         args.append("@mode/mac")
+    if sys.platform == "win32":
+        args.append("@mode/win")
     result = await buck.build(*args)
     output_dict = result.get_target_to_build_output()
     for _target, output in output_dict.items():
-        gen_folder = output[: output.find("/gen/") + 4]
-        symlink = Path(
-            gen_folder,
-            # v1:   buck2/tests/targets/rules/python/hello_world/welcome.par
-            "fbcode/buck2/tests/targets/rules/python/hello_world/welcome.par",
+        gen_folder = extract_gen_folder(output)
+        # v1:   buck2/tests/targets/rules/python/hello_world/welcome.par
+        symlink = (
+            Path(gen_folder)
+            / "fbcode"
+            / "buck2"
+            / "tests"
+            / "targets"
+            / "rules"
+            / "python"
+            / "hello_world"
+            / "welcome.par"
         )
         assert os.path.exists(symlink)
 
         # Run the resulting binary under pdb
-        proc = await asyncio.create_subprocess_exec(
+        stdout = await run(
             symlink,
-            stdin=asyncio.subprocess.PIPE,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
             env={
                 "PYTHONDEBUGWITHPDB": "1",
                 "PYTHONPDBINITIALCOMMANDS": "break hello_world.py:2|continue",
             },
+            input=b"continue\n",
         )
-
-        # Send the continue command to Pdb's command prompt
-        stdout, _stderr = await proc.communicate(input=b"continue\n")
 
         # pdb was started
         assert b"(Pdb)" in stdout
@@ -57,14 +67,23 @@ async def test_python_par_decorating_main(buck: Buck) -> None:
     ]
     if sys.platform == "darwin":
         args.append("@mode/mac")
+    if sys.platform == "win32":
+        args.append("@mode/win")
     result = await buck.build(*args)
     output_dict = result.get_target_to_build_output()
     for _target, output in output_dict.items():
-        gen_folder = output[: output.find("/gen/") + 4]
-        symlink = Path(
-            gen_folder,
-            # v1:   buck2/tests/targets/rules/python/decorate_main/decorate_main.par
-            "fbcode/buck2/tests/targets/rules/python/decorate_main/decorate_main.par",
+        gen_folder = extract_gen_folder(output)
+        # v1:   buck2/tests/targets/rules/python/decorate_main/decorate_main.par
+        symlink = (
+            Path(gen_folder)
+            / "fbcode"
+            / "buck2"
+            / "tests"
+            / "targets"
+            / "rules"
+            / "python"
+            / "decorate_main"
+            / "decorate_main.par"
         )
         assert os.path.exists(symlink)
 
@@ -74,19 +93,32 @@ async def test_python_par_decorating_main(buck: Buck) -> None:
 
         # Run the resulting binary and decorate main
         stdout = await run(symlink, env={"PAR_MAIN_OVERRIDE": "main_wrapper"})
-        assert b"Hello from main wrapper for original_main!" in stdout
+        # On Windows there is an extra wrapper
+        if platform.system() == "Windows":
+            assert b"Hello from main wrapper for __run_pex_main__!" in stdout
+        else:
+            assert b"Hello from main wrapper for original_main!" in stdout
 
 
 # Run the binary. Returns stdout.
-async def run(binary: Path, env) -> bytes:
+async def run(
+    binary: Path, env: Dict[str, str], input: Optional[bytes] = None
+) -> bytes:
+    args = [binary]
+    if platform.system() == "Windows":
+        # Par can't be executed directly
+        args = [sys.executable] + args
+        # CreateProcess fails without SYSTEMROOT
+        for required in ["SYSTEMROOT"]:
+            env[required] = os.getenv(required, "")
     proc = await asyncio.create_subprocess_exec(
-        binary,
+        *args,
         stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
         env=env,
     )
 
-    stdout, _stderr = await proc.communicate()
+    stdout, _stderr = await proc.communicate(input=input)
 
     return stdout
