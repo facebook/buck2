23563f55a422481a7e7cf56fed0d7f221266449a
Thomas Orozco <torozco@fb.com>
2022-09-07 07:20:43 -0700

    buck2: stdin: don't read from stdin until a read is actually attempted
    Summary:
    This is going to be useful when combined with the next diff, which causes us to
    not attempt reads in the interactive console if stdin is a tty.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D39304617
    
    fbshipit-source-id: 006dbaaca8594b5a4e52ec229bcb74a765bd58ff

--- buck2_client/src/stdin.rs
+++ buck2_client/src/stdin.rs
@@ -1,13 +1,18 @@
 use std::io;
 use std::io::Read;
 use std::pin::Pin;
+use std::sync::Arc;
 use std::task::Context;
 use std::task::Poll;
+use std::thread::JoinHandle;
 
 use buck2_core::env_helper::EnvHelper;
 use bytes::Bytes;
 use futures::stream::Fuse;
 use futures::stream::StreamExt;
+use gazebo::dupe::Dupe;
+use parking_lot::Condvar;
+use parking_lot::Mutex;
 use pin_project::pin_project;
 use tokio::io::AsyncRead;
 use tokio::io::ReadBuf;
@@ -21,6 +26,9 @@ use crate::console_interaction_stream::ConsoleInteractionStream;
 pub struct Stdin {
     #[pin]
     stream: StreamReader<Fuse<ReceiverStream<io::Result<Bytes>>>, Bytes>,
+
+    // We don't start reading until poll_read is called.
+    waiter: Option<Arc<Waiter>>,
 }
 
 impl AsyncRead for Stdin {
@@ -29,7 +37,11 @@ impl AsyncRead for Stdin {
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<io::Result<()>> {
-        self.project().stream.poll_read(cx, buf)
+        let this = self.project();
+        if let Some(waiter) = this.waiter.take() {
+            waiter.release();
+        }
+        this.stream.poll_read(cx, buf)
     }
 }
 
@@ -37,26 +49,53 @@ impl Stdin {
     pub fn new() -> anyhow::Result<Self> {
         static STDIN_BUFFER_SIZE: EnvHelper<usize> = EnvHelper::new("BUCK2_TEST_STDIN_BUFFER_SIZE");
         let buffer_size = STDIN_BUFFER_SIZE.get()?.copied().unwrap_or(8192);
+        let (stdin, _handle) = Self::new_for_reader(
+            || {
+                // NOTE: We ignore send errors since there is no point in reading without a receiver.
+                let stdin = std::io::stdin().lock();
+
+                // Disable buffering, since we don't do small reads anyway (we have a 8KB buffer
+                // already). We probably need something similar on Windows here.
+                #[cfg(unix)]
+                let stdin = raw_reader::RawReader::new(stdin);
+
+                stdin
+            },
+            buffer_size,
+        );
+        Ok(stdin)
+    }
+
+    fn new_for_reader<R, F>(acquire_reader: F, buffer_size: usize) -> (Self, JoinHandle<()>)
+    where
+        R: Read,
+        F: FnOnce() -> R + Send + 'static,
+    {
+        let waiter = Arc::new(Waiter {
+            mutex: Mutex::new(false),
+            var: Condvar::new(),
+        });
 
         // Small buffer, this isn't bytes we're buffering, just buffers of bytes. That said, sicne
         // we're on separate threads, give ourselves a bit of buffering.
         let (mut tx, rx) = mpsc::channel(4);
 
-        std::thread::spawn(move || {
-            // NOTE: We ignore send errors since there is no point in reading without a receiver.
-            let stdin = std::io::stdin().lock();
-
-            // Disable buffering, since we don't do small reads anyway (we have a 8KB buffer
-            // already). We probably need something similar on Windows here.
-            #[cfg(unix)]
-            let stdin = raw_reader::RawReader::new(stdin);
-
-            let _ignored = read_and_forward(stdin, &mut tx, buffer_size);
+        let handle = std::thread::spawn({
+            let waiter = waiter.dupe();
+            move || {
+                let reader = acquire_reader();
+                waiter.block_until_released();
+                let _ignored = read_and_forward(reader, &mut tx, buffer_size);
+            }
         });
 
-        Ok(Self {
-            stream: StreamReader::new(ReceiverStream::new(rx).fuse()),
-        })
+        (
+            Self {
+                stream: StreamReader::new(ReceiverStream::new(rx).fuse()),
+                waiter: Some(waiter),
+            },
+            handle,
+        )
     }
 
     pub fn console_interaction_stream(&mut self) -> ConsoleInteractionStream<'_> {
@@ -69,6 +108,7 @@ fn read_and_forward(
     tx: &mut mpsc::Sender<io::Result<Bytes>>,
     buffer_size: usize,
 ) -> Result<(), mpsc::error::SendError<io::Result<Bytes>>> {
+    tracing::debug!("stdin: start reading");
     let mut buff = vec![0; buffer_size];
 
     loop {
@@ -92,6 +132,25 @@ fn read_and_forward(
     }
 
     Ok(())
+}
+
+struct Waiter {
+    mutex: Mutex<bool>,
+    var: Condvar,
+}
+
+impl Waiter {
+    fn release(&self) {
+        *self.mutex.lock() = true;
+        self.var.notify_all();
+    }
+
+    fn block_until_released(&self) {
+        let mut guard = self.mutex.lock();
+        while !*guard {
+            self.var.wait(&mut guard);
+        }
+    }
 }
 
 #[cfg(unix)]
@@ -126,5 +185,29 @@ mod raw_reader {
         fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
             unistd::read(self.fd, buf).map_err(io::Error::from)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+    use std::io::Read;
+
+    use super::*;
+
+    struct PanicReader;
+
+    impl Read for PanicReader {
+        fn read(&mut self, _buf: &mut [u8]) -> io::Result<usize> {
+            panic!("!")
+        }
+    }
+
+    #[test]
+    fn read_waits_for_signal() {
+        // If we didn't wait then we'd panic in the reader thread.
+        let (_stdin, thread) = Stdin::new_for_reader(|| PanicReader, 8);
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        assert!(!thread.is_finished());
     }
 }
