9e7b219e25ca07db944990358121eef1b4369ff4
Stiopa Koltsov <nga@fb.com>
2022-09-08 11:42:53 -0700

    Press 'r' to show detailed RE stats
    Summary: When buck2 stuck, this should help to understand if it stuck because of RE.
    
    Reviewed By: krallin
    
    Differential Revision: D39319599
    
    fbshipit-source-id: bb354d3d7463bfa807aaf90c30ba8a715e3225df

--- buck2_client/src/subscribers/re.rs
+++ buck2_client/src/subscribers/re.rs
@@ -57,13 +57,85 @@ impl ReState {
         Some(format!("RE: {}", parts.join(" ")))
     }
 
+    fn render_detailed_items(
+        &self,
+        name: &str,
+        started: u32,
+        finished_successfully: u32,
+        finished_with_error: u32,
+    ) -> anyhow::Result<Option<Line>> {
+        let in_progress = started
+            .saturating_sub(finished_successfully)
+            .saturating_sub(finished_with_error);
+        if in_progress == 0 && finished_successfully == 0 && finished_with_error == 0 {
+            return Ok(None);
+        }
+        let line = format!(
+            "{name:<20}: \
+            {in_progress:>5} in progress, \
+            {finished_successfully:>5} success, \
+            {finished_with_error:>5} error"
+        );
+        Ok(Some(Line::from_iter([superconsole::Span::new_unstyled(
+            line,
+        )?])))
+    }
+
+    fn render_detailed(&self) -> anyhow::Result<Vec<Line>> {
+        let mut r = Vec::new();
+        if let Some(last) = &self.last {
+            let last = &last.snapshot;
+            r.extend(self.render_detailed_items(
+                "uploads",
+                last.re_uploads_started,
+                last.re_uploads_finished_successfully,
+                last.re_uploads_finished_with_error,
+            )?);
+            r.extend(self.render_detailed_items(
+                "downloads",
+                last.re_downloads_started,
+                last.re_downloads_finished_successfully,
+                last.re_downloads_finished_with_error,
+            )?);
+            r.extend(self.render_detailed_items(
+                "action_cache",
+                last.re_action_cache_started,
+                last.re_action_cache_finished_successfully,
+                last.re_action_cache_finished_with_error,
+            )?);
+            r.extend(self.render_detailed_items(
+                "executes",
+                last.re_executes_started,
+                last.re_executes_finished_successfully,
+                last.re_executes_finished_with_error,
+            )?);
+            r.extend(self.render_detailed_items(
+                "materializes",
+                last.re_materializes_started,
+                last.re_materializes_finished_successfully,
+                last.re_materializes_finished_with_error,
+            )?);
+            r.extend(self.render_detailed_items(
+                "write_action_results",
+                last.re_write_action_results_started,
+                last.re_write_action_results_finished_successfully,
+                last.re_write_action_results_finished_with_error,
+            )?);
+        }
+        Ok(r)
+    }
+
     pub(crate) fn render(&self) -> anyhow::Result<Vec<Line>> {
         let header = match self.render_header() {
             Some(header) => header,
             None => return Ok(Vec::new()),
         };
         let header = superconsole::Span::new_unstyled(header)?;
-        Ok(vec![Line::from_iter([header])])
+        let mut lines = vec![Line::from_iter([header])];
+        if self.detailed {
+            lines.extend(self.render_detailed()?);
+        }
+        Ok(lines)
     }
 }
 
