d801a4914dfbf1df982215fba6aa9b9feccdce1a
Stiopa Koltsov <nga@fb.com>
2022-09-14 14:07:12 -0700

    Correctly handle UTF-8 in RawOutputWriter
    Summary: Do not split in the middle of UTF-8 encoded character.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D39513944
    
    fbshipit-source-id: 44abb9c50254cee931d0a6a46f9b54c9dd2e321a

--- buck2_server_ctx/src/raw_output.rs
+++ buck2_server_ctx/src/raw_output.rs
@@ -11,6 +11,7 @@ use std::io;
 use std::io::BufWriter;
 use std::io::Write;
 use std::marker::PhantomData;
+use std::str;
 
 use buck2_core::env_helper::EnvHelper;
 use buck2_events::dispatch::EventDispatcher;
@@ -73,24 +74,92 @@ impl RawOutputWriter {
         static CHUNK_SIZE: EnvHelper<usize> = EnvHelper::new("BUCK2_DEBUG_RAWOUTPUT_CHUNK_SIZE");
         Ok(CHUNK_SIZE.get_copied()?.unwrap_or(DEFAULT_CHUNK_SIZE))
     }
+
+    /// Given complete valid UTF-8 string, truncate it to be no longer than given limit.
+    fn truncate_str(s: &[u8], limit: usize) -> io::Result<&str> {
+        fn err() -> io::Error {
+            io::Error::new(io::ErrorKind::InvalidInput, "Output is not utf-8")
+        }
+
+        if limit == s.len() {
+            // Input string must not be truncated, so special case for limit = len.
+            str::from_utf8(s).map_err(|_| err())
+        } else {
+            match str::from_utf8(&s[..limit]) {
+                Ok(s) => Ok(s),
+                Err(e) => match e.error_len() {
+                    None => {
+                        // String is truncated in the middle of UTF-8 character.
+                        //
+                        // SAFETY: `e.valid_up_to()` is guaranteed to provide
+                        // the valid utf8 string range.
+                        unsafe { Ok(str::from_utf8_unchecked(&s[..e.valid_up_to()])) }
+                    }
+                    Some(_) => {
+                        // Invalid UTF-8 sequence.
+                        Err(err())
+                    }
+                },
+            }
+        }
+    }
 }
 
 impl Write for RawOutputWriter {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let len = std::cmp::min(buf.len(), self.chunk_size);
         if len > 0 {
+            let s = RawOutputWriter::truncate_str(buf, len)?;
+            if s.is_empty() {
+                return Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    "Configured chunk size is not enough to hold a single utf-8 character",
+                ));
+            }
             let raw_output = buck2_data::RawOutput {
-                // TODO(nga): does not handle UTF-8 correctly.
-                raw_output: String::from_utf8(buf[..len].to_vec()).map_err(|_| {
-                    io::Error::new(io::ErrorKind::InvalidInput, "Output is not utf-8")
-                })?,
+                raw_output: s.to_owned(),
             };
             self.dispatcher.instant_event(raw_output);
+            Ok(s.len())
+        } else {
+            Ok(0)
         }
-        Ok(len)
     }
 
     fn flush(&mut self) -> io::Result<()> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::raw_output::RawOutputWriter;
+
+    #[test]
+    fn test_truncate_str() {
+        let s = "Маяк".as_bytes();
+        assert_eq!("", RawOutputWriter::truncate_str(s, 0).unwrap());
+        assert_eq!("", RawOutputWriter::truncate_str(s, 1).unwrap());
+        assert_eq!("М", RawOutputWriter::truncate_str(s, 2).unwrap());
+        assert_eq!("М", RawOutputWriter::truncate_str(s, 3).unwrap());
+        assert_eq!("Ма", RawOutputWriter::truncate_str(s, 4).unwrap());
+        assert_eq!("Ма", RawOutputWriter::truncate_str(s, 5).unwrap());
+        assert_eq!("Мая", RawOutputWriter::truncate_str(s, 6).unwrap());
+        assert_eq!("Мая", RawOutputWriter::truncate_str(s, 7).unwrap());
+        assert_eq!("Маяк", RawOutputWriter::truncate_str(s, 8).unwrap());
+
+        // Now test corrupted UTF-8.
+        for i in 0..s.len() {
+            let mut s0 = s.to_vec();
+            // Invalid UTF-8 byte.
+            s0[i] = 0xff;
+            for j in 0..=s.len() {
+                if j <= i {
+                    assert!(RawOutputWriter::truncate_str(&s0, j).is_ok());
+                } else {
+                    assert!(RawOutputWriter::truncate_str(&s0, j).is_err());
+                }
+            }
+        }
     }
 }
