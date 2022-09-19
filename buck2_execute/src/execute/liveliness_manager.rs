27120b93c696561e18003100854354ee72684104
Thomas Orozco <torozco@fb.com>
2022-09-19 06:51:00 -0700

    buck2: liveliness: implement a guard-based liveliness manager
    Summary:
    This will let you instantiate a liveliness manager that is a live until you've
    released a guard it provides you with. This is conceptually a RwLock, so that's
    what we use here to implement it.
    
    Reviewed By: stepancheg
    
    Differential Revision: D39547693
    
    fbshipit-source-id: e589c058cf97c11085c0957dd2afda33b4b76b26

--- buck2_execute/src/execute/liveliness_manager.rs
+++ buck2_execute/src/execute/liveliness_manager.rs
@@ -1,11 +1,49 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use gazebo::prelude::*;
+use tokio::sync::OwnedRwLockWriteGuard;
+use tokio::sync::RwLock;
 
 #[async_trait]
 pub trait LivelinessManager: Send + Sync {
     /// Pending while we are alive. Ready when we aren't.
     async fn while_alive(&self);
+}
+
+/// While the associated LivelinessGuard is alive, `while_alive` will be Pending. When the guard is
+/// dropped, that stops being the case.
+#[derive(Clone, Dupe)]
+pub struct GuardedLivelinessManager {
+    mutex: Arc<RwLock<()>>,
+}
+
+pub struct LivelinessGuard {
+    // This is only used for the fact that it releases the lock on drop.
+    _guard: OwnedRwLockWriteGuard<LivelinessGuardMarker>,
+}
+
+impl LivelinessGuard {
+    pub fn create() -> (Arc<dyn LivelinessManager>, LivelinessGuard) {
+        let mutex = Arc::new(RwLock::new(LivelinessGuardMarker));
+
+        let guard = mutex
+            .dupe()
+            .try_write_owned()
+            .expect("This mutex was just created");
+
+        (mutex as _, LivelinessGuard { _guard: guard })
+    }
+}
+
+/// Just a type that can't be constructed elesewhere
+struct LivelinessGuardMarker;
+
+#[async_trait]
+impl LivelinessManager for RwLock<LivelinessGuardMarker> {
+    async fn while_alive(&self) {
+        let _unused = self.read().await;
+    }
 }
 
 /// Always alive.
