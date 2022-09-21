4cc0a31b7a8350028d7a7348f8ef21a3b59b423d
Marie Bremner <marwhal@fb.com>
2022-09-21 06:45:36 -0700

    Enhance e2e test for clean
    Summary:
    Enhance our e2e clean tests a tad more before I make changes to the logic.
    
    Additional checks added in this PR:
    1. Assert that all paths returned (printed to console) as part of `clean --dry-run` exist
    2. Assert that paths returned as part of `clean --dry-run` are the same as running `clean`
    
    Reviewed By: ndmitchell
    
    Differential Revision: D39632024
    
    fbshipit-source-id: 12b64c0925759f6af3eabeef87123913b492df03

--- tests/e2e/test_clean.py
+++ tests/e2e/test_clean.py
@@ -1,4 +1,5 @@
 import os
+from typing import Iterable
 
 from xplat.build_infra.buck_e2e.api.buck import Buck
 from xplat.build_infra.buck_e2e.buck_workspace import buck_test
@@ -20,8 +21,7 @@ async def test_clean(buck: Buck) -> None:
     for output in build_report_outputs:
         assert output.startswith(clean_paths)
 
-    for output in build_report_outputs:
-        assert os.path.exists(output) is False
+    assert_all_paths_do_not_exist(build_report_outputs)
 
 
 @buck_test(inplace=True)
@@ -32,11 +32,48 @@ async def test_clean_dry_run(buck: Buck) -> None:
         str(output) for output in build_report.outputs_for_target(TARGET)
     ]
 
-    clean_result = await buck.clean("--dry-run")
-    clean_paths = tuple(filter(None, clean_result.stderr.split("\n")))
+    dry_clean_result = await buck.clean("--dry-run")
 
-    for output in build_report_outputs:
-        assert output.startswith(clean_paths)
+    dry_clean_paths = set(
+        filter(
+            is_buck_path,
+            dry_clean_result.stderr.split("\n"),
+        )
+    )
+    assert_all_paths_exist(dry_clean_paths)
 
+    dry_clean_paths = tuple(i for i in dry_clean_paths)
     for output in build_report_outputs:
-        assert os.path.exists(output) is True
+        assert output.startswith(dry_clean_paths)
+
+    assert_all_paths_exist(build_report_outputs)
+
+    # Run clean without dry-run and make sure all files are removed now
+    clean_result = await buck.clean()
+    clean_paths = set(
+        filter(
+            is_buck_path,
+            clean_result.stderr.split("\n"),
+        )
+    )
+    # dry_clean_paths and clean_paths should be the same
+    for clean_path in clean_paths:
+        assert clean_path in dry_clean_paths
+    for dry_clean_path in dry_clean_paths:
+        assert dry_clean_path in clean_paths
+
+    assert_all_paths_do_not_exist(clean_paths)
+
+
+def is_buck_path(x: str):
+    return "/.buck/buckd/" in x or "/buck-out/" in x
+
+
+def assert_all_paths_exist(paths: Iterable):
+    for path in paths:
+        assert os.path.exists(path) is True
+
+
+def assert_all_paths_do_not_exist(paths: Iterable):
+    for path in paths:
+        assert os.path.exists(path) is False
