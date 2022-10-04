4b40886394a2d7ad8afbe1066ce21b5c4a2f7e01
Stiopa Koltsov <nga@meta.com>
2022-10-04 10:26:58 -0700

    spawn_background_process_on_windows
    Summary:
    Extract a function which spawn a background process on Windows.
    
    Used in the following diff D40021507 but useful as is as an abstraction (for example, following diff D40024718 adds a test for it).
    
    The function is available (but does not work) on Unix, because with this approach `cargo check` does more checks on Unix, making development faster without access to Windows machine. It is used in the following diff D40021507.
    
    Reviewed By: KapJI
    
    Differential Revision: D40021239
    
    fbshipit-source-id: 76afac0d3055aa241ad7c84505e82377deb789e5

--- buck2_client/src/daemon/mod.rs
+++ buck2_client/src/daemon/mod.rs
@@ -8,3 +8,4 @@
  */
 
 pub mod client;
+pub mod daemon_windows;
--- cli/src/commands/daemon.rs
+++ cli/src/commands/daemon.rs
@@ -515,127 +515,16 @@ impl DaemonCommand {
         _stdout: File,
         _stderr: File,
     ) -> anyhow::Result<()> {
-        // We have to call CreateProcessW manually because std::process::Command
-        // doesn't allow to set 'bInheritHandles' to false. Without this waiting on
-        // parent process will also wait on inherited handles of daemon process.
         use std::env;
-        use std::ffi::OsStr;
-        use std::ffi::OsString;
         use std::iter;
-        use std::mem;
-        use std::os::windows::ffi::OsStrExt;
-        use std::ptr;
 
-        use winapi::shared::minwindef::DWORD;
-        use winapi::shared::minwindef::FALSE;
-        use winapi::um::handleapi::CloseHandle;
-        use winapi::um::processthreadsapi::CreateProcessW;
-        use winapi::um::processthreadsapi::PROCESS_INFORMATION;
-        use winapi::um::processthreadsapi::STARTUPINFOW;
-        use winapi::um::winbase::CREATE_NEW_PROCESS_GROUP;
-        use winapi::um::winbase::CREATE_UNICODE_ENVIRONMENT;
-        use winapi::um::winbase::DETACHED_PROCESS;
-
-        fn to_nullterm(s: &OsStr) -> Vec<u16> {
-            s.encode_wide().chain(iter::once(0)).collect()
-        }
-
-        // Translated from ArgvQuote at http://tinyurl.com/zmgtnls
-        fn append_quoted(arg: &OsStr, cmdline: &mut Vec<u16>) {
-            if !arg.is_empty()
-                && !arg.encode_wide().any(|c| {
-                    c == ' ' as u16
-                        || c == '\t' as u16
-                        || c == '\n' as u16
-                        || c == '\x0b' as u16
-                        || c == '\"' as u16
-                })
-            {
-                cmdline.extend(arg.encode_wide());
-                return;
-            }
-            cmdline.push('"' as u16);
-
-            let arg: Vec<_> = arg.encode_wide().collect();
-            let mut i = 0;
-            while i < arg.len() {
-                let mut num_backslashes = 0;
-                while i < arg.len() && arg[i] == '\\' as u16 {
-                    i += 1;
-                    num_backslashes += 1;
-                }
-
-                if i == arg.len() {
-                    for _ in 0..num_backslashes * 2 {
-                        cmdline.push('\\' as u16);
-                    }
-                    break;
-                } else if arg[i] == b'"' as u16 {
-                    for _ in 0..num_backslashes * 2 + 1 {
-                        cmdline.push('\\' as u16);
-                    }
-                    cmdline.push(arg[i]);
-                } else {
-                    for _ in 0..num_backslashes {
-                        cmdline.push('\\' as u16);
-                    }
-                    cmdline.push(arg[i]);
-                }
-                i += 1;
-            }
-            cmdline.push('"' as u16);
-        }
-
-        #[allow(clippy::vec_init_then_push)]
-        fn make_command_line(program: &OsStr, args: impl Iterator<Item = String>) -> Vec<u16> {
-            let mut cmd: Vec<u16> = Vec::new();
-            cmd.push(b'"' as u16);
-            cmd.extend(program.encode_wide());
-            cmd.push(b'"' as u16);
-            for arg in args.map(OsString::from) {
-                cmd.push(b' ' as u16);
-                append_quoted(&arg, &mut cmd);
-            }
-            cmd.push(0); // null terminator
-            cmd
-        }
-
-        let exe_path = env::current_exe().expect("somehow couldn't get current exe");
-        let program = exe_path.as_os_str();
-        let cwd = project_root.as_os_str();
-
-        let mut cmd = make_command_line(
-            program,
-            env::args()
-                .skip(1)
-                .chain(iter::once("--dont-daemonize".to_owned())),
-        );
-
-        let mut sinfo: STARTUPINFOW = unsafe { mem::zeroed() };
-        sinfo.cb = mem::size_of::<STARTUPINFOW>() as DWORD;
-        let mut pinfo: PROCESS_INFORMATION = unsafe { mem::zeroed() };
-        let creation_flags =
-            CREATE_NEW_PROCESS_GROUP | DETACHED_PROCESS | CREATE_UNICODE_ENVIRONMENT;
-
-        let status = unsafe {
-            CreateProcessW(
-                to_nullterm(program).as_ptr(), // lpApplicationName
-                cmd.as_mut_ptr(),              // lpCommandLine
-                ptr::null_mut(),               // lpProcessAttributes
-                ptr::null_mut(),               // lpThreadAttributes
-                FALSE,                         // bInheritHandles
-                creation_flags,                // dwCreationFlags
-                ptr::null_mut(),               // lpEnvironment
-                to_nullterm(cwd).as_ptr(),     // lpCurrentDirectory
-                &mut sinfo,
-                &mut pinfo,
-            )
-        };
-        if status == 0 {
-            return Err(anyhow::anyhow!(std::io::Error::last_os_error()));
-        }
-        unsafe { CloseHandle(pinfo.hThread) };
-        unsafe { CloseHandle(pinfo.hProcess) };
-        Ok(())
+        let args: Vec<String> = env::args().collect();
+        buck2_client::daemon::daemon_windows::spawn_background_process_on_windows(
+            project_root,
+            &env::current_exe()?,
+            args.iter()
+                .map(|s| s.as_str())
+                .chain(iter::once("--dont-daemonize")),
+        )
     }
 }
