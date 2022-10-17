8f2d7dc49b1e749a36715dcff68845d64f439b57
Stiopa Koltsov <nga@meta.com>
2022-10-17 10:35:25 -0700

    Remove OrderedMap type aliases
    Summary: Dead code.
    
    Reviewed By: ndmitchell, IanChilds
    
    Differential Revision: D40420572
    
    fbshipit-source-id: 732fb0dba0aeef423f170caca46f8b7b8117b3bc

--- buck2_build_api/src/attrs/mod.rs
+++ buck2_build_api/src/attrs/mod.rs
@@ -46,14 +46,7 @@
 //! may be strings or targets or some other thing (e.g. a lazy glob, maybe).
 
 use buck2_node::attrs::coerced_attr::CoercedAttr;
-use starlark_map::small_map;
 
 pub mod resolve;
 #[cfg(test)]
 mod tests;
-
-/// A simple map that maintains insertion order.
-pub type OrderedMap<K, V> = small_map::SmallMap<K, V>;
-pub type OrderedMapEntry<'a, K, V> = small_map::Entry<'a, K, V>;
-pub type OrderedMapOccupiedEntry<'a, K, V> = small_map::OccupiedEntry<'a, K, V>;
-pub type OrderedMapVacantEntry<'a, K, V> = small_map::VacantEntry<'a, K, V>;
