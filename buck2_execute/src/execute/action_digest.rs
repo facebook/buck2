c544f228fc80317d9ec00e992aa1932831786ea2
Thomas Orozco <torozco@meta.com>
2022-10-19 10:07:55 -0700

    buck2: action_digest: add a tracked variant
    Summary:
    I want to sue this in the deferred materializer to find artifacts that need
    renewing.
    
    Reviewed By: IanChilds
    
    Differential Revision: D40467951
    
    fbshipit-source-id: bfa177830b6fa526e4d28c95cac16d4dea52c99a

--- buck2_execute/src/execute/action_digest.rs
+++ buck2_execute/src/execute/action_digest.rs
@@ -8,9 +8,20 @@
  */
 
 use buck2_common::cas_digest::CasDigest;
+use buck2_common::cas_digest::TrackedCasDigest;
+use buck2_common::cas_digest::TrackedCasDigestKind;
+use once_cell::sync::OnceCell;
 
 pub struct ActionDigestKind {
     _private: (),
 }
 
+impl TrackedCasDigestKind for ActionDigestKind {
+    fn cell_for_empty_digest() -> &'static OnceCell<TrackedCasDigest<Self>> {
+        static EMPTY_DIGEST: OnceCell<TrackedCasDigest<ActionDigestKind>> = OnceCell::new();
+        &EMPTY_DIGEST
+    }
+}
+
 pub type ActionDigest = CasDigest<ActionDigestKind>;
+pub type TrackedActionDigest = TrackedCasDigest<ActionDigestKind>;
