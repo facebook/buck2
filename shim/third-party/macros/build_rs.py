c770f68879529b84f3f9411d3c6006bf381cc77a
Andreas Herrmann <andreas.herrmann@tweag.io>
2022-10-19 17:41:47 +0200

    set HOST env-var in build_rs.py
    Cargo also sets this variable next to TARGET

--- shim/third-party/macros/build_rs.py
+++ shim/third-party/macros/build_rs.py
@@ -39,6 +39,7 @@ def mk_env(args, parent):
     env["RUSTC"] = "rustc"
     for x in args.feature:
         env[mk_feature(x)] = "1"
+    env["HOST"] = "x86_64-unknown-linux-gnu"
     env["TARGET"] = "x86_64-unknown-linux-gnu"
     env["CARGO_CFG_TARGET_ARCH"] = "x86_64"
     env["CARGO_CFG_TARGET_OS"] = "linux"
