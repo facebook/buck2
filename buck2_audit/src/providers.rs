29825f51004194943bcb039764c058b10b9a6560
Stiopa Koltsov <nga@meta.com>
2022-10-23 13:58:05 -0700

    audit providers: extract function
    Summary: Indentation is too large otherwise.
    
    Reviewed By: IanChilds
    
    Differential Revision: D40616198
    
    fbshipit-source-id: 1d400e8f7c1a75eb239616aaf15fa0bce7693183

--- buck2_audit/src/providers.rs
+++ buck2_audit/src/providers.rs
@@ -24,6 +24,7 @@ use buck2_server_ctx::pattern::parse_patterns_from_cli_args;
 use buck2_server_ctx::pattern::resolve_patterns;
 use buck2_server_ctx::pattern::target_platform_from_client_context;
 use cli_proto::ClientContext;
+use dice::DiceTransaction;
 use futures::stream::FuturesOrdered;
 use futures::StreamExt;
 use gazebo::prelude::*;
@@ -61,102 +62,102 @@ impl AuditSubcommand for AuditProvidersCommand {
         client_ctx: ClientContext,
     ) -> anyhow::Result<()> {
         server_ctx
-            .with_dice_ctx(async move |mut server_ctx, ctx| {
-                let cells = ctx.get_cell_resolver().await?;
-                let target_platform = target_platform_from_client_context(
-                    Some(&client_ctx),
-                    &cells,
-                    server_ctx.working_dir(),
-                )
-                .await?;
-
-                let parsed_patterns = parse_patterns_from_cli_args::<ProvidersPattern>(
-                    &self
-                        .patterns
-                        .map(|pat| buck2_data::TargetPattern { value: pat.clone() }),
-                    &cells,
-                    &ctx.get_legacy_configs().await?,
-                    server_ctx.working_dir(),
-                )?;
-                let resolved_pattern =
-                    resolve_patterns(&parsed_patterns, &cells, &ctx.file_ops()).await?;
-
-                let mut futs = FuturesOrdered::new();
-                for (package, spec) in resolved_pattern.specs {
-                    let ctx = &ctx;
-                    let targets = match spec {
-                        buck2_core::pattern::PackageSpec::Targets(targets) => targets,
-                        buck2_core::pattern::PackageSpec::All => {
-                            let interpreter_results = ctx.get_interpreter_results(&package).await?;
-                            interpreter_results
-                                .targets()
-                                .keys()
-                                .duped()
-                                .map(|target| ProvidersPattern {
-                                    target,
-                                    providers: ProvidersName::Default,
-                                })
-                                .collect()
-                        }
-                    };
-
-                    for pattern in targets {
-                        let label = pattern.into_providers_label(package.dupe());
-                        let providers_label = ctx
-                            .get_configured_target(&label, target_platform.as_ref())
-                            .await?;
-
-                        // `.push` is deprecated in newer `futures`,
-                        // but we did not updated vendored `futures` yet.
-                        #[allow(deprecated)]
-                        futs.push(async move {
-                            let result = ctx.get_providers(&providers_label).await;
-                            (providers_label, result)
-                        });
-                    }
-                }
-
-                let mut stdout = server_ctx.stdout()?;
-
-                while let Some((target, result)) = futs.next().await {
-                    match result {
-                        Ok(v) => {
-                            let v: FrozenProviderCollectionValue = v.require_compatible()?;
-
-                            if self.quiet {
-                                writeln!(&mut stdout, "{}", target)?
-                            } else if self.print_debug {
-                                writeln!(
-                                    &mut stdout,
-                                    "{}:\n{:?}\n",
-                                    target,
-                                    v.provider_collection()
-                                )?;
-                            } else {
-                                writeln!(
-                                    &mut stdout,
-                                    "{}:\n{:#}\n",
-                                    target,
-                                    v.provider_collection()
-                                )?;
-                            }
-                        }
-                        Err(e) => {
-                            if self.quiet {
-                                buck2_client::eprintln!("{}: failed: {:#}", target, e)?;
-                            } else {
-                                buck2_client::eprintln!("{}: \n{:#}", target, e)?;
-                            }
-                        }
-                    }
-                }
-
-                Ok(())
+            .with_dice_ctx(move |server_ctx, ctx| {
+                self.server_execute_with_dice(client_ctx, server_ctx, ctx)
             })
             .await
     }
 
     fn common_opts(&self) -> &AuditCommandCommonOptions {
         &self.common_opts
+    }
+}
+
+impl AuditProvidersCommand {
+    async fn server_execute_with_dice(
+        &self,
+        client_ctx: ClientContext,
+        mut server_ctx: Box<dyn ServerCommandContextTrait>,
+        ctx: DiceTransaction,
+    ) -> anyhow::Result<()> {
+        let cells = ctx.get_cell_resolver().await?;
+        let target_platform = target_platform_from_client_context(
+            Some(&client_ctx),
+            &cells,
+            server_ctx.working_dir(),
+        )
+        .await?;
+
+        let parsed_patterns = parse_patterns_from_cli_args::<ProvidersPattern>(
+            &self
+                .patterns
+                .map(|pat| buck2_data::TargetPattern { value: pat.clone() }),
+            &cells,
+            &ctx.get_legacy_configs().await?,
+            server_ctx.working_dir(),
+        )?;
+        let resolved_pattern = resolve_patterns(&parsed_patterns, &cells, &ctx.file_ops()).await?;
+
+        let mut futs = FuturesOrdered::new();
+        for (package, spec) in resolved_pattern.specs {
+            let ctx = &ctx;
+            let targets = match spec {
+                buck2_core::pattern::PackageSpec::Targets(targets) => targets,
+                buck2_core::pattern::PackageSpec::All => {
+                    let interpreter_results = ctx.get_interpreter_results(&package).await?;
+                    interpreter_results
+                        .targets()
+                        .keys()
+                        .duped()
+                        .map(|target| ProvidersPattern {
+                            target,
+                            providers: ProvidersName::Default,
+                        })
+                        .collect()
+                }
+            };
+
+            for pattern in targets {
+                let label = pattern.into_providers_label(package.dupe());
+                let providers_label = ctx
+                    .get_configured_target(&label, target_platform.as_ref())
+                    .await?;
+
+                // `.push` is deprecated in newer `futures`,
+                // but we did not updated vendored `futures` yet.
+                #[allow(deprecated)]
+                futs.push(async move {
+                    let result = ctx.get_providers(&providers_label).await;
+                    (providers_label, result)
+                });
+            }
+        }
+
+        let mut stdout = server_ctx.stdout()?;
+
+        while let Some((target, result)) = futs.next().await {
+            match result {
+                Ok(v) => {
+                    let v: FrozenProviderCollectionValue = v.require_compatible()?;
+
+                    if self.quiet {
+                        writeln!(&mut stdout, "{}", target)?
+                    } else if self.print_debug {
+                        writeln!(&mut stdout, "{}:\n{:?}\n", target, v.provider_collection())?;
+                    } else {
+                        writeln!(&mut stdout, "{}:\n{:#}\n", target, v.provider_collection())?;
+                    }
+                }
+                Err(e) => {
+                    if self.quiet {
+                        buck2_client::eprintln!("{}: failed: {:#}", target, e)?;
+                    } else {
+                        buck2_client::eprintln!("{}: \n{:#}", target, e)?;
+                    }
+                }
+            }
+        }
+
+        Ok(())
     }
 }
