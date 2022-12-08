83ca51ea8cbc3ed211f09829cb0b9855b26cb045
Thomas Orozco <torozco@meta.com>
2022-12-08 06:02:37 -0800

    buck2: materializer: deferred: move tests to their own module
    Summary: Like it says in the title.
    
    Reviewed By: stepancheg
    
    Differential Revision: D41774540
    
    fbshipit-source-id: 9e23dabf3ed12c22d17793c8f7f71845e3df568f

--- buck2_execute_impl/src/materializers/deferred/mod.rs
+++ buck2_execute_impl/src/materializers/deferred/mod.rs
@@ -12,6 +12,9 @@ mod extension;
 mod file_tree;
 mod io_handler;
 
+#[cfg(test)]
+mod tests;
+
 use std::collections::VecDeque;
 use std::sync::Arc;
 
@@ -1603,85 +1606,4 @@ pub struct WriteFile {
     compressed_data: Box<[u8]>,
     decompressed_size: usize,
     is_executable: bool,
-}
-
-#[cfg(test)]
-mod tests {
-    use std::collections::HashMap;
-    use std::collections::HashSet;
-
-    use buck2_common::file_ops::FileMetadata;
-    use buck2_execute::directory::insert_file;
-    use buck2_execute::directory::ActionDirectoryBuilder;
-    use gazebo::dupe::Dupe;
-
-    use super::*;
-
-    #[test]
-    fn test_find_artifacts() -> anyhow::Result<()> {
-        let artifact1 = ProjectRelativePathBuf::unchecked_new("foo/bar/baz".to_owned());
-        let artifact2 = ProjectRelativePathBuf::unchecked_new("foo/bar/bar/qux".to_owned());
-        let artifact3 = ProjectRelativePathBuf::unchecked_new("foo/bar/bar/quux".to_owned());
-        let artifact4 = ProjectRelativePathBuf::unchecked_new("foo/bar/qux/quuz".to_owned());
-        let non_artifact1 = ProjectRelativePathBuf::unchecked_new("foo/bar/qux".to_owned());
-        let non_artifact2 = ProjectRelativePathBuf::unchecked_new("foo/bar/bar/corge".to_owned());
-
-        let file = FileMetadata::empty();
-
-        // Build deps with artifacts 1-3, and non-artifacts 1-2
-        let mut builder = ActionDirectoryBuilder::empty();
-        insert_file(
-            &mut builder,
-            artifact1.join_normalized("f1")?.as_ref(),
-            file.dupe(),
-        )?;
-        insert_file(
-            &mut builder,
-            artifact2.join_normalized("d/f1")?.as_ref(),
-            file.dupe(),
-        )?;
-        insert_file(&mut builder, artifact3.as_ref(), file.dupe())?;
-        insert_file(&mut builder, non_artifact2.as_ref(), file.dupe())?;
-        builder.mkdir(&non_artifact1)?;
-
-        // Build tree with artifacts 1-4
-        let mut tree: FileTree<()> = FileTree::new();
-        tree.insert(artifact1.iter().map(|f| f.to_owned()), ());
-        tree.insert(artifact2.iter().map(|f| f.to_owned()), ());
-        tree.insert(artifact3.iter().map(|f| f.to_owned()), ());
-        tree.insert(artifact4.iter().map(|f| f.to_owned()), ());
-
-        let expected_artifacts: HashSet<_> =
-            vec![artifact1, artifact2, artifact3].into_iter().collect();
-        let found_artifacts: HashSet<_> = tree.find_artifacts(&builder).into_iter().collect();
-        assert_eq!(found_artifacts, expected_artifacts);
-        Ok(())
-    }
-
-    #[test]
-    fn test_remove_path() {
-        fn insert(tree: &mut FileTree<String>, path: &str) {
-            tree.insert(
-                ProjectRelativePath::unchecked_new(path)
-                    .iter()
-                    .map(|f| f.to_owned()),
-                path.to_owned(),
-            );
-        }
-
-        let mut tree: FileTree<String> = FileTree::new();
-        insert(&mut tree, "a/b/c/d");
-        insert(&mut tree, "a/b/c/e");
-        insert(&mut tree, "a/c");
-
-        let removed_subtree = tree.remove_path(ProjectRelativePath::unchecked_new("a/b"));
-        // Convert to HashMap<String, String> so it's easier to test
-        let removed_subtree: HashMap<String, String> = removed_subtree
-            .map(|(k, v)| (k.as_str().to_owned(), v))
-            .collect();
-
-        assert_eq!(removed_subtree.len(), 2);
-        assert_eq!(removed_subtree.get("a/b/c/d"), Some(&"a/b/c/d".to_owned()));
-        assert_eq!(removed_subtree.get("a/b/c/e"), Some(&"a/b/c/e".to_owned()));
-    }
 }
