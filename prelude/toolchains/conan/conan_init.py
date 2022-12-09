9c208b73b7ee5cb1383d876ee30b52a391c5c48b
Andreas Herrmann <andreas.herrmann@tweag.io>
2022-12-09 14:55:41 +0100

    Factor out common Conan functionality

--- prelude/toolchains/conan/conan_init.py
+++ prelude/toolchains/conan/conan_init.py
@@ -4,42 +4,16 @@ import os
 import shutil
 import subprocess
 
-
-def conan_dir():
-    """Conan folder under the Conen user home."""
-    return ".conan"
-
-
-def conan_env(user_home, trace_log):
-    """Generate environment variables needed to invoke Conan."""
-    env = dict(os.environ)
-
-    # TODO[AH] Enable Conan revisions for reproducibility
-    # Enable Conan revisions for reproducibility
-    #env["CONAN_REVISIONS_ENABLED"] = "1"
-    # Prevent over-allocation.
-    env["CONAN_CPU_COUNT"] = "1"
-    # Prevent interactive prompts.
-    env["CONAN_NON_INTERACTIVE"] = "1"
-    # Print every `self.run` invokation.
-    # TODO[AH] Remove this debug output.
-    env["CONAN_PRINT_RUN_COMMANDS"] = "1"
-    # Set the Conan base directory.
-    env["CONAN_USER_HOME"] = os.path.abspath(user_home)
-    # Disable the short paths feature on Windows.
-    # TODO[AH] Enable if needed with a hermetic short path.
-    env["CONAN_USER_HOME_SHORT"] = "None"
-    # Enable Conan debug trace.
-    env["CONAN_TRACE_FILE"] = os.path.abspath(trace_log)
-
-    return env
+import conan_common
 
 
 def conan_profile(
         conan,
         user_home,
         trace_log):
-    env = conan_env(user_home, trace_log)
+    env = conan_common.conan_env(
+            user_home=user_home,
+            trace_log=trace_log)
 
     # TODO[AH] Do we need to support multiple profiles for cross-compilation?
     profile_name = "default"
@@ -50,9 +24,9 @@ def conan_profile(
         ("settings.compiler.libcxx", "libstdc++11"),
     ]
 
-    subprocess.check_call([conan, "profile", "new", profile_name], env=env)
+    conan_common.run_conan(conan, "profile", "new", profile_name, env=env)
     for key, value in settings:
-        subprocess.check_call([conan, "profile", "update", "{}={}".format(key, value), profile_name], env=env)
+        conan_common.run_conan(conan, "profile", "update", "{}={}".format(key, value), profile_name, env=env)
 
 
 def main():
