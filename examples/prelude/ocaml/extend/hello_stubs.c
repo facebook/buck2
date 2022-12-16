b0db6dbc9b778dd12f13645f165112a459991957
Shayne Fletcher <shaynefletcher@fb.com>
2022-12-16 09:41:33 -0800

    ocaml: add an example for extending ocaml from C
    Differential Revision: D42100526
    
    fbshipit-source-id: b5d6e3badcba6c106648407f380f4dc769e963ec

---
+++ examples/prelude/ocaml/extend/hello_stubs.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+#include <caml/mlvalues.h>
+#include <stdio.h>
+
+CAMLprim value caml_print_hello(value unit) {
+  (void)unit;
+  printf("Hello\n");
+  return Val_unit;
+}
