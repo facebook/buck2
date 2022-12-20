52c7fb4a041011a96a80d4f9257fc1acdafc41b1
Andreas Herrmann <andreas.herrmann@tweag.io>
2022-12-20 16:29:52 +0100

    Test zlib using adler32
    To test that the headers are exposed and the library is linked properly.

--- examples/prelude/cpp/conan/main.cpp
+++ examples/prelude/cpp/conan/main.cpp
@@ -1,5 +1,12 @@
+#include <cstring>
 #include <iostream>
+#include <zlib.h>
 
 int main() {
-  std::cout << "Hello Conan" << std::endl;
+  uLong adler = adler32(0L, Z_NULL, 0);
+
+  const char msg[] = "Hello Conan";
+  adler = adler32(adler, reinterpret_cast<const Bytef *>(msg), strlen(msg));
+
+  std::cout << adler << std::endl;
 }
