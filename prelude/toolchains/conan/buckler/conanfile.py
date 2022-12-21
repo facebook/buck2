29e7c2a49040380f6654c2e3314ff55be193ee56
Andreas Herrmann <andreas.herrmann@tweag.io>
2022-12-21 17:45:23 +0100

    Buckler only provides a generator
    A toolchain is for the use-case of Conan exposing its configuration and
    compiler toolchain to another build tool. We have the opposite use-case,
    and use Conan profiles to that end instead.

--- prelude/toolchains/conan/buckler/conanfile.py
+++ prelude/toolchains/conan/buckler/conanfile.py
@@ -233,10 +233,8 @@ Buckler - Conan extension for Buck2
 
 This package provides a
 - [Generator][generator] to import Conan built packages into Buck2.
-- [Toolchain][toolchain] to expose the Buck2 configuration to Conan.
 
 [generator]: https://docs.conan.io/en/latest/reference/generators.html#generators-reference
-[toolchain]: https://docs.conan.io/en/latest/creating_packages/toolchains.html
 """
     url = "https://github.com/facebookincubator/buck2"
     license = "Apache-2.0"
