39d477ec478d37b76572665ca692079753570f13
Alexey Kozhevnikov <akozhevnikov@meta.com>
2023-01-12 04:12:14 -0800

    always parse Xcode version from plist
    Summary:
    we just want to always use the plist for two reasons:
    
    - plist always has all fields which we need for our exec platforms and correct identification of xcode
    - our version checker uses plist, so we want to be using teh same source of info (otherwise we run into mismatch issues)
    
    Reviewed By: milend
    
    Differential Revision: D42475371
    
    fbshipit-source-id: 25673de1a0223bb912e108310ea970d47b1933d7

--- buck2_interpreter/src/extra/xcode.rs
+++ buck2_interpreter/src/extra/xcode.rs
@@ -39,55 +39,18 @@ pub struct XcodeVersionInfo {
 }
 
 impl XcodeVersionInfo {
-    // Construct from Xcode app name, falling back to Info.plist if possible.
+    // Construct from Info.plist in root of Xcode install dir.
     pub fn new() -> anyhow::Result<Self> {
         let resolved_xcode_path = fs_util::canonicalize(&PathBuf::from(XCODE_SELECT_SYMLINK))
             .context("resolve selected xcode link")?;
-        Self::from_path(&resolved_xcode_path).or_else(|_| {
-            let plist_path = resolved_xcode_path
-                .parent()
-                .map(|base| base.join("Info.plist"))
-                .ok_or_else(|| anyhow::anyhow!("unable to construct path to Xcode Info.plist"))?;
-            Self::from_info_plist(&plist_path)
-        })
+        let plist_path = resolved_xcode_path
+            .parent()
+            .map(|base| base.join("Info.plist"))
+            .ok_or_else(|| anyhow::anyhow!("unable to construct path to Xcode Info.plist"))?;
+        Self::from_info_plist(&plist_path)
     }
 
-    // Parse version info from paths like
-    // /Applications/Xcode_14.0.0_14A309_fb.app/Contents/Developer
-    pub fn from_path(path: &Path) -> anyhow::Result<Self> {
-        Self::from_path_impl(path)
-            .ok_or_else(|| anyhow::anyhow!("failed to parse path: {}", path.display()))
-    }
-
-    fn from_path_impl(path: &Path) -> Option<Self> {
-        let app_name = path.components().nth(2)?.as_os_str().to_str()?;
-        let mut app_name_parts = app_name.split('_').skip(1);
-
-        // Version number information.
-        let version = app_name_parts.next()?;
-        let mut version_parts = version.split('.');
-        let major = version_parts.next()?;
-        let minor = version_parts.next()?;
-        let patch = version_parts.next()?;
-
-        // Build number is optional.
-        let build_number = match app_name_parts.next() {
-            Some("beta") => None,
-            Some(build_id) => Some(build_id.to_owned()),
-            _ => None,
-        };
-
-        Some(Self {
-            version_string: Some(version.to_owned()),
-            major_version: Some(major.to_owned()),
-            minor_version: Some(minor.to_owned()),
-            patch_version: Some(patch.to_owned()),
-            build_number,
-        })
-    }
-
-    // Parse version information from Info.plist in root of Xcode install dir.
-    pub fn from_info_plist(plist_path: &Path) -> anyhow::Result<Self> {
+    pub(crate) fn from_info_plist(plist_path: &Path) -> anyhow::Result<Self> {
         let plist: XcodeInfoPlist =
             plist::from_file(plist_path).context("deserializing Xcode Info.plist")?;
 
@@ -115,54 +78,6 @@ mod tests {
     use std::fs;
 
     use super::*;
-
-    #[test]
-    fn test_fails_to_resolve_from_path() {
-        let got = XcodeVersionInfo::from_path(&PathBuf::from(
-            "/Applications/Xcode.app/Contents/Developer",
-        ));
-        assert!(got.is_err());
-    }
-
-    #[test]
-    fn test_resolves_version_from_path() {
-        let got = XcodeVersionInfo::from_path(&PathBuf::from(
-            "/Applications/Xcode_14.0.0_14A309_fb.app/Contents/Developer",
-        ))
-        .unwrap();
-        let want = XcodeVersionInfo {
-            version_string: Some("14.0.0".to_owned()),
-            major_version: Some("14".to_owned()),
-            minor_version: Some("0".to_owned()),
-            patch_version: Some("0".to_owned()),
-            build_number: Some("14A309".to_owned()),
-        };
-        assert_eq!(want, got);
-    }
-
-    #[test]
-    fn test_resolves_beta_version_from_path() {
-        let got = XcodeVersionInfo::from_path(&PathBuf::from(
-            "/Applications/Xcode_14.0.0_beta_6.app/Contents/Developer",
-        ))
-        .unwrap();
-        let want = XcodeVersionInfo {
-            version_string: Some("14.0.0".to_owned()),
-            major_version: Some("14".to_owned()),
-            minor_version: Some("0".to_owned()),
-            patch_version: Some("0".to_owned()),
-            build_number: None,
-        };
-        assert_eq!(want, got);
-    }
-
-    #[test]
-    fn test_resolves_version_from_path_fail() {
-        let got = XcodeVersionInfo::from_path(&PathBuf::from(
-            "/Applications/Xcode_foo_bar.app/Contents/Developer",
-        ));
-        assert!(got.is_err());
-    }
 
     fn write_plist(plist_content: &str) -> (tempfile::TempDir, PathBuf) {
         let workspace = tempfile::tempdir().expect("failed to create tempdir");
