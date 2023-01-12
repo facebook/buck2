7cfcb632c93fce631f55b8a9b47ea09510702524
Neil Mitchell <ndmitchell@meta.com>
2023-01-12 05:03:23 -0800

    Note why MAX_FILE_CHANGE is 100
    Summary: Useful context.
    
    Reviewed By: krallin
    
    Differential Revision: D42470784
    
    fbshipit-source-id: aed1544d0fe2f63efa1cebca7df33e1b7f35ffb4

--- buck2_server/src/file_watcher/stats.rs
+++ buck2_server/src/file_watcher/stats.rs
@@ -9,6 +9,11 @@
 
 use allocative::Allocative;
 
+/// We limit the number of file change records so we don't use too much memory
+/// or too much space in scribe.
+///
+/// 100 entries covers everything required for 96% of updates, which seems sufficient.
+/// Number needs to be < 850 or it is often bigger than a scribe message.
 const MAX_FILE_CHANGE_RECORDS: usize = 100;
 
 #[derive(Allocative)]
