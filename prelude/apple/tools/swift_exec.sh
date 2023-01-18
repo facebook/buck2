2eb2c5630c8eb0d59bf55c0b3d34628fd55e25f5
Milen Dzhumerov <mgd@meta.com>
2023-01-18 08:06:19 -0800

    Swift: guard against compiler not producing output files
    Summary:
    We've observed Swift compiler failures which result in no output file being created while exiting with a zero exit code. This ends up creating corrupted cache artifacts on RE leading to cache poisoning issues.
    
    Add a post-compiler check to ensure the output file always exist.
    
    Reviewed By: rmaz
    
    Differential Revision: D42573208
    
    fbshipit-source-id: 4853beeecd40807880a505aaee395a35ef0e9bfa

--- prelude/apple/tools/swift_exec.sh
+++ prelude/apple/tools/swift_exec.sh
@@ -6,6 +6,8 @@
 # License, Version 2.0 found in the LICENSE-APACHE file in the root directory
 # of this source tree.
 
+set -e
+
 # - Apply a debug prefix map for the current directory
 # to make debug info relocatable.
 # - Use $TMPDIR for the module cache location. This
@@ -23,4 +25,31 @@ else
     MODULE_CACHE_PATH="/tmp/buck-module-cache"
 fi
 
-exec "$@" -debug-prefix-map "$PWD"=. -module-cache-path "$MODULE_CACHE_PATH"
+"$@" -debug-prefix-map "$PWD"=. -module-cache-path "$MODULE_CACHE_PATH"
+
+OUTPUT_PATHS=()
+for ARG in "$@"
+do
+    if [ "${FOUND_OUTPUT_ARG}" = 1 ]; then
+        OUTPUT_PATHS+=( "${ARG}" )
+    fi
+    FOUND_OUTPUT_ARG=0
+    if [ "${ARG}" = "-o" ] || [ "${ARG}" = "-emit-module-path" ] || [ "${ARG}" = "-emit-objc-header-path" ]; then
+        FOUND_OUTPUT_ARG=1
+    fi
+done
+
+if [ ${#OUTPUT_PATHS[@]} -eq 0 ]; then
+    >&2 echo "No output paths found, ensure output args are not passed in argfiles"
+    exit 1
+fi
+
+for OUTPUT_PATH in "${OUTPUT_PATHS[@]}"
+do
+    # We've observed cases where the Swift compiler would return with a zero exit code but
+    # would not have created the output file, correctly a non-zero exit code in such cases.
+    if [ ! -f "${OUTPUT_PATH}" ]; then
+        >&2 echo "Output file does not exist: '${OUTPUT_PATH}'"
+        exit 1
+    fi
+done
