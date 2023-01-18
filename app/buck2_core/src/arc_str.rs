6e1db02b99fed38e4b434a76275033de214f909b
Stiopa Koltsov <nga@meta.com>
2023-01-17 20:06:52 -0800

    Rewrite ArcStr
    Summary:
    Rewrite without `Arc<str>`. Copy-paste parts of `Arc<str>`.
    
    In new version:
    * 8 bytes less per allocation because there's no `Weak`
    * one atomic decrement on the last drop, not two, because there's no `Weak`
    * size_of::<Option<ArcStr>>` is 2, not 3
    
    # Why not use triomphe
    
    [Arc from triomphe](https://docs.rs/triomphe/latest/triomphe/struct.Arc.html)
    
    Three reasons:
    * do not allocate anything for empty string
      * or if we store it as `Option<Arc<str>>`, then `Option<ArcStr>` is three words instead of two
    * following diff D42537186 makes length 32 bit instead of 64
    * potentially implement inline string optimization D41325906
    
    Differential Revision: D41253745
    
    fbshipit-source-id: fca6530eb3f9786f48fea26022a7e5dca9d20c88

--- app/buck2_core/src/arc_str.rs
+++ app/buck2_core/src/arc_str.rs
@@ -7,12 +7,21 @@
  * of this source tree.
  */
 
+use core::slice;
+use std::alloc;
+use std::alloc::Layout;
 use std::borrow::Borrow;
 use std::cmp::Ordering;
 use std::fmt::Debug;
 use std::fmt::Display;
 use std::hash::Hash;
+use std::mem;
 use std::ops::Deref;
+use std::ptr;
+use std::ptr::NonNull;
+use std::str;
+use std::sync::atomic;
+use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 
 use allocative::Allocative;
@@ -20,25 +29,60 @@ use dupe::Dupe;
 use serde::Serialize;
 use static_assertions::assert_eq_size;
 
+#[repr(C)]
+struct ArcStrInner {
+    counter: AtomicUsize,
+    // data: [],
+}
+
+impl ArcStrInner {
+    const OFFSET_OF_DATA: usize = mem::size_of::<ArcStrInner>();
+
+    fn layout_for_len(len: usize) -> Layout {
+        let size = Self::OFFSET_OF_DATA.checked_add(len).unwrap();
+        let align = mem::align_of::<ArcStrInner>();
+        Layout::from_size_align(size, align).unwrap()
+    }
+
+    unsafe fn layout_for_len_unchecked(len: usize) -> Layout {
+        let size = Self::OFFSET_OF_DATA + len;
+        let align = mem::align_of::<ArcStrInner>();
+        unsafe { Layout::from_size_align_unchecked(size, align) }
+    }
+}
+
 /// Wrapper for `Arc<str>`.
-#[derive(Clone, Dupe, Allocative)]
-#[derive(Default)]
-pub struct ArcStr(
-    /// `None` is used to represent the empty string.
-    Option<Arc<str>>,
-);
+pub struct ArcStr {
+    /// Points to the end of `ArcStrInner` struct.
+    /// Dangling pointer when empty.
+    data: NonNull<u8>,
+    len: usize,
+}
+
+unsafe impl Send for ArcStr {}
+unsafe impl Sync for ArcStr {}
+
+impl Default for ArcStr {
+    fn default() -> ArcStr {
+        ArcStr::EMPTY
+    }
+}
 
 assert_eq_size!(ArcStr, Arc<str>);
+assert_eq_size!(Option<ArcStr>, Arc<str>);
 
 impl ArcStr {
     /// Empty string.
-    pub const EMPTY: ArcStr = ArcStr(None);
+    pub const EMPTY: ArcStr = ArcStr {
+        data: NonNull::dangling(),
+        len: 0,
+    };
 
     #[inline]
     pub fn as_str(&self) -> &str {
-        match &self.0 {
-            Some(s) => s,
-            None => "",
+        unsafe {
+            let bytes = slice::from_raw_parts(self.data.as_ptr(), self.len);
+            str::from_utf8_unchecked(bytes)
         }
     }
 }
@@ -69,7 +113,7 @@ impl Hash for ArcStr {
 impl PartialEq for ArcStr {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
-        self.as_str() == other.as_str()
+        ptr::eq(self.data.as_ptr(), other.data.as_ptr()) || self.as_str() == other.as_str()
     }
 }
 
@@ -113,17 +157,106 @@ impl Debug for ArcStr {
 impl<'a> From<&'a str> for ArcStr {
     fn from(s: &'a str) -> ArcStr {
         if s.is_empty() {
-            ArcStr::default()
+            ArcStr::EMPTY
         } else {
-            ArcStr(Some(Arc::from(s)))
+            let layout = ArcStrInner::layout_for_len(s.len());
+            unsafe {
+                let alloc = alloc::alloc(layout);
+                ptr::write(
+                    alloc.cast::<ArcStrInner>(),
+                    ArcStrInner {
+                        counter: AtomicUsize::new(1),
+                    },
+                );
+                let data = alloc.add(ArcStrInner::OFFSET_OF_DATA);
+                ptr::copy_nonoverlapping(s.as_ptr(), data, s.len());
+                ArcStr {
+                    data: NonNull::new(data).unwrap(),
+                    len: s.len(),
+                }
+            }
+        }
+    }
+}
+
+impl Clone for ArcStr {
+    fn clone(&self) -> Self {
+        if self.len == 0 {
+            ArcStr::EMPTY
+        } else {
+            unsafe {
+                let arc_str_inner = &*self
+                    .data
+                    .as_ptr()
+                    .sub(ArcStrInner::OFFSET_OF_DATA)
+                    .cast::<ArcStrInner>();
+                // This is what `Arc::clone` does.
+                let old_count = arc_str_inner
+                    .counter
+                    .fetch_add(1, atomic::Ordering::Relaxed);
+                const MAX_REFCOUNT: usize = isize::MAX as usize;
+                if old_count > MAX_REFCOUNT {
+                    arc_str_inner
+                        .counter
+                        .fetch_sub(1, atomic::Ordering::Relaxed);
+                    panic!("refcount overflow");
+                }
+                ArcStr {
+                    data: self.data,
+                    len: self.len,
+                }
+            }
+        }
+    }
+}
+
+impl Dupe for ArcStr {}
+
+impl Drop for ArcStr {
+    fn drop(&mut self) {
+        if self.len != 0 {
+            unsafe {
+                let arc_str_inner = self
+                    .data
+                    .as_ptr()
+                    .sub(ArcStrInner::OFFSET_OF_DATA)
+                    .cast::<ArcStrInner>();
+                // Again, copy-paste from `Arc::drop`.
+                if (*arc_str_inner)
+                    .counter
+                    .fetch_sub(1, atomic::Ordering::Release)
+                    != 1
+                {
+                    return;
+                }
+                atomic::fence(atomic::Ordering::Acquire);
+                let layout = ArcStrInner::layout_for_len_unchecked(self.len);
+                alloc::dealloc(arc_str_inner.cast::<u8>(), layout);
+            }
         }
     }
 }
 
 impl From<String> for ArcStr {
-    #[inline]
     fn from(s: String) -> ArcStr {
         ArcStr::from(s.as_str())
+    }
+}
+
+impl Allocative for ArcStr {
+    fn visit<'a, 'b: 'a>(&self, visitor: &'a mut allocative::Visitor<'b>) {
+        let mut visitor = visitor.enter_self_sized::<Self>();
+        if self.len != 0 {
+            let mut visitor =
+                visitor.enter_unique(allocative::Key::new("data"), mem::size_of::<*const u8>());
+            visitor.visit_simple(
+                allocative::Key::new("ArcStrInner"),
+                mem::size_of::<ArcStrInner>(),
+            );
+            visitor.visit_simple(allocative::Key::new("data"), self.len);
+            visitor.exit();
+        }
+        visitor.exit();
     }
 }
 
@@ -138,8 +271,12 @@ mod tests {
     use std::collections::hash_map::DefaultHasher;
     use std::hash::Hash;
     use std::hash::Hasher;
+    use std::sync::atomic;
+
+    use dupe::Dupe;
 
     use crate::arc_str::ArcStr;
+    use crate::arc_str::ArcStrInner;
 
     #[test]
     fn test_serialize() {
@@ -160,5 +297,39 @@ mod tests {
 
         assert_eq!(hash(""), hash(&ArcStr::from("")));
         assert_eq!(hash("hello"), hash(&ArcStr::from("hello")));
+    }
+
+    #[test]
+    fn test_clone_drop_ref_count() {
+        fn ref_count(s: &ArcStr) -> usize {
+            assert!(!s.is_empty());
+            unsafe {
+                (*(s.data.as_ptr().sub(ArcStrInner::OFFSET_OF_DATA) as *const ArcStrInner))
+                    .counter
+                    .load(atomic::Ordering::Relaxed)
+            }
+        }
+
+        let s = ArcStr::from("hello");
+        assert_eq!(1, ref_count(&s));
+        let s2 = s.dupe();
+        assert_eq!(2, ref_count(&s));
+        drop(s);
+        assert_eq!(1, ref_count(&s2));
+    }
+
+    #[test]
+    fn test_little_stress_test() {
+        for i in 0..100 {
+            let strings = (0..i)
+                .map(|i| ArcStr::from(i.to_string()))
+                .collect::<Vec<_>>();
+            for j in 0..1000 {
+                let _ignore = ArcStr::from(j.to_string());
+            }
+            for (k, s) in strings.iter().enumerate() {
+                assert_eq!(k.to_string(), s.as_str());
+            }
+        }
     }
 }
