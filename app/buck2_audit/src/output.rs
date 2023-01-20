de612e934fafed2c9afc565f839bfc407a415572
Wendy Yu <wendyy@meta.com>
2023-01-20 12:59:37 -0800

    audit output buck-out validation and parsing
    Summary: Useful helper method. Used in next diff
    
    Reviewed By: krallin
    
    Differential Revision: D42371674
    
    fbshipit-source-id: 03651be6f15f77bf2d5ccfee02c796819d910fbb

--- app/buck2_audit/src/output.rs
+++ app/buck2_audit/src/output.rs
@@ -8,11 +8,31 @@
  */
 
 use async_trait::async_trait;
+use buck2_build_api::calculation::Calculation;
 use buck2_cli_proto::ClientContext;
+use buck2_common::dice::cells::HasCellResolver;
+use buck2_core::configuration::Configuration;
+use buck2_execute::path::buck_out_path::BuckOutPathParser;
+use buck2_execute::path::buck_out_path::BuckOutPathType;
 use buck2_server_ctx::ctx::ServerCommandContextTrait;
+use buck2_server_ctx::ctx::ServerCommandDiceContext;
+use buck2_server_ctx::pattern::target_platform_from_client_context;
+use thiserror::Error;
 
 use crate::AuditCommandCommonOptions;
 use crate::AuditSubcommand;
+
+#[derive(Debug, Error)]
+pub(crate) enum AuditOutputError {
+    #[error(
+        "BXL, anonymous target, test, and tmp artifacts are not supported for audit output. Only rule output artifacts are supported. Path: `{0}`"
+    )]
+    UnsupportedPathType(String),
+    #[error(
+        "Current platform does not match the configuration of the artifact path. Current platform: `{0}` with hash: `{1}`. Artifact platform hash: `{2}`"
+    )]
+    PlatformMismatch(Configuration, String, String),
+}
 
 #[derive(Debug, clap::Parser, serde::Serialize, serde::Deserialize)]
 #[clap(
@@ -37,11 +57,54 @@ pub struct AuditOutputCommand {
 impl AuditSubcommand for AuditOutputCommand {
     async fn server_execute(
         &self,
-        _server_ctx: Box<dyn ServerCommandContextTrait>,
-        _client_ctx: ClientContext,
+        server_ctx: Box<dyn ServerCommandContextTrait>,
+        client_ctx: ClientContext,
     ) -> anyhow::Result<()> {
-        // TODO(@wendyy) implement audit-output
-        Ok(())
+        server_ctx
+            .with_dice_ctx(async move |server_ctx, dice_ctx| {
+                let working_dir = server_ctx.working_dir();
+                let cell_resolver = dice_ctx.get_cell_resolver().await?;
+
+                let buck_out_parser = BuckOutPathParser::new(&cell_resolver);
+
+                let parsed = buck_out_parser.parse(&self.output_path)?;
+
+                match parsed {
+                    BuckOutPathType::RuleOutput {
+                        target_label,
+                        config_hash,
+                        ..
+                    } => {
+                        let global_target_platform = target_platform_from_client_context(
+                            Some(&client_ctx),
+                            &cell_resolver,
+                            working_dir,
+                        )
+                        .await?;
+
+                        let configured_target_label = dice_ctx
+                            .get_configured_target(&target_label, global_target_platform.as_ref())
+                            .await?;
+
+                        let command_config = configured_target_label.cfg();
+                        let command_config_hash = command_config.output_hash().to_owned();
+                        if !command_config_hash.eq(&config_hash) {
+                            return Err(anyhow::anyhow!(AuditOutputError::PlatformMismatch(
+                                command_config.clone(),
+                                command_config_hash,
+                                config_hash,
+                            )));
+                        }
+
+                        // TODO(@wendyy) implement running analysis and looking through actions
+                        Ok(())
+                    }
+                    _ => Err(anyhow::anyhow!(AuditOutputError::UnsupportedPathType(
+                        self.output_path.to_owned()
+                    ))),
+                }
+            })
+            .await
     }
 
     fn common_opts(&self) -> &AuditCommandCommonOptions {
