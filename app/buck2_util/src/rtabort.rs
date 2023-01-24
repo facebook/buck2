3a9380b694ac442479e11275089f6c0647245323
Stiopa Koltsov <nga@meta.com>
2023-01-23 20:38:27 -0800

    Extract rtabort! macro body in a function
    Summary: Smaller functions calling `rtabort!`, better for inlining.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D42665962
    
    fbshipit-source-id: 60d5ceabab7bb6bad7718809ead092ca374715e4

--- app/buck2_util/src/rtabort.rs
+++ app/buck2_util/src/rtabort.rs
@@ -7,6 +7,8 @@
  * of this source tree.
  */
 
+use std::fmt::Arguments;
+use std::io;
 use std::process;
 
 /// Like `panic!`, but aborts the process instead of unwinding.
@@ -15,20 +17,41 @@ use std::process;
 /// others may copy-paste code.
 #[macro_export]
 macro_rules! rtabort {
+    ($m:literal) => {
+        $crate::rtabort::rtabort_impl_fixed_string(
+            file!(),
+            line!(),
+            $m,
+        )
+    };
     ($($t:tt)*) => {
-        {
-            // Make sure we abort even if formatting panics.
-            let _abort = $crate::rtabort::AbortOnDrop;
+        $crate::rtabort::rtabort_impl(
+            file!(),
+            line!(),
+            format_args!($($t)*),
+        )
+    };
+}
 
-            // `eprintln!` followed by `abort` does not print anything in tests.
-            std::io::Write::write_fmt(
-                &mut std::io::stderr(),
-                format_args!("{}:{}: abort: {}\n", file!(), line!(), format_args!($($t)*))).ok();
+#[cold]
+pub fn rtabort_impl_fixed_string(file: &str, line: u32, message: &str) -> ! {
+    rtabort_impl(file, line, format_args!("{}", message));
+}
 
-            // Tell the compiler that we never return.
-            std::process::abort();
-        }
-    }
+#[cold]
+pub fn rtabort_impl(file: &str, line: u32, msg: Arguments) -> ! {
+    // Make sure we abort even if formatting panics.
+    let _abort = AbortOnDrop;
+
+    // `eprintln!` followed by `abort` does not print anything in tests.
+    io::Write::write_fmt(
+        &mut io::stderr(),
+        format_args!("{}:{}: abort: {}\n", file, line, msg),
+    )
+    .ok();
+
+    // Tell the compiler that we never return.
+    process::abort();
 }
 
 pub struct AbortOnDrop;
@@ -41,6 +64,14 @@ impl Drop for AbortOnDrop {
 
 #[cfg(test)]
 mod tests {
+    fn _test_compiles_fixed_string() {
+        rtabort!("test");
+    }
+
+    fn _test_compiles_with_format_args() {
+        rtabort!("test {}", 17);
+    }
+
     #[test]
     fn test_rtabort() {
         // Uncomment to test.
