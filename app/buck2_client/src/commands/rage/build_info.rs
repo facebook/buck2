68d40bc21adb19bc34bcc15f72f631debefa0c26
Ignacio Guridi <iguridi@meta.com>
2023-02-16 13:37:00 -0800

    buck2: rage: don't abort when corrupted log is found
    Reviewed By: krallin
    
    Differential Revision: D43325372
    
    fbshipit-source-id: 887bfa1e2e0a2af731338386c90339fd149a3426

--- app/buck2_client/src/commands/rage/build_info.rs
+++ app/buck2_client/src/commands/rage/build_info.rs
@@ -52,8 +52,16 @@ pub async fn get(log: &EventLogPathBuf) -> anyhow::Result<String> {
         daemon_uptime_s: None,
         timestamp_end: None,
     };
-    while let Some(event) = filtered_events.try_next().await? {
-        extract_info(&mut info, event)?;
+    loop {
+        let res = match filtered_events.try_next().await {
+            Ok(Some(event)) => extract_info(&mut info, event),
+            Ok(None) => break,
+            Err(e) => Err(e),
+        };
+        if let Err(e) = res {
+            buck2_client_ctx::eprintln!("Error found when iterating through logs: {:#}", e)?;
+            break;
+        }
     }
 
     let timestamp_start = first_event.timestamp();
