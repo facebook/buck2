0cf4c9f053b1a7179286f00448177f2a8d99f837
Nathaniel Brough <nathaniel.brough@gmail.com>
2023-03-01 09:26:21 -0800

    feat(fuzz): Adds a fuzz harness for the starlark executor (#64)
    Summary:
    This PR adds a basic fuzz test for the starlark parser and executor. The fuzzer has found a couple of non-critical bugs already :) so hopefully it'll be of some use to you :)
    
    X-link: https://github.com/facebookexperimental/starlark-rust/pull/64
    
    Reviewed By: stepancheg
    
    Differential Revision: D43697975
    
    Pulled By: ndmitchell
    
    fbshipit-source-id: f8274390e804bafc4003d7018930ede14a506dd4

---
+++ starlark-rust/starlark/fuzz/fuzz_targets/starlark.rs
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 The Starlark in Rust Authors.
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#![no_main]
+
+use std::hint::black_box;
+
+use libfuzzer_sys::fuzz_target;
+use starlark::environment::Globals;
+use starlark::environment::Module;
+use starlark::eval::Evaluator;
+use starlark::syntax::AstModule;
+use starlark::syntax::Dialect;
+
+fn run_arbitrary_starlark(content: &str) -> anyhow::Result<()> {
+    let ast: AstModule =
+        AstModule::parse("hello_world.star", content.to_owned(), &Dialect::Standard)?;
+    let globals: Globals = Globals::standard();
+    let module: Module = Module::new();
+    let mut eval: Evaluator = Evaluator::new(&module);
+    let value = black_box(eval.eval_module(ast, &globals)?);
+    _ = black_box(format!("{value:?}"));
+    return Ok(());
+}
+
+fuzz_target!(|content: &str| {
+    if let Err(e) = black_box(run_arbitrary_starlark(content)) {
+        _ = black_box(format!("{e:?}"));
+    }
+});
