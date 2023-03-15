c18ebfdb84f0e57b66331e3ac0e5f207badae75c
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2023-03-15 04:42:48 -0700

    Warn user when running buck2 in elevated shell
    Summary: We didn't have a windows impl for this.
    
    Reviewed By: krallin
    
    Differential Revision: D43438086
    
    fbshipit-source-id: ce2cbcfbc7f5a00422ff19bc93da272b765b1d7b

--- cli/src/check_user_allowed.rs
+++ cli/src/check_user_allowed.rs
@@ -9,7 +9,69 @@
 
 #[cfg(windows)]
 pub(crate) fn check_user_allowed() -> anyhow::Result<()> {
-    Ok(())
+    use std::io;
+    use std::mem;
+    use std::mem::MaybeUninit;
+    use std::ptr;
+
+    use winapi::ctypes::c_void;
+    use winapi::shared::minwindef::DWORD;
+    use winapi::um::handleapi::CloseHandle;
+    use winapi::um::processthreadsapi::GetCurrentProcess;
+    use winapi::um::processthreadsapi::OpenProcessToken;
+    use winapi::um::securitybaseapi::GetTokenInformation;
+    use winapi::um::winnt::TokenElevation;
+    use winapi::um::winnt::HANDLE;
+    use winapi::um::winnt::TOKEN_ELEVATION;
+    use winapi::um::winnt::TOKEN_QUERY;
+
+    struct Handle(HANDLE);
+    impl Drop for Handle {
+        fn drop(&mut self) {
+            let exit = unsafe { CloseHandle(self.0) };
+            if exit == 0 {
+                panic!("Failed closing process token handle")
+            }
+        }
+    }
+
+    let mut handle = ptr::null_mut();
+    let token_ok = unsafe { OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &mut handle) };
+    if !handle.is_null() && token_ok != 0 {
+        let handle = Handle(handle);
+        let size = mem::size_of::<TOKEN_ELEVATION>();
+        let elevation: MaybeUninit<TOKEN_ELEVATION> = MaybeUninit::zeroed();
+        let mut ret_size = 0;
+
+        let success_get = unsafe {
+            GetTokenInformation(
+                handle.0,
+                TokenElevation,
+                elevation.as_ptr() as *mut c_void,
+                size as DWORD,
+                &mut ret_size,
+            )
+        };
+
+        if success_get != 0 {
+            let elevation_struct: TOKEN_ELEVATION = unsafe { elevation.assume_init() };
+            if elevation_struct.TokenIsElevated == 1 {
+                tracing::warn!(
+                    "You're running buck2 from an admin shell. Invocations from non-admin shells will likely fail going forward. To remediate, run `buck2 clean` in this admin shell, then switch to a non-admin shell."
+                );
+            }
+            return Ok(());
+        }
+    }
+    let err = io::Error::last_os_error();
+    Err(io::Error::new(
+        err.kind(),
+        format!(
+            "Failed while trying to determine shell permissions: {}",
+            err
+        ),
+    )
+    .into())
 }
 
 #[cfg(not(windows))]
