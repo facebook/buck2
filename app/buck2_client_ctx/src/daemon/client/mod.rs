6d5908d3e277f0befd2eac51fb7951ffd425bace
Thomas Orozco <torozco@meta.com>
2023-03-15 10:27:16 -0700

    buck2: cli: split out kill into its own module
    Summary: Like it says in the title. I need this in the next diff.
    
    Differential Revision: D43977629
    
    fbshipit-source-id: a915b6c2418e1622d80173c636ee18c01686ae86

--- app/buck2_client_ctx/src/daemon/client/mod.rs
+++ app/buck2_client_ctx/src/daemon/client/mod.rs
@@ -11,7 +11,6 @@ use std::fs::create_dir_all;
 use std::fs::File;
 use std::pin::Pin;
 use std::time::Duration;
-use std::time::Instant;
 
 use anyhow::Context;
 use async_trait::async_trait;
@@ -28,12 +27,6 @@ use futures::Stream;
 use futures::StreamExt;
 use futures::TryStreamExt;
 use prost::Message;
-use sysinfo::Pid;
-use sysinfo::PidExt;
-use sysinfo::ProcessExt;
-use sysinfo::ProcessRefreshKind;
-use sysinfo::System;
-use sysinfo::SystemExt;
 use tonic::codegen::InterceptedService;
 use tonic::transport::Channel;
 use tonic::Request;
@@ -50,12 +43,10 @@ use crate::events_ctx::PartialResultHandler;
 use crate::stream_value::StreamValue;
 
 pub mod connect;
+pub mod kill;
 
 use crate::replayer::Replayer;
 use crate::startup_deadline::StartupDeadline;
-
-static GRACEFUL_SHUTDOWN_TIMEOUT: Duration = Duration::from_secs(4);
-static FORCE_SHUTDOWN_TIMEOUT: Duration = Duration::from_secs(2);
 
 pub enum VersionCheckResult {
     Match,
@@ -228,11 +219,6 @@ fn grpc_to_stream(
     .right_stream()
 }
 
-enum KillBehavior {
-    WaitForExit,
-    TerminateFirst,
-}
-
 impl BuckdClient {
     fn open_tailers(&mut self) -> anyhow::Result<()> {
         match &self.client {
@@ -303,168 +289,7 @@ impl BuckdClient {
     }
 
     pub async fn kill(&mut self, reason: &str) -> anyhow::Result<()> {
-        let pid = self.info.pid;
-        let callers = get_callers_for_kill();
-
-        let request_fut = self
-            .client
-            .daemon_only_mut()
-            .kill(Request::new(KillRequest {
-                reason: reason.to_owned(),
-                timeout: Some(GRACEFUL_SHUTDOWN_TIMEOUT.try_into()?),
-                callers,
-            }));
-        let time_to_kill = GRACEFUL_SHUTDOWN_TIMEOUT + FORCE_SHUTDOWN_TIMEOUT;
-        let time_req_sent = Instant::now();
-        // First we send a Kill request
-        let kill_behavior = match tokio::time::timeout(time_to_kill, request_fut).await {
-            Ok(inner_result) => {
-                match inner_result {
-                    Ok(_) => KillBehavior::WaitForExit,
-                    Err(e) => {
-                        // The kill request can fail if the server is in a bad state and we cannot
-                        // authenticate to it.
-                        crate::eprintln!("Error requesting graceful shutdown: {}", e)?;
-                        // Try an OS-level terminate next.
-                        KillBehavior::TerminateFirst
-                    }
-                }
-            }
-            Err(_) => KillBehavior::WaitForExit,
-        };
-        // Then we do a wait_for on the pid, and if that times out, we kill it harder
-        Self::kill_impl(
-            pid,
-            kill_behavior,
-            time_to_kill.saturating_sub(time_req_sent.elapsed()),
-        )
-        .await
-    }
-
-    #[cfg(unix)]
-    async fn kill_impl(pid: i64, behavior: KillBehavior, timeout: Duration) -> anyhow::Result<()> {
-        use nix::sys::signal::Signal;
-
-        let daemon_pid = nix::unistd::Pid::from_raw(pid as i32);
-        enum WaitFor {
-            Exited,
-            WaitTimedOut,
-            Err(anyhow::Error),
-        }
-        async fn wait_for(pid: nix::unistd::Pid, timeout: Duration) -> WaitFor {
-            let start = Instant::now();
-            while Instant::now() - start < timeout {
-                match nix::sys::signal::kill(pid, None) {
-                    Ok(_) => {}
-                    Err(nix::errno::Errno::ESRCH) => {
-                        return WaitFor::Exited;
-                    }
-                    Err(e) => {
-                        return WaitFor::Err(anyhow::anyhow!(
-                            "unexpected system error waiting for daemon to terminate (`{}`)",
-                            e
-                        ));
-                    }
-                }
-                tokio::time::sleep(Duration::from_millis(100)).await;
-            }
-            WaitFor::WaitTimedOut
-        }
-
-        match behavior {
-            KillBehavior::TerminateFirst => {
-                crate::eprintln!("Sending SIGTERM.")?;
-                // We send SIGKILL below even if this fails.
-                let _ignored = nix::sys::signal::kill(daemon_pid, Signal::SIGTERM);
-            }
-            KillBehavior::WaitForExit => {}
-        };
-
-        match wait_for(daemon_pid, timeout).await {
-            WaitFor::Exited => Ok(()),
-            WaitFor::Err(e) => Err(e),
-            WaitFor::WaitTimedOut => {
-                match nix::sys::signal::kill(daemon_pid, Signal::SIGKILL) {
-                    Ok(()) => {
-                        crate::eprintln!("Graceful shutdown timed out. Sending SIGKILL.")?;
-                    }
-                    Err(nix::errno::Errno::ESRCH) => return Ok(()),
-                    Err(e) => return Err(e).context("Failed to kill daemon"),
-                };
-
-                loop {
-                    match nix::sys::signal::kill(daemon_pid, None) {
-                        Ok(_) => {}
-                        Err(nix::errno::Errno::ESRCH) => {
-                            return Ok(());
-                        }
-                        Err(e) => {
-                            return Err(anyhow::anyhow!(
-                                "unexpected system error waiting for daemon to terminate (`{}`)",
-                                e
-                            ));
-                        }
-                    }
-                    tokio::time::sleep(Duration::from_millis(100)).await;
-                }
-            }
-        }
-    }
-
-    #[cfg(windows)]
-    async fn kill_impl(pid: i64, behavior: KillBehavior, timeout: Duration) -> anyhow::Result<()> {
-        use winapi::shared::winerror::WAIT_TIMEOUT;
-        use winapi::um::handleapi::CloseHandle;
-        use winapi::um::processthreadsapi::OpenProcess;
-        use winapi::um::processthreadsapi::TerminateProcess;
-        use winapi::um::synchapi::WaitForSingleObject;
-        use winapi::um::winbase::WAIT_OBJECT_0;
-        use winapi::um::winnt::HANDLE;
-        use winapi::um::winnt::PROCESS_TERMINATE;
-        use winapi::um::winnt::SYNCHRONIZE;
-
-        struct HandleWrapper {
-            handle: HANDLE,
-        }
-        impl Drop for HandleWrapper {
-            fn drop(&mut self) {
-                unsafe { CloseHandle(self.handle) };
-            }
-        }
-
-        let daemon_pid = pid as u32;
-        let proc_handle = unsafe { OpenProcess(SYNCHRONIZE | PROCESS_TERMINATE, 0, daemon_pid) };
-        // If proc_handle is null, process died already.
-        if proc_handle.is_null() {
-            return Ok(());
-        }
-        let proc_handle = HandleWrapper {
-            handle: proc_handle,
-        };
-        let wait_result = match behavior {
-            KillBehavior::WaitForExit => unsafe {
-                WaitForSingleObject(proc_handle.handle, timeout.as_millis().try_into()?)
-            },
-            KillBehavior::TerminateFirst => {
-                // Don't wait for the process to die first if we were asked to just terminate it,
-                // fall through directly to TerminateProcess instead.
-                WAIT_TIMEOUT
-            }
-        };
-        match wait_result {
-            WAIT_OBJECT_0 => Ok(()), // process exited successfully
-            WAIT_TIMEOUT => {
-                // If process isn't signalled, terminate it forcefully.
-                match unsafe { TerminateProcess(proc_handle.handle, 1) } {
-                    0 => Err(anyhow::anyhow!("Failed to kill daemon ({})", daemon_pid)),
-                    _ => Ok(()),
-                }
-            }
-            error_code => Err(anyhow::anyhow!(
-                "Waiting for daemon process failed. Error code: {:#x}",
-                error_code
-            )),
-        }
+        kill::kill(self.client.daemon_only_mut(), &self.info, reason).await
     }
 
     pub async fn status(&mut self, snapshot: bool) -> anyhow::Result<StatusResponse> {
@@ -889,27 +714,6 @@ fn create_client_stream<
         request: Some(streaming_request::Request::Context(context)),
     };
     stream::once(async move { init_req }).chain(requests.map(|request| request.into()))
-}
-
-fn get_callers_for_kill() -> Vec<String> {
-    /// Add a proess to our parts, and return its parent PID.
-    fn push_process(pid: Pid, system: &mut System, process_tree: &mut Vec<String>) -> Option<Pid> {
-        system.refresh_process_specifics(pid, ProcessRefreshKind::new());
-        let proc = system.process(pid)?;
-        let title = shlex::join(proc.cmd().iter().map(|s| s.as_str()));
-        process_tree.push(title);
-        proc.parent()
-    }
-
-    let mut system = System::new();
-    let mut process_tree = Vec::new();
-
-    let mut pid = Some(Pid::from_u32(std::process::id()));
-    while let Some(p) = pid {
-        pid = push_process(p, &mut system, &mut process_tree);
-    }
-
-    process_tree.into_iter().rev().collect()
 }
 
 #[cfg(test)]
