f312a3526c38ea4fbce5d71bda9a5bb5722a98a7
Sean Vong-Karlage <skarlage@meta.com>
2023-03-22 11:50:35 -0700

    offline builds: Export I/O trace as a manifest with some additional information
    Summary:
    Exports the I/O trace as a well-formatted structure. Currently we include:
    * The list of paths accessed by builds
    * The repo revision (still tbd)
    * The path to the buck2 executable (currently hardcoded). In actuality this can probably stay hardcoded since we want to get the actual dotslash file
    
    After this, we'll hand off the manifest to a separate binary that will transform various file paths (e.g. materialize dotslash files) and package this up into a single zstd compressed archive. This is introduced in later diffs.
    
    Reviewed By: krallin
    
    Differential Revision: D44154910
    
    fbshipit-source-id: b124076583c1450516040df19144a4985b3ca017

--- app/buck2_client/src/commands/debug/trace_io.rs
+++ app/buck2_client/src/commands/debug/trace_io.rs
@@ -7,11 +7,16 @@
  * of this source tree.
  */
 
+use std::path::PathBuf;
+
+use anyhow::Context;
 use async_trait::async_trait;
 use buck2_cli_proto::daemon_constraints::TraceIoState;
 use buck2_cli_proto::trace_io_request;
 use buck2_cli_proto::TraceIoRequest;
+use buck2_cli_proto::TraceIoResponse;
 use buck2_client_ctx::client_ctx::ClientCommandContext;
+use buck2_client_ctx::command_outcome::CommandOutcome;
 use buck2_client_ctx::common::CommonBuildConfigurationOptions;
 use buck2_client_ctx::common::CommonConsoleOptions;
 use buck2_client_ctx::common::CommonDaemonCommandOptions;
@@ -19,6 +24,9 @@ use buck2_client_ctx::daemon::client::BuckdClientConnector;
 use buck2_client_ctx::daemon::client::NoPartialResultHandler;
 use buck2_client_ctx::exit_result::ExitResult;
 use buck2_client_ctx::streaming::StreamingCommand;
+use buck2_core::fs::fs_util;
+use buck2_offline_archive::OfflineArchiveManifest;
+use tokio::process::Command;
 
 /// Enable I/O tracing in the buck daemon so we keep track of which files
 /// go into a build.
@@ -37,8 +45,50 @@ enum Subcommand {
     Disable,
     /// Return whether I/O tracing is enabled.
     Status,
-    /// Dumps the current I/O trace stored by the daemon.
-    Dump,
+    /// Exports the I/O trace taken by the daemon in a structured manifest format.
+    ExportManifest {
+        #[clap(short, long, help = "Output path to write manifest to")]
+        out: Option<PathBuf>,
+    },
+}
+
+/// Fetch the current hg revision.
+async fn hg_revision() -> anyhow::Result<Option<String>> {
+    let result = Command::new("hg")
+        .arg("whereami")
+        .env("HGPLAIN", "1")
+        .output()
+        .await?;
+    if result.status.success() {
+        let out = String::from_utf8(result.stdout).context("hg stdout to string")?;
+        let out = out.trim();
+        if out.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(out.to_owned()))
+        }
+    } else {
+        let err = String::from_utf8(result.stderr).context("hg stderr to string")?;
+        Err(anyhow::anyhow!(err))
+    }
+}
+
+impl TraceIoCommand {
+    async fn send_request(
+        &self,
+        req: TraceIoRequest,
+        buckd: &mut BuckdClientConnector,
+        mut ctx: ClientCommandContext,
+    ) -> anyhow::Result<CommandOutcome<TraceIoResponse>> {
+        buckd
+            .with_flushing()
+            .trace_io(
+                req,
+                ctx.stdin().console_interaction_stream(self.console_opts()),
+                &mut NoPartialResultHandler,
+            )
+            .await
+    }
 }
 
 #[async_trait]
@@ -49,30 +99,46 @@ impl StreamingCommand for TraceIoCommand {
         self,
         buckd: &mut BuckdClientConnector,
         matches: &clap::ArgMatches,
-        mut ctx: ClientCommandContext,
+        ctx: ClientCommandContext,
     ) -> ExitResult {
-        if let Subcommand::Status | Subcommand::Dump = self.trace_io_action {
-            let context = ctx.client_context(self.common_opts(), matches, self.sanitized_argv())?;
-            let req = TraceIoRequest {
-                context: Some(context),
-                read_state: Some(trace_io_request::ReadIoTracingState {
-                    with_trace: std::matches!(self.trace_io_action, Subcommand::Dump),
-                }),
-            };
-            let resp = buckd
-                .with_flushing()
-                .trace_io(
-                    req,
-                    ctx.stdin().console_interaction_stream(self.console_opts()),
-                    &mut NoPartialResultHandler,
-                )
-                .await??;
-            tracing::warn!("I/O tracing status: {}", resp.enabled);
-            if let Subcommand::Dump = self.trace_io_action {
-                for path in &resp.trace {
-                    tracing::warn!("{:?}", path);
+        let context = ctx.client_context(self.common_opts(), matches, self.sanitized_argv())?;
+        match &self.trace_io_action {
+            Subcommand::Status => {
+                let req = TraceIoRequest {
+                    context: Some(context),
+                    read_state: Some(trace_io_request::ReadIoTracingState { with_trace: false }),
+                };
+                let resp = self.send_request(req, buckd, ctx).await??;
+                buck2_client_ctx::println!("I/O tracing status: {}", resp.enabled)?;
+            }
+            Subcommand::ExportManifest { out } => {
+                let req = TraceIoRequest {
+                    context: Some(context),
+                    read_state: Some(trace_io_request::ReadIoTracingState { with_trace: true }),
+                };
+                let resp = self.send_request(req, buckd, ctx).await??;
+                let mut trace = resp.trace;
+
+                // Incorporate buck2 executable files.
+                trace.push(".buck2".to_owned());
+                trace.push(".buck2-previous".to_owned());
+
+                let manifest = OfflineArchiveManifest {
+                    paths: trace,
+                    repo_revision: hg_revision().await.context("fetching hg revision")?,
+                };
+                let serialized = serde_json::to_string(&manifest)
+                    .context("serializing offline archive manifest to json")?;
+                if let Some(output_path) = &out {
+                    fs_util::write(output_path, &serialized)
+                        .context("writing offline archive manifest")?;
+                } else {
+                    buck2_client_ctx::println!("{}", serialized)?;
                 }
             }
+            // Subcommand::{Enable, Disable} handled by StreamingCommand::trace_io()
+            // via implicit daemon restart.
+            _ => {}
         }
 
         ExitResult::success()
