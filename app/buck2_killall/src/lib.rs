04c739a0c978739f4151a007f122ed620bbf3ab8
Thomas Orozco <torozco@meta.com>
2023-03-27 06:53:05 -0700

    buck2: extract killall implementation into Buck2
    Summary: For now, keep calling it from the wrapper (see the top diff here).
    
    Reviewed By: stepancheg
    
    Differential Revision: D44217165
    
    fbshipit-source-id: b18329b5b4093e34eb66d3c94fee97cea1f7513d

---
+++ app/buck2_killall/src/lib.rs
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+use std::collections::HashSet;
+
+use sysinfo::Pid;
+use sysinfo::PidExt;
+use sysinfo::ProcessExt;
+use sysinfo::System;
+use sysinfo::SystemExt;
+
+/// Kills all running Buck2 processes, except this process's hierarchy. Returns whether it
+/// succeeded without errors.
+pub fn killall(write: impl Fn(String)) -> bool {
+    let mut system = System::new();
+    system.refresh_processes();
+
+    let mut current_parents = HashSet::new();
+    let mut parent = Some(Pid::from_u32(std::process::id()));
+    while let Some(pid) = parent {
+        current_parents.insert(pid);
+        parent = system.process(pid).and_then(|p| p.parent());
+    }
+
+    let mut buck2_processes = Vec::new();
+    for (pid, process) in system.processes() {
+        let exe_name = process.exe().file_stem().and_then(|s| s.to_str());
+        if exe_name == Some("buck2") && !current_parents.contains(pid) {
+            buck2_processes.push(process);
+        }
+    }
+
+    if buck2_processes.is_empty() {
+        write("No buck2 processes found".to_owned());
+        return true;
+    }
+
+    let mut ok = true;
+
+    for process in buck2_processes {
+        let result = if process.kill() {
+            // TODO(nga): this says "killed", but it doesn't wait for the process to exit.
+            "Killed"
+        } else {
+            ok = false;
+            "Failed to kill"
+        };
+
+        write(format!(
+            "{} {} ({}). {}",
+            result,
+            process.name(),
+            process.pid(),
+            process.cmd().join(" ")
+        ));
+    }
+
+    ok
+}
