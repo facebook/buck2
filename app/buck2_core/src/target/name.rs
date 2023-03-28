3f2e7957e0c88950352f2a9eb902ccb74ed485f8
Stiopa Koltsov <nga@meta.com>
2023-03-28 16:22:41 -0700

    Prohibit ... in target name
    Summary:
    Command like
    
    ```
    buck2 build foo/...
    ```
    
    should be unambiguous: it should always work as target pattern `...` instead of possibility of being interpreted as
    
    ```
    buck2 build foo/...:...
    ```
    
    Reviewed By: bobyangyf
    
    Differential Revision: D44474817
    
    fbshipit-source-id: 00e532be975bed68b09d1cbde444ba0f1465b52d

--- app/buck2_core/src/target/name.rs
+++ app/buck2_core/src/target/name.rs
@@ -121,9 +121,17 @@ impl Deref for TargetName {
 pub(crate) enum LabelValidationError {
     #[error("Label `{0}` has special character `{1}`, which is discouraged")]
     LabelHasSpecialCharacter(String, String),
+    #[error("Target name must not be equal to `...`")]
+    DotDotDot,
 }
 
 pub(crate) fn emit_label_validation_errors(name: &str) -> anyhow::Result<()> {
+    if name == "..." {
+        soft_error!(
+            "label_is_dot_dot_dot",
+            LabelValidationError::DotDotDot.into()
+        )?;
+    }
     if name.contains(',') {
         quiet_soft_error!(
             "label_has_comma",
