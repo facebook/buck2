e2ec51e35984b8b01cb0d4cf76fa79b9f49d141d
Chris Hopman <cjhopman@meta.com>
2023-04-07 13:02:11 -0700

    impl Stream for StreamingRequestHandler
    Summary: ^
    
    Reviewed By: krallin
    
    Differential Revision: D43941401
    
    fbshipit-source-id: c7c5f132f3ad12778e976a4e2265b3f55c970370

--- app/buck2_server/src/streaming_request_handler.rs
+++ app/buck2_server/src/streaming_request_handler.rs
@@ -8,15 +8,22 @@
  */
 
 use std::marker::PhantomData;
+use std::pin::Pin;
+use std::task::Context;
+use std::task::Poll;
 
 use buck2_cli_proto::StreamingRequest;
+use futures::Stream;
+use pin_project::pin_project;
 use tonic::Status;
 
 /// Simple container that holds onto a stream of incoming client requests.
 ///
 /// The primary use for this is pulling messages of a specific type from
 /// the client via [`StreamingRequestHandler::message`]
+#[pin_project]
 pub struct StreamingRequestHandler<T: TryFrom<StreamingRequest, Error = Status>> {
+    #[pin]
     client_stream: tonic::Streaming<StreamingRequest>,
     _phantom: PhantomData<T>,
 }
@@ -40,5 +47,23 @@ impl<T: TryFrom<StreamingRequest, Error = Status>> StreamingRequestHandler<T> {
             )),
         }?;
         request.try_into()
+    }
+
+    fn map_poll(v: Option<Result<StreamingRequest, Status>>) -> Option<Result<T, Status>> {
+        v.map(|v| v.and_then(|v| v.try_into()))
+    }
+}
+
+impl<T: TryFrom<StreamingRequest, Error = Status>> Stream for StreamingRequestHandler<T> {
+    type Item = Result<T, Status>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = self.project();
+        let inner: Pin<&mut _> = this.client_stream;
+
+        match inner.poll_next(cx) {
+            Poll::Ready(v) => Poll::Ready(Self::map_poll(v)),
+            Poll::Pending => Poll::Pending,
+        }
     }
 }
