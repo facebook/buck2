d2dbb296318bd22248e42cdd1d8cd6cc14fb61d5
Ruslan Sayfutdinov <sayfutdinov@meta.com>
2023-04-12 14:32:46 -0700

    buck2: correctly check process parents on Windows
    Summary:
    At least on Windows parent can be dead by the time we try to access it and parent's PID can be assigned to unrelated process.
    
    It seems Windows has different strategy to assigning PIDs and while on Unix PIDs are usually unique until they run out and start to be reused, on Windows it's just random.
    
    Because of this on Windows it's more likely that we can have a loop while iterating over `Process::parent()` when parent of current process died and child of current process spawned another process which PID of current process parent.
    
    To avoid that we can check parent's start time and make sure it's started earlier than current process.
    
    `Process::start_time()` returns time in second which is not precise enough to detect parents reliably.
    
    So use `GetProcessTimes` instead which counts 100 ns intervals.
    
    Docs:
    https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getprocesstimes
    
    Process creation on Windows takes several milliseconds, so it's pretty much guaranteed that all processes will be strictly ordered by `lpCreationTime`.
    
    Also on Windows current Buck process may not have enough rights to read information about all other processes. `sysinfo` doesn't return an error in this case and just set all fields to default values.
    
    We probably shouldn't log information about such processes.
    So `process_creation_time` returns `None` if process information can't be read.
    
    Reviewed By: krallin
    
    Differential Revision: D44831994
    
    fbshipit-source-id: 93748ba8ff52876ec07c00818a5a4925b6b14049

--- app/buck2_client_ctx/src/daemon/client/kill.rs
+++ app/buck2_client_ctx/src/daemon/client/kill.rs
@@ -64,7 +64,7 @@ pub async fn kill(
         Err(_) => KillBehavior::WaitForExit,
     };
     // Then we do a wait_for on the pid, and if that times out, we kill it harder
-    kill_impl(
+    os_specific::kill_impl(
         pid,
         kill_behavior,
         time_to_kill.saturating_sub(time_req_sent.elapsed()),
@@ -73,65 +73,38 @@ pub async fn kill(
 }
 
 #[cfg(unix)]
-async fn kill_impl(pid: i64, behavior: KillBehavior, timeout: Duration) -> anyhow::Result<()> {
+mod os_specific {
+    use std::time::Duration;
+    use std::time::Instant;
+
     use anyhow::Context as _;
     use nix::sys::signal::Signal;
+    use sysinfo::Process;
+    use sysinfo::ProcessExt;
 
-    let daemon_pid = nix::unistd::Pid::from_raw(pid as i32);
-    enum WaitFor {
-        Exited,
-        WaitTimedOut,
-        Err(anyhow::Error),
-    }
-    async fn wait_for(pid: nix::unistd::Pid, timeout: Duration) -> WaitFor {
-        let start = Instant::now();
-        while Instant::now() - start < timeout {
-            match nix::sys::signal::kill(pid, None) {
-                Ok(_) => {}
-                Err(nix::errno::Errno::ESRCH) => {
-                    return WaitFor::Exited;
-                }
-                Err(e) => {
-                    return WaitFor::Err(anyhow::anyhow!(
-                        "unexpected system error waiting for daemon to terminate (`{}`)",
-                        e
-                    ));
-                }
-            }
-            tokio::time::sleep(Duration::from_millis(100)).await;
+    use super::KillBehavior;
+
+    pub(super) async fn kill_impl(
+        pid: i64,
+        behavior: KillBehavior,
+        timeout: Duration,
+    ) -> anyhow::Result<()> {
+        let daemon_pid = nix::unistd::Pid::from_raw(pid as i32);
+        enum WaitFor {
+            Exited,
+            WaitTimedOut,
+            Err(anyhow::Error),
         }
-        WaitFor::WaitTimedOut
-    }
-
-    match behavior {
-        KillBehavior::TerminateFirst => {
-            crate::eprintln!("Sending SIGTERM.")?;
-            // We send SIGKILL below even if this fails.
-            let _ignored = nix::sys::signal::kill(daemon_pid, Signal::SIGTERM);
-        }
-        KillBehavior::WaitForExit => {}
-    };
-
-    match wait_for(daemon_pid, timeout).await {
-        WaitFor::Exited => Ok(()),
-        WaitFor::Err(e) => Err(e),
-        WaitFor::WaitTimedOut => {
-            match nix::sys::signal::kill(daemon_pid, Signal::SIGKILL) {
-                Ok(()) => {
-                    crate::eprintln!("Graceful shutdown timed out. Sending SIGKILL.")?;
-                }
-                Err(nix::errno::Errno::ESRCH) => return Ok(()),
-                Err(e) => return Err(e).context("Failed to kill daemon"),
-            };
-
-            loop {
-                match nix::sys::signal::kill(daemon_pid, None) {
+        async fn wait_for(pid: nix::unistd::Pid, timeout: Duration) -> WaitFor {
+            let start = Instant::now();
+            while Instant::now() - start < timeout {
+                match nix::sys::signal::kill(pid, None) {
                     Ok(_) => {}
                     Err(nix::errno::Errno::ESRCH) => {
-                        return Ok(());
+                        return WaitFor::Exited;
                     }
                     Err(e) => {
-                        return Err(anyhow::anyhow!(
+                        return WaitFor::Err(anyhow::anyhow!(
                             "unexpected system error waiting for daemon to terminate (`{}`)",
                             e
                         ));
@@ -139,21 +112,76 @@ async fn kill_impl(pid: i64, behavior: KillBehavior, timeout: Duration) -> anyho
                 }
                 tokio::time::sleep(Duration::from_millis(100)).await;
             }
+            WaitFor::WaitTimedOut
         }
+
+        match behavior {
+            KillBehavior::TerminateFirst => {
+                crate::eprintln!("Sending SIGTERM.")?;
+                // We send SIGKILL below even if this fails.
+                let _ignored = nix::sys::signal::kill(daemon_pid, Signal::SIGTERM);
+            }
+            KillBehavior::WaitForExit => {}
+        };
+
+        match wait_for(daemon_pid, timeout).await {
+            WaitFor::Exited => Ok(()),
+            WaitFor::Err(e) => Err(e),
+            WaitFor::WaitTimedOut => {
+                match nix::sys::signal::kill(daemon_pid, Signal::SIGKILL) {
+                    Ok(()) => {
+                        crate::eprintln!("Graceful shutdown timed out. Sending SIGKILL.")?;
+                    }
+                    Err(nix::errno::Errno::ESRCH) => return Ok(()),
+                    Err(e) => return Err(e).context("Failed to kill daemon"),
+                };
+
+                loop {
+                    match nix::sys::signal::kill(daemon_pid, None) {
+                        Ok(_) => {}
+                        Err(nix::errno::Errno::ESRCH) => {
+                            return Ok(());
+                        }
+                        Err(e) => {
+                            return Err(anyhow::anyhow!(
+                                "unexpected system error waiting for daemon to terminate (`{}`)",
+                                e
+                            ));
+                        }
+                    }
+                    tokio::time::sleep(Duration::from_millis(100)).await;
+                }
+            }
+        }
+    }
+
+    pub(super) fn process_creation_time(process: &Process) -> Option<Duration> {
+        // Returns process creation time with 1 second precision.
+        Some(Duration::from_secs(process.start_time()))
     }
 }
 
 #[cfg(windows)]
-async fn kill_impl(pid: i64, behavior: KillBehavior, timeout: Duration) -> anyhow::Result<()> {
+mod os_specific {
+    use std::time::Duration;
+
+    use sysinfo::PidExt;
+    use sysinfo::Process;
+    use sysinfo::ProcessExt;
+    use winapi::shared::minwindef::FILETIME;
     use winapi::shared::winerror::WAIT_TIMEOUT;
     use winapi::um::handleapi::CloseHandle;
+    use winapi::um::processthreadsapi::GetProcessTimes;
     use winapi::um::processthreadsapi::OpenProcess;
     use winapi::um::processthreadsapi::TerminateProcess;
     use winapi::um::synchapi::WaitForSingleObject;
     use winapi::um::winbase::WAIT_OBJECT_0;
     use winapi::um::winnt::HANDLE;
+    use winapi::um::winnt::PROCESS_QUERY_INFORMATION;
     use winapi::um::winnt::PROCESS_TERMINATE;
     use winapi::um::winnt::SYNCHRONIZE;
+
+    use super::KillBehavior;
 
     struct HandleWrapper {
         handle: HANDLE,
@@ -164,62 +192,116 @@ async fn kill_impl(pid: i64, behavior: KillBehavior, timeout: Duration) -> anyho
         }
     }
 
-    let daemon_pid = pid as u32;
-    let proc_handle = unsafe { OpenProcess(SYNCHRONIZE | PROCESS_TERMINATE, 0, daemon_pid) };
-    // If proc_handle is null, process died already.
-    if proc_handle.is_null() {
-        return Ok(());
-    }
-    let proc_handle = HandleWrapper {
-        handle: proc_handle,
-    };
-    let wait_result = match behavior {
-        KillBehavior::WaitForExit => unsafe {
-            WaitForSingleObject(proc_handle.handle, timeout.as_millis().try_into()?)
-        },
-        KillBehavior::TerminateFirst => {
-            // Don't wait for the process to die first if we were asked to just terminate it,
-            // fall through directly to TerminateProcess instead.
-            WAIT_TIMEOUT
+    pub(super) async fn kill_impl(
+        pid: i64,
+        behavior: KillBehavior,
+        timeout: Duration,
+    ) -> anyhow::Result<()> {
+        let daemon_pid = pid as u32;
+        let proc_handle = unsafe { OpenProcess(SYNCHRONIZE | PROCESS_TERMINATE, 0, daemon_pid) };
+        // If proc_handle is null, process died already.
+        if proc_handle.is_null() {
+            return Ok(());
         }
-    };
-    match wait_result {
-        WAIT_OBJECT_0 => Ok(()), // process exited successfully
-        WAIT_TIMEOUT => {
-            // If process isn't signalled, terminate it forcefully.
-            match unsafe { TerminateProcess(proc_handle.handle, 1) } {
-                0 => Err(anyhow::anyhow!("Failed to kill daemon ({})", daemon_pid)),
-                _ => Ok(()),
+        let proc_handle = HandleWrapper {
+            handle: proc_handle,
+        };
+        let wait_result = match behavior {
+            KillBehavior::WaitForExit => unsafe {
+                WaitForSingleObject(proc_handle.handle, timeout.as_millis().try_into()?)
+            },
+            KillBehavior::TerminateFirst => {
+                // Don't wait for the process to die first if we were asked to just terminate it,
+                // fall through directly to TerminateProcess instead.
+                WAIT_TIMEOUT
             }
+        };
+        match wait_result {
+            WAIT_OBJECT_0 => Ok(()), // process exited successfully
+            WAIT_TIMEOUT => {
+                // If process isn't signalled, terminate it forcefully.
+                match unsafe { TerminateProcess(proc_handle.handle, 1) } {
+                    0 => Err(anyhow::anyhow!("Failed to kill daemon ({})", daemon_pid)),
+                    _ => Ok(()),
+                }
+            }
+            error_code => Err(anyhow::anyhow!(
+                "Waiting for daemon process failed. Error code: {:#x}",
+                error_code
+            )),
         }
-        error_code => Err(anyhow::anyhow!(
-            "Waiting for daemon process failed. Error code: {:#x}",
-            error_code
-        )),
+    }
+
+    pub(super) fn process_creation_time(process: &Process) -> Option<Duration> {
+        // Returns process creation time with 100 ns precision.
+        let proc_handle =
+            unsafe { OpenProcess(PROCESS_QUERY_INFORMATION, 0, process.pid().as_u32()) };
+        if proc_handle.is_null() {
+            return None;
+        }
+        let proc_handle = HandleWrapper {
+            handle: proc_handle,
+        };
+        let mut creation_time: FILETIME = unsafe { std::mem::zeroed() };
+        let mut exit_time: FILETIME = unsafe { std::mem::zeroed() };
+        let mut kernel_time: FILETIME = unsafe { std::mem::zeroed() };
+        let mut user_time: FILETIME = unsafe { std::mem::zeroed() };
+
+        let result = unsafe {
+            GetProcessTimes(
+                proc_handle.handle,
+                &mut creation_time,
+                &mut exit_time,
+                &mut kernel_time,
+                &mut user_time,
+            )
+        };
+
+        if result != 0 {
+            // It stores 100 ns intervals, multiple by 100 to get proper nanoseconds.
+            // u64 will run out approximately in the year of 2185 :)
+            let intervals = ((creation_time.dwHighDateTime as u64) << 32)
+                | (creation_time.dwLowDateTime as u64);
+            Some(Duration::from_nanos(intervals * 100))
+        } else {
+            None
+        }
     }
 }
 
 fn get_callers_for_kill() -> Vec<String> {
-    /// Add a proess to our parts, and return its parent PID.
-    fn push_process(pid: Pid, system: &mut System, process_tree: &mut Vec<String>) -> Option<Pid> {
-        system.refresh_process_specifics(pid, ProcessRefreshKind::new());
+    /// Add a process to our parts and return its parent PID.
+    fn push_process(
+        pid: Pid,
+        creation_time: Duration,
+        system: &mut System,
+        process_tree: &mut Vec<String>,
+    ) -> Option<(Pid, Duration)> {
+        // Specifics about this process need to be refreshed by this time.
         let proc = system.process(pid)?;
         let title = shlex::join(proc.cmd().iter().map(|s| s.as_str()));
         process_tree.push(title);
-        proc.parent()
+        let parent_pid = proc.parent()?;
+        system.refresh_process_specifics(parent_pid, ProcessRefreshKind::new());
+        let parent_proc = system.process(parent_pid)?;
+        let parent_creation_time = os_specific::process_creation_time(parent_proc)?;
+        if parent_creation_time <= creation_time {
+            Some((parent_pid, parent_creation_time))
+        } else {
+            None
+        }
     }
 
     let mut system = System::new();
     let mut process_tree = Vec::new();
 
-    let mut pid = Some(Pid::from_u32(std::process::id()));
-    // FIXME: This while loop of going up the process tree occasionally gets stuck on Windows. Limit to 20 levels.
-    let max_process_count = 20;
-    while let Some(p) = pid {
-        if process_tree.len() > max_process_count {
-            break;
-        }
-        pid = push_process(p, &mut system, &mut process_tree);
+    let pid = Pid::from_u32(std::process::id());
+    system.refresh_process_specifics(pid, ProcessRefreshKind::new());
+    let mut curr = system
+        .process(pid)
+        .and_then(|proc| Some((pid, os_specific::process_creation_time(proc)?)));
+    while let Some((pid, creation_time)) = curr {
+        curr = push_process(pid, creation_time, &mut system, &mut process_tree);
     }
 
     process_tree.into_iter().rev().collect()
