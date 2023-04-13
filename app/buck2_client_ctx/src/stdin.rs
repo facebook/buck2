d4b1273677b0c67ba3c33d31227415a3f54fa41b
Thomas Orozco <torozco@meta.com>
2023-04-13 08:14:07 -0700

    buck2: stdin: don't spawn a thread until we read
    Summary:
    Like it says in the title, because in the daemon we fork, and I want
    to move this up, so that means we need to defer this until later.
    
    I removed the test we had here since the code it was testing is
    gone.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D44922531
    
    fbshipit-source-id: c6625d637e84d9f5538ece976946b5603c103f8b

--- app/buck2_client_ctx/src/stdin.rs
+++ app/buck2_client_ctx/src/stdin.rs
@@ -10,18 +10,14 @@
 use std::io;
 use std::io::Read;
 use std::pin::Pin;
-use std::sync::Arc;
 use std::task::Context;
 use std::task::Poll;
 use std::thread::JoinHandle;
 
 use buck2_core::env_helper::EnvHelper;
 use bytes::Bytes;
-use dupe::Dupe;
 use futures::stream::Fuse;
 use futures::stream::StreamExt;
-use parking_lot::Condvar;
-use parking_lot::Mutex;
 use pin_project::pin_project;
 use tokio::io::AsyncRead;
 use tokio::io::ReadBuf;
@@ -38,7 +34,7 @@ pub struct Stdin {
     stream: StreamReader<Fuse<ReceiverStream<io::Result<Bytes>>>, Bytes>,
 
     // We don't start reading until poll_read is called.
-    waiter: Option<Arc<Waiter>>,
+    state: State,
 }
 
 impl AsyncRead for Stdin {
@@ -48,9 +44,7 @@ impl AsyncRead for Stdin {
         buf: &mut ReadBuf<'_>,
     ) -> Poll<io::Result<()>> {
         let this = self.project();
-        if let Some(waiter) = this.waiter.take() {
-            waiter.release();
-        }
+        this.state.ensure_started();
         this.stream.poll_read(cx, buf)
     }
 }
@@ -59,54 +53,15 @@ impl Stdin {
     pub fn new() -> anyhow::Result<Self> {
         static STDIN_BUFFER_SIZE: EnvHelper<usize> = EnvHelper::new("BUCK2_TEST_STDIN_BUFFER_SIZE");
         let buffer_size = STDIN_BUFFER_SIZE.get()?.copied().unwrap_or(8192);
-        let (stdin, _handle) = Self::new_for_reader(
-            || {
-                // NOTE: We ignore send errors since there is no point in reading without a receiver.
-                #[allow(clippy::let_and_return)]
-                let stdin = std::io::stdin().lock();
-
-                // Disable buffering, since we don't do small reads anyway (we have a 8KB buffer
-                // already). We probably need something similar on Windows here.
-                #[cfg(unix)]
-                let stdin = raw_reader::RawReader::new(stdin);
-
-                stdin
-            },
-            buffer_size,
-        );
-        Ok(stdin)
-    }
-
-    fn new_for_reader<R, F>(acquire_reader: F, buffer_size: usize) -> (Self, JoinHandle<()>)
-    where
-        R: Read,
-        F: FnOnce() -> R + Send + 'static,
-    {
-        let waiter = Arc::new(Waiter {
-            mutex: Mutex::new(false),
-            var: Condvar::new(),
-        });
 
         // Small buffer, this isn't bytes we're buffering, just buffers of bytes. That said, since
         // we're on separate threads, give ourselves a bit of buffering.
-        let (mut tx, rx) = mpsc::channel(4);
+        let (tx, rx) = mpsc::channel(4);
 
-        let handle = std::thread::spawn({
-            let waiter = waiter.dupe();
-            move || {
-                let reader = acquire_reader();
-                waiter.block_until_released();
-                let _ignored = read_and_forward(reader, &mut tx, buffer_size);
-            }
-        });
-
-        (
-            Self {
-                stream: StreamReader::new(ReceiverStream::new(rx).fuse()),
-                waiter: Some(waiter),
-            },
-            handle,
-        )
+        Ok(Self {
+            stream: StreamReader::new(ReceiverStream::new(rx).fuse()),
+            state: State::Pending { buffer_size, tx },
+        })
     }
 
     pub fn console_interaction_stream(
@@ -119,6 +74,49 @@ impl Stdin {
         }
 
         ConsoleInteractionStream::new(self)
+    }
+}
+
+enum State {
+    Pending {
+        buffer_size: usize,
+        tx: mpsc::Sender<io::Result<Bytes>>,
+    },
+    Started(JoinHandle<()>),
+}
+
+impl State {
+    fn ensure_started(&mut self) {
+        if !matches!(self, Self::Pending { .. }) {
+            return;
+        }
+
+        take_mut::take(self, |this| {
+            match this {
+                Self::Pending {
+                    buffer_size,
+                    mut tx,
+                } => {
+                    let handle = std::thread::spawn({
+                        move || {
+                            #[allow(clippy::let_and_return)]
+                            let stdin = std::io::stdin().lock();
+
+                            // Disable buffering, since we don't do small reads anyway (we have a 8KB buffer
+                            // already). We probably need something similar on Windows here.
+                            #[cfg(unix)]
+                            let stdin = raw_reader::RawReader::new(stdin);
+
+                            // NOTE: We ignore send errors since there is no point in reading without a receiver.
+                            let _ignored = read_and_forward(stdin, &mut tx, buffer_size);
+                        }
+                    });
+
+                    Self::Started(handle)
+                }
+                v => v,
+            }
+        });
     }
 }
 
@@ -153,25 +151,6 @@ fn read_and_forward(
     Ok(())
 }
 
-struct Waiter {
-    mutex: Mutex<bool>,
-    var: Condvar,
-}
-
-impl Waiter {
-    fn release(&self) {
-        *self.mutex.lock() = true;
-        self.var.notify_all();
-    }
-
-    fn block_until_released(&self) {
-        let mut guard = self.mutex.lock();
-        while !*guard {
-            self.var.wait(&mut guard);
-        }
-    }
-}
-
 #[cfg(unix)]
 mod raw_reader {
     use std::io;
@@ -204,29 +183,5 @@ mod raw_reader {
         fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
             unistd::read(self.fd, buf).map_err(io::Error::from)
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::io;
-    use std::io::Read;
-
-    use super::*;
-
-    struct PanicReader;
-
-    impl Read for PanicReader {
-        fn read(&mut self, _buf: &mut [u8]) -> io::Result<usize> {
-            panic!("!")
-        }
-    }
-
-    #[test]
-    fn read_waits_for_signal() {
-        // If we didn't wait then we'd panic in the reader thread.
-        let (_stdin, thread) = Stdin::new_for_reader(|| PanicReader, 8);
-        std::thread::sleep(std::time::Duration::from_millis(100));
-        assert!(!thread.is_finished());
     }
 }
