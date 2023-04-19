087e2a18f8d88014fd3e15043f3127a4273993c8
Bob Yang <bobyf@meta.com>
2023-04-18 22:23:43 -0700

    Add critical_section to CancellationContext
    Summary: copy old behaviour while we do mass refactors
    
    Reviewed By: krallin
    
    Differential Revision: D44853024
    
    fbshipit-source-id: f090e193a8ae5a462a11a1a0a28177b1abae60e8

--- shed/more_futures/src/cancellation.rs
+++ shed/more_futures/src/cancellation.rs
@@ -9,7 +9,10 @@
 
 //! Defines a future with explicit cancellation
 
+use std::future::Future;
 use std::sync::Arc;
+
+use crate::cancellable_future::critical_section;
 
 /// Context available to the function running inside the future to control and manage it's own
 /// cancellation
@@ -19,6 +22,26 @@ impl CancellationContext {
     /// TODO replace with real initialization
     pub fn todo() -> Self {
         Self(Arc::new(CancellationContextShared {}))
+    }
+
+    /// Enter a critical section during which the current future (if supports explicit cancellation)
+    /// should not be dropped. If the future was not cancelled before entering the critical section,
+    /// it becomes non-cancellable during the critical section. If it *was* cancelled before
+    /// entering the critical section (i.e. the last ref was dropped during `poll`), then the
+    /// future is allowed to continue executing until this future resolves.
+    ///
+    /// TODO(bobyf) this needs to be updated with the new implementation
+    pub fn critical_section<'a, F, Fut>(
+        &'a self,
+        make: F,
+    ) -> impl Future<Output = <Fut as Future>::Output> + 'a
+    where
+        F: FnOnce() -> Fut + 'a,
+        Fut: Future + 'a,
+    {
+        // placeholder to just delegate to existing critical_section code for now until we migrate
+        // the callsites
+        critical_section(make)
     }
 }
 
