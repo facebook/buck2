435584596ab81780730f4266cb0705b478a32953
Thomas Orozco <torozco@meta.com>
2023-04-26 07:27:34 -0700

    buck2: fix a help message
    Summary: Like it say in the title.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D45279621
    
    fbshipit-source-id: 7014d13c77acfb3bb2b54f452b4b72aff8944709

--- app/buck2_client/src/commands/log/critical_path.rs
+++ app/buck2_client/src/commands/log/critical_path.rs
@@ -20,7 +20,15 @@ use tokio_stream::StreamExt;
 
 use crate::commands::log::options::EventLogOptions;
 
-/// This command outputs stats about uploads to RE from the selected invocation.
+/// Show the critical path for a build.
+///
+/// This produces tab-delimited output listing every node on the critical path.
+///
+/// It includes the kind of node, its name, category and identfier, as well as total duration
+/// (runtime of this node), user duration (duration the user can improve) and potential improvement
+/// before this node stops being on the critical path.
+///
+/// All durations are in microseconds.
 #[derive(Debug, clap::Parser)]
 pub struct CriticalPathCommand {
     #[clap(flatten)]
