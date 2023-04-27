6b5249db3e5876e8b970b704f6d39a59d2d6275f
Bob Yang <bobyf@meta.com>
2023-04-27 16:19:08 -0700

    Add enum of implementations for cancellation context
    Summary: so that we can plumb the threadlocal and a new explicltly pass future implementation through
    
    Reviewed By: krallin
    
    Differential Revision: D45255914
    
    fbshipit-source-id: 673a48be7ada3175e147ae15be4534bdb57b411f

--- shed/more_futures/src/cancellation.rs
+++ shed/more_futures/src/cancellation.rs
@@ -10,7 +10,6 @@
 //! Defines a future with explicit cancellation
 
 use std::future::Future;
-use std::sync::Arc;
 
 use once_cell::sync::Lazy;
 
@@ -21,16 +20,16 @@ use crate::cancellable_future::CancellationObserver;
 use crate::cancellable_future::DisableCancellationGuard;
 
 static INSTANCE: Lazy<CancellationContext> =
-    Lazy::new(|| CancellationContext(Arc::new(CancellationContextShared {})));
+    Lazy::new(|| CancellationContext(CancellationContextInner::ThreadLocal));
 
 /// Context available to the function running inside the future to control and manage it's own
 /// cancellation
-pub struct CancellationContext(Arc<CancellationContextShared>);
+pub struct CancellationContext(CancellationContextInner);
 
 impl CancellationContext {
     /// TODO replace with real initialization
     pub fn todo() -> Self {
-        Self(Arc::new(CancellationContextShared {}))
+        CancellationContext(CancellationContextInner::ThreadLocal)
     }
 
     pub fn testing<'a>() -> &'a Self {
@@ -59,7 +58,7 @@ impl CancellationContext {
     {
         // placeholder to just delegate to existing critical_section code for now until we migrate
         // the callsites
-        critical_section(make)
+        self.0.critical_section(make)
     }
 
     /// Enter a structured cancellation section. The caller receives a CancellationObserver. The
@@ -75,7 +74,7 @@ impl CancellationContext {
         Fut: Future + 'a,
         F: FnOnce(CancellationObserver) -> Fut + 'a,
     {
-        with_structured_cancellation(make)
+        self.0.with_structured_cancellation(make)
     }
 
     /// Obtain a StrongRefCount for the current task. This will return None if the task *is* within a
@@ -83,9 +82,73 @@ impl CancellationContext {
     ///
     /// TODO(bobyf) this needs to be updated with the new implementation
     pub fn try_to_disable_cancellation(&self) -> Option<DisableCancellationGuard> {
-        try_to_disable_cancellation()
+        self.0.try_to_disable_cancellation()
     }
 }
 
-#[allow(unused)]
-struct CancellationContextShared {}
+enum CancellationContextInner {
+    /// The old thread local based implementation
+    ThreadLocal,
+    /// The cancellation context passed explicitly
+    #[allow(unused)]
+    Explicit,
+}
+
+impl CancellationContextInner {
+    /// Enter a critical section during which the current future (if supports explicit cancellation)
+    /// should not be dropped. If the future was not cancelled before entering the critical section,
+    /// it becomes non-cancellable during the critical section. If it *was* cancelled before
+    /// entering the critical section (i.e. the last ref was dropped during `poll`), then the
+    /// future is allowed to continue executing until this future resolves.
+    ///
+    /// TODO(bobyf) this needs to be updated with the new implementation
+    pub fn critical_section<'a, F, Fut>(
+        &'a self,
+        make: F,
+    ) -> impl Future<Output = <Fut as Future>::Output> + 'a
+    where
+        F: FnOnce() -> Fut + 'a,
+        Fut: Future + 'a,
+    {
+        match self {
+            CancellationContextInner::ThreadLocal => critical_section(make),
+            CancellationContextInner::Explicit => {
+                unimplemented!("todo")
+            }
+        }
+    }
+
+    /// Enter a structured cancellation section. The caller receives a CancellationObserver. The
+    /// CancellationObserver is a future that resolves when cancellation is requested (or when this
+    /// section exits).
+    ///
+    /// TODO(bobyf) this needs to be updated with the new implementation
+    pub fn with_structured_cancellation<'a, F, Fut>(
+        &'a self,
+        make: F,
+    ) -> impl Future<Output = <Fut as Future>::Output> + 'a
+    where
+        Fut: Future + 'a,
+        F: FnOnce(CancellationObserver) -> Fut + 'a,
+    {
+        match self {
+            CancellationContextInner::ThreadLocal => with_structured_cancellation(make),
+            CancellationContextInner::Explicit => {
+                unimplemented!("todo")
+            }
+        }
+    }
+
+    /// Obtain a StrongRefCount for the current task. This will return None if the task *is* within a
+    /// CancellableFuture but has already been cancelled.
+    ///
+    /// TODO(bobyf) this needs to be updated with the new implementation
+    pub fn try_to_disable_cancellation(&self) -> Option<DisableCancellationGuard> {
+        match self {
+            CancellationContextInner::ThreadLocal => try_to_disable_cancellation(),
+            CancellationContextInner::Explicit => {
+                unimplemented!("todo")
+            }
+        }
+    }
+}
