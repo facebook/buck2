9ee9858a2c80ead9047778490e65f0cdb3eda078
Jakob Degen <jakobdegen@meta.com>
2023-04-28 10:17:09 -0700

    log: More options for `log path`
    Summary: This makes the other log options work with `log path` too
    
    Reviewed By: stepancheg
    
    Differential Revision: D45380437
    
    fbshipit-source-id: 1ad45d24f7e87429dc3626698aefe70dde6823c5

--- app/buck2_client/src/commands/log/path_log.rs
+++ app/buck2_client/src/commands/log/path_log.rs
@@ -10,37 +10,43 @@
 use buck2_client_ctx::client_ctx::ClientCommandContext;
 use buck2_client_ctx::exit_result::ExitResult;
 use buck2_client_ctx::subscribers::event_log::file_names::retrieve_all_logs;
-use buck2_client_ctx::subscribers::event_log::file_names::retrieve_nth_recent_log;
+use buck2_client_ctx::tokio_runtime_setup::client_tokio_runtime;
+
+use crate::commands::log::options::EventLogOptions;
 
 /// This command outputs the path to a recent log.
 #[derive(Debug, clap::Parser)]
-#[clap(group = clap::ArgGroup::with_name("event_log"))]
 pub struct PathLogCommand {
     /// Find the log from the Nth most recent command (`--recent 0` is the most recent).
-    #[clap(
-        long,
-        group = "event_log",
-        value_name = "NUMBER",
-        conflicts_with = "all"
-    )]
-    recent: Option<usize>,
+    #[clap(flatten)]
+    event_log_options: EventLogOptions,
 
     /// List all the logs.
-    #[clap(long, group = "event_log", conflicts_with = "recent")]
+    #[clap(long, group = "event_log")]
     all: bool,
 }
 
 impl PathLogCommand {
     pub fn exec(self, _matches: &clap::ArgMatches, ctx: ClientCommandContext<'_>) -> ExitResult {
-        let Self { recent, all } = self;
-        let paths = if all {
-            retrieve_all_logs(&ctx)?
-        } else {
-            vec![retrieve_nth_recent_log(&ctx, recent.unwrap_or(0))?]
-        };
-        for path in paths {
-            buck2_client_ctx::println!("{}", path.path().display())?;
-        }
+        let Self {
+            event_log_options,
+            all,
+        } = self;
+
+        let rt = client_tokio_runtime()?;
+
+        rt.block_on(async move {
+            let paths = if all {
+                retrieve_all_logs(&ctx)?
+            } else {
+                vec![event_log_options.get(&ctx).await?]
+            };
+            for path in paths {
+                buck2_client_ctx::println!("{}", path.path().display())?;
+            }
+            anyhow::Ok(())
+        })?;
+
         ExitResult::success()
     }
 }
