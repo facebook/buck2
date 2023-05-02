51b49a06eb3c435e66b32c9fb95a0d5131188df5
Ruslan Sayfutdinov <sayfutdinov@meta.com>
2023-05-02 04:10:04 -0700

    buck2: unquote args for find_and_replace.bat
    Summary:
    RE may quote arguments even if they are not quoted by buck2. This may produce incorrect output file.
    
    This syntax unquotes arguments, so both cases work.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D45404339
    
    fbshipit-source-id: 4d9c1c28d481493a50f4677b30a6f5652997f5a0

--- prelude/tools/find_and_replace.bat
+++ prelude/tools/find_and_replace.bat
@@ -12,8 +12,9 @@
 :: arg4 = output file
 :: Take all instances of arg1 in arg3 and replace it with arg2
 :: The modified string is outputted into arg4, arg3 will not be modified
-set BEFORE=%1
-set AFTER=%2
+:: Note that BEFORE and AFTER are unquoted with %~1 syntax.
+set BEFORE=%~1
+set AFTER=%~2
 set IN=%3
 set OUT=%4
 (for /f "delims=" %%i in (%IN%) do (
