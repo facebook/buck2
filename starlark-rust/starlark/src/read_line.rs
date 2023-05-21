7f351af294d1b179586830ebfa40d601dcefa3aa
Stiopa Koltsov <nga@meta.com>
2023-05-21 13:41:11 -0700

    Disable rustyline for wasm32
    Reviewed By: ndmitchell
    
    Differential Revision: D46051038
    
    fbshipit-source-id: e8c22b7966e8b33608bab4f536b831b29822bd0d

--- starlark-rust/starlark/src/read_line.rs
+++ starlark-rust/starlark/src/read_line.rs
@@ -18,51 +18,75 @@
 // This is not public API, but it is used by Starlark command line utility.
 #![doc(hidden)]
 
-use std::env;
-use std::io;
+#[cfg(not(target_arch = "wasm32"))]
+mod with_or_without_rustyline {
+    use std::env;
+    use std::io;
 
-use rustyline::error::ReadlineError;
-use rustyline::history::DefaultHistory;
-use rustyline::Editor;
+    use rustyline::error::ReadlineError;
+    use rustyline::history::DefaultHistory;
+    use rustyline::Editor;
 
-/// Wrapper for the readline library, whichever we are using at the moment.
-pub struct ReadLine {
-    editor: Editor<(), DefaultHistory>,
-    histfile: Option<String>,
-}
-
-impl ReadLine {
-    pub fn new(histfile_env: &str) -> anyhow::Result<ReadLine> {
-        let mut editor = Editor::new()?;
-        let histfile = if let Ok(histfile) = env::var(histfile_env) {
-            if let Err(e) = editor.load_history(&histfile) {
-                match e {
-                    ReadlineError::Io(e) if e.kind() == io::ErrorKind::NotFound => {}
-                    e => eprintln!("Failed to load history from `{}`: {}", histfile, e),
-                }
-            }
-            Some(histfile)
-        } else {
-            None
-        };
-        Ok(ReadLine { editor, histfile })
+    /// Wrapper for the readline library, whichever we are using at the moment.
+    pub struct ReadLine {
+        editor: Editor<(), DefaultHistory>,
+        histfile: Option<String>,
     }
 
-    /// Read line. Return `None` on EOF or interrupt.
-    pub fn read_line(&mut self, prompt: &str) -> anyhow::Result<Option<String>> {
-        match self.editor.readline(prompt) {
-            Ok(line) => {
-                self.editor.add_history_entry(line.as_str())?;
-                if let Some(histfile) = &self.histfile {
-                    if let Err(e) = self.editor.save_history(&histfile) {
-                        eprintln!("Failed to save history to `{}`: {}", histfile, e);
+    impl ReadLine {
+        pub fn new(histfile_env: &str) -> anyhow::Result<ReadLine> {
+            let mut editor = Editor::new()?;
+            let histfile = if let Ok(histfile) = env::var(histfile_env) {
+                if let Err(e) = editor.load_history(&histfile) {
+                    match e {
+                        ReadlineError::Io(e) if e.kind() == io::ErrorKind::NotFound => {}
+                        e => eprintln!("Failed to load history from `{}`: {}", histfile, e),
                     }
                 }
-                Ok(Some(line))
+                Some(histfile)
+            } else {
+                None
+            };
+            Ok(ReadLine { editor, histfile })
+        }
+
+        /// Read line. Return `None` on EOF or interrupt.
+        pub fn read_line(&mut self, prompt: &str) -> anyhow::Result<Option<String>> {
+            match self.editor.readline(prompt) {
+                Ok(line) => {
+                    self.editor.add_history_entry(line.as_str())?;
+                    if let Some(histfile) = &self.histfile {
+                        if let Err(e) = self.editor.save_history(&histfile) {
+                            eprintln!("Failed to save history to `{}`: {}", histfile, e);
+                        }
+                    }
+                    Ok(Some(line))
+                }
+                // User pressed EOF - disconnected terminal, or similar
+                Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => Ok(None),
+                Err(e) => Err(e.into()),
             }
-            // User pressed EOF - disconnected terminal, or similar
-            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => Ok(None),
-            Err(e) => Err(e.into()),
         }
     }
 }
+
+#[cfg(target_arch = "wasm32")]
+mod with_or_without_rustyline {
+    #[derive(thiserror::Error, Debug)]
+    #[error("Rustyline is not supported on wasm32")]
+    struct NoRustyline;
+
+    pub struct ReadLine(());
+
+    impl ReadLine {
+        pub fn new(_histfile_env: &str) -> anyhow::Result<ReadLine> {
+            Err(NoRustyline.into())
+        }
+
+        pub fn read_line(&mut self, _prompt: &str) -> anyhow::Result<Option<String>> {
+            Err(NoRustyline.into())
+        }
+    }
+}
+
+pub use with_or_without_rustyline::ReadLine;
