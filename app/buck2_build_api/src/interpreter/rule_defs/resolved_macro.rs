3aa5ea12b9eb0f330cb48c2ebf6687523c86f421
Stiopa Koltsov <nga@meta.com>
2023-05-30 14:52:22 -0700

    Move ConfiguredMacro resolve to mod resolve
    Summary: Preparation to move attribute resolution to `buck2_analysis` crate (D46209543).
    
    Reviewed By: iguridi
    
    Differential Revision: D46208800
    
    fbshipit-source-id: f936ec370c80f1757bd822ca6a359cdd0c65090a

--- app/buck2_build_api/src/interpreter/rule_defs/resolved_macro.rs
+++ app/buck2_build_api/src/interpreter/rule_defs/resolved_macro.rs
@@ -14,11 +14,7 @@ use std::fmt::Debug;
 use std::fmt::Display;
 
 use allocative::Allocative;
-use buck2_core::provider::label::ConfiguredProvidersLabel;
-use buck2_node::attrs::attr_type::arg::ConfiguredMacro;
-use buck2_node::attrs::attr_type::arg::UnrecognizedMacro;
 use buck2_util::arc_str::ArcStr;
-use either::Either;
 use starlark::any::ProvidesStaticType;
 use starlark::starlark_type;
 use starlark::values::Demand;
@@ -28,8 +24,6 @@ use starlark::values::StarlarkValue;
 use starlark::values::Value;
 use static_assertions::assert_eq_size;
 
-use crate::attrs::resolve::attr_type::arg::query::ConfiguredQueryMacroBaseExt;
-use crate::attrs::resolve::ctx::AttrResolutionContext;
 use crate::interpreter::rule_defs::artifact::StarlarkArtifact;
 use crate::interpreter::rule_defs::artifact::StarlarkArtifactLike;
 use crate::interpreter::rule_defs::cmd_args::arg_builder::ArgBuilder;
@@ -41,8 +35,6 @@ use crate::interpreter::rule_defs::cmd_args::CommandLineBuilder;
 use crate::interpreter::rule_defs::cmd_args::CommandLineContext;
 use crate::interpreter::rule_defs::cmd_args::WriteToFileMacroVisitor;
 use crate::interpreter::rule_defs::provider::builtin::default_info::FrozenDefaultInfo;
-use crate::interpreter::rule_defs::provider::builtin::run_info::RunInfoCallable;
-use crate::interpreter::rule_defs::provider::builtin::template_placeholder_info::FrozenTemplatePlaceholderInfo;
 use crate::interpreter::rule_defs::resolve_query_macro::ResolvedQueryMacro;
 
 // TODO(cjhopman): Consider making DefaultOutputs implement CommandLineArgLike
@@ -102,108 +94,7 @@ fn add_outputs_to_arg(
     Ok(())
 }
 
-#[derive(Debug, thiserror::Error)]
-enum ResolvedMacroError {
-    #[error("Can't expand unrecognized macros (`{0}`).")]
-    UnrecognizedMacroUnimplemented(String),
-    #[error("Expected a RunInfo provider from target `{0}`.")]
-    ExpectedRunInfo(String),
-
-    #[error("There was no TemplatePlaceholderInfo for {0}.")]
-    KeyedPlaceholderInfoMissing(ConfiguredProvidersLabel),
-    #[error("There was no mapping for {0} in the TemplatePlaceholderInfo for {1}.")]
-    KeyedPlaceholderMappingMissing(String, ConfiguredProvidersLabel),
-    #[error(
-        "The mapping for {0} in the TemplatePlaceholderInfo for {1} was not a dictionary (required because requested arg `{2}`)."
-    )]
-    KeyedPlaceholderMappingNotADict(String, ConfiguredProvidersLabel, String),
-    #[error(
-        "The mapping for {0} in the TemplatePlaceholderInfo for {1} had no mapping for arg `{2}`."
-    )]
-    KeyedPlaceholderArgMissing(String, ConfiguredProvidersLabel, String),
-    #[error("There was no mapping for {0}.")]
-    UnkeyedPlaceholderUnresolved(String),
-}
-
 impl ResolvedMacro {
-    pub(crate) fn resolved(
-        configured_macro: &ConfiguredMacro,
-        ctx: &dyn AttrResolutionContext,
-    ) -> anyhow::Result<ResolvedMacro> {
-        match configured_macro {
-            ConfiguredMacro::Location(target) => {
-                let providers_value = ctx.get_dep(target)?;
-                let providers = providers_value.provider_collection();
-                Ok(ResolvedMacro::Location(providers.default_info()))
-            }
-            ConfiguredMacro::Exe { label, .. } => {
-                // Don't need to consider exec_dep as it already was applied when configuring the label.
-                let providers_value = ctx.get_dep(label)?;
-                let providers = providers_value.provider_collection();
-                let run_info = match providers.get_provider_raw(RunInfoCallable::provider_id()) {
-                    Some(value) => *value,
-                    None => {
-                        return Err(ResolvedMacroError::ExpectedRunInfo(label.to_string()).into());
-                    }
-                };
-                // A RunInfo is an arg-like value.
-                Ok(ResolvedMacro::ArgLike(FrozenCommandLineArg::new(run_info)?))
-            }
-            ConfiguredMacro::UserUnkeyedPlaceholder(name) => {
-                let provider = ctx.resolve_unkeyed_placeholder(name)?.ok_or_else(|| {
-                    ResolvedMacroError::UnkeyedPlaceholderUnresolved((**name).to_owned())
-                })?;
-                Ok(ResolvedMacro::ArgLike(provider))
-            }
-            ConfiguredMacro::UserKeyedPlaceholder(box (name, label, arg)) => {
-                let providers = ctx.get_dep(label)?;
-                let placeholder_info =
-                    FrozenTemplatePlaceholderInfo::from_providers(providers.provider_collection())
-                        .ok_or_else(|| {
-                            ResolvedMacroError::KeyedPlaceholderInfoMissing(label.clone())
-                        })?;
-                let keyed_variables = placeholder_info.keyed_variables();
-                let either_cmd_or_mapping = keyed_variables.get(&**name).ok_or_else(|| {
-                    ResolvedMacroError::KeyedPlaceholderMappingMissing(
-                        (**name).to_owned(),
-                        label.to_owned(),
-                    )
-                })?;
-
-                let value: FrozenCommandLineArg = match (arg, either_cmd_or_mapping) {
-                    (None, Either::Left(mapping)) => *mapping,
-                    (Some(arg), Either::Left(_)) => {
-                        return Err(ResolvedMacroError::KeyedPlaceholderMappingNotADict(
-                            (**name).to_owned(),
-                            label.clone(),
-                            (**arg).to_owned(),
-                        )
-                        .into());
-                    }
-                    (arg, Either::Right(mapping)) => {
-                        let arg = arg.as_deref().unwrap_or("DEFAULT");
-                        mapping.get(arg).copied().ok_or_else(|| {
-                            ResolvedMacroError::KeyedPlaceholderArgMissing(
-                                (**name).to_owned(),
-                                label.clone(),
-                                arg.to_owned(),
-                            )
-                        })?
-                    }
-                };
-
-                Ok(ResolvedMacro::ArgLike(value))
-            }
-            ConfiguredMacro::Query(query) => Ok(ResolvedMacro::Query(query.resolve(ctx)?)),
-            ConfiguredMacro::UnrecognizedMacro(box UnrecognizedMacro {
-                macro_type,
-                args: _,
-            }) => Err(anyhow::anyhow!(
-                ResolvedMacroError::UnrecognizedMacroUnimplemented((**macro_type).to_owned())
-            )),
-        }
-    }
-
     pub fn add_to_arg(
         &self,
         builder: &mut dyn ArgBuilder,
