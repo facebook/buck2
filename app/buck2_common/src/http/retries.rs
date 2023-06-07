3c4ba9afa55862bb493b303f0e604b0fabc7ba10
Ignacio Guridi <iguridi@meta.com>
2023-06-06 17:29:03 -0700

    buck2: http retries: add unit tests
    Reviewed By: stepancheg
    
    Differential Revision: D46414773
    
    fbshipit-source-id: 7bce866b56d66f17e8535774f82487f9fd8fe47c

--- app/buck2_common/src/http/retries.rs
+++ app/buck2_common/src/http/retries.rs
@@ -79,3 +79,93 @@ where
 
     unreachable!("The loop above will exit before we get to the end")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Mock {
+        responses: std::cell::RefCell<Vec<Result<String, HttpTestError>>>,
+    }
+
+    impl Mock {
+        fn new(responses: Vec<Result<String, HttpTestError>>) -> Self {
+            Self {
+                responses: std::cell::RefCell::new(responses),
+            }
+        }
+
+        async fn exec(&self) -> Result<String, HttpTestError> {
+            // This function is expected to be of type Fn and not FnMut
+            // We use internal mutability (RefCell) here to ensure that
+            let mut responses = self.responses.borrow_mut();
+            responses.remove(0)
+        }
+    }
+
+    #[derive(Debug, Error)]
+    enum HttpTestError {
+        #[error("Error in test")]
+        Client(#[from] HttpError),
+    }
+
+    impl AsHttpError for HttpTestError {
+        fn as_http_error(&self) -> Option<&HttpError> {
+            match self {
+                Self::Client(e) => Some(e),
+            }
+        }
+    }
+
+    fn ok_response() -> Result<String, HttpTestError> {
+        Ok("Success".to_owned()).map_err(|_: anyhow::Error| test_error(StatusCode::IM_A_TEAPOT))
+    }
+
+    fn retryable() -> Result<String, HttpTestError> {
+        Err(test_error(StatusCode::INTERNAL_SERVER_ERROR))
+    }
+
+    fn non_retryable() -> Result<String, HttpTestError> {
+        Err(test_error(StatusCode::NOT_FOUND))
+    }
+
+    fn test_error(status: StatusCode) -> HttpTestError {
+        HttpTestError::Client(HttpError::Client(crate::http::HttpError::Status {
+            status,
+            uri: "something".to_owned(),
+            text: "something else".to_owned(),
+        }))
+    }
+
+    fn retries(n: usize) -> Vec<Duration> {
+        vec![Duration::from_secs(0); n]
+    }
+
+    #[tokio::test]
+    async fn test_http_retry_success() {
+        let mock = Mock::new(vec![ok_response()]);
+        let result = http_retry(|| mock.exec(), retries(0)).await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_http_retry_retryable() {
+        let mock = Mock::new(vec![retryable(), ok_response()]);
+        let result = http_retry(|| mock.exec(), retries(1)).await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_http_retry_exhaust_retries() {
+        let mock = Mock::new(vec![retryable(), ok_response()]);
+        let result = http_retry(|| mock.exec(), retries(0)).await;
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_http_retry_non_retryable() {
+        let mock = Mock::new(vec![non_retryable(), ok_response()]);
+        let result = http_retry(|| mock.exec(), retries(1)).await;
+        assert!(result.is_err());
+    }
+}
