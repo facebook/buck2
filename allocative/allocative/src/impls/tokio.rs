f809a8493643951e19899335eb7b1e31ae61c40e
Bob Yang <bobyf@meta.com>
2023-06-07 23:16:08 -0700

    Allocative for tokio Mutex
    Reviewed By: stepancheg
    
    Differential Revision: D46464343
    
    fbshipit-source-id: 3b05fdffc29311dfbab1d8cc25f885077f1e2212

--- allocative/allocative/src/impls/tokio.rs
+++ allocative/allocative/src/impls/tokio.rs
@@ -9,6 +9,7 @@
 
 #![cfg(feature = "tokio")]
 
+use tokio::sync::Mutex;
 use tokio::sync::RwLock;
 
 use crate::Allocative;
@@ -19,6 +20,16 @@ impl<T: Allocative> Allocative for RwLock<T> {
     fn visit<'a, 'b: 'a>(&self, visitor: &'a mut Visitor<'b>) {
         let mut visitor = visitor.enter_self_sized::<Self>();
         if let Ok(data) = self.try_read() {
+            visitor.visit_field::<T>(Key::new("data"), &*data);
+        }
+        visitor.exit();
+    }
+}
+
+impl<T: Allocative> Allocative for Mutex<T> {
+    fn visit<'a, 'b: 'a>(&self, visitor: &'a mut Visitor<'b>) {
+        let mut visitor = visitor.enter_self_sized::<Self>();
+        if let Ok(data) = self.try_lock() {
             visitor.visit_field::<T>(Key::new("data"), &*data);
         }
         visitor.exit();
