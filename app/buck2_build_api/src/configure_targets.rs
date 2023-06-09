f16dd51054b8e04a50ada8c4554e7d263404e563
Bob Yang <bobyf@meta.com>
2023-06-09 13:23:38 -0700

    Don't use temporary_spawn in maybe_compatible_targets
    Summary:
    we want to kill temporary_spawn
    see D46410142 for why
    
    Reviewed By: stepancheg
    
    Differential Revision: D46466303
    
    fbshipit-source-id: 17c347e397071903fde166b729d12517c1a2a964

--- app/buck2_build_api/src/configure_targets.rs
+++ app/buck2_build_api/src/configure_targets.rs
@@ -24,8 +24,7 @@ use buck2_node::target_calculation::ConfiguredTargetCalculation;
 use buck2_query::query::syntax::simple::eval::set::TargetSet;
 use dice::DiceComputations;
 use dupe::Dupe;
-use futures::future::FutureExt;
-use gazebo::prelude::SliceExt;
+use gazebo::prelude::VecExt;
 use starlark_map::small_set::SmallSet;
 
 // Returns a tuple of compatible and incompatible targets.
@@ -59,30 +58,21 @@ pub async fn get_maybe_compatible_targets(
     let mut by_package_futs: Vec<_> = Vec::new();
     for (_package, result) in loaded_targets {
         let targets = result?;
-        let global_target_platform = global_target_platform.dupe();
 
-        by_package_futs.push({
-            ctx.temporary_spawn(|ctx, _cancellation| {
-                async move {
-                    let ctx = &ctx;
-                    let global_target_platform = global_target_platform.as_ref();
-                    let target_futs: Vec<_> = targets.map(|target| async move {
-                        let target = ctx
-                            .get_configured_target(target.label(), global_target_platform)
-                            .await?;
-                        anyhow::Ok(ctx.get_configured_target_node(&target).await?)
-                    });
-                    futures::future::join_all(target_futs).await
-                }
-                .boxed()
-            })
+        by_package_futs.extend({
+            let global_target_platform = global_target_platform.as_ref();
+            let target_futs: Vec<_> = targets.into_map(|target| async move {
+                let target = ctx
+                    .get_configured_target(target.label(), global_target_platform)
+                    .await?;
+                anyhow::Ok(ctx.get_configured_target_node(&target).await?)
+            });
+
+            target_futs
         });
     }
 
-    Ok(futures::future::join_all(by_package_futs)
-        .await
-        .into_iter()
-        .flatten())
+    Ok(futures::future::join_all(by_package_futs).await.into_iter())
 }
 
 /// Converts target nodes to a set of compatible configured target nodes.
