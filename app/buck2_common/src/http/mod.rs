405ab17aaca491110269e30466c5cc86c5d62ce3
Sho Naganuma <shou@meta.com>
2023-07-10 13:35:07 -0700

    Allow deprecated with_single_cert until rustls gets 0.21.4 in fbsource
    Summary: This is due to Rustls change in OSS: https://github.com/rustls/rustls/commit/0381e5a455e8cc7e64c6987e4d76daeeaaacce60
    
    Reviewed By: iguridi
    
    Differential Revision: D47341151
    
    fbshipit-source-id: 695708334601c83a6a8abe6820c33a3bc08030e8

--- app/buck2_common/src/http/mod.rs
+++ app/buck2_common/src/http/mod.rs
@@ -243,6 +243,9 @@ fn tls_config_with_single_cert<P: AsRef<Path>>(
 ) -> anyhow::Result<ClientConfig> {
     let system_roots = load_system_root_certs()?;
     let (cert, key) = load_cert_pair(cert_path, key_path)?;
+    // TODO: replace with_single_cert with with_client_auth_cert
+    //       once rustls get upgraded to >0.21.4
+    #[allow(deprecated)]
     ClientConfig::builder()
         .with_safe_defaults()
         .with_root_certificates(system_roots)
