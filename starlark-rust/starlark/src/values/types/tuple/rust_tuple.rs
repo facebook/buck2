fcb142c1e62adf8cc4c5f0f3a3d8f8d49d42312a
Peter Hallam <peterhal@meta.com>
2023-07-18 08:23:09 -0700

    Add AllocFrozenValue impls for tuples
    Summary: Also AllocValue for empty tuples.
    
    Reviewed By: stepancheg
    
    Differential Revision: D47457713
    
    fbshipit-source-id: 1b079945cdb458a48f715e9077f24794007ab61e

--- starlark-rust/starlark/src/values/types/tuple/rust_tuple.rs
+++ starlark-rust/starlark/src/values/types/tuple/rust_tuple.rs
@@ -20,7 +20,10 @@
 use crate::typing::Ty;
 use crate::values::type_repr::StarlarkTypeRepr;
 use crate::values::types::tuple::value::Tuple;
+use crate::values::AllocFrozenValue;
 use crate::values::AllocValue;
+use crate::values::FrozenHeap;
+use crate::values::FrozenValue;
 use crate::values::Heap;
 use crate::values::UnpackValue;
 use crate::values::Value;
@@ -45,6 +48,33 @@ impl<'v, T1: AllocValue<'v>, T2: AllocValue<'v>, T3: AllocValue<'v>> AllocValue<
             self.0.alloc_value(heap),
             self.1.alloc_value(heap),
             self.2.alloc_value(heap),
+        ])
+    }
+}
+
+impl<T1: AllocFrozenValue> AllocFrozenValue for (T1,) {
+    fn alloc_frozen_value(self, heap: &FrozenHeap) -> FrozenValue {
+        heap.alloc_tuple(&[self.0.alloc_frozen_value(heap)])
+    }
+}
+
+impl<T1: AllocFrozenValue, T2: AllocFrozenValue> AllocFrozenValue for (T1, T2) {
+    fn alloc_frozen_value(self, heap: &FrozenHeap) -> FrozenValue {
+        heap.alloc_tuple(&[
+            self.0.alloc_frozen_value(heap),
+            self.1.alloc_frozen_value(heap),
+        ])
+    }
+}
+
+impl<T1: AllocFrozenValue, T2: AllocFrozenValue, T3: AllocFrozenValue> AllocFrozenValue
+    for (T1, T2, T3)
+{
+    fn alloc_frozen_value(self, heap: &FrozenHeap) -> FrozenValue {
+        heap.alloc_tuple(&[
+            self.0.alloc_frozen_value(heap),
+            self.1.alloc_frozen_value(heap),
+            self.2.alloc_frozen_value(heap),
         ])
     }
 }
