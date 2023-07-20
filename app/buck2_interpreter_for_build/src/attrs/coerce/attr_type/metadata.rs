19bfa8b5f6c0b30bf664aa6d919c0ad4c64ba925
Thomas Orozco <torozco@meta.com>
2023-07-20 06:28:20 -0700

    buck2: metadata attribute: add coercion
    Summary:
    Like it says in the title. Actually wire this attr up by implementing coercion
    for it.
    
    Reviewed By: stepancheg
    
    Differential Revision: D47586460
    
    fbshipit-source-id: 002440f7ea64f8a47e4ffde27ab59c46b69c6a86

--- app/buck2_interpreter_for_build/src/attrs/coerce/attr_type/metadata.rs
+++ app/buck2_interpreter_for_build/src/attrs/coerce/attr_type/metadata.rs
@@ -7,23 +7,36 @@
  * of this source tree.
  */
 
+use anyhow::Context as _;
+use buck2_core::metadata_key::MetadataKey;
+use buck2_core::metadata_key::MetadataKeyRef;
 use buck2_interpreter::types::opaque_metadata::OpaqueMetadata;
 use buck2_node::attrs::attr_type::metadata::MetadataAttrType;
 use buck2_node::attrs::coerced_attr::CoercedAttr;
 use buck2_node::attrs::coercion_context::AttrCoercionContext;
 use buck2_node::attrs::configurable::AttrIsConfigurable;
+use buck2_node::metadata::MetadataMap;
+use buck2_util::collections::ordered_map::OrderedMap;
 use starlark::typing::Ty;
+use starlark::values::dict::Dict;
+use starlark::values::dict::DictRef;
+use starlark::values::string::STRING_TYPE;
 use starlark::values::type_repr::StarlarkTypeRepr;
 use starlark::values::Value;
 
+use crate::attrs::coerce::error::CoercionError;
 use crate::attrs::coerce::AttrTypeCoerce;
 
 #[derive(Debug, thiserror::Error)]
 enum MetadataAttrTypeCoerceError {
     #[error("Metadata attribute is not configurable (internal error)")]
     AttrTypeNotConfigurable,
-    #[error("Metadata attribute is not implemented yet")]
-    AttrTypeNotImplemented,
+    #[error(
+        "Metadata attribute with key {} is not convertible to JSON: {}",
+        .key,
+        .value
+    )]
+    ValueIsNotJson { key: MetadataKey, value: String },
 }
 
 impl AttrTypeCoerce for MetadataAttrType {
@@ -31,12 +44,37 @@ impl AttrTypeCoerce for MetadataAttrType {
         &self,
         configurable: AttrIsConfigurable,
         _ctx: &dyn AttrCoercionContext,
-        _value: Value,
+        value: Value,
     ) -> anyhow::Result<CoercedAttr> {
         if configurable == AttrIsConfigurable::Yes {
             return Err(MetadataAttrTypeCoerceError::AttrTypeNotConfigurable.into());
         }
-        Err(MetadataAttrTypeCoerceError::AttrTypeNotImplemented.into())
+
+        let dict = match DictRef::from_value(value) {
+            Some(d) => d,
+            None => return Err(CoercionError::type_error(Dict::TYPE, value).into()),
+        };
+
+        let mut map = OrderedMap::with_capacity(dict.len());
+        for (key, value) in dict.iter() {
+            let key = match key.unpack_str() {
+                Some(k) => k,
+                None => return Err(CoercionError::type_error(STRING_TYPE, key).into()),
+            };
+
+            let key = MetadataKeyRef::new(key)?;
+
+            let value = value.to_json_value().with_context(|| {
+                MetadataAttrTypeCoerceError::ValueIsNotJson {
+                    key: key.to_owned(),
+                    value: value.to_repr(),
+                }
+            })?;
+
+            map.insert(key.to_owned(), value);
+        }
+
+        Ok(CoercedAttr::Metadata(MetadataMap::new(map)))
     }
 
     fn starlark_type(&self) -> Ty {
