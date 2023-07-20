133271b8e008b48816f5d94d8b0608acbba80760
Thomas Orozco <torozco@meta.com>
2023-07-20 06:28:20 -0700

    buck2: fill in the `metadata` attribute
    Summary:
    Like it says in the title, this makes the attribute actually hold metadata
    (but it cannot be constructed yet).
    
    I don't really love the current design of using `serde_json::Value`. I'm doing
    this to be consistent with package values, but I think we should change that.
    
    There are a few reasons:
    
    - Hashing `serde_json::Value` is annoying.
    - We can't intern any nested keys.
    - We don't really control the representation (e.g. `serde_json::Value` uses
      either BTreeMap or IndexMap for its maps, depending on features, and we
      probably want to make this a more explicit decision, especially within Meta
      where features on third-party crates are a bit of a pain).
    
    That said, for now this provides a reasonable stand in before I go and refactor
    both this and package values, which can be done without changing the API.
    
    Reviewed By: stepancheg
    
    Differential Revision: D47586464
    
    fbshipit-source-id: 807d811426017c0e5f9f4a43ad2c771b913ec413

--- app/buck2_node/src/metadata.rs
+++ app/buck2_node/src/metadata.rs
@@ -8,34 +8,154 @@
  */
 
 use std::fmt;
+use std::hash::Hash;
+use std::hash::Hasher;
 
 use allocative::Allocative;
+use buck2_core::metadata_key::MetadataKey;
+use buck2_util::collections::ordered_map::OrderedMap;
 
 use crate::attrs::attr_type::any_matches::AnyMatches;
 
-#[derive(Debug, Eq, PartialEq, Hash, Clone, Allocative)]
+#[derive(Debug, Eq, PartialEq, Clone, Allocative, Default)]
 pub struct MetadataMap {
-    // TODO: Fill this in.
+    values: Box<OrderedMap<MetadataKey, serde_json::Value>>,
+}
+
+impl MetadataMap {
+    pub fn new(values: OrderedMap<MetadataKey, serde_json::Value>) -> Self {
+        Self {
+            values: Box::new(values),
+        }
+    }
+}
+
+impl Hash for MetadataMap {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let Self { values } = self;
+        state.write_usize(values.len());
+        for (k, v) in values.iter_hashed() {
+            Hash::hash(&k, state);
+            hash_value(v, state);
+        }
+    }
+}
+
+fn hash_value<H: Hasher>(v: &serde_json::Value, state: &mut H) {
+    use serde_json::Value;
+
+    match v {
+        Value::Null => {
+            state.write_u8(0);
+        }
+        Value::Bool(v) => {
+            state.write_u8(1);
+            v.hash(state);
+        }
+        Value::Number(v) => {
+            state.write_u8(2);
+            if let Some(v) = v.as_u64() {
+                state.write_u8(1);
+                v.hash(state);
+            } else if let Some(v) = v.as_i64() {
+                state.write_u8(2);
+                v.hash(state);
+            } else {
+                state.write_u8(3);
+                v.to_string().hash(state);
+            }
+        }
+        Value::String(v) => {
+            state.write_u8(3);
+            v.hash(state);
+        }
+        Value::Array(vals) => {
+            state.write_u8(4);
+            state.write_usize(vals.len());
+            for v in vals {
+                hash_value(v, state);
+            }
+        }
+        Value::Object(vals) => {
+            state.write_u8(5);
+            state.write_usize(vals.len());
+            for (k, v) in vals {
+                k.hash(state);
+                hash_value(v, state);
+            }
+        }
+    }
 }
 
 impl MetadataMap {
     pub fn to_value(&self) -> serde_json::Value {
-        let Self {} = self; // TODO: Fill this in.
-        serde_json::Value::Object(serde_json::Map::new())
+        let Self { values } = self;
+        let map = values
+            .iter()
+            .map(|(k, v)| (k.as_str().to_owned(), v.clone()))
+            .collect();
+        serde_json::Value::Object(map)
     }
 }
 
 impl fmt::Display for MetadataMap {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let Self {} = self; // TODO: Fill this in.
-        f.write_str("{}")?;
+        let Self { values } = self;
+        let s = serde_json::to_string(values).map_err(|_| fmt::Error)?;
+        f.write_str(&s)?;
         Ok(())
     }
 }
 
 impl AnyMatches for MetadataMap {
-    fn any_matches(&self, _filter: &dyn Fn(&str) -> anyhow::Result<bool>) -> anyhow::Result<bool> {
-        let Self {} = self; // TODO: Fill this in.
+    fn any_matches(&self, filter: &dyn Fn(&str) -> anyhow::Result<bool>) -> anyhow::Result<bool> {
+        let Self { values } = self;
+
+        for (k, v) in values.iter() {
+            if filter(k.as_str())? {
+                return Ok(true);
+            }
+            if v.any_matches(filter)? {
+                return Ok(true);
+            }
+        }
+
         Ok(false)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn make() -> MetadataMap {
+        let mut map = OrderedMap::new();
+        map.insert(
+            "foo.bar".to_owned().try_into().unwrap(),
+            serde_json::Value::String("baz".to_owned()),
+        );
+        MetadataMap::new(map)
+    }
+
+    #[test]
+    fn test_display() {
+        let spec = make();
+        assert_eq!(spec.to_string(), "{\"foo.bar\":\"baz\"}");
+    }
+
+    #[test]
+    fn test_to_value() {
+        let spec = make();
+        assert_eq!(spec.to_value(), serde_json::json!({"foo.bar": "baz"}));
+    }
+
+    #[test]
+    fn test_any_matches() {
+        // NOTE: We have more comprehensive tests for serde_json's any_matches, so only test our
+        // own map's role here.
+        let spec = make();
+        assert!(spec.any_matches(&|s| Ok(s == "foo.bar")).unwrap());
+        assert!(spec.any_matches(&|s| Ok(s == "baz")).unwrap());
+        assert!(!spec.any_matches(&|_s| Ok(false)).unwrap());
     }
 }
