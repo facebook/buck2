0b9b35601bc0cecf613fce8ebb6432eae14b74d4
Wendy Yu <wendyy@meta.com>
2023-08-04 10:45:36 -0700

    Unpack collections for user metadata in `instant_event()`
    Summary: Supports lists and dicts. Tuples may come later if that's something that's needed
    
    Reviewed By: bobyangyf
    
    Differential Revision: D48033291
    
    fbshipit-source-id: daa1663dfd18af91444d2e8a3e95c412a9745974

--- app/buck2_bxl/src/bxl/starlark_defs/event.rs
+++ app/buck2_bxl/src/bxl/starlark_defs/event.rs
@@ -13,11 +13,16 @@
 use std::collections::HashMap;
 
 use buck2_data::starlark_user_metadata_value::Value::BoolValue;
+use buck2_data::starlark_user_metadata_value::Value::DictValue;
 use buck2_data::starlark_user_metadata_value::Value::IntValue;
+use buck2_data::starlark_user_metadata_value::Value::ListValue;
 use buck2_data::starlark_user_metadata_value::Value::StringValue;
 use buck2_data::StarlarkUserEvent;
+use buck2_data::StarlarkUserMetadataDictValue;
+use buck2_data::StarlarkUserMetadataListValue;
 use buck2_data::StarlarkUserMetadataValue;
 use starlark::values::dict::DictRef;
+use starlark::values::list::ListRef;
 use starlark::values::UnpackValue;
 use starlark::values::Value;
 use thiserror::Error;
@@ -25,12 +30,14 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 enum StarlarkUserEventUnpack {
     #[error(
-        "Metadata should be a dict where keys are strings, and values are strings, ints, or bools. Got type: `{0}`"
+        "Metadata should be a dict where keys are strings, and values are strings, ints, bools, or dicts/lists of the mentioned types. Got type: `{0}`"
     )]
     InvalidMetadata(String),
     #[error("Metadata keys should be strings. Got type: `{0}`")]
     InvalidKey(String),
-    #[error("Metadata values should be strings, ints, or bools. Key `{0}` had value type `{1}`")]
+    #[error(
+        "Metadata values should be strings, ints, bools, or dicts/lists of the mentioned types. Key `{0}` had value type `{1}`"
+    )]
     InvalidValue(String, String),
 }
 
@@ -89,6 +96,21 @@ fn get_metadata_value<'v>(k: &str, v: Value<'v>) -> anyhow::Result<StarlarkUserM
     } else if let Some(v) = f64::unpack_value(v) {
         Ok(StarlarkUserMetadataValue {
             value: Some(IntValue(v as i32)),
+        })
+    } else if DictRef::from_value(v).is_some() {
+        let dict = unpack_metadata_map(v)?;
+        let dict = StarlarkUserMetadataDictValue { value: dict };
+        Ok(StarlarkUserMetadataValue {
+            value: Some(DictValue(dict)),
+        })
+    } else if let Some(v) = ListRef::from_value(v) {
+        let list = v
+            .iter()
+            .map(|e| get_metadata_value(k, e))
+            .collect::<anyhow::Result<Vec<_>>>()?;
+        let list = StarlarkUserMetadataListValue { value: list };
+        Ok(StarlarkUserMetadataValue {
+            value: Some(ListValue(list)),
         })
     } else {
         Err(StarlarkUserEventUnpack::InvalidValue(k.to_owned(), v.get_type().to_owned()).into())
