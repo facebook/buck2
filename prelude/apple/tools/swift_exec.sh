86859c42f6a06db6b958e826d48c011c7fc8de65
Richard Howell <rhow@meta.com>
2023-08-04 03:32:48 -0700

    remove output checking
    Summary: Remove output checking for swift_exec.sh. This was added some time ago to triage an issue with missing RE outputs, and shouldn't be needed any more.
    
    Reviewed By: krallin
    
    Differential Revision: D48059843
    
    fbshipit-source-id: b8e0db59c9fa4de221f9a1e4ad9e6fb53def97eb

--- prelude/apple/tools/swift_exec.sh
+++ prelude/apple/tools/swift_exec.sh
@@ -31,31 +31,4 @@ if [ -z "$EXPLICIT_MODULES_ENABLED" ]; then
     module_cache_path_args+=("$MODULE_CACHE_PATH")
 fi
 
-"$@" -debug-prefix-map "$PWD"=. "${module_cache_path_args[@]}"
-
-OUTPUT_PATHS=()
-for ARG in "$@"
-do
-    if [ "${FOUND_OUTPUT_ARG}" = 1 ]; then
-        OUTPUT_PATHS+=( "${ARG}" )
-    fi
-    FOUND_OUTPUT_ARG=0
-    if [ "${ARG}" = "-o" ] || [ "${ARG}" = "-emit-module-path" ] || [ "${ARG}" = "-emit-objc-header-path" ]; then
-        FOUND_OUTPUT_ARG=1
-    fi
-done
-
-if [ ${#OUTPUT_PATHS[@]} -eq 0 ]; then
-    >&2 echo "No output paths found, ensure output args are not passed in argfiles"
-    exit 1
-fi
-
-for OUTPUT_PATH in "${OUTPUT_PATHS[@]}"
-do
-    # We've observed cases where the Swift compiler would return with a zero exit code but
-    # would not have created the output file, correctly a non-zero exit code in such cases.
-    if [ ! -f "${OUTPUT_PATH}" ]; then
-        >&2 echo "Output file does not exist: '${OUTPUT_PATH}'"
-        exit 1
-    fi
-done
+exec "$@" -debug-prefix-map "$PWD"=. "${module_cache_path_args[@]}"
