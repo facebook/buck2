eed0b74ff677254ff0920deee5ccc812107980a9
Bob Yang <bobyf@meta.com>
2023-08-09 01:48:09 -0700

    Add map_taking_data to OwningFuture
    Summary: Allow one to map the OwningFuture's result along with the data that it owns once the future is complete.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D47983558
    
    fbshipit-source-id: 3ce1bf494967b604567b1b4489eada11a8f74ada

--- shed/more_futures/src/owning_future.rs
+++ shed/more_futures/src/owning_future.rs
@@ -18,6 +18,7 @@ use std::future::Future;
 use std::marker::PhantomPinned;
 use std::mem;
 use std::pin::Pin;
+use std::task::ready;
 use std::task::Context;
 use std::task::Poll;
 
@@ -32,7 +33,7 @@ pub struct OwningFuture<T, D> {
     // reference to the `context`, and so absolutely must be dropped first.
     #[pin]
     fut: MaybeFuture<BoxFuture<'static, T>>, // not actually static but the lifetime of this struct
-    data: D,
+    data: Option<D>,
 
     _p: PhantomPinned,
 }
@@ -43,7 +44,7 @@ impl<T, D> OwningFuture<T, D> {
         F: for<'a> FnOnce(&'a mut D) -> BoxFuture<'a, T> + Send,
     {
         let this = OwningFuture {
-            data,
+            data: Some(data),
             fut: MaybeFuture::None,
             _p: Default::default(),
         };
@@ -54,7 +55,10 @@ impl<T, D> OwningFuture<T, D> {
             // construction, so the self-reference will remain valid at least until this value
             // is dropped. Furthermore, the inner future is dropped before the `context` is dropped,
             // and so the reference will remain valid even during the drop of the `Future`
-            mem::transmute::<BoxFuture<'_, T>, BoxFuture<'static, T>>(f(&mut this.data))
+            mem::transmute::<BoxFuture<'_, T>, BoxFuture<'static, T>>(f(this
+                .data
+                .as_mut()
+                .unwrap()))
         };
 
         {
@@ -63,6 +67,56 @@ impl<T, D> OwningFuture<T, D> {
         }
 
         this
+    }
+
+    /// Maps the future with the given 'f', except it takes the data that the original future
+    /// owns as part of the mapping.
+    pub fn map_taking_data<U>(
+        self: Pin<Box<OwningFuture<T, D>>>,
+        f: impl FnOnce(T, D) -> U + Unpin,
+    ) -> impl Future<Output = U> {
+        #[must_use = "futures do nothing unless you `.await` or poll them"]
+        pub enum Map<T, D, F> {
+            Incomplete {
+                future: Pin<Box<OwningFuture<T, D>>>,
+                f: F,
+            },
+            Complete,
+        }
+
+        impl<T, D, F, U> Future for Map<T, D, F>
+        where
+            F: FnOnce(T, D) -> U + Unpin,
+        {
+            type Output = U;
+
+            fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+                let (output, d) = match self.as_mut().get_mut() {
+                    Map::Incomplete { ref mut future, .. } => {
+                        let output = ready!(future.as_mut().poll(cx));
+
+                        // drop the future first before taking the object it references
+                        future.fut = MaybeFuture::None;
+
+                        let d = future.data.take().expect("taken twice");
+
+                        (output, d)
+                    }
+                    Map::Complete => {
+                        panic!("Map must not be polled after it returned `Poll::Ready`")
+                    }
+                };
+
+                match mem::replace(self.get_mut(), Map::Complete) {
+                    Map::Incomplete { f, .. } => Poll::Ready(f(output, d)),
+                    Map::Complete => {
+                        unreachable!()
+                    }
+                }
+            }
+        }
+
+        Map::Incomplete { future: self, f }
     }
 }
 
@@ -95,5 +149,22 @@ mod tests {
         // we can spawn the future such that it owns the data
         let data = tokio::spawn(fut).await.unwrap();
         assert_eq!(data, 2);
+    }
+
+    #[tokio::test]
+    async fn test_future_mapping() {
+        struct SomeData(usize);
+
+        fn some_fn(d: &mut SomeData) -> BoxFuture<usize> {
+            async move { d.0 }.boxed()
+        }
+
+        let data = SomeData(2);
+        let fut = OwningFuture::new(data, some_fn);
+        let fut = fut.map_taking_data(|x, data| x + data.0);
+
+        // we can spawn the future such that it owns the data
+        let data = tokio::spawn(fut).await.unwrap();
+        assert_eq!(data, 4);
     }
 }
