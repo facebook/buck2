482e925c45b831cc5beabeb3973ebe44aca3b4ac
Stiopa Koltsov <nga@meta.com>
2023-08-10 21:08:54 -0700

    Type of **kwargs is incorrect
    Summary:
    In
    
    ```
    def foo(**kwargs: int): ...
    ```
    
    type of `kwargs` should be `dict[str, int]`, as in Python. Mentioned for example [there](https://peps.python.org/pep-0692/).
    
    We should do the same. Currently we expect this type to be written as
    
    ```
    def foo(**kwargs: dict[str, int]): ...
    ```
    
    Reviewed By: krallin
    
    Differential Revision: D48206408
    
    fbshipit-source-id: ab91d2fbc78f6ba5e7e157f6bdaeb23f0085ae08

--- starlark-rust/starlark/src/typing/tests.rs
+++ starlark-rust/starlark/src/typing/tests.rs
@@ -240,6 +240,18 @@ foo(**{1: "x"})
 }
 
 #[test]
+fn test_types_of_args_kwargs() {
+    // TODO(nga): type of `**kwargs: X` should be `dict[str, X]`.
+    TypeCheck::new().ty("args").ty("kwargs").check(
+        "types_of_args_kwargs",
+        r#"
+def foo(*args: tuple, **kwargs: dict[str, int]):
+    pass
+"#,
+    );
+}
+
+#[test]
 fn test_dot_type() {
     TypeCheck::new().check(
         "dot_type_0",
