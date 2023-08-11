80f93aee92aa5a7352c60a0f996f98b99858d5c7
Stiopa Koltsov <nga@meta.com>
2023-08-10 17:57:05 -0700

    Add bxl namespace
    Reviewed By: bobyangyf
    
    Differential Revision: D48245844
    
    fbshipit-source-id: 740b7dd5b30c9a452c76fef98bc937c6d41fdd3d

--- app/buck2_bxl/src/bxl/starlark_defs/globals.rs
+++ app/buck2_bxl/src/bxl/starlark_defs/globals.rs
@@ -10,6 +10,7 @@
 //! Starlark globals for BXL.
 
 use buck2_interpreter::bxl::BXL_SPECIFIC_GLOBALS;
+use starlark::environment::GlobalsBuilder;
 
 use crate::bxl::starlark_defs::bxl_function::register_bxl_function;
 use crate::bxl::starlark_defs::cli_args;
@@ -20,8 +21,21 @@ use crate::bxl::starlark_defs::functions::register_label_function;
 use crate::bxl::starlark_defs::functions::register_target_function;
 use crate::bxl::starlark_defs::type_names::register_bxl_type_names;
 
+fn bxl_namespace(g: &mut GlobalsBuilder) {
+    g.struct_("cli_args", cli_args::register_cli_args_module);
+    // TODO(nga): add `main` function here.
+    register_artifact_function(g);
+    register_label_function(g);
+    register_target_function(g);
+    register_instant_function(g);
+    register_error_handling_function(g);
+    // TODO(nga): add type constants here.
+}
+
 pub(crate) fn init_bxl_specific_globals() {
     BXL_SPECIFIC_GLOBALS.init(|g| {
+        g.struct_("bxl", bxl_namespace);
+        // TODO(nga): move these into `bxl` namespace.
         g.struct_("cli_args", cli_args::register_cli_args_module);
         register_bxl_function(g);
         register_artifact_function(g);
