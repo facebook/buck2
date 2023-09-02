7e75691c7c67c8a01b3a56e9ed0d4a3752d8ff2b
Neil Mitchell <ndmitchell@meta.com>
2023-09-02 10:35:22 -0700

    Delete an entirely unused function
    Summary: Not needed
    
    Reviewed By: iguridi
    
    Differential Revision: D48929741
    
    fbshipit-source-id: a7201969763e0222638b879e779a0964d5c99143

--- app/buck2_common/src/legacy_configs/view.rs
+++ app/buck2_common/src/legacy_configs/view.rs
@@ -8,12 +8,9 @@
  */
 
 use std::fmt::Debug;
-use std::str::FromStr;
 use std::sync::Arc;
 
 use buck2_core::cells::name::CellName;
-
-use crate::legacy_configs::LegacyBuckConfig;
 
 /// Buckconfig trait.
 ///
@@ -22,17 +19,6 @@ use crate::legacy_configs::LegacyBuckConfig;
 /// * DICE-backed implementation which records a dependency on buckconfig property in DICE
 pub trait LegacyBuckConfigView: Debug {
     fn get(&self, section: &str, key: &str) -> anyhow::Result<Option<Arc<str>>>;
-}
-
-impl<'a> dyn LegacyBuckConfigView + 'a {
-    pub fn parse<T: FromStr>(&self, section: &str, key: &str) -> anyhow::Result<Option<T>>
-    where
-        <T as FromStr>::Err: std::error::Error + Send + Sync + 'static,
-    {
-        self.get(section, key)?
-            .map(|s| LegacyBuckConfig::parse_impl(section, key, &s))
-            .transpose()
-    }
 }
 
 /// All cell buckconfigs traits.
