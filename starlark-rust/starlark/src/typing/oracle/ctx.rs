b043c7ff9be2baea270c4736cfe1060a1cb918a8
Stiopa Koltsov <nga@meta.com>
2023-09-06 00:44:29 -0700

    Make TyName intersect with TyStarlarkValue
    Summary:
    If a value is known to be generic unnamed `transitive_set` (expressed as `TyStarlarkValue`), and function signature has `"XxxTSet"`, there's not much better we can do.
    
    Following diff D48932839 does not work without it.
    
    `TyName` is going away anyway.
    
    Reviewed By: ianlevesque
    
    Differential Revision: D48935085
    
    fbshipit-source-id: d9ec1d31161e6ad00652ea49bf1006d44800e6c6

--- starlark-rust/starlark/src/typing/oracle/ctx.rs
+++ starlark-rust/starlark/src/typing/oracle/ctx.rs
@@ -848,6 +848,7 @@ impl<'a> TypingOracleCtx<'a> {
             (TyBasic::Any, _) => true,
             (TyBasic::Name(x), TyBasic::Name(y)) => self.intersects_name(x, y),
             (TyBasic::Name(_), TyBasic::Custom(_)) => true,
+            (TyBasic::Name(_), TyBasic::StarlarkValue(_)) => true,
             (TyBasic::Name(x), y) => Some(x.as_str()) == y.as_name(),
             (TyBasic::List(x), TyBasic::List(y)) => self.intersects(x, y),
             (TyBasic::Dict(x_k, x_v), TyBasic::Dict(y_k, y_v)) => {
