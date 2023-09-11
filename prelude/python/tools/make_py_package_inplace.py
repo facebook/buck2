11cae963e204b3880ff4f1d7d41067d0245b2942
Josiah Gaskin <jcg@meta.com>
2023-09-11 13:17:19 -0700

    Back out "Revert D46152851: Inplace par support for Multiprocessing"
    Summary:
    Original commit changeset: 71fa11665390
    
    Original Phabricator Diff: D46152851
    
    Reviewed By: christycylee
    
    Differential Revision: D49120658
    
    fbshipit-source-id: bc9fa5cebdcbd9a598b25c4e90429c4ecdc25a8c

--- prelude/python/tools/make_py_package_inplace.py
+++ prelude/python/tools/make_py_package_inplace.py
@@ -118,6 +118,11 @@ def parse_args() -> argparse.Namespace:
         help="Where to write the bootstrapper script to",
     )
     parser.add_argument(
+        "--add-multiprocessing-executable",
+        type=Path,
+        help="If set, where to write the multiprocessing executable",
+    )
+    parser.add_argument(
         "--native-libs-env-var",
         default=(
             "DYLD_LIBRARY_PATH" if platform.system() == "Darwin" else "LD_LIBRARY_PATH"
@@ -157,9 +162,7 @@ def write_bootstrapper(args: argparse.Namespace) -> None:
 
     new_data = data.replace("<PYTHON>", "/usr/bin/env " + str(args.python))
     new_data = new_data.replace("<PYTHON_INTERPRETER_FLAGS>", "")
-    # new_data = new_data.replace(
-    #    "<PYTHON_INTERPRETER_FLAGS>", args.python_interpreter_flags
-    # )
+
     new_data = new_data.replace("<MODULES_DIR>", str(relative_modules_dir))
     new_data = new_data.replace(
         "<MAIN_MODULE>", args.entry_point if args.entry_point else ""
@@ -167,6 +170,27 @@ def write_bootstrapper(args: argparse.Namespace) -> None:
     new_data = new_data.replace(
         "<MAIN_FUNCTION>", args.main_function if args.main_function else ""
     )
+
+    if args.add_multiprocessing_executable:
+        # Handle enabling multiprocessing for inplace par
+        new_data = new_data.replace("<SHOULD_ADD_MULTIPROCESSING_WRAPPER>", str(True))
+        env_vals_for_subprocesses = [
+            f"PYTHONPATH=$(dirname $0)/{relative_modules_dir}",
+            f"LD_PRELOAD={ld_preload}",
+            f"{args.native_libs_env_var}=$(dirname $0)/{relative_modules_dir}",
+        ]
+        # env_str should contain PYTHONPATH, LD_LIBRARY_PATH, and LD_PRELOAD (if set)
+        env_str = " ".join(env_vals_for_subprocesses)
+        script_contents = f'#!/usr/bin/env bash\n\n{env_str} {str(args.python)} "$@"'
+        mp_executable = args.add_multiprocessing_executable
+        with open(mp_executable, "w") as handle:
+            handle.write(script_contents)
+        os.chmod(mp_executable, 0o755)
+        relative_mp = os.path.relpath(mp_executable.as_posix(), args.output.parent)
+        new_data = new_data.replace("<MP_EXECUTABLE>", relative_mp)
+    else:
+        new_data = new_data.replace("<MP_EXECUTABLE>", str(args.python))
+        new_data = new_data.replace("<SHOULD_ADD_MULTIPROCESSING_WRAPPER>", str(False))
 
     # Things that are only required for the full template
     new_data = new_data.replace("<NATIVE_LIBS_ENV_VAR>", args.native_libs_env_var)
