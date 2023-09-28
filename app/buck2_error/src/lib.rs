24fde55a32c299025f971b868607ff78315df1fc
Jakob Degen <jakobdegen@meta.com>
2023-09-28 14:00:11 -0700

    buck2_error: Derive macro
    Summary: This diff provides the `#[buck2_error::Error]` attribute macro, which is a drop-in for `thiserror::Error`. In the short term, we use this to make some error types slightly more buckler compatible - the actual long term goal is to use this to support annotating errors with additional context information directly on the error definition.
    
    Reviewed By: stepancheg
    
    Differential Revision: D48672468
    
    fbshipit-source-id: 14555bdefab602a56879cfd0df6fec0d7d4b516c

--- app/buck2_error/src/lib.rs
+++ app/buck2_error/src/lib.rs
@@ -17,3 +17,32 @@ pub use context::Context;
 pub use error::Error;
 
 pub type Result<T> = std::result::Result<T, crate::Error>;
+
+/// Generates an error impl for the type.
+///
+/// This macro is a drop-in replacement for [`thiserror::Error`]. In the near future, all uses of
+/// `thiserror` in `buck2/app` will be replaced with this macro.
+///
+/// Currently, the only distinction from `thiserror::Error` is that an additional impl of
+/// [`AnyError`] is generated for the type, which makes some of the interactions with `buck2_error` more
+/// ergonomic. In the future, this macro will also be used to be able to annotate errors with
+/// additional structured context information.
+///
+/// ## Example
+///
+/// ```rust
+/// #[derive(Debug, buck2_error::Error)]
+/// #[error("My error type")]
+/// struct MyError;
+///
+/// let e = buck2_error::Error::from(MyError);
+/// assert_eq!(&format!("{}", e), "My error type");
+/// ```
+#[doc(inline)]
+pub use buck2_error_derive::Error;
+
+#[doc(hidden)]
+pub mod __for_macro {
+    pub use buck2_error_derive::exterminate;
+    pub use thiserror;
+}
---
+++ app/buck2_error_derive/src/lib.rs
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+use proc_macro::TokenStream;
+use syn::parse_macro_input;
+
+#[proc_macro_derive(Error, attributes(backtrace, error, from, source))]
+pub fn derive_error(input: TokenStream) -> TokenStream {
+    // For now, this is more or less just a stub that forwards to `thiserror`. It doesn't yet do
+    // anything interesting.
+    let input = parse_macro_input!(input as syn::DeriveInput);
+
+    // W need to generate an invocation of `thiserror::Derive`. This comes with two pieces of
+    // complexity, each requiring some cleverness:
+    //  1. We can't just generate a `#[derive(thiserror::Error)]`, because we cannot modify the
+    //     input! The trick we use is to generate an exact copy of our input, prefixed with the two
+    //     attributes below. The `derive` will run first and do all the normal work of the derive
+    //     macro, including generating the items we want. `exterminate` will then run next - all it
+    //     does is delete the item it's attached to, leaving us with the result we want.
+    //  2. `thiserror::Error` generates code which refers to the `thiserror` crate. However, users
+    //     of `buck2_error` will want to avoid importing this crate. We could deal with this by
+    //     adding a `use buck2_error::reexport::thiserror;` - that in turn has the disadvantage that
+    //     the `thiserror` name will show up in the module (and possibly in IDE suggestions). So
+    //     instead, we put the entire thing inside a function, to limit the scope of the reexport.
+    let name = syn::Ident::new(
+        &format!("__macro_generated_by_buck2_error_hidden_{}", input.ident),
+        input.ident.span(),
+    );
+    quote::quote! {
+        #[doc(hidden)]
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        fn #name() {
+            use ::buck2_error::__for_macro::thiserror;
+
+            #[derive(thiserror::Error)]
+            #[::buck2_error::__for_macro::exterminate]
+            #input
+        }
+    }
+    .into()
+}
+
+// Implementation detail of `derive_error`
+#[doc(hidden)]
+#[proc_macro_attribute]
+pub fn exterminate(_attr: TokenStream, _input: TokenStream) -> TokenStream {
+    TokenStream::default()
+}
