7b713a68c49c6be472ea97c0be30adc4b79a3874
Stiopa Koltsov <nga@meta.com>
2023-11-08 18:58:53 -0800

    Document what labels() query function is doing in buck1
    Summary: ... and why it won't be implemented in buck2.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D50914309
    
    fbshipit-source-id: 645277f81f51b0ca8be951d720183a4ff0815ff8

--- app/buck2_query/src/query/syntax/simple/functions/mod.rs
+++ app/buck2_query/src/query/syntax/simple/functions/mod.rs
@@ -336,6 +336,13 @@ impl<Env: QueryEnvironment> DefaultQueryFunctionsModule<Env> {
         Ok(targets.kind(&regex)?.into())
     }
 
+    /// This function is not implemented, and won't be, because buck2 query core does not support
+    /// returning both files and targets from a single function.
+    ///
+    /// In buck1 it returns targets and files referenced by the given attribute
+    /// in the given targets.
+    ///
+    /// Some discussion in T126638795.
     async fn labels(&self, attr: String, targets: TargetSet<Env::Target>) -> QueryFuncResult<Env> {
         self.implementation.labels(&attr, &targets)
     }
