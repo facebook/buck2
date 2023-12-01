53bd9767ea492e4a565e01b39c6d04ef2c60d0de
Jakob Degen <jakobdegen@meta.com>
2023-12-01 14:39:29 -0800

    errors: `WithDiagnostic` type
    Summary: Use case is explained in a doc comment. Next diff will refactor `starlark::Error` to use this
    
    Reviewed By: stepancheg
    
    Differential Revision: D51735664
    
    fbshipit-source-id: 425c6e9cc3c21eb9b4bb172f9aaafd8207cb049d

--- starlark-rust/starlark_syntax/src/diagnostic.rs
+++ starlark-rust/starlark_syntax/src/diagnostic.rs
@@ -15,12 +15,95 @@
  * limitations under the License.
  */
 
+use std::error::Error as StdError;
 use std::fmt;
 use std::fmt::Display;
 
 use crate::call_stack::CallStack;
+use crate::codemap::CodeMap;
 use crate::codemap::FileSpan;
+use crate::codemap::Span;
 use crate::span_display::span_display;
+
+/// A value of type `T`, together with some diagnostic information.
+///
+/// Most code in starlark should be using `starlark::Error` as the error type. However, some code
+/// may want to have strongly typed errors, while still being able to have diagnostics.
+/// `WithDiagnostic<MyErrorType>` is the tool for that. `WithDiagnostic` is always one word in size,
+/// and so can be used as an error type in performance sensitive code.
+///
+/// `WithDiagnostic` is `pub`, but only within the starlark crates, it's not a part of the API.
+///
+/// Returning a `WithDiagnostic` value guarantees that a diagnostic is actually present, ie the
+/// diagnostic is not optional.
+pub struct WithDiagnostic<T>(Box<WithDiagnosticInner<T>>);
+
+struct WithDiagnosticInner<T> {
+    t: T,
+    diagnostic: Diagnostic,
+}
+
+impl<T> WithDiagnostic<T> {
+    pub fn new_spanned(t: T, span: Span, codemap: &CodeMap) -> Self {
+        Self(Box::new(WithDiagnosticInner {
+            t,
+            diagnostic: Diagnostic {
+                span: Some(codemap.file_span(span)),
+                call_stack: CallStack::default(),
+            },
+        }))
+    }
+
+    pub fn inner(&self) -> &T {
+        &self.0.t
+    }
+
+    pub fn into_inner(self) -> T {
+        self.0.t
+    }
+
+    pub fn span(&self) -> Option<&FileSpan> {
+        self.0.diagnostic.span.as_ref()
+    }
+
+    pub fn call_stack(&self) -> &CallStack {
+        &self.0.diagnostic.call_stack
+    }
+
+    /// Set the span, unless it's already been set.
+    pub fn set_span(&mut self, span: Span, codemap: &CodeMap) {
+        if self.0.diagnostic.span.is_none() {
+            self.0.diagnostic.span = Some(codemap.file_span(span));
+        }
+    }
+
+    /// Set the `call_stack` field, unless it's already been set.
+    pub fn set_call_stack(&mut self, call_stack: impl FnOnce() -> CallStack) {
+        if self.0.diagnostic.call_stack.is_empty() {
+            self.0.diagnostic.call_stack = call_stack();
+        }
+    }
+}
+
+impl<T: StdError> fmt::Display for WithDiagnostic<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Not showing the context trace without `{:#}` or `{:?}` is the same thing that anyhow does
+        let with_context = f.alternate() && self.0.t.source().is_some();
+        diagnostic_display(&self.0.t, &self.0.diagnostic, false, f, with_context)
+    }
+}
+
+impl<T: StdError> fmt::Debug for WithDiagnostic<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        diagnostic_display(
+            &self.0.t,
+            &self.0.diagnostic,
+            false,
+            f,
+            /* with_context */ true,
+        )
+    }
+}
 
 /// A description of where in starlark execution the error happened.
 #[derive(Debug, Default)]
