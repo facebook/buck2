49a431f81acae79ce7eb8e37daf8232092616ded
Justus Adam <dev@justus.science>
2024-01-09 14:03:40 -0800

    Implement simple non-empty verbatim bound
    Summary:
    This implements the non-empty `allocative(bound = "...")` as a simple verbatim bounds override. This is similar to how `serde` handles its `bound` annotations where the user simply gets full control to add whatever they like. The "downside" of this is that the user *must* make sure to list all type variables that are used in the type.
    
    The upshot is that it gives a lot of flexibility to the user to also list additional constraints,(e.g. lifetimes) and it is very efficient to imlpement.
    
    For example
    
    ```
    #[derive(allocative::Allocative)]
    #[allocative(bound = "N: allocative::Allocative, E: allocative::Allocative, Ty")]
    pub struct GraphMap<N, E, Ty> {
        nodes: IndexMap<N, Vec<(N, CompactDirection)>>,
        edges: IndexMap<(N, N), E>,
        #[allocative(skip)]
        ty: PhantomData<Ty>,
    }
    ```
    
    Creates
    
    ```
    impl<N: allocative::Allocative, E: allocative::Allocative, Ty> allocative::Allocative for GraphMap<N, E, Ty> {
        ....
    }
    ```
    
    X-link: https://github.com/facebookexperimental/allocative/pull/6
    
    Reviewed By: ndmitchell
    
    Differential Revision: D52629410
    
    Pulled By: stepancheg
    
    fbshipit-source-id: a115886ac65c8e1f5f13b2633e3c6893054e74e2

--- allocative/allocative/src/allocative_trait.rs
+++ allocative/allocative/src/allocative_trait.rs
@@ -25,13 +25,14 @@ use crate::Visitor;
 /// }
 /// ```
 ///
-/// Proc macro supports two attributes: `#[allocative(skip)]` and `#[allocative(bound = "")]`.
+/// Proc macro supports two attributes: `#[allocative(skip)]` and
+/// `#[allocative(bound = "...")]`.
 ///
 /// ## `#[allocative(skip)]`
 ///
-/// `#[allocative(skip)]` can be used to skip field from traversal
-/// (for example, to skip fields which are not `Allocative`,
-/// and can be skipped because they are cheap).
+/// `#[allocative(skip)]` can be used to skip field from traversal (for example,
+/// to skip fields which are not `Allocative`, and can be skipped because they
+/// are cheap).
 ///
 /// ```
 /// use allocative::Allocative;
@@ -46,13 +47,19 @@ use crate::Visitor;
 /// }
 /// ```
 ///
-/// ## `#[allocative(bound = "")]`
+/// ## `#[allocative(bound = "...")]`
 ///
-/// `#[allocative(bound = "")]` can be used to not add `T: Allocative` bound
-/// to `Allocative` trait implementation, like this:
+/// `#[allocative(bound = "...")]` can be used to overwrite the bounds that are
+/// added to the generics of the implementation.
+///
+/// An empty string (`#[allocative(bound = "")]`) simply erases all bounds. It
+/// adds all type variables found in the type to the list of generics but with
+/// an empty bound. As an example
+///
 ///
 /// ```
 /// use std::marker::PhantomData;
+///
 /// use allocative::Allocative;
 ///
 /// struct Unsupported;
@@ -62,9 +69,34 @@ use crate::Visitor;
 /// struct Baz<T> {
 ///     _marker: PhantomData<T>,
 /// }
+/// ```
 ///
-/// // So `Baz<Unsupported>` is `Allocative` even though `Unsupported` is not.
-/// let allocative: &dyn Allocative = &Baz::<Unsupported> { _marker: PhantomData };
+/// Would generate an instance
+///
+/// ```ignore
+/// impl<T> Allocative for Baz<T> { ... }
+/// ```
+///
+/// Alternatively you can use the string to provide custom bounds. The string in
+/// this case is used *verbatim* as the bounds, which affords great flexibility,
+/// but also necessitates that all type variables must be mentioned or will be
+/// unbound (compile error). As an example we may derive a size of a `HashMap`
+/// by ignoring the hasher type.
+///
+///
+/// ```ignore
+/// #[allocative(bound = "K: Allocative, V:Allocative, S")]
+/// struct HashMap<K, V, S = RandomState> {
+///    ...
+/// }
+/// ```
+///
+/// Which generates
+///
+/// ```ignore
+/// impl<K: Allocative, V:Allocative, S> Allocative for HashMap<K, V, S> {
+///    ...
+/// }
 /// ```
 pub trait Allocative {
     fn visit<'a, 'b: 'a>(&self, visitor: &'a mut Visitor<'b>);
