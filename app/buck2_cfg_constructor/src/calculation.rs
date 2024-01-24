d62030f8b557b7a93be782dc0cb9729328937493
Natasha Badami <nbadami@meta.com>
2024-01-24 15:50:21 -0800

    enable cli modifiers
    Summary: Enables cli modifiers and adds e2e test.
    
    Reviewed By: scottcao
    
    Differential Revision: D52983306
    
    fbshipit-source-id: d4c3e3136799a082916ff5150f6eeea6ef58bcb0

--- app/buck2_cfg_constructor/src/calculation.rs
+++ app/buck2_cfg_constructor/src/calculation.rs
@@ -141,9 +141,12 @@ impl CfgConstructorCalculationImpl for CfgConstructorCalculationInstance {
             .get_package_value_json(modifier_key)?
             .map(MetadataValue::new);
         let target_cfg_modifiers = target.metadata()?.and_then(|m| m.get(modifier_key)).duped();
-        // If there are no PACKAGE or target modifiers, return the original configuration without computing DICE call
+        // If there are no PACKAGE/target/cli modifiers, return the original configuration without computing DICE call
         // TODO(scottcao): This is just for rollout purpose. Remove once modifier is rolled out
-        if package_cfg_modifiers.is_none() && target_cfg_modifiers.is_none() {
+        if package_cfg_modifiers.is_none()
+            && target_cfg_modifiers.is_none()
+            && cli_modifiers.is_empty()
+        {
             return Ok(cfg);
         }
 
