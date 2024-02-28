a00d9259fd4c8c77502aa57702402887d0b5d131
Stiopa Koltsov <nga@meta.com>
2024-02-28 14:31:48 -0800

    Fix StarlarkHasher
    Summary:
    Default implementation of `FxHasher64` for function like `write64` converts `u64` to bytes, and then extracts `u64` from bytes. This conversion is mostly optimized away, but not perfectly, I think.
    
    This change omits this step of converting to bytes.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D54317681
    
    fbshipit-source-id: 15fcd739075a23de4d922f1c610797ce79fbe5dd

--- starlark-rust/starlark_map/src/hasher.rs
+++ starlark-rust/starlark_map/src/hasher.rs
@@ -59,6 +59,36 @@ impl Hasher for StarlarkHasher {
     fn write(&mut self, bytes: &[u8]) {
         self.0.write(bytes)
     }
+
+    #[inline]
+    fn write_u8(&mut self, i: u8) {
+        self.0.write_u8(i)
+    }
+
+    #[inline]
+    fn write_u16(&mut self, i: u16) {
+        self.0.write_u16(i)
+    }
+
+    #[inline]
+    fn write_u32(&mut self, i: u32) {
+        self.0.write_u32(i)
+    }
+
+    #[inline]
+    fn write_u64(&mut self, i: u64) {
+        self.0.write_u64(i)
+    }
+
+    #[inline]
+    fn write_u128(&mut self, i: u128) {
+        self.0.write_u128(i)
+    }
+
+    #[inline]
+    fn write_usize(&mut self, i: usize) {
+        self.0.write_usize(i)
+    }
 }
 
 /// [`BuildHasher`] implementation which produces [`StarlarkHasher`].
