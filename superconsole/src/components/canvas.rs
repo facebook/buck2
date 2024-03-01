229657dfec0589525294cea4401c44d02eb8b8c1
Neil Mitchell <ndmitchell@meta.com>
2024-03-01 08:51:46 -0800

    Delete inaccurate comments
    Summary: Nothing about this comment was true anymore.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D54412169
    
    fbshipit-source-id: 6d73236fc206d1d1f9dfa24811021aa27ce78dc7

--- superconsole/src/components/canvas.rs
+++ superconsole/src/components/canvas.rs
@@ -7,10 +7,6 @@
  * of this source tree.
  */
 
-//! The root component which manages all other components.
-//! This is a fake component since it 1) manages state and 2) cannot be made by users and 3) is heavily coupled with [`superconsole`](crate::SuperConsole).
-//! Do not make this somewhere down the component hierarchy unless you have a good reason for it.
-
 use std::cell::Cell;
 
 use crossterm::cursor::MoveToColumn;
