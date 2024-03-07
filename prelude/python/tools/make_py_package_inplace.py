3f0c31ebf9f084df139cb44f7b26295bb1619489
Conner Nilsen <connernilsen@meta.com>
2024-03-06 20:06:11 -0800

    Pyre Configurationless migration for] [batch:22/244]
    Reviewed By: grievejia
    
    Differential Revision: D54558228
    
    fbshipit-source-id: 11abf3cec53ad912904e8accd5a25271c4ae72d8

--- prelude/python/tools/make_py_package_inplace.py
+++ prelude/python/tools/make_py_package_inplace.py
@@ -6,6 +6,8 @@
 # License, Version 2.0 found in the LICENSE-APACHE file in the root directory
 # of this source tree.
 
+# pyre-strict
+
 """
 Create a bootstrapper pex for inplace python binaries
 
--- prelude/python/tools/py38stdlib.py
+++ prelude/python/tools/py38stdlib.py
@@ -5,6 +5,8 @@
 # License, Version 2.0 found in the LICENSE-APACHE file in the root directory
 # of this source tree.
 
+# pyre-strict
+
 # This is list is "borrowed" from https://github.com/amyreese/stdlibs
 from typing import FrozenSet
 
