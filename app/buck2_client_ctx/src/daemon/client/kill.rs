8a3de528a76cd6f922a0817b729bc471e5fbb53b
Jakob Degen <jakobdegen@meta.com>
2024-03-13 09:43:26 -0700

    client: Tag errors when daemon won't die
    Summary: ^
    
    Reviewed By: samkevich
    
    Differential Revision: D54832786
    
    fbshipit-source-id: 4451d07ee9fcb0215b1b543fe5ac673e7948909c

--- app/buck2_client_ctx/src/daemon/client/kill.rs
+++ app/buck2_client_ctx/src/daemon/client/kill.rs
@@ -28,6 +28,7 @@ use crate::daemon::client::connect::BuckAddAuthTokenInterceptor;
 #[derive(Debug, buck2_error::Error)]
 enum KillError {
     #[error("Daemon pid {} did not die after kill within {:.1}s (status: {})", _0, _1.as_secs_f32(), _2)]
+    #[buck2(tag = DaemonWontDieFromKill)]
     DidNotDie(Pid, Duration, String),
 }
 
