b39ac04396c6ada18b4d5360449f3a9f14e8575a
Yury Samkevich <yurysamkevich@meta.com>
2024-03-14 18:38:00 -0700

    associate job object with completion port
    Summary:
    Implementation according to [the article](https://devblogs.microsoft.com/oldnewthing/20130405-00/?p=4743)
    We need to associate job object with completion port in order to listen job object events
    
    Reviewed By: KapJI
    
    Differential Revision: D54903624
    
    fbshipit-source-id: f77b0aba99be1b49e3620aed2c130702ab74c35c

--- app/buck2_forkserver/src/win/job_object.rs
+++ app/buck2_forkserver/src/win/job_object.rs
@@ -7,17 +7,28 @@
  * of this source tree.
  */
 
+use std::mem;
 use std::ptr;
 
 use buck2_wrapper_common::win::winapi_handle::WinapiHandle;
+use winapi::shared::minwindef::LPVOID;
+use winapi::um::handleapi;
+use winapi::um::ioapiset;
 use winapi::um::jobapi2;
+use winapi::um::winnt::JobObjectAssociateCompletionPortInformation;
+use winapi::um::winnt::JobObjectExtendedLimitInformation;
 use winapi::um::winnt::HANDLE;
+use winapi::um::winnt::JOBOBJECT_ASSOCIATE_COMPLETION_PORT;
+use winapi::um::winnt::JOBOBJECT_EXTENDED_LIMIT_INFORMATION;
+use winapi::um::winnt::JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
 
 use crate::win::utils::result_bool;
 use crate::win::utils::result_handle;
 
 pub(crate) struct JobObject {
     job_handle: WinapiHandle,
+    #[allow(dead_code)]
+    completion_handle: WinapiHandle,
 }
 
 impl JobObject {
@@ -25,7 +36,23 @@ impl JobObject {
         let job_handle = result_handle(unsafe {
             WinapiHandle::new(jobapi2::CreateJobObjectW(ptr::null_mut(), ptr::null_mut()))
         })?;
-        Ok(Self { job_handle })
+
+        let completion_handle = result_handle(unsafe {
+            WinapiHandle::new(ioapiset::CreateIoCompletionPort(
+                handleapi::INVALID_HANDLE_VALUE, // FileHandle
+                ptr::null_mut(),                 // ExistingCompletionPort
+                0,                               // CompletionKey
+                1,                               // NumberOfConcurrentThreads
+            ))
+        })?;
+
+        associate_job_with_completion_port(&job_handle, &completion_handle)?;
+        set_job_limits(&job_handle)?;
+
+        Ok(Self {
+            job_handle,
+            completion_handle,
+        })
     }
 
     pub(crate) fn assign_process(&self, process: HANDLE) -> anyhow::Result<()> {
@@ -35,4 +62,41 @@ impl JobObject {
     pub(crate) fn terminate(&self, exit_code: u32) -> anyhow::Result<()> {
         result_bool(unsafe { jobapi2::TerminateJobObject(self.job_handle.handle(), exit_code) })
     }
+}
+
+fn associate_job_with_completion_port(
+    job: &WinapiHandle,
+    completion_port: &WinapiHandle,
+) -> anyhow::Result<()> {
+    let mut associate_completion = JOBOBJECT_ASSOCIATE_COMPLETION_PORT {
+        CompletionKey: job.handle(),
+        CompletionPort: completion_port.handle(),
+    };
+
+    result_bool(unsafe {
+        jobapi2::SetInformationJobObject(
+            job.handle(),
+            JobObjectAssociateCompletionPortInformation,
+            &mut associate_completion as *mut _ as LPVOID,
+            mem::size_of_val(&associate_completion)
+                .try_into()
+                .expect("cannot safely cast to DWORD"),
+        )
+    })
+}
+
+fn set_job_limits(job: &WinapiHandle) -> anyhow::Result<()> {
+    let mut info = JOBOBJECT_EXTENDED_LIMIT_INFORMATION::default();
+    info.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
+
+    result_bool(unsafe {
+        jobapi2::SetInformationJobObject(
+            job.handle(),
+            JobObjectExtendedLimitInformation,
+            &mut info as *mut _ as LPVOID,
+            mem::size_of_val(&info)
+                .try_into()
+                .expect("cannot safely cast to DWORD"),
+        )
+    })
 }
