dda965d6b558d3f4458258c53cc40112b2d1c609
Conner Nilsen <connernilsen@meta.com>
2024-03-21 14:21:09 -0700

    Remigrate nested configurations [1/5]
    Summary:
    (This diff retries the configurationless re-migration after rerunning due to CI errors)
    
    When doing the Pyre Configurationless migration, I didn't realize that .pyre_configuration.local files could be nested, and how that would affect the migration.
    
    When the migration actually happened, and since we were processing configurations alphabetically, parent configurations got migrated first. These parent configurations often have the nested projects in the ignore all errors list, so those headers got applied instead of the actual headers that they should be using.
    
    This diff fixes the headers for a number of projects and applies suppressions for any type errors that may have slipped through since the project got migrated.
    
    ignore-nocommit
    
    Reviewed By: grievejia
    
    Differential Revision: D55148328
    
    fbshipit-source-id: 5a618b747c27246434020fb140dc851543a16fb9

--- prelude/python/tools/sourcedb_merger/inputs.py
+++ prelude/python/tools/sourcedb_merger/inputs.py
@@ -5,7 +5,8 @@
 # License, Version 2.0 found in the LICENSE-APACHE file in the root directory
 # of this source tree.
 
-# pyre-ignore-all-errors
+# pyre-strict
+
 
 import dataclasses
 import json
--- prelude/python/tools/sourcedb_merger/legacy_merge.py
+++ prelude/python/tools/sourcedb_merger/legacy_merge.py
@@ -6,7 +6,8 @@
 # License, Version 2.0 found in the LICENSE-APACHE file in the root directory
 # of this source tree.
 
-# pyre-ignore-all-errors
+# pyre-strict
+
 
 import argparse
 import pathlib
