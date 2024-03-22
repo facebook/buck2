31c4d5dd50105df44a32f341026127cd4d247670
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-03-21 17:52:48 -0700

    suppress errors in `buck2/prelude/python/tools/sourcedb_merger`
    Differential Revision: D55225350
    
    fbshipit-source-id: acd6978256719b96ec92a03a56866a0fc40ed425

--- prelude/python/tools/sourcedb_merger/tests/__init__.py
+++ prelude/python/tools/sourcedb_merger/tests/__init__.py
@@ -8,6 +8,29 @@
 # pyre-strict
 
 
+# pyre-fixme[21]: Could not find name `BuildMapLoadError` in `tests.inputs_test`.
+# pyre-fixme[21]: Could not find name `PartialBuildMap` in `tests.inputs_test`.
+# pyre-fixme[21]: Could not find name `Target` in `tests.inputs_test`.
+# pyre-fixme[21]: Could not find name `TargetEntry` in `tests.inputs_test`.
+# pyre-fixme[21]: Could not find name `load_targets_and_build_maps_from_json` in
+#  `tests.inputs_test`.
 from .inputs_test import *  # noqa
+
+# pyre-fixme[21]: Could not find name `ConflictInfo` in `tests.legacy_output_test`.
+# pyre-fixme[21]: Could not find name `ConflictMap` in `tests.legacy_output_test`.
+# pyre-fixme[21]: Could not find name `FullBuildMap` in `tests.legacy_output_test`.
+# pyre-fixme[21]: Could not find name `MergeResult` in `tests.legacy_output_test`.
+# pyre-fixme[21]: Could not find name `PartialBuildMap` in `tests.legacy_output_test`.
+# pyre-fixme[21]: Could not find name `SourceInfo` in `tests.legacy_output_test`.
+# pyre-fixme[21]: Could not find name `Target` in `tests.legacy_output_test`.
+# pyre-fixme[21]: Could not find name `TargetEntry` in `tests.legacy_output_test`.
+# pyre-fixme[21]: Could not find name `merge_partial_build_maps` in
+#  `tests.legacy_output_test`.
 from .legacy_output_test import *  # noqa
+
+# pyre-fixme[21]: Could not find name `PartialBuildMap` in `tests.outputs_test`.
+# pyre-fixme[21]: Could not find name `Target` in `tests.outputs_test`.
+# pyre-fixme[21]: Could not find name `TargetEntry` in `tests.outputs_test`.
+# pyre-fixme[21]: Could not find name `merge_partial_build_maps` in
+#  `tests.outputs_test`.
 from .outputs_test import *  # noqa
