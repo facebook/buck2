70faadea2fed325528ade87387d91031e174a1fd
Stiopa Koltsov <nga@meta.com>
2024-04-01 14:35:28 -0700

    "providers_label" -> ProvidersLabel in test
    Summary: Following diff D55566351 makes it an error.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D55577075
    
    fbshipit-source-id: af368f0703ddd949bba01b0b10953eff465c6988

--- app/buck2_interpreter_for_build_tests/src/functions/with_sub_target.rs
+++ app/buck2_interpreter_for_build_tests/src/functions/with_sub_target.rs
@@ -9,6 +9,7 @@
 
 use buck2_core::configuration::data::ConfigurationData;
 use buck2_core::target::label::TargetLabel;
+use buck2_interpreter::types::configured_providers_label::register_providers_label;
 use buck2_interpreter::types::target_label::StarlarkConfiguredTargetLabel;
 use buck2_interpreter::types::target_label::StarlarkTargetLabel;
 use buck2_interpreter_for_build::interpreter::testing::Tester;
@@ -18,6 +19,7 @@ use indoc::indoc;
 #[test]
 fn test_with_sub_target() -> anyhow::Result<()> {
     let mut tester = Tester::new().unwrap();
+    tester.additional_globals(register_providers_label);
     tester.additional_globals(|globals| {
         let target = TargetLabel::testing_parse("cell//pkg:target");
         globals.set(
@@ -35,12 +37,12 @@ fn test_with_sub_target() -> anyhow::Result<()> {
         r#"
             def test():
                 unconf_providers = unconf.with_sub_target(["ab", "cd"])
-                assert_eq(isinstance(unconf_providers, "providers_label"), True)
+                assert_eq(isinstance(unconf_providers, ProvidersLabel), True)
                 assert_eq(unconf_providers.raw_target(), unconf)
                 assert_eq(str(unconf_providers), "cell//pkg:target[ab][cd]")
 
                 conf_providers = conf.with_sub_target(["ab", "cd"])
-                assert_eq(isinstance(conf_providers, "label"), True)
+                assert_eq(isinstance(conf_providers, ConfiguredProvidersLabel), True)
                 assert_eq(conf_providers.configured_target(), conf)
                 assert_eq(str(conf_providers), "cell//pkg:target[ab][cd] (<unbound>)")
             "#
