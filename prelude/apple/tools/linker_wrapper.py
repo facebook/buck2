b6276cd7ca5eb391c85d42aeda470597d44468a1
Nuri Amari <nuriamari@meta.com>
2024-04-01 16:56:52 -0700

    Create linker wrapper to diagnose unexported symbol problems
    Summary:
    A developer may make a change that introduces a cross dylib symbol
    reference that didn't previously exist. This means a symbol that
    was previously not exported, must now be exported.
    
    This linker wrapper intercepts linker error messages and compares
    them against the current set of unexported_symbols lists to diagnose
    such errors, and update the lists by invoking a tool.
    
    Reviewed By: narissiam, rmaz
    
    Differential Revision: D55246335
    
    fbshipit-source-id: db0aaa5e2eeb1d6db28594be3f3abad29c0abacf

---
+++ prelude/apple/tools/linker_wrapper.py
@@ -0,0 +1,114 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+import argparse
+import re
+import subprocess
+import sys
+
+from typing import List, Optional, Tuple
+
+
+def _eprintln(msg: str) -> None:
+    print(msg, flush=True, file=sys.stderr)
+
+
+def _expand_arg_files(args: List[str]) -> List[str]:
+    expanded_args = []
+    for arg in args:
+        if arg.startswith("@"):
+            with open(arg[1:]) as argfile:
+                expanded_args.extend(
+                    [line.strip('"') for line in argfile.read().splitlines()]
+                )
+        else:
+            expanded_args.append(arg)
+    return expanded_args
+
+
+def _seperate_wrapper_args_from_linker_args(
+    args: List[str],
+) -> Tuple[List[str], List[str]]:
+    wrapper_args = []
+    linker_args = []
+    expanded_args = _expand_arg_files(args)
+
+    i = 0
+    while i < len(expanded_args):
+        if expanded_args[i] == "-Xwrapper":
+            wrapper_args.append(expanded_args[i + 1])
+            i += 1
+        else:
+            linker_args.append(expanded_args[i])
+        i += 1
+
+    return wrapper_args, linker_args
+
+
+def _diagnose_potential_unexported_symbol_issue(
+    unexported_symbol_lists: List[str], stderr: str
+) -> Optional[str]:
+    stderr_lines = stderr.splitlines()
+    undefined_symbol_re = re.compile(r"undefined symbol:.*\(mangled: (\S+)\)")
+    undefined_symbols = set()
+    for stderr_line in stderr_lines:
+        match = re.search(undefined_symbol_re, stderr_line)
+        if match:
+            undefined_symbols.add(match.group(1))
+
+    if not undefined_symbols:
+        return None
+
+    unexported_symbols = set()
+    incorrectly_unexported_symbols = set()
+    incorrect_unexported_symbol_lists = []
+    for unexported_symbol_list in unexported_symbol_lists:
+        target_name, file_path = unexported_symbol_list.split(",")
+        with open(file_path, "r") as unexported_symbol_list_file:
+            unexported_symbols = set(unexported_symbol_list_file.read().splitlines())
+            intersection = undefined_symbols & unexported_symbols
+            if intersection:
+                incorrectly_unexported_symbols.update(intersection)
+                incorrect_unexported_symbol_lists.append(target_name)
+
+    if not incorrect_unexported_symbol_lists:
+        return None
+
+    return f"""
+UNEXPORTED SYMBOLS ERROR:
+
+At least one symbol is included in an unexported symbol list, but referenced across dylib boundaries. Please
+run the following command to fix the unexported symbol lists:
+
+arc fix-unexported-symbol-lists {"".join(["--target " + target for target in incorrect_unexported_symbol_lists])} {" ".join(["--symbol " + symbol for symbol in sorted(incorrectly_unexported_symbols)])}
+
+Here is the linker failure message:
+"""
+
+
+def main(argv: List[str]) -> int:
+    wrapper_args, linker_args = _seperate_wrapper_args_from_linker_args(argv[1:])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-linker")
+    parser.add_argument("-lld", action="store_true")
+    parser.add_argument("-unexported_symbol_list", action="append")
+    args = parser.parse_args(wrapper_args)
+    result = subprocess.run([args.linker] + linker_args, capture_output=True, text=True)
+    if result.returncode != 0:
+        if args.unexported_symbol_list and args.lld:
+            diagnosis = _diagnose_potential_unexported_symbol_issue(
+                args.unexported_symbol_list, result.stderr
+            )
+            if diagnosis:
+                _eprintln(diagnosis)
+        _eprintln(result.stderr)
+    return result.returncode
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
