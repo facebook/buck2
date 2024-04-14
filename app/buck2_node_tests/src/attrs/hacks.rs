d776123dc374c27d9374e8979d3b0624c2843daf
Stiopa Koltsov <nga@meta.com>
2024-04-14 16:14:29 -0700

    Remove not needed dependencies from buck2_node_tests
    Summary: Compile tests faster.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D56115774
    
    fbshipit-source-id: 9952fcb5c23164e59db92c17a94f0b65087fbff6

--- app/buck2_node_tests/src/attrs/hacks.rs
+++ app/buck2_node_tests/src/attrs/hacks.rs
@@ -11,31 +11,17 @@ use buck2_core::cells::name::CellName;
 use buck2_core::cells::paths::CellRelativePath;
 use buck2_core::fs::paths::forward_rel_path::ForwardRelativePath;
 use buck2_core::package::PackageLabel;
-use buck2_interpreter_for_build::attrs::coerce::attr_type::AttrTypeExt;
-use buck2_interpreter_for_build::attrs::coerce::ctx::BuildAttrCoercionContext;
-use buck2_interpreter_for_build::interpreter::testing::cells;
-use buck2_node::attrs::attr_type::AttrType;
-use buck2_node::attrs::configurable::AttrIsConfigurable;
+use buck2_node::attrs::attr_type::list::ListLiteral;
+use buck2_node::attrs::attr_type::string::StringLiteral;
+use buck2_node::attrs::coerced_attr::CoercedAttr;
 use buck2_node::attrs::hacks;
+use buck2_util::arc_str::ArcSlice;
+use buck2_util::arc_str::ArcStr;
 use dupe::Dupe;
-use starlark::values::Heap;
 
 #[test]
 fn stringifies_correctly() -> anyhow::Result<()> {
-    let heap = Heap::new();
-    let coercer_ctx = BuildAttrCoercionContext::new_no_package(
-        cells(None)?.1,
-        cells(None)?.0.resolve_self(),
-        cells(None)?.0,
-    );
-    let coercer = AttrType::string();
-    let coerced = coercer
-        .coerce(
-            AttrIsConfigurable::Yes,
-            &coercer_ctx,
-            heap.alloc("Hello, world!"),
-        )
-        .unwrap();
+    let coerced = CoercedAttr::String(StringLiteral(ArcStr::from("Hello, world!")));
 
     let package = PackageLabel::new(
         CellName::testing_new("root"),
@@ -47,11 +33,9 @@ fn stringifies_correctly() -> anyhow::Result<()> {
         hacks::value_to_string(&coerced, package.dupe())?
     );
 
-    let list = AttrType::list(coercer).coerce(
-        AttrIsConfigurable::Yes,
-        &coercer_ctx,
-        heap.alloc(vec!["Hello, world!"]),
-    )?;
+    let list = CoercedAttr::List(ListLiteral(ArcSlice::new([CoercedAttr::String(
+        StringLiteral(ArcStr::from("Hello, world!")),
+    )])));
     assert!(hacks::value_to_string(&list, package.dupe()).is_err());
     Ok(())
 }
