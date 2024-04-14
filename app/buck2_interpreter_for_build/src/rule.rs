0e542fb3078f25ef9755e680d5274cb7124b1b05
Jakob Degen <jakobdegen@meta.com>
2024-04-14 16:52:24 -0700

    docs: Fix strings as types
    Summary: ^
    
    Reviewed By: stepancheg
    
    Differential Revision: D56122573
    
    fbshipit-source-id: e10b89d53c1e3ea04ad67786a03ff5b6839a71f9

--- app/buck2_interpreter_for_build/src/rule.rs
+++ app/buck2_interpreter_for_build/src/rule.rs
@@ -434,7 +434,7 @@ pub fn register_rule_function(builder: &mut GlobalsBuilder) {
     /// Define a rule. As a simple example:
     ///
     /// ```python
-    /// def _my_rule(ctx: "context") -> ["provider"]:
+    /// def _my_rule(ctx: AnalysisContext) -> list[Provider]:
     ///     output = ctx.actions.write("hello.txt", ctx.attrs.contents, executable = ctx.attrs.exe)
     ///     return [DefaultInfo(outputs = [output])]
     ///
