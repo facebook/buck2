fa4751534aa91a1f74af5f061d753db7064c8841
Ignacio Guridi <iguridi@meta.com>
2024-04-25 13:31:03 -0700

    buck2: explain: js: vscode format
    Reviewed By: stepancheg
    
    Differential Revision: D56480175
    
    fbshipit-source-id: 2108774b4d497da35d6ee89688abfb5b3d3dd85f

--- app/buck2_explain/js/app.jsx
+++ app/buck2_explain/js/app.jsx
@@ -16,43 +16,43 @@ import { Build } from './fbs/explain'
 
 
 function App() {
-    const target = 'fbcode//buck2:buck2'
+  const target = 'fbcode//buck2:buck2'
 
-    const [blobBase64, setBlobBase64] = useState("XXDATAXX");
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          // keep this line as is, it will be replaced later
-          const data = (await import('./data')).DATA;
-          setBlobBase64(data);
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      };
-      fetchData();
-    }, []);
+  const [blobBase64, setBlobBase64] = useState("XXDATAXX");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // keep this line as is, it will be replaced later
+        const data = (await import('./data')).DATA;
+        setBlobBase64(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
-    const decodedString = atob(blobBase64);
-    let bytes = new Uint8Array(decodedString);
+  const decodedString = atob(blobBase64);
+  let bytes = new Uint8Array(decodedString);
 
-    let buf = new ByteBuffer(bytes);
+  let buf = new ByteBuffer(bytes);
 
-    // Get an accessor to the root object inside the buffer.
-    var build = Build.getRootAsBuild(buf);
+  // Get an accessor to the root object inside the buffer.
+  var build = Build.getRootAsBuild(buf);
 
-    var target2 = build.targets(1);
+  var target2 = build.targets(1);
 
-    console.log(target2?.name());
+  console.log(target2?.name());
 
-    const name = target2?.name();
+  const name = target2?.name();
 
-    return (
-      <>
-        <p><i><span>{target}</span></i></p>
-            <h2>{name}</h2>
-      </>
-    )
-  }
+  return (
+    <>
+      <p><i><span>{target}</span></i></p>
+      <h2>{name}</h2>
+    </>
+  )
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container)
