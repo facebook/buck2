1e0d23339071c342cc9b91d56a0281b38fa9a105
Neil Mitchell <ndmitchell@meta.com>
2024-04-29 04:03:50 -0700

    Add more types
    Summary: Reapply D56576153, which was reverted in D56684291, but this time don't do the `list[x]`, just use `List[x]`.
    
    Reviewed By: VladimirMakaev
    
    Differential Revision: D56692303
    
    fbshipit-source-id: 4eebca5145ad9c4bede88a4ce405b9c263f2dae8

--- prelude/python/tools/compile.py
+++ prelude/python/tools/compile.py
@@ -16,37 +16,29 @@ $ find out-dir -type f
 out-dir/foo.pyc
 """
 
-# pyre-fixme[51]: Mode `pyre-unsafe` is unused. This conflicts with `pyre-strict`
-#  mode set on line 8.
-# pyre-unsafe
-
 import argparse
 import errno
 import json
 import os
 import sys
 from py_compile import compile, PycInvalidationMode
+from typing import List
 
 
 if sys.version_info[0] == 3:
     import importlib
     import importlib.util
 
-    # pyre-fixme[5]: Global expression must be annotated.
-    DEFAULT_FORMAT = importlib.util.cache_from_source("{pkg}/{name}.py")
+    DEFAULT_FORMAT: str = importlib.util.cache_from_source("{pkg}/{name}.py")
 else:
-    DEFAULT_FORMAT = "{pkg}/{name}.pyc"
+    DEFAULT_FORMAT: str = "{pkg}/{name}.pyc"
 
 
-# pyre-fixme[3]: Return type must be annotated.
-# pyre-fixme[2]: Parameter must be annotated.
-def get_py_path(module):
+def get_py_path(module: str) -> str:
     return module.replace(".", os.sep) + ".py"
 
 
-# pyre-fixme[3]: Return type must be annotated.
-# pyre-fixme[2]: Parameter must be annotated.
-def get_pyc_path(module, fmt):
+def get_pyc_path(module: str, fmt: str) -> str:
     try:
         package, name = module.rsplit(".", 1)
     except ValueError:
@@ -60,9 +52,7 @@ def get_pyc_path(module, fmt):
     return os.path.join(*parts)
 
 
-# pyre-fixme[3]: Return type must be annotated.
-# pyre-fixme[2]: Parameter must be annotated.
-def _mkdirs(dirpath):
+def _mkdirs(dirpath: str) -> None:
     try:
         os.makedirs(dirpath)
     except OSError as e:
@@ -70,9 +60,7 @@ def _mkdirs(dirpath):
             raise
 
 
-# pyre-fixme[3]: Return type must be annotated.
-# pyre-fixme[2]: Parameter must be annotated.
-def main(argv):
+def main(argv: List[str]) -> None:
     parser = argparse.ArgumentParser(fromfile_prefix_chars="@")
     parser.add_argument("-o", "--output", required=True)
     parser.add_argument(
