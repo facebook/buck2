cef979df756d519b471c79214e7108bceef8ed0e
Ignacio Guridi <iguridi@meta.com>
2024-05-02 15:25:23 -0700

    buck2: explain: add unit test
    Reviewed By: christolliday
    
    Differential Revision: D56853430
    
    fbshipit-source-id: 7efb5d05a409e3a0dea20cdd1e3ccf54bbda4f12

--- app/buck2_explain/src/lib.rs
+++ app/buck2_explain/src/lib.rs
@@ -33,48 +33,9 @@ pub fn main(
     output: &AbsPathBuf,
     fbs_dump: Option<&AbsPathBuf>,
 ) -> anyhow::Result<()> {
-    let mut builder = FlatBufferBuilder::new();
+    let fbs = gen_fbs(data);
 
-    // TODO iguridi: just 1 node for now
-    let node = &data[0];
-    let name = builder.create_shared_string(&node.name());
-
-    // TODO iguridi: fill in other fields
-    let fbs = {
-        let target = fbs::ConfiguredTargetNode::create(
-            &mut builder,
-            &fbs::ConfiguredTargetNodeArgs {
-                configured_target_label: None,
-                type_: None,
-                name: Some(name),
-                default_target_platform: None,
-                deps: None,
-                package: None,
-                oncall: None,
-                target_configuration: None,
-                execution_platform: None,
-                plugins: None,
-                target_compatible_with: None,
-                compatible_with: None,
-                exec_compatible_with: None,
-                visibility: None,
-                within_view: None,
-                tests: None,
-                attrs: None,
-            },
-        );
-
-        let targets = builder.create_vector(&[target]);
-        let build = fbs::Build::create(
-            &mut builder,
-            &fbs::BuildArgs {
-                targets: Some(targets),
-            },
-        );
-        builder.finish(build, None);
-        builder.finished_data()
-    };
-
+    let fbs = fbs.finished_data();
     let base64 = base64::encode(&fbs);
 
     // For dev purposes, dump the base64 encoded flatbuffer to a file
@@ -94,4 +55,77 @@ pub fn main(
 
     fs::write(output, &html_out)?;
     Ok(())
+}
+
+fn gen_fbs(data: Vec<ConfiguredTargetNode>) -> FlatBufferBuilder<'static> {
+    let mut builder = FlatBufferBuilder::new();
+
+    // TODO iguridi: just 1 node for now
+    let node = &data[0];
+    let name = builder.create_shared_string(&node.name());
+
+    // TODO iguridi: fill in other fields
+    let target = fbs::ConfiguredTargetNode::create(
+        &mut builder,
+        &fbs::ConfiguredTargetNodeArgs {
+            configured_target_label: None,
+            type_: None,
+            name: Some(name),
+            default_target_platform: None,
+            deps: None,
+            package: None,
+            oncall: None,
+            target_configuration: None,
+            execution_platform: None,
+            plugins: None,
+            target_compatible_with: None,
+            compatible_with: None,
+            exec_compatible_with: None,
+            visibility: None,
+            within_view: None,
+            tests: None,
+            attrs: None,
+        },
+    );
+
+    let targets = builder.create_vector(&[target]);
+    let build = fbs::Build::create(
+        &mut builder,
+        &fbs::BuildArgs {
+            targets: Some(targets),
+        },
+    );
+    builder.finish(build, None);
+    builder
+}
+
+#[cfg(test)]
+mod tests {
+    use buck2_core::configuration::data::ConfigurationData;
+    use buck2_core::target::label::label::TargetLabel;
+
+    use super::*;
+    pub use crate::explain_generated::explain::Build;
+    #[test]
+    fn test_gen_fbs() {
+        // Setup data
+        let data = {
+            let target_label = TargetLabel::testing_parse("cell//pkg:foo");
+            let configured_target_label = target_label.configure(ConfigurationData::testing_new());
+
+            let target = ConfiguredTargetNode::testing_new(configured_target_label, "foo_lib");
+            vec![target]
+        };
+
+        // Generate fbs
+        let fbs = gen_fbs(data);
+        let fbs = fbs.finished_data();
+
+        // Read fbs
+        let build = flatbuffers::root::<Build>(fbs).unwrap();
+        let target = build.targets().unwrap().get(0);
+
+        // Assert contents
+        assert_eq!(target.name(), Some("foo"));
+    }
 }
