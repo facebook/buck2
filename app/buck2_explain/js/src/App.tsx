eb11d7d9493612e7d14b2565788f6e1f8947e55b
Ignacio Guridi <iguridi@meta.com>
2024-05-09 12:41:13 -0700

    buck2: explain: add navigation
    Reviewed By: samkevich
    
    Differential Revision: D57090938
    
    fbshipit-source-id: 57ac90c8d9b5be614999d4fef346740183e9e9fe

--- app/buck2_explain/js/src/App.tsx
+++ app/buck2_explain/js/src/App.tsx
@@ -14,6 +14,35 @@ import {ByteBuffer} from 'flatbuffers'
 import {Build, ConfiguredTargetNode} from './fbs/explain'
 import {Target} from './Target'
 
+const INITIAL_STATE = {
+  build: null,
+  rootTarget: null,
+  allTargets: {},
+}
+
+type STATE_TYPE = {
+  build: Build | null
+  rootTarget: ConfiguredTargetNode | null
+  allTargets: {[key: string]: number}
+}
+
+function fromUrl(
+  data: STATE_TYPE,
+  currentTarget: ConfiguredTargetNode | null,
+  setCurrentTarget: Dispatch<SetStateAction<ConfiguredTargetNode | null>>,
+) {
+  const {build, allTargets, rootTarget} = data
+  const params = new URLSearchParams(window.location.search)
+  const target_param = params.get('target')
+
+  let target = target_param == null ? null : build?.targets(allTargets[target_param])
+  target = target || rootTarget
+
+  if (target != null && currentTarget?.configuredTargetLabel() != target?.configuredTargetLabel()) {
+    setCurrentTarget(target)
+  }
+}
+
 function RootSpan(props: {
   target: ConfiguredTargetNode
   setCurrentTarget: Dispatch<SetStateAction<ConfiguredTargetNode | null>>
@@ -36,15 +65,57 @@ function RootSpan(props: {
 }
 
 function App() {
-  const [blobBase64, setBlobBase64] = useState('XXDATAXX')
+  const [data, setData] = useState<STATE_TYPE>(INITIAL_STATE)
+
   const [currentTarget, setCurrentTarget] = useState<ConfiguredTargetNode | null>(null)
+
+  useEffect(() => {
+    // This is triggered every time the user goes back in history
+    const f = () => {
+      fromUrl(data, currentTarget, setCurrentTarget)
+    }
+    window.addEventListener('popstate', f)
+    return () => {
+      window.removeEventListener('popstate', f)
+    }
+  }, [data, currentTarget])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         // keep this line as is, it will be replaced later
         const data = (await import('./data')).DATA
-        setBlobBase64(data)
+        const blobBase64 = data
+        const decodedString = atob(blobBase64)
+        // TODO iguridi: decode blob better
+        const byteArray = new Uint8Array(decodedString.length)
+        for (let i = 0; i < decodedString.length; i++) {
+          byteArray[i] = decodedString.charCodeAt(i)
+        }
+
+        const buf = new ByteBuffer(byteArray)
+
+        // Get an accessor to the root object inside the buffer.
+        const build = Build.getRootAsBuild(buf)
+        // TODO iguridi: just show 1 target for now
+        const rootTarget = build.targets(0)
+
+        const allTargets: {[key: string]: number} = {}
+        for (let i = 0; i < build.targetsLength(); i++) {
+          let target = build.targets(i)
+          let label = target?.configuredTargetLabel()
+          if (label == null) {
+            continue
+          }
+          allTargets[label] = i
+        }
+
+        const data2 = {build, allTargets, rootTarget}
+
+        setData(data2)
+
+        // This should run just once total
+        fromUrl(data2, currentTarget, setCurrentTarget)
       } catch (error) {
         console.error('Error:', error)
       }
@@ -52,39 +123,7 @@ function App() {
     fetchData()
   }, [])
 
-  const decodedString = atob(blobBase64)
-  // TODO iguridi: decode blob better
-  const byteArray = new Uint8Array(decodedString.length)
-  for (let i = 0; i < decodedString.length; i++) {
-    byteArray[i] = decodedString.charCodeAt(i)
-  }
-
-  let buf = new ByteBuffer(byteArray)
-
-  // Get an accessor to the root object inside the buffer.
-  const build = Build.getRootAsBuild(buf)
-
-  const rootTarget = build.targets(0)
-
-  const allTargets: {[key: string]: number} = {}
-  for (let i = 0; i < build.targetsLength(); i++) {
-    let target = build.targets(i)
-    let label = target?.configuredTargetLabel()
-    if (label == null) {
-      continue
-    }
-    allTargets[label] = i
-  }
-
-  const params = new URLSearchParams(window.location.search)
-  const target_param = params.get('target')
-
-  let target = target_param == null ? null : build.targets(allTargets[target_param])
-  target = target || rootTarget
-
-  if (target != null && target?.configuredTargetLabel() != currentTarget?.configuredTargetLabel()) {
-    setCurrentTarget(target)
-  }
+  const rootTarget = data.rootTarget
 
   if (currentTarget == null || rootTarget == null) return <p>Loading...</p>
   else {
