64d63fccd657ee9aa11f4063ebe454154796cc92
Ignacio Guridi <iguridi@meta.com>
2024-05-09 12:41:13 -0700

    buck2: explain: show internal and special attrs
    Summary: Probably we want to hide the empty ones, and set some classifications based on importance. For now just show all
    
    Reviewed By: samkevich
    
    Differential Revision: D57084544
    
    fbshipit-source-id: bd9963aa1f0da566242ce670c040c49ed1c7ccf1

--- app/buck2_explain/js/src/App.tsx
+++ app/buck2_explain/js/src/App.tsx
@@ -13,6 +13,16 @@ import {createRoot} from 'react-dom/client'
 import {ByteBuffer} from 'flatbuffers'
 import {Build} from './fbs/explain'
 
+function List(props: {attr: (i: number) => String; length: number}): JSX.Element {
+  const items: JSX.Element[] = []
+  for (let i = 0; i < props.length; i++) {
+    const value = props.attr(i)
+    const row = <li key={i}>{value}</li>
+    items.push(row)
+  }
+  return <ul>{items}</ul>
+}
+
 function App() {
   const [blobBase64, setBlobBase64] = useState('XXDATAXX')
   useEffect(() => {
@@ -51,7 +61,77 @@ function App() {
             <span>{rootTarget.configuredTargetLabel()}</span>
           </i>
         </p>
-        <h2>{rootTarget.name()}</h2>
+        <ul>
+          <li>
+            <b>Name: </b>
+            <span>{rootTarget.name()}</span>
+          </li>
+          <li>
+            <b>Default target platform: </b>
+            <span>{rootTarget.defaultTargetPlatform()}</span>
+          </li>
+          <li>
+            <b>Target compatible with: </b>
+            <List
+              attr={i => rootTarget.targetCompatibleWith(i)}
+              length={rootTarget.targetCompatibleWithLength()}
+            />
+          </li>
+          <li>
+            <b>Compatible with: </b>
+            <List
+              attr={i => rootTarget.compatibleWith(i)}
+              length={rootTarget.compatibleWithLength()}
+            />
+          </li>
+          <li>
+            <b>Exec compatible with: </b>
+            <List
+              attr={i => rootTarget.execCompatibleWith(i)}
+              length={rootTarget.execCompatibleWithLength()}
+            />
+          </li>
+          <li>
+            <b>Visibility: </b>
+            <List attr={i => rootTarget.visibility(i)} length={rootTarget.visibilityLength()} />
+          </li>
+          <li>
+            <b>Within view: </b>
+            <List attr={i => rootTarget.withinView(i)} length={rootTarget.withinViewLength()} />
+          </li>
+          <li>
+            <b>Tests: </b>
+            <List attr={i => rootTarget.tests(i)} length={rootTarget.testsLength()} />
+          </li>
+          <li>
+            <b>Type: </b>
+            <span>{rootTarget.type()}</span>
+          </li>
+          <li>
+            <b>Deps: </b>
+            <List attr={i => rootTarget.deps(i)} length={rootTarget.depsLength()} />
+          </li>
+          <li>
+            <b>Package: </b>
+            <span>{rootTarget.package_()}</span>
+          </li>
+          <li>
+            <b>Oncall: </b>
+            <span>{rootTarget.oncall()}</span>
+          </li>
+          <li>
+            <b>Target configuration: </b>
+            <span>{rootTarget.targetConfiguration()}</span>
+          </li>
+          <li>
+            <b>Execution platform: </b>
+            <span>{rootTarget.executionPlatform()}</span>
+          </li>
+          <li>
+            <b>Plugins: </b>
+            <List attr={i => rootTarget.plugins(i)} length={rootTarget.pluginsLength()} />
+          </li>
+        </ul>
       </>
     )
   }
