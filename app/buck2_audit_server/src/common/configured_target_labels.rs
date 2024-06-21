69fc1e972d426f9a87778e958aa1600bfe65ea7e
Stiopa Koltsov <nga@meta.com>
2024-06-21 08:50:25 -0700

    TargetResolutionConfig::get_configured_targets_for_configured_target_literals
    Summary: Extract code from audit commands. To be used in profiler (probably).
    
    Reviewed By: cjhopman
    
    Differential Revision: D58632170
    
    fbshipit-source-id: 72b98c2492ca5207127087c3a4239148c6cf4db1

--- app/buck2_audit_server/src/common/configured_target_labels.rs
+++ app/buck2_audit_server/src/common/configured_target_labels.rs
@@ -8,28 +8,13 @@
  */
 
 use buck2_client_ctx::common::target_cfg::TargetCfgWithUniverseOptions;
-use buck2_core::configuration::bound_id::BoundConfigurationId;
-use buck2_core::configuration::data::ConfigurationData;
-use buck2_core::pattern::pattern_type::ConfigurationPredicate;
 use buck2_core::pattern::pattern_type::ConfiguredTargetPatternExtra;
-use buck2_core::pattern::TargetLabelWithExtra;
 use buck2_core::target::configured_target_label::ConfiguredTargetLabel;
 use buck2_server_ctx::ctx::ServerCommandContextTrait;
 use buck2_server_ctx::pattern_parse_and_resolve::parse_and_resolve_patterns_to_targets_from_cli_args;
-use buck2_server_ctx::target_resolution_config::TargetResolutionConfig;
 use dice::DiceComputations;
 
 use crate::common::target_resolution_config::audit_command_target_resolution_config;
-
-#[derive(Debug, buck2_error::Error)]
-enum PatternNotSupportedError {
-    #[error("Builtin configurations are not supported: `{0}`")]
-    BuiltinConfigurationsNotSupported(String),
-    #[error(
-        "Patterns with configuration label without configuration hash are not supported: `{0}`"
-    )]
-    ConfigurationLabelWithoutHashNotSupported(String),
-}
 
 pub(crate) async fn audit_command_configured_target_labels(
     ctx: &mut DiceComputations<'_>,
@@ -49,48 +34,12 @@ pub(crate) async fn audit_command_configured_target_labels(
         audit_command_target_resolution_config(ctx, target_cfg, server_ctx).await?;
 
     let mut configured_target_labels: Vec<ConfiguredTargetLabel> = Vec::new();
-    for TargetLabelWithExtra {
-        target_label,
-        extra,
-    } in targets
-    {
-        match extra.cfg {
-            ConfigurationPredicate::Any => {
-                configured_target_labels.extend(
-                    target_resolution_config
-                        .get_configured_target(ctx, &target_label)
-                        .await?,
-                );
-            }
-            ConfigurationPredicate::Builtin(p) => {
-                return Err(PatternNotSupportedError::BuiltinConfigurationsNotSupported(
-                    p.to_string(),
-                )
-                .into());
-            }
-            ConfigurationPredicate::Bound(label, None) => {
-                return Err(
-                    PatternNotSupportedError::ConfigurationLabelWithoutHashNotSupported(
-                        label.to_string(),
-                    )
-                    .into(),
-                );
-            }
-            ConfigurationPredicate::Bound(label, Some(hash)) => {
-                let cfg = ConfigurationData::lookup_bound(BoundConfigurationId { label, hash })?;
-                let configured = target_label.configure(cfg);
-                match &target_resolution_config {
-                    TargetResolutionConfig::Default(_) => {
-                        configured_target_labels.push(configured);
-                    }
-                    TargetResolutionConfig::Universe(universe) => {
-                        if universe.contains(&configured) {
-                            configured_target_labels.push(configured);
-                        }
-                    }
-                }
-            }
-        }
+    for target in targets {
+        configured_target_labels.extend(
+            target_resolution_config
+                .get_configured_targets_for_configured_target_literals(ctx, &target)
+                .await?,
+        );
     }
 
     Ok(configured_target_labels)
--- app/buck2_server_ctx/src/target_resolution_config.rs
+++ app/buck2_server_ctx/src/target_resolution_config.rs
@@ -9,6 +9,11 @@
 
 use buck2_cli_proto::TargetCfg;
 use buck2_common::global_cfg_options::GlobalCfgOptions;
+use buck2_core::configuration::bound_id::BoundConfigurationId;
+use buck2_core::configuration::data::ConfigurationData;
+use buck2_core::pattern::pattern_type::ConfigurationPredicate;
+use buck2_core::pattern::pattern_type::ConfiguredTargetPatternExtra;
+use buck2_core::pattern::TargetLabelWithExtra;
 use buck2_core::provider::label::ConfiguredProvidersLabel;
 use buck2_core::provider::label::ProvidersLabel;
 use buck2_core::target::configured_target_label::ConfiguredTargetLabel;
@@ -21,6 +26,16 @@ use gazebo::prelude::VecExt;
 
 use crate::ctx::ServerCommandContextTrait;
 use crate::global_cfg_options::global_cfg_options_from_client_context;
+
+#[derive(Debug, buck2_error::Error)]
+enum PatternNotSupportedError {
+    #[error("Builtin configurations are not supported: `{0}`")]
+    BuiltinConfigurationsNotSupported(String),
+    #[error(
+        "Patterns with configuration label without configuration hash are not supported: `{0}`"
+    )]
+    ConfigurationLabelWithoutHashNotSupported(String),
+}
 
 pub enum TargetResolutionConfig {
     /// Resolve using target platform.
@@ -81,5 +96,45 @@ impl TargetResolutionConfig {
             .into_map(|configured_target_label| {
                 ConfiguredProvidersLabel::new(configured_target_label, label.name().clone())
             }))
+    }
+
+    pub async fn get_configured_targets_for_configured_target_literals(
+        &self,
+        ctx: &mut DiceComputations<'_>,
+        label: &TargetLabelWithExtra<ConfiguredTargetPatternExtra>,
+    ) -> anyhow::Result<Vec<ConfiguredTargetLabel>> {
+        let TargetLabelWithExtra {
+            target_label,
+            extra,
+        } = &label;
+        match &extra.cfg {
+            ConfigurationPredicate::Any => self.get_configured_target(ctx, &target_label).await,
+            ConfigurationPredicate::Builtin(p) => Err(
+                PatternNotSupportedError::BuiltinConfigurationsNotSupported(p.to_string()).into(),
+            ),
+            ConfigurationPredicate::Bound(label, None) => Err(
+                PatternNotSupportedError::ConfigurationLabelWithoutHashNotSupported(
+                    label.to_string(),
+                )
+                .into(),
+            ),
+            ConfigurationPredicate::Bound(label, Some(hash)) => {
+                let cfg = ConfigurationData::lookup_bound(BoundConfigurationId {
+                    label: label.clone(),
+                    hash: hash.clone(),
+                })?;
+                let configured = target_label.configure(cfg);
+                match self {
+                    TargetResolutionConfig::Default(_) => Ok(vec![configured]),
+                    TargetResolutionConfig::Universe(universe) => {
+                        if universe.contains(&configured) {
+                            Ok(vec![configured])
+                        } else {
+                            Ok(Vec::new())
+                        }
+                    }
+                }
+            }
+        }
     }
 }
