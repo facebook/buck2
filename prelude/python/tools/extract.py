3fa94423ff0558de40cd69fe78128a9c155ae84e
Andrew Gallagher <andrewjcg@meta.com>
2024-06-25 07:53:48 -0700

    Support `.tar.gz` prebuilt python libs
    Summary:
    Some older prebuilt python libs are packaged as `.tar.gz`s, so
    support extracting them too (e.g. https://pypi.org/project/iopath/#files).
    
    Reviewed By: zsol
    
    Differential Revision: D58919309
    
    fbshipit-source-id: 02981fc0c24fc8e960ef971392274d69747f248b

--- prelude/python/tools/extract.py
+++ prelude/python/tools/extract.py
@@ -15,8 +15,12 @@ extract.py my_zip_file.zip --output=output_directory
 """
 
 import argparse
+import glob
 import os
+import shutil
 import stat
+import tarfile
+import tempfile
 import zipfile
 from pathlib import Path
 
@@ -25,24 +29,37 @@ from pathlib import Path
 # (see https://bugs.python.org/issue15795, https://stackoverflow.com/questions/39296101/python-zipfile-removes-execute-permissions-from-binaries).
 #
 # We need to preserve at least the executable bit.
-def extract_zip_with_permissions(src: Path, dst_dir: Path) -> None:
-    z = zipfile.ZipFile(src)
-    for info in z.infolist():
-        outfile = z.extract(info.filename, dst_dir)
+def extract(src: Path, dst_dir: Path) -> None:
+    if src.suffixes[-2:] == [".tar", ".gz"]:
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            with tarfile.open(src) as tf:
+                tf.extractall(tmp_dir)
 
-        execute_perms = (info.external_attr >> 16) & (
-            stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
-        )
-        if execute_perms:
-            st = os.stat(outfile)
-            new_mode = stat.S_IMODE(st.st_mode | execute_perms)
-            if new_mode != st.st_mode:
-                os.chmod(outfile, new_mode)
+            # We expect the tgz to contain a single top-level dir with all the
+            # items to unpack.
+            (path,) = glob.glob(os.path.join(tmp_dir, "*"))
+            for ent in os.listdir(path):
+                fsrc = os.path.join(path, ent)
+                fdst = os.path.join(dst_dir, ent)
+                shutil.move(fsrc, fdst)
+
+    else:
+        with zipfile.ZipFile(src) as z:
+            for info in z.infolist():
+                outfile = z.extract(info.filename, dst_dir)
+                execute_perms = (info.external_attr >> 16) & (
+                    stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+                )
+                if execute_perms:
+                    st = os.stat(outfile)
+                    new_mode = stat.S_IMODE(st.st_mode | execute_perms)
+                    if new_mode != st.st_mode:
+                        os.chmod(outfile, new_mode)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(
-        description="Extract .zip files to a directory in a cross platform manner"
+        description="Extract .zip/.tar.gz archives to a directory in a cross platform manner"
     )
     parser.add_argument(
         "--output", type=Path, required=True, help="The directory to write to"
@@ -52,7 +69,7 @@ def main() -> None:
 
     args.output.mkdir(parents=True, exist_ok=True)
 
-    extract_zip_with_permissions(args.src, args.output)
+    extract(args.src, args.output)
 
 
 if __name__ == "__main__":
