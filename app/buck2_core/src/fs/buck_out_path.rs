f19b9e2b28eeeaf574ed21c583ba06db335f1868
Jakob Degen <jakobdegen@meta.com>
2024-07-07 00:46:57 -0700

    Back out "re-apply: "[Bucks2] Fix BUCK_SCRATCH_PATH uniqueness""
    Summary: Reverts D59320755 with the goal of unblocking the bump on Monday. Will reapply fix with a buckconfig gate for next rollout
    
    Reviewed By: Will-MingLun-Li
    
    Differential Revision: D59424072
    
    fbshipit-source-id: c94b292c380fe402e5448dd4180ce10fd3fe911b

--- app/buck2_core/src/fs/buck_out_path.rs
+++ app/buck2_core/src/fs/buck_out_path.rs
@@ -90,8 +90,6 @@ pub struct BuckOutScratchPath {
     owner: BaseDeferredKey,
     /// The path relative to that target.
     path: ForwardRelativePathBuf,
-    /// The unique identifier for this action
-    action_key: String,
 }
 
 impl BuckOutScratchPath {
@@ -141,6 +139,7 @@ impl BuckOutScratchPath {
                     // FIXME: Should this be a crypto hasher?
                     let mut hasher = DefaultHasher::new();
                     v.hash(&mut hasher);
+                    action_key.hash(&mut hasher);
                     let output_hash = format!("{}{:x}", MAKE_SENSIBLE_PREFIX, hasher.finish());
                     path.join_normalized(ForwardRelativePath::new(&output_hash)?)?
                 }
@@ -148,11 +147,7 @@ impl BuckOutScratchPath {
             _ => path.to_buf(),
         };
 
-        Ok(Self {
-            owner,
-            path,
-            action_key,
-        })
+        Ok(Self { owner, path })
     }
 }
 
@@ -241,7 +236,7 @@ impl BuckOutPathResolver {
         self.prefixed_path_for_owner(
             ForwardRelativePath::unchecked_new("tmp"),
             &path.owner,
-            Some(&path.action_key),
+            None,
             &path.path,
             // Fully hash scratch path as it can be very long and cause path too long issue on Windows.
             true,
@@ -575,7 +570,8 @@ mod tests {
         assert_ne!(mk("same_key", "_buck_1"), mk("same_key", "_buck_2"));
 
         // Different action_key, same identifier are not equal
-        assert_ne!(mk("diff_key1", "same_id"), mk("diff_key2", "same_id"));
+        // TODO: This should not be equal, but it is currently due to a bug
+        assert_eq!(mk("diff_key1", "same_id"), mk("diff_key2", "same_id"));
         assert_ne!(mk("diff_key1", "_buck_same"), mk("diff_key2", "_buck_same"));
 
         // Different action_key, different identifier are not equal
