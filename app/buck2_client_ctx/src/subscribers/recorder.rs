d2f617b80d200f4f63a29783ba23f31b37430a2c
Ezgi Çiçek <ezgi@meta.com>
2024-07-12 08:32:07 -0700

    Set total_disk_space_bytes for InvocationRecorder via SystemInfo
    Summary: Forgot to set `total_disk_space_bytes` in D59280429 which is recorded at the beginning of a command via `SystemInfo` event. No wonder nothing shows up in Scuba for `total_disk_space_bytes` even if we record it in ingress D59322534.
    
    Reviewed By: samkevich
    
    Differential Revision: D59680986
    
    fbshipit-source-id: f755f40e6df8c8fbc5d14277225e87cbaee96c41

--- app/buck2_client_ctx/src/subscribers/recorder.rs
+++ app/buck2_client_ctx/src/subscribers/recorder.rs
@@ -845,6 +845,7 @@ impl<'a> InvocationRecorder<'a> {
 
     fn handle_system_info(&mut self, system_info: &buck2_data::SystemInfo) -> anyhow::Result<()> {
         self.system_total_memory_bytes = system_info.system_total_memory_bytes;
+        self.total_disk_space_bytes = system_info.total_disk_space_bytes;
         Ok(())
     }
 
