47afa87883731a24b81741dc8818387af3c95d93
Ignacio Guridi <iguridi@meta.com>
2024-07-15 16:14:54 -0700

    buck2: explain: update load.sh
    Summary: we need no target as of D59504550
    
    Reviewed By: samkevich
    
    Differential Revision: D59647893
    
    fbshipit-source-id: 3132cd335546916b51e0924260819a9cdb29b5a1

--- app/buck2_explain/load.sh
+++ app/buck2_explain/load.sh
@@ -6,9 +6,9 @@
 # License, Version 2.0 found in the LICENSE-APACHE file in the root directory
 # of this source tree.
 
+# We use this script to load the data for quick local iteration on the frontend
 # $1: buck2 executable
-# $2: target
 
-$1 explain --target "$2" --output /dev/null --fbs-dump /tmp/fbs
+$1 explain --output /dev/null --fbs-dump /tmp/fbs
 echo "export const DATA = '$(cat /tmp/fbs)';" > js/src/data.ts
 buck2 build :schema_ts --out js/src/fbs
