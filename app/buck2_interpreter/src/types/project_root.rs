3b4d3dc629abde73150898f0b6f8280106fc78d7
Stiopa Koltsov <nga@meta.com>
2024-07-18 06:58:05 -0700

    Allocate ProjectRoot statically
    Summary: Mostly because we can.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D59891525
    
    fbshipit-source-id: 38dc8565c2f70bec1a4c88a986a76e0eda2fe185

--- app/buck2_interpreter/src/types/project_root.rs
+++ app/buck2_interpreter/src/types/project_root.rs
@@ -12,19 +12,41 @@ use derive_more::Display;
 use starlark::any::ProvidesStaticType;
 use starlark::environment::GlobalsBuilder;
 use starlark::starlark_module;
-use starlark::starlark_simple_value;
 use starlark::values::starlark_value;
 use starlark::values::starlark_value_as_type::StarlarkValueAsType;
+use starlark::values::AllocFrozenValue;
+use starlark::values::AllocStaticSimple;
+use starlark::values::AllocValue;
+use starlark::values::FrozenHeap;
+use starlark::values::FrozenValue;
+use starlark::values::Heap;
 use starlark::values::NoSerialize;
 use starlark::values::StarlarkValue;
+use starlark::values::Value;
 
 #[derive(Debug, PartialEq, Display, ProvidesStaticType, NoSerialize, Allocative)]
 pub struct StarlarkProjectRoot;
 
-starlark_simple_value!(StarlarkProjectRoot);
-
-#[starlark_value(type = "project_root")]
+#[starlark_value(type = "project_root", StarlarkTypeRepr, UnpackValue)]
 impl<'v> StarlarkValue<'v> for StarlarkProjectRoot {}
+
+fn instance() -> FrozenValue {
+    static INSTANCE: AllocStaticSimple<StarlarkProjectRoot> =
+        AllocStaticSimple::alloc(StarlarkProjectRoot);
+    INSTANCE.to_frozen_value()
+}
+
+impl<'v> AllocValue<'v> for StarlarkProjectRoot {
+    fn alloc_value(self, _heap: &'v Heap) -> Value<'v> {
+        instance().to_value()
+    }
+}
+
+impl AllocFrozenValue for StarlarkProjectRoot {
+    fn alloc_frozen_value(self, _heap: &FrozenHeap) -> FrozenValue {
+        instance()
+    }
+}
 
 #[starlark_module]
 pub fn register_project_root(globals: &mut GlobalsBuilder) {
