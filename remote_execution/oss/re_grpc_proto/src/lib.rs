41d410553a524627dcf4a426a523f3d8d2bca1bf
Son Luong Ngoc <sluongng@gmail.com>
2024-07-19 14:54:21 -0700

    re_grpc_proto: add serde support (#680)
    Summary:
    Add support for serde::{Serialize, Deserialize} to remote execution
    protobuf.
    Short term, this enables quick debugging use cases in OSS setup as user
    could print out the requests and responses using serde_json.
    
    Pull Request resolved: https://github.com/facebook/buck2/pull/680
    
    Reviewed By: iguridi
    
    Differential Revision: D59866240
    
    Pulled By: JakobDegen
    
    fbshipit-source-id: b26a8a7b2c827de1a6b145d8af08be97f76a2c72

--- remote_execution/oss/re_grpc_proto/src/lib.rs
+++ remote_execution/oss/re_grpc_proto/src/lib.rs
@@ -35,3 +35,88 @@ pub mod build {
         }
     }
 }
+
+pub mod serialize_vec_any {
+    use serde::Deserialize;
+    use serde::Deserializer;
+    use serde::Serialize;
+    use serde::Serializer;
+
+    pub fn serialize<S>(value: &[::prost_types::Any], serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let d: Vec<(String, Vec<u8>)> = value
+            .iter()
+            .map(|v| (v.type_url.clone(), v.value.clone()))
+            .collect();
+        d.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<::prost_types::Any>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let d: Vec<::prost_types::Any> = Vec::deserialize(deserializer)?
+            .into_iter()
+            .map(|(type_url, value)| ::prost_types::Any { type_url, value })
+            .collect();
+        Ok(d)
+    }
+}
+
+pub mod serialize_option_any {
+    use serde::Deserialize;
+    use serde::Deserializer;
+    use serde::Serialize;
+    use serde::Serializer;
+
+    pub fn serialize<S>(
+        value: &Option<::prost_types::Any>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let d = value
+            .as_ref()
+            .map(|v| (v.type_url.clone(), v.value.clone()));
+        d.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<::prost_types::Any>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let d = Option::<(String, Vec<u8>)>::deserialize(deserializer)?
+            .map(|(type_url, value)| ::prost_types::Any { type_url, value });
+        Ok(d)
+    }
+}
+
+pub mod serialize_any {
+    use serde::Deserialize;
+    use serde::Deserializer;
+    use serde::Serialize;
+    use serde::Serializer;
+
+    pub fn serialize<S>(value: &::prost_types::Any, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let d = (value.type_url.clone(), value.value.clone());
+        d.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<::prost_types::Any, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let d = <(String, Vec<u8>)>::deserialize(deserializer)?;
+        let d = ::prost_types::Any {
+            type_url: d.0,
+            value: d.1,
+        };
+        Ok(d)
+    }
+}
