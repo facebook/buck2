145bbfa05d89063616b2fd0544541fb4b88e5a6f
Stiopa Koltsov <nga@meta.com>
2024-07-21 16:32:09 -0700

    impl_directory_search! to generic
    Summary: Similar to D60010599 (and reuse `WalkType` trait).
    
    Reviewed By: JakobDegen
    
    Differential Revision: D60012691
    
    fbshipit-source-id: 181ce7a20e6db625753beec2c42b5d1af5e29d8c

--- app/buck2_core/src/directory/directory_selector.rs
+++ app/buck2_core/src/directory/directory_selector.rs
@@ -10,17 +10,16 @@
 use starlark_map::small_map::SmallMap;
 
 use crate::directory::builder::DirectoryBuilder;
-use crate::directory::directory::Directory;
-use crate::directory::directory::DirectoryEntries;
 use crate::directory::directory_hasher::DirectoryDigest;
 use crate::directory::directory_iterator::DirectoryIterator;
 use crate::directory::directory_iterator::DirectoryIteratorPathAccessor;
 use crate::directory::directory_iterator::DirectoryIteratorPathStack;
-use crate::directory::entries::FingerprintedOrderedDirectoryEntries;
-use crate::directory::entries::OrderedDirectoryEntries;
 use crate::directory::entry::DirectoryEntry;
-use crate::directory::fingerprinted_directory::FingerprintedDirectory;
-use crate::directory::fingerprinted_directory::FingerprintedDirectoryEntries;
+use crate::directory::walk::FingerprintedOrderedDirectoryWalkType;
+use crate::directory::walk::FingerprintedUnorderedDirectoryWalkType;
+use crate::directory::walk::OrderedDirectoryWalkType;
+use crate::directory::walk::UnorderedDirectoryWalkType;
+use crate::directory::walk::WalkType;
 use crate::fs::paths::file_name::FileName;
 use crate::fs::paths::file_name::FileNameBuf;
 use crate::fs::paths::IntoFileNameBufIterator;
@@ -140,191 +139,158 @@ fn filter_inner<L, H>(
     }
 }
 
-macro_rules! impl_directory_search {
-    (
-        $search_ty: ident,
-        $dir_ty: ident,
-        $entries_ty: ident,
-        $entries_method: ident,
-        $mod: ident,
-        $selector_search_method: ident,
-    ) => {
-        mod $mod {
-            use super::*;
+enum SearchFrame<'a, 'b, T: WalkType<'b>> {
+    /// The search consists of just returning the root.
+    ReturnRoot {
+        root: Option<DirectoryEntry<T::Directory, &'b T::Leaf>>,
+    },
 
-            enum SearchFrame<'a, 'b, L, H> {
-                /// The search consists of just returning the root.
-                ReturnRoot {
-                    root: Option<DirectoryEntry<&'b dyn $dir_ty<L, H>, &'b L>>,
-                },
-
-                /// Continue the search.
-                Search {
-                    search: &'a SmallMap<FileNameBuf, DirectorySelector>,
-                    name: Option<&'b FileName>,
-                    entries: $entries_ty<'b, L, H>,
-                },
-            }
-
-            pub struct $search_ty<'a, 'b, L, H> {
-                stack: Vec<SearchFrame<'a, 'b, L, H>>,
-            }
-
-            impl<'a, 'b, L, H> $search_ty<'a, 'b, L, H> {
-                pub fn new<D>(selector: &'a DirectorySelector, root: &'b D) -> Self
-                where
-                    D: $dir_ty<L, H>,
-                {
-                    match selector {
-                        DirectorySelector::Traverse(ref search) => Self {
-                            stack: vec![SearchFrame::Search {
-                                search,
-                                name: None,
-                                entries: $entries_ty::from(root.$entries_method()),
-                            }],
-                        },
-                        DirectorySelector::Take => Self {
-                            stack: vec![SearchFrame::ReturnRoot {
-                                root: Some(DirectoryEntry::Dir(root as &dyn $dir_ty<L, H>)),
-                            }],
-                        },
-                    }
-                }
-            }
-
-            impl<'a, 'b, L, H> DirectoryIterator for $search_ty<'a, 'b, L, H> {
-                type PathStack = Self;
-                type Item = Result<
-                    DirectoryEntry<&'b dyn $dir_ty<L, H>, &'b L>,
-                    DirectorySearchError<&'b L>,
-                >;
-
-                fn next<'c>(
-                    &'c mut self,
-                ) -> Option<(DirectoryIteratorPathAccessor<'c, Self>, Self::Item)> {
-                    loop {
-                        let frame = self.stack.last_mut()?;
-
-                        match frame {
-                            SearchFrame::ReturnRoot { root } => {
-                                let root = root.take()?;
-                                return Some((
-                                    DirectoryIteratorPathAccessor {
-                                        leaf: None,
-                                        stack: self,
-                                    },
-                                    Ok(root),
-                                ));
-                            }
-                            SearchFrame::Search {
-                                search, entries, ..
-                            } => {
-                                if let Some((name, entry)) = entries.next() {
-                                    let search = search.get(name);
-
-                                    match search {
-                                        Some(DirectorySelector::Traverse(t)) => match entry {
-                                            // Traverse into this directory ... assuming it's a directory :)
-                                            DirectoryEntry::Dir(d) => {
-                                                self.stack.push(SearchFrame::Search {
-                                                    name: Some(name),
-                                                    search: t,
-                                                    entries: $entries_ty::from(d.$entries_method()),
-                                                });
-                                                continue;
-                                            }
-                                            DirectoryEntry::Leaf(leaf) => {
-                                                return Some((
-                                                    DirectoryIteratorPathAccessor {
-                                                        leaf: Some(name),
-                                                        stack: self,
-                                                    },
-                                                    Err(DirectorySearchError::CannotTraverseLeaf {
-                                                        leaf,
-                                                    }),
-                                                ));
-                                            }
-                                        },
-                                        Some(DirectorySelector::Take) => {
-                                            // Return the entry. Do not traverse further.
-                                            return Some((
-                                                DirectoryIteratorPathAccessor {
-                                                    leaf: Some(name),
-                                                    stack: self,
-                                                },
-                                                Ok(entry),
-                                            ));
-                                        }
-                                        None => {
-                                            // Ignore this node entirely.
-                                            continue;
-                                        }
-                                    }
-                                }
-                            }
-                        };
-
-                        // We've exhausted this iterator. Go back to the previous stack frame.
-                        self.stack.pop();
-                    }
-                }
-            }
-
-            impl<'a, 'b, L, H> DirectoryIteratorPathStack for $search_ty<'a, 'b, L, H> {
-                fn path(&self) -> impl Iterator<Item = &FileName> {
-                    self.stack.iter().filter_map(|frame| match frame {
-                        SearchFrame::ReturnRoot { .. } => None,
-                        SearchFrame::Search { name, .. } => name.as_deref(),
-                    })
-                }
-            }
-        }
-
-        impl DirectorySelector {
-            pub fn $selector_search_method<'a, 'b, L, H, D: $dir_ty<L, H>>(
-                &'a self,
-                dir: &'b D,
-            ) -> $search_ty<'a, 'b, L, H> {
-                $search_ty::new(self, dir)
-            }
-        }
-
-        pub use $mod::$search_ty;
-    };
+    /// Continue the search.
+    Search {
+        search: &'a SmallMap<FileNameBuf, DirectorySelector>,
+        name: Option<&'b FileName>,
+        entries: T::Entries,
+    },
 }
 
-impl_directory_search!(
-    UnorderedDirectorySearch,
-    Directory,
-    DirectoryEntries,
-    entries,
-    unordered_directory_search,
-    unordered_search,
-);
+pub struct Search<'a, 'b, T: WalkType<'b>> {
+    stack: Vec<SearchFrame<'a, 'b, T>>,
+}
 
-impl_directory_search!(
-    OrderedDirectorySearch,
-    Directory,
-    OrderedDirectoryEntries,
-    entries,
-    ordered_directory_search,
-    ordered_search,
-);
+impl<'a, 'b, T: WalkType<'b>> Search<'a, 'b, T> {
+    pub fn new(selector: &'a DirectorySelector, root: T::Directory) -> Self {
+        match selector {
+            DirectorySelector::Traverse(ref search) => Self {
+                stack: vec![SearchFrame::Search {
+                    search,
+                    name: None,
+                    entries: T::directory_entries(root).into(),
+                }],
+            },
+            DirectorySelector::Take => Self {
+                stack: vec![SearchFrame::ReturnRoot {
+                    root: Some(DirectoryEntry::Dir(root)),
+                }],
+            },
+        }
+    }
+}
 
-impl_directory_search!(
-    FingerprintedUnorderedDirectorySearch,
-    FingerprintedDirectory,
-    FingerprintedDirectoryEntries,
-    fingerprinted_entries,
-    fingerprinted_unordered_directory_search,
-    fingerprinted_unordered_search,
-);
+impl<'a, 'b, T: WalkType<'b>> DirectoryIterator for Search<'a, 'b, T> {
+    type PathStack = Self;
+    type Item =
+        Result<DirectoryEntry<T::Directory, &'b T::Leaf>, DirectorySearchError<&'b T::Leaf>>;
 
-impl_directory_search!(
-    FingerprintedOrderedDirectorySearch,
-    FingerprintedDirectory,
-    FingerprintedOrderedDirectoryEntries,
-    fingerprinted_entries,
-    fingerprinted_ordered_directory_search,
-    fingerprinted_ordered_search,
-);
+    fn next<'c>(&'c mut self) -> Option<(DirectoryIteratorPathAccessor<'c, Self>, Self::Item)> {
+        loop {
+            let frame = self.stack.last_mut()?;
+
+            match frame {
+                SearchFrame::ReturnRoot { root } => {
+                    let root = root.take()?;
+                    return Some((
+                        DirectoryIteratorPathAccessor {
+                            leaf: None,
+                            stack: self,
+                        },
+                        Ok(root),
+                    ));
+                }
+                SearchFrame::Search {
+                    search, entries, ..
+                } => {
+                    if let Some((name, entry)) = entries.next() {
+                        let search = search.get(name);
+
+                        match search {
+                            Some(DirectorySelector::Traverse(t)) => match entry {
+                                // Traverse into this directory ... assuming it's a directory :)
+                                DirectoryEntry::Dir(d) => {
+                                    self.stack.push(SearchFrame::Search {
+                                        name: Some(name),
+                                        search: t,
+                                        entries: T::directory_entries(d).into(),
+                                    });
+                                    continue;
+                                }
+                                DirectoryEntry::Leaf(leaf) => {
+                                    return Some((
+                                        DirectoryIteratorPathAccessor {
+                                            leaf: Some(name),
+                                            stack: self,
+                                        },
+                                        Err(DirectorySearchError::CannotTraverseLeaf { leaf }),
+                                    ));
+                                }
+                            },
+                            Some(DirectorySelector::Take) => {
+                                // Return the entry. Do not traverse further.
+                                return Some((
+                                    DirectoryIteratorPathAccessor {
+                                        leaf: Some(name),
+                                        stack: self,
+                                    },
+                                    Ok(entry),
+                                ));
+                            }
+                            None => {
+                                // Ignore this node entirely.
+                                continue;
+                            }
+                        }
+                    }
+                }
+            };
+
+            // We've exhausted this iterator. Go back to the previous stack frame.
+            self.stack.pop();
+        }
+    }
+}
+
+impl<'a, 'b, T: WalkType<'b>> DirectoryIteratorPathStack for Search<'a, 'b, T> {
+    fn path(&self) -> impl Iterator<Item = &FileName> {
+        self.stack.iter().filter_map(|frame| match frame {
+            SearchFrame::ReturnRoot { .. } => None,
+            SearchFrame::Search { name, .. } => name.as_deref(),
+        })
+    }
+}
+
+pub type UnorderedDirectorySearch<'a, 'b, L, H> =
+    Search<'a, 'b, UnorderedDirectoryWalkType<'b, L, H>>;
+pub type OrderedDirectorySearch<'a, 'b, L, H> = Search<'a, 'b, OrderedDirectoryWalkType<'b, L, H>>;
+pub type FingerprintedUnorderedDirectorySearch<'a, 'b, L, H> =
+    Search<'a, 'b, FingerprintedUnorderedDirectoryWalkType<'b, L, H>>;
+pub type FingerprintedOrderedDirectorySearch<'a, 'b, L, H> =
+    Search<'a, 'b, FingerprintedOrderedDirectoryWalkType<'b, L, H>>;
+
+impl DirectorySelector {
+    pub fn unordered_search<'a, 'b, L, H>(
+        &'a self,
+        dir: <UnorderedDirectoryWalkType<'b, L, H> as WalkType<'b>>::Directory,
+    ) -> UnorderedDirectorySearch<'a, 'b, L, H> {
+        UnorderedDirectorySearch::new(self, dir)
+    }
+
+    pub fn ordered_search<'a, 'b, L, H>(
+        &'a self,
+        dir: <OrderedDirectoryWalkType<'b, L, H> as WalkType<'b>>::Directory,
+    ) -> OrderedDirectorySearch<'a, 'b, L, H> {
+        OrderedDirectorySearch::new(self, dir)
+    }
+
+    pub fn fingerprinted_unordered_search<'a, 'b, L, H>(
+        &'a self,
+        dir: <FingerprintedUnorderedDirectoryWalkType<'b, L, H> as WalkType<'b>>::Directory,
+    ) -> FingerprintedUnorderedDirectorySearch<'a, 'b, L, H> {
+        FingerprintedUnorderedDirectorySearch::new(self, dir)
+    }
+
+    pub fn fingerprinted_ordered_search<'a, 'b, L, H>(
+        &'a self,
+        dir: <FingerprintedOrderedDirectoryWalkType<'b, L, H> as WalkType<'b>>::Directory,
+    ) -> FingerprintedOrderedDirectorySearch<'a, 'b, L, H> {
+        FingerprintedOrderedDirectorySearch::new(self, dir)
+    }
+}
