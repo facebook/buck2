c1c4037ad23484fe0ee4db781a9cec1d8f36a505
Ignacio Guridi <iguridi@meta.com>
2024-07-28 23:00:19 -0700

    buck2: explain: filter by category
    Summary:
    It's a nice entrypoint to the graph. Even if the user doesn't know anything about buck2, it allows them to start playing with it
    
    It's not our target audience, but it's cool
    
    Some nodes are left dangling. That's fixed in subsequent diffs
    
    Reviewed By: stepancheg
    
    Differential Revision: D60269525
    
    fbshipit-source-id: e71ba528a57cbf1c13a5de33afa00b1ef358660d

--- app/buck2_explain/js/src/GraphView.tsx
+++ app/buck2_explain/js/src/GraphView.tsx
@@ -7,7 +7,7 @@
  * of this source tree.
  */
 
-import React, {useContext, useRef} from 'react'
+import React, {useContext, useRef, useState} from 'react'
 import {Target} from './Target'
 import {DataContext} from './App'
 import ForceGraph2D, {LinkObject, NodeObject} from 'react-force-graph-2d'
@@ -16,12 +16,14 @@ import {Build} from './fbs/explain'
 type Node = {
   value: number
   deps: number[]
+  allow: boolean
 }
 
 function defaultNode(): Node {
   return {
     value: 0,
     deps: [],
+    allow: false,
   }
 }
 
@@ -34,7 +36,7 @@ export function GraphView(props: {view: string}) {
 
   // build better data structure
   let nodeMap = new Map<number, Node>()
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < build.targetsLength(); i++) {
     const target = build.targets(i)!
 
     // Create node object
@@ -55,17 +57,55 @@ export function GraphView(props: {view: string}) {
     }
   }
 
-  return <GraphImpl nodeMap={nodeMap} build={build} />
+  let categoriesCounter = new Map()
+  for (const [k, node] of nodeMap) {
+    const target = build.targets(k)
+    const type = target!.type()
+    categoriesCounter.set(type, (categoriesCounter.get(type) ?? 0) + 1)
+  }
+
+  let categoryOptions: {category: string; count: number; checked: false}[] = []
+  for (const [category, count] of categoriesCounter) {
+    categoryOptions.push({category, count, checked: false})
+  }
+
+  return <GraphImpl nodeMap={nodeMap} build={build} categoryOptions={categoryOptions} />
 }
 
 // Here it goes everything that should reload on user interaction
-function GraphImpl(props: {nodeMap: Map<number, Node>; build: Build}) {
-  const {nodeMap, build} = props
+function GraphImpl(props: {
+  nodeMap: Map<number, Node>
+  build: Build
+  categoryOptions: {category: string; count: number; checked: boolean}[]
+}) {
+  const {nodeMap, build, categoryOptions} = props
+
+  const [categories, setCategories] = useState(categoryOptions)
+
+  const toggleCategory = (id: number) => {
+    const c = [...categories]
+    c[id].checked = !c[id].checked
+    setCategories(c)
+  }
+
+  const activeCategories = categories.filter(v => v.checked).map(v => v.category)
+
+  for (const [k, node] of nodeMap) {
+    const target = build.targets(k)!
+    node.allow = activeCategories.includes(target.type()!)
+  }
+
+  let filteredNodes = new Map()
+  for (const [k, node] of nodeMap) {
+    if (node.allow) {
+      filteredNodes.set(k, node)
+    }
+  }
 
   const data: NodeObject[] = []
   const edges: LinkObject[] = []
 
-  for (const [k, _] of nodeMap) {
+  for (const [k, _] of filteredNodes) {
     const target = build.targets(k)!
 
     // Add nodes to graph
@@ -78,10 +118,10 @@ function GraphImpl(props: {nodeMap: Map<number, Node>; build: Build}) {
     })
   }
 
-  for (const [k, node] of nodeMap) {
+  for (const [k, node] of filteredNodes) {
     // Add edges
     for (const d of node.deps) {
-      if (nodeMap.has(d)) {
+      if (filteredNodes.has(d)) {
         edges.push({
           source: k,
           target: d,
@@ -93,7 +133,9 @@ function GraphImpl(props: {nodeMap: Map<number, Node>; build: Build}) {
   const graphRef = useRef<any>(null)
 
   return (
-    <ForceGraph2D
+    <>
+      <RuleTypeDropdown options={categories} handleCheckboxChange={toggleCategory} />
+      <ForceGraph2D
       ref={graphRef}
       graphData={{nodes: data, links: edges}}
       onNodeClick={(node, _event) => console.log(node.nodeVal, 'click!')}
@@ -106,5 +148,63 @@ function GraphImpl(props: {nodeMap: Map<number, Node>; build: Build}) {
       }}
       cooldownTime={2000}
     />
+    </>
+  )
+}
+
+function RuleTypeDropdown(props: {
+  options: {category: string; checked: boolean; count: number}[]
+  handleCheckboxChange: (i: number) => void
+}) {
+  const [dropdownActive, setDropdownActive] = useState(false)
+
+  return (
+    <div className={'dropdown ' + (dropdownActive ? 'is-active' : '')}>
+      <div className="dropdown-trigger">
+        <button
+          className="button"
+          aria-haspopup="true"
+          aria-controls="dropdown-menu"
+          onClick={() => setDropdownActive(!dropdownActive)}>
+          <span>Select rule types</span>
+          <span className="icon is-small">
+            <i className="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </div>
+      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          {props.options.map((v, index: number) => (
+            <CheckboxItem
+              key={index}
+              label={v.category}
+              count={v.count}
+              checked={v.checked}
+              handleCheckboxChange={() => props.handleCheckboxChange(index)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function CheckboxItem(props: {
+  label: string
+  count: number
+  checked: boolean
+  handleCheckboxChange: () => void
+}) {
+  return (
+    <div className="dropdown-item">
+      <label className="checkbox">
+        <input
+          checked={props.checked}
+          type="checkbox"
+          onChange={() => props.handleCheckboxChange()}
+        />
+        {' ' + props.label + ' (' + props.count + ')'}
+      </label>
+    </div>
   )
 }
