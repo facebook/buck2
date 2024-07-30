0045e600589c79fceb77db05726b129a4fcfdc25
Chris Hopman <cjhopman@meta.com>
2024-07-30 15:02:50 -0700

    Add directory for buck2_build_api::dynamic
    Summary: Will add a little bit more here.
    
    Reviewed By: stepancheg
    
    Differential Revision: D60138555
    
    fbshipit-source-id: 3f99a76072d02ac8d84c4f1eace11f62b0c3d7cc

--- app/buck2_build_api/src/dynamic.rs
+++ app/buck2_build_api/src/dynamic.rs
@@ -7,31 +7,4 @@
  * of this source tree.
  */
 
-use allocative::Allocative;
-use buck2_artifact::artifact::artifact_type::Artifact;
-use buck2_artifact::artifact::artifact_type::OutputArtifact;
-use buck2_artifact::deferred::id::DeferredId;
-use buck2_core::base_deferred_key::BaseDeferredKey;
-use buck2_util::late_binding::LateBinding;
-use indexmap::IndexSet;
-
-use crate::analysis::registry::AnalysisValueFetcher;
-use crate::deferred::types::DeferredRegistry;
-
-pub trait DynamicRegistryDyn: Allocative + 'static {
-    fn register(
-        &mut self,
-        dynamic: IndexSet<Artifact>,
-        outputs: IndexSet<OutputArtifact>,
-        registry: &mut DeferredRegistry,
-    ) -> anyhow::Result<DeferredId>;
-
-    fn ensure_bound(
-        self: Box<Self>,
-        registry: &mut DeferredRegistry,
-        analysis_value_fetcher: &AnalysisValueFetcher,
-    ) -> anyhow::Result<()>;
-}
-
-pub static DYNAMIC_REGISTRY_NEW: LateBinding<fn(BaseDeferredKey) -> Box<dyn DynamicRegistryDyn>> =
-    LateBinding::new("DYNAMIC_REGISTRY_NEW");
+pub mod registry;
