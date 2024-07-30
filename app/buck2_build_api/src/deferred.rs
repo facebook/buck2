109347c00718f4ee4f53bf9db51116cea7989fb0
Chris Hopman <cjhopman@meta.com>
2024-07-30 15:02:50 -0700

    Delete outdated comment
    Summary: The deferred framework is gone.
    
    Reviewed By: stepancheg
    
    Differential Revision: D60146245
    
    fbshipit-source-id: 9f05c497fdea16892d1818de498e900ef2cdfc4b

--- app/buck2_build_api/src/deferred.rs
+++ app/buck2_build_api/src/deferred.rs
@@ -7,15 +7,4 @@
  * of this source tree.
  */
 
-//! 'DeferredData's are deferred work registered by a rule implementation that can be stored in
-//! fields of providers.
-//!
-//! These are usually slow operations required to generate the information on 'Provider's.
-//! For example, information that depend on the execution of some actions, or information that
-//! require reading the filesystem.
-//!
-//! We model these asynchronous data to be stored on fields of 'Provider's as 'DeferredData'. Each
-//! 'DeferredData' will hold an 'DeferredKey'. That key that maps to an implementation of
-//! 'Deferred', which is the actual work to be ran when execution of the deferred is needed.
-
 pub mod calculation;
