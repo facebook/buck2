d62af836ffad52d9d462ed27a8da2e8359b5af40
Milen Dzhumerov <mgd@meta.com>
2024-08-14 15:18:29 -0700

    Fix package maker to be Linux compatible
    Summary: Handle symlinks to non-existing paths on Linux.
    
    Differential Revision: D61275616
    
    fbshipit-source-id: 9c97988f9846ca854c31020be6a7d76d1ab2ac5a

--- prelude/apple/tools/re_compatibility_utils/writable.py
+++ prelude/apple/tools/re_compatibility_utils/writable.py
@@ -8,8 +8,10 @@
 # pyre-strict
 
 import os
+import pathlib
 import platform
 import stat
+import sys
 
 
 def make_path_user_writable(path: str) -> None:
@@ -25,7 +27,23 @@ def make_path_user_writable(path: str) -> None:
     # Darwin supports permission setting on symlinks.
     follow_symlinks = platform.system() != "Darwin"
     st = os.stat(path, follow_symlinks=False)
-    os.chmod(path, st.st_mode | stat.S_IWUSR, follow_symlinks=follow_symlinks)
+
+    try:
+        os.chmod(path, st.st_mode | stat.S_IWUSR, follow_symlinks=follow_symlinks)
+    except FileNotFoundError as e:
+        path_obj = pathlib.Path(path)
+        if path_obj.is_symlink():
+            resolved_path_obj = path_obj.resolve()
+            if not resolved_path_obj.exists():
+                # On Linux systems, all symlinks are followed when `chmod`-ing
+                # (see comment above about `AT_SYMLINK_NOFOLLOW`). If that happens,
+                # we can ignore the `chmod` error as its harmless.
+                print(
+                    f"Tried setting permission on a symlink to a non-existing path, ignoring error... {e}",
+                    file=sys.stderr,
+                )
+                return
+        raise e
 
 
 def make_dir_recursively_writable(dir: str) -> None:
