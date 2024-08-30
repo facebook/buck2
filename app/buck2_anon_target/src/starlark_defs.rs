066934e3967030258b3629ad3334796bc2e80253
Will Li <minglunli@meta.com>
2024-08-30 12:48:58 -0700

    Keep anon_target artifacts error as starlark error
    Summary: My understanding is that errors from `starlark_module` should be kept as starlark errors wherever possible. So converting this one as it's easy to do
    
    Reviewed By: JakobDegen
    
    Differential Revision: D61931241
    
    fbshipit-source-id: b7aae30b83cd9010039b7dd132f7ff71cafec07d

--- app/buck2_anon_target/src/starlark_defs.rs
+++ app/buck2_anon_target/src/starlark_defs.rs
@@ -18,7 +18,6 @@ use buck2_build_api::interpreter::rule_defs::context::AnalysisActions;
 use buck2_build_api::interpreter::rule_defs::context::ANALYSIS_ACTIONS_METHODS_ANON_TARGET;
 use buck2_core::fs::paths::forward_rel_path::ForwardRelativePathBuf;
 use buck2_interpreter::downstream_crate_starlark_defs::REGISTER_BUCK2_ANON_TARGETS_GLOBALS;
-use buck2_interpreter::error::BuckStarlarkError;
 use buck2_interpreter::starlark_promise::StarlarkPromise;
 use buck2_interpreter_for_build::rule::FrozenArtifactPromiseMappings;
 use buck2_interpreter_for_build::rule::FrozenRuleCallable;
@@ -136,13 +135,13 @@ fn anon_target_methods(builder: &mut MethodsBuilder) {
     fn artifacts<'v>(
         this: &StarlarkAnonTarget<'v>,
         eval: &mut Evaluator<'v, '_, '_>,
-    ) -> anyhow::Result<Value<'v>> {
+    ) -> starlark::Result<Value<'v>> {
         Ok(eval.heap().alloc(Dict::new(
             this.artifacts
                 .iter()
                 .map(|(k, v)| {
                     Ok((
-                        k.to_value().get_hashed().map_err(BuckStarlarkError::new)?,
+                        k.to_value().get_hashed()?,
                         eval.heap().alloc(StarlarkPromiseArtifact::new(
                             eval.call_stack_top_location(),
                             v.clone(),
@@ -150,7 +149,7 @@ fn anon_target_methods(builder: &mut MethodsBuilder) {
                         )),
                     ))
                 })
-                .collect::<anyhow::Result<_>>()?,
+                .collect::<starlark::Result<_>>()?,
         )))
     }
 
