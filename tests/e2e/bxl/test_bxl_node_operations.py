0cbe7c672057abd64ebd53a5d4e8c7c60363e17e
Chenhao Zuo <nero@meta.com>
2024-09-06 00:49:58 -0700

    Add test for configured target node's node_attrs
    Summary: Add test for configured target node's  node_attrs
    
    Reviewed By: stepancheg
    
    Differential Revision: D61252679
    
    fbshipit-source-id: defe6964945a0b6106b4ac6f796791aa4bd3c148

--- tests/e2e/bxl/test_bxl_node_operations.py
+++ tests/e2e/bxl/test_bxl_node_operations.py
@@ -173,3 +173,10 @@ async def test_unconfigured_target_node_attrs(buck: Buck) -> None:
     await buck.bxl(
         "//bxl/unconfigure_target_node_attrs.bxl:concat_attrs_test",
     )
+
+
+@buck_test(inplace=False, data_dir="bxl/simple")
+async def test_configured_target_node_attrs(buck: Buck) -> None:
+    await buck.bxl(
+        "//bxl:configured_target_node_attrs.bxl:attrs_test",
+    )
