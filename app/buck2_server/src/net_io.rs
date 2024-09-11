b48c483ff6285e05a7c544b1d71604e5d5ad911b
Yury Samkevich <yurysamkevich@meta.com>
2024-09-11 11:42:57 -0700

    add network type net io stat
    Summary:
    We can determine network type on windows using WinAPI. On Mac and Linux it's a bit more complicated. For now we just determine it based on name. en0 for  mac is always WiFi.
    There is an API for MacOs `SCNetworkInterfaceGetInterfaceType` but we need to add dependency on Appple system-configuration framework, I'm going to do it later.
    
    Reviewed By: ezgicicek
    
    Differential Revision: D62440864
    
    fbshipit-source-id: 975e4e9026b29af6b49bdbbce5b9a7a600953989

--- app/buck2_server/src/net_io.rs
+++ app/buck2_server/src/net_io.rs
@@ -7,10 +7,19 @@
  * of this source tree.
  */
 
+#[derive(Copy, Clone, Debug)]
+pub enum NetworkKind {
+    WiFi,
+    Ethernet,
+    Unknown,
+}
+
 #[derive(Clone, Debug)]
-pub struct Counters {
+#[allow(dead_code)]
+pub struct NetworkStat {
     pub bytes_sent: u64,
     pub bytes_recv: u64,
+    pub network_kind: NetworkKind,
 }
 
 #[cfg(any(target_os = "macos", target_os = "linux"))]
@@ -44,25 +53,53 @@ mod collector {
         /// * If a new NIC appears between collection periods, we'll start keeping
         ///   track of it.
         /// * If a NIC *disappears*, then we stop reporting on its stats.
-        pub fn collect(&self) -> anyhow::Result<Option<HashMap<String, Counters>>> {
+        pub fn collect(&self) -> anyhow::Result<Option<HashMap<String, NetworkStat>>> {
             let mut collector = self.collector.lock().expect("poisoned lock");
             let counters: HashMap<_, _> = collector
                 .net_io_counters_pernic()
                 .context("collecting old counters")?
                 .into_iter()
-                .filter(|(s, _)| ["en", "eth"].iter().any(|prefix| s.starts_with(prefix)))
+                .filter(|(s, _)| {
+                    ["en", "eth", "wlan"]
+                        .iter()
+                        .any(|prefix| s.starts_with(prefix))
+                })
                 .map(|(nic, counters)| {
+                    let network_kind = NetworkKind::from_name(&nic);
                     (
                         nic,
-                        Counters {
+                        NetworkStat {
                             bytes_sent: counters.bytes_sent(),
                             bytes_recv: counters.bytes_recv(),
+                            network_kind,
                         },
                     )
                 })
                 .collect();
 
             Ok(Some(counters))
+        }
+    }
+
+    impl NetworkKind {
+        #[cfg(target_os = "macos")]
+        fn from_name(name: &str) -> NetworkKind {
+            match name {
+                // on macbook en0 is always WiFi
+                // TODO(yurysamkevich): properly detect device type using SCNetworkInterfaceGetInterfaceType Apple API
+                "en0" => NetworkKind::WiFi,
+                n if n.starts_with("en") => NetworkKind::Ethernet,
+                _ => NetworkKind::Unknown,
+            }
+        }
+
+        #[cfg(target_os = "linux")]
+        fn from_name(name: &str) -> NetworkKind {
+            match name {
+                n if n.starts_with("eth") => NetworkKind::Ethernet,
+                n if n.starts_with("wlan") => NetworkKind::WiFi,
+                _ => NetworkKind::Unknown,
+            }
         }
     }
 }
@@ -74,6 +111,7 @@ mod collector {
     use std::slice::from_raw_parts;
 
     use dupe::Dupe;
+    use winapi::shared::ipifcons;
     use winapi::shared::netioapi::FreeMibTable;
     use winapi::shared::netioapi::GetIfTable2;
     use winapi::shared::netioapi::MIB_IF_TABLE2;
@@ -100,7 +138,7 @@ mod collector {
             Self
         }
 
-        pub fn collect(&self) -> anyhow::Result<Option<HashMap<String, Counters>>> {
+        pub fn collect(&self) -> anyhow::Result<Option<HashMap<String, NetworkStat>>> {
             let mut counters = HashMap::new();
             let (_guard, entries) = unsafe {
                 let mut table: *mut MIB_IF_TABLE2 = std::ptr::null_mut();
@@ -134,11 +172,17 @@ mod collector {
                     .unwrap_or_else(|_| String::from("<Unknown>"));
                 let bytes_sent = entry.OutOctets;
                 let bytes_recv = entry.InOctets;
+                let network_kind = match entry.Type {
+                    ipifcons::IF_TYPE_ETHERNET_CSMACD => NetworkKind::Ethernet,
+                    ipifcons::IF_TYPE_IEEE80211 => NetworkKind::WiFi,
+                    _ => NetworkKind::Unknown,
+                };
                 counters.insert(
                     interface_name,
-                    Counters {
+                    NetworkStat {
                         bytes_sent,
                         bytes_recv,
+                        network_kind,
                     },
                 );
             }
@@ -164,7 +208,7 @@ mod collector {
             Self
         }
 
-        pub fn collect(&self) -> anyhow::Result<Option<HashMap<String, Counters>>> {
+        pub fn collect(&self) -> anyhow::Result<Option<HashMap<String, NetworkStat>>> {
             Ok(None)
         }
     }
