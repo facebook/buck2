f51f65ee4098e8fb3b6afdfae074a346e5ec3b1d
David Tolnay <dtolnay@meta.com>
2024-09-13 19:48:43 -0700

    Streaming subprocess stderr
    Summary:
    Previously, if stderr_to_file.py was killed before arriving to `shutil.copyfileobj(out, sys.stderr)`, we wouldn't see any stderr that the subprocess had printed so far. This doesn't make a difference for a successful `…[check]` subtarget artifact, but does make a difference to debugging when we rerun an action locally and may hit Ctrl+C.
    
    This diff changes to a streaming implementation so that subprocess stderr is copied in real time.
    
    Reviewed By: zertosh
    
    Differential Revision: D62670979
    
    fbshipit-source-id: fd792d88564153313b661cc23745b320ec02e76d

--- prelude/cxx/tools/stderr_to_file.py
+++ prelude/cxx/tools/stderr_to_file.py
@@ -11,11 +11,16 @@ Usage: stderr_to_file.py --out=path/to/output path/to/clang++ [args...]
 """
 
 import argparse
-import shutil
+import asyncio
+import signal
 import subprocess
 import sys
 from pathlib import Path
 from typing import List, NamedTuple
+
+
+# Exit code of `bash -c 'sleep 100'` <CTRL+C>
+_INTERRUPTED = 128 + signal.SIGINT.value
 
 
 class Args(NamedTuple):
@@ -23,19 +28,75 @@ class Args(NamedTuple):
     command: List[str]
 
 
-def main():
+class SubprocessProtocol(asyncio.SubprocessProtocol):
+    """Write subprocess stderr to both self.out and sys.stderr"""
+
+    def __init__(self, out, exit_future):
+        self.out = out
+        self.exit_future = exit_future
+        self.pipe_closed = False
+        self.exited = False
+
+    def pipe_data_received(self, fd, data):
+        if fd == sys.stderr.fileno():
+            # Blocking write to file. This is buffered in a Python
+            # io.BufferedRandom.
+            self.out.write(data)
+            # Blocking unbuffered write to stderr. Our writes will be exactly as
+            # buffered as the subprocess's writes.
+            sys.stderr.buffer.write(data)
+            sys.stderr.flush()
+
+    def pipe_connection_lost(self, fd, exc):
+        if fd == sys.stderr.fileno():
+            self.pipe_closed = True
+            # Either of pipe_connection_lost() or process_exited() can be called
+            # before the other. Wait until both methods are called.
+            self._check_for_exit()
+
+    def process_exited(self):
+        self.exited = True
+        # Either of pipe_connection_lost() or process_exited() can be called
+        # before the other. Wait until both methods are called.
+        self._check_for_exit()
+
+    def _check_for_exit(self):
+        if self.pipe_closed and self.exited:
+            try:
+                self.exit_future.set_result(True)
+            except asyncio.InvalidStateError:
+                # Event loop has shut down.
+                pass
+
+
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--out", type=Path, required=True)
     parser.add_argument("command", nargs=argparse.REMAINDER)
     args = Args(**vars(parser.parse_args()))
 
-    with open(args.out, "w+") as out:
-        ret = subprocess.call(args.command, stderr=out)
-        out.seek(0)
-        shutil.copyfileobj(out, sys.stderr)
+    loop = asyncio.get_running_loop()
+    exit_future = asyncio.Future(loop=loop)
 
-    sys.exit(ret)
+    with open(args.out, "wb+") as out:
+        transport, protocol = await loop.subprocess_exec(
+            lambda: SubprocessProtocol(out, exit_future),
+            *args.command,
+            stdin=None,  # inherit
+            stdout=None,  # inherit
+            stderr=subprocess.PIPE,
+        )
+        await exit_future
+        transport.close()
+
+    returncode = transport.get_returncode()
+    if returncode is None:
+        return _INTERRUPTED
+    else:
+        return returncode
 
 
-if __name__ == "__main__":
-    main()
+try:
+    sys.exit(asyncio.run(main()))
+except KeyboardInterrupt:
+    sys.exit(_INTERRUPTED)
