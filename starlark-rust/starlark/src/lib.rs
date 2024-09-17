d0660b6e0547abf700d2ee293a54299210e1363a
Stiopa Koltsov <nga@meta.com>
2024-09-17 15:54:45 -0700

    Promote __derive_refs to a separate file
    Summary: To add more code there in D62728487.
    
    Reviewed By: IanChilds
    
    Differential Revision: D62728488
    
    fbshipit-source-id: 7f08c4585b2be811a80021bad6062d89cccf227d

--- starlark-rust/starlark/src/lib.rs
+++ starlark-rust/starlark/src/lib.rs
@@ -474,15 +474,4 @@ pub mod __macro_refs {
     pub use crate::coerce::coerce;
 }
 
-/// __derive_refs allows us to reference other crates in starlark_derive without users needing to be
-///  aware of those dependencies. We make them public here and then can reference them like
-///  `starlark::__derive_refs::foo`.
-#[doc(hidden)]
-pub mod __derive_refs {
-    pub mod serde {
-        pub use serde::ser::Error;
-        pub use serde::Serialize;
-        pub use serde::Serializer;
-    }
-    pub use inventory;
-}
+pub mod __derive_refs;
