1cca3a0bf8c38fdfab5139340eb9cb05da4c18ee
Alexey Kozhevnikov <akozhevnikov@meta.com>
2024-09-24 15:45:17 -0700

    minor simplification
    Summary: remove code duplication
    
    Reviewed By: samkevich
    
    Differential Revision: D63320137
    
    fbshipit-source-id: ce76149be9feb0eef4b27a2446596ee34ae212c5

--- app/buck2_execute_impl/src/executors/hybrid.rs
+++ app/buck2_execute_impl/src/executors/hybrid.rs
@@ -212,12 +212,8 @@ where
 
         let weight = match command.request.host_sharing_requirements() {
             HostSharingRequirements::ExclusiveAccess => self.low_pass_filter.capacity(),
-            HostSharingRequirements::OnePerToken(.., class) => self
-                .local
-                .host_sharing_broker
-                .requested_permits(class)
-                .into_count_uncapped(),
-            HostSharingRequirements::Shared(class) => self
+            HostSharingRequirements::OnePerToken(.., class)
+            | HostSharingRequirements::Shared(class) => self
                 .local
                 .host_sharing_broker
                 .requested_permits(class)
