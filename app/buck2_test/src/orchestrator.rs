003b58df5050de4a13e385da18c26c057ae57039
Andreas Herrmann <andreas.herrmann@tweag.io>
2024-09-25 16:43:30 +0200

    Implement support for remote persistent workers

--- app/buck2_test/src/orchestrator.rs
+++ app/buck2_test/src/orchestrator.rs
@@ -1171,6 +1171,7 @@ impl<'b> BuckTestOrchestrator<'b> {
             options: CommandGenerationOptions {
                 path_separator: PathSeparatorKind::system_default(),
                 output_paths_behavior: Default::default(),
+                use_remote_persistent_workers: false,
             },
         };
         let CommandExecutorResponse {
@@ -1698,6 +1699,7 @@ impl<'a> Execute2RequestExpander<'a> {
                     exe: worker_rendered,
                     id: WorkerId(worker.id),
                     concurrency: worker.concurrency(),
+                    remote_key: None,
                 })
             }
             _ => None,
