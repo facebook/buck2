8dfa9d9e97548ebf0eacf6b7913a76db18dde6f4
Stiopa Koltsov <nga@meta.com>
2024-09-27 16:13:36 -0700

    Make transition test isolated
    Summary: One of several.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D63500342
    
    fbshipit-source-id: 86b37e9c2330d58923128b88a26ed5ae5fe47c47

--- tests/e2e/configurations/transition/test_rule.py
+++ tests/e2e/configurations/transition/test_rule.py
@@ -19,12 +19,10 @@ def _replace_hash(s: str) -> str:
     return re.sub(r"\b[0-9a-f]{16}\b", "<HASH>", s)
 
 
-@buck_test(inplace=True)
+@buck_test(inplace=False)
 async def test_configuration_transition_rule_cquery(buck: Buck) -> None:
     # For the reference, cquery output is: P467297091. Note the "forward" node.
-    result = await buck.cquery(
-        "deps(fbcode//buck2/tests/targets/configurations/transition/rule:the-test)"
-    )
+    result = await buck.cquery("deps(root//:the-test)")
     result.check_returncode()
     # Watchos resource should be present twice: as forward and as transitioned.
     assert result.stdout.count(":watchos-resource") == 2
@@ -32,19 +30,19 @@ async def test_configuration_transition_rule_cquery(buck: Buck) -> None:
     assert result.stdout.count(":default-resource") == 1
 
 
-@buck_test(inplace=True)
+@buck_test(inplace=False)
 async def test_configuration_transition_rule_cquery_actual_attr(buck: Buck) -> None:
     result = await buck.cquery(
-        "--target-platforms=fbcode//buck2/tests/targets/configurations/transition/rule:iphoneos-p",
-        "fbcode//buck2/tests/targets/configurations/transition/rule:watchos-resource",
+        "--target-platforms=root//:iphoneos-p",
+        "root//:watchos-resource",
         "--output-attribute=actual",
     )
     result.check_returncode()
     q = json.loads(result.stdout)
 
     # Each key in the JSON output is a different configuration of the same rule `watchos-resource`
-    configuration_default = "fbcode//buck2/tests/targets/configurations/transition/rule:watchos-resource (<transitioned-to-watch>#<HASH>)"
-    configuration_transition = "fbcode//buck2/tests/targets/configurations/transition/rule:watchos-resource (fbcode//buck2/tests/targets/configurations/transition/rule:iphoneos-p#<HASH>)"
+    configuration_default = "root//:watchos-resource (<transitioned-to-watch>#<HASH>)"
+    configuration_transition = "root//:watchos-resource (root//:iphoneos-p#<HASH>)"
     configurations = [_replace_hash(c) for c in q.keys()]
     assert configuration_default in configurations
     assert configuration_transition in configurations
@@ -61,10 +59,8 @@ async def test_configuration_transition_rule_cquery_actual_attr(buck: Buck) -> N
     assert config_transition_has_no_attributes
 
 
-@buck_test(inplace=True)
+@buck_test(inplace=False)
 async def test_configuration_transition_rule_build(buck: Buck) -> None:
     # Rule implementations do the assertions.
-    result = await buck.build(
-        "fbcode//buck2/tests/targets/configurations/transition/rule:the-test"
-    )
+    result = await buck.build("root//:the-test")
     result.check_returncode()
