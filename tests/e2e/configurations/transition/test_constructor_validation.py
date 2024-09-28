31627d35491a68169bd85e2cced7e18f1096aa5e
Stiopa Koltsov <nga@meta.com>
2024-09-27 18:11:18 -0700

    Test for validation of transition() call
    Summary:
    We validate `transition()` function construction parameter names. Which is helpful.
    
    I'm thinking whether we can also validate types.
    
    I'm experimenting here to use this validation machinery in dynamic actions impl.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D63502000
    
    fbshipit-source-id: c89a3f8f64606767322eebc7049181a185bcfdce

---
+++ tests/e2e/configurations/transition/test_constructor_validation.py
@@ -0,0 +1,26 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+# pyre-strict
+
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.asserts import expect_failure
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test(inplace=False)
+async def test_construction_validation_good(buck: Buck) -> None:
+    await buck.targets("//good:")
+
+
+@buck_test(inplace=False)
+async def test_construction_validation_bad(buck: Buck) -> None:
+    await expect_failure(
+        buck.targets("//bad:"),
+        stderr_regex="`impl` parameter for `transition` call must be a function with",
+    )
