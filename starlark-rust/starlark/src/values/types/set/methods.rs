307a33fc52940fee321d8d226f2d32e58aa445cb
Roman Perehonchuk <romanp@meta.com>
2024-10-01 02:42:33 -0700

    union
    Summary: set([1,2]).union(set([2,3])) == set([1,2,3])
    
    Reviewed By: stepancheg
    
    Differential Revision: D59967392
    
    fbshipit-source-id: 4e5d642ace435bbdc4451c0c36aadb8c2469ffb9

--- starlark-rust/starlark/src/values/types/set/methods.rs
+++ starlark-rust/starlark/src/values/types/set/methods.rs
@@ -23,7 +23,12 @@ use crate as starlark;
 use crate::environment::MethodsBuilder;
 use crate::values::none::NoneType;
 use crate::values::set::refs::SetMut;
+use crate::values::set::refs::SetRef;
+use crate::values::set::value::SetData;
+use crate::values::typing::StarlarkIter;
+use crate::values::Heap;
 use crate::values::Value;
+use crate::values::ValueOfUnchecked;
 
 #[starlark_module]
 pub(crate) fn set_methods(builder: &mut MethodsBuilder) {
@@ -31,6 +36,30 @@ pub(crate) fn set_methods(builder: &mut MethodsBuilder) {
         let mut this = SetMut::from_value(this)?;
         this.clear();
         Ok(NoneType)
+    }
+
+    /// Return a new set with elements from the set and all others.
+    /// Unlike Python does not support variable number of arguments.
+    /// ```
+    /// # starlark::assert::is_true(r#"
+    /// x = set([1, 2, 3])
+    /// y = [3, 4, 5]
+    /// x.union(y) == set([1, 2, 3, 4, 5])
+    /// # "#);
+    /// ```
+    fn union<'v>(
+        this: SetRef<'v>,
+        #[starlark(require=pos)] other: ValueOfUnchecked<'v, StarlarkIter<Value<'v>>>,
+        heap: &'v Heap,
+    ) -> starlark::Result<SetData<'v>> {
+        let it = other.get().iterate(heap)?;
+        // TODO(romanp) omptimize if this is empty
+        let mut data = this.content.clone();
+        for elem in it {
+            let hashed = elem.get_hashed()?;
+            data.insert_hashed(hashed);
+        }
+        Ok(SetData { content: data })
     }
 }
 #[cfg(test)]
@@ -75,5 +104,28 @@ mod tests {
     #[test]
     fn test_bool_false() {
         assert::is_false("bool(set())")
+    }
+
+    #[test]
+    fn test_union() {
+        assert::eq(
+            "set([1, 2, 3]).union(set([3, 4, 5]))",
+            "set([1, 2, 3, 4, 5])",
+        )
+    }
+
+    #[test]
+    fn test_union_empty() {
+        assert::eq("set([1, 2, 3]).union(set([]))", "set([1, 2, 3])")
+    }
+
+    #[test]
+    fn test_union_iter() {
+        assert::eq("set([1, 2, 3]).union([3, 4])", "set([1, 2, 3, 4])")
+    }
+
+    #[test]
+    fn test_union_ordering_mixed() {
+        assert::eq("list(set([1, 3, 5]).union(set([4, 3])))", "[1, 3, 5, 4]");
     }
 }
--- tests/e2e/docs/test_builtin_docs_data/buck2-golden-docs/standard/set.md
+++ tests/e2e/docs/test_builtin_docs_data/buck2-golden-docs/standard/set.md
@@ -2,3 +2,4 @@
 
 # `set` type
 ## set.clear
+## set.union
