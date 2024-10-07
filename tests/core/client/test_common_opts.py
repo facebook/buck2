42f387d4beda14bfad5f6c639018dbc8d072f418
Jakob Degen <jakobdegen@meta.com>
2024-10-07 12:49:52 -0700

    tests: Move `test_common_opts` to `core/client`
    Summary: And isolate them better. One of these tests might be broken
    
    Reviewed By: IanChilds
    
    Differential Revision: D63964416
    
    fbshipit-source-id: d36376494a2f9961910ac61430a9e72cd02caf90

--- tests/core/client/test_common_opts.py
+++ tests/core/client/test_common_opts.py
@@ -17,7 +17,7 @@ from buck2.tests.e2e_util.asserts import expect_failure
 from buck2.tests.e2e_util.buck_workspace import buck_test
 
 
-@buck_test(inplace=False, data_dir="bxl/simple")
+@buck_test()
 @pytest.mark.parametrize(  # type: ignore
     "cmd",
     ["build", "targets", "cquery", "bxl", "uquery"],
@@ -31,7 +31,7 @@ async def test_write_uuid(buck: Buck, cmd: str) -> None:
         assert len(file.read()) > 0
 
 
-@buck_test(inplace=False, data_dir="bxl/simple")
+@buck_test()
 @pytest.mark.parametrize(  # type: ignore
     "cmd",
     ["build", "targets", "cquery", "bxl", "uquery"],
