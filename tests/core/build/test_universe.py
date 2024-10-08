38f3084c970ac54ebed8e2708804733eb474849a
Jakob Degen <jakobdegen@meta.com>
2024-10-08 10:19:07 -0700

    tests: Move `test_build_universe` to `core`
    Summary: Isolate and rename it to `build_universe` in the process. Also fix it on Windows
    
    Reviewed By: IanChilds
    
    Differential Revision: D64019970
    
    fbshipit-source-id: b3f10d5e6c158e03dbd425e7b18e7b520d12d3fc

--- tests/core/build/test_universe.py
+++ tests/core/build/test_universe.py
@@ -12,34 +12,20 @@ from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.buck_workspace import buck_test
 
 
-@buck_test(inplace=True)
-async def test_dummy_to_make_this_file_not_empty_on_windows(buck: Buck) -> None:
-    pass
-
-
-# TODO(nga): fix and enable on Windows.
-@buck_test(inplace=True, skip_for_os=["windows"])
+@buck_test()
 async def test_build_universe(buck: Buck) -> None:
     # Run the build without universe.
-    result = await buck.build(
-        "fbcode//buck2/tests/e2e/build/test_build_universe_data:test"
-    )
+    result = await buck.build("//:test")
     build_report = result.get_build_report()
-
-    output = build_report.output_for_target(
-        "fbcode//buck2/tests/e2e/build/test_build_universe_data:test"
-    )
+    output = build_report.output_for_target("//:test")
     assert output.read_text().rstrip() == "default"
 
     # Now build the same target, but with the universe.
     result = await buck.build(
-        "fbcode//buck2/tests/e2e/build/test_build_universe_data:test",
+        "//:test",
         "--target-universe",
-        "fbcode//buck2/tests/e2e/build/test_build_universe_data:universe",
+        "//:universe",
     )
     build_report = result.get_build_report()
-
-    output = build_report.output_for_target(
-        "fbcode//buck2/tests/e2e/build/test_build_universe_data:test"
-    )
+    output = build_report.output_for_target("//:test")
     assert output.read_text().rstrip() == "cat"
