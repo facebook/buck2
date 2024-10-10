33ec7363d587a0b169816ebbe011ca43f676a1af
Roman Perehonchuk <romanp@meta.com>
2024-10-10 14:21:40 -0700

    target_modifiers_attr test
    Summary: context: https://fb.workplace.com/groups/3454295018049629/posts/3454310088048122/?comment_id=3454542898024841
    
    Reviewed By: scottcao
    
    Differential Revision: D63836517
    
    fbshipit-source-id: f4b1d0d35828cc916d302205516b2bf779e4fe32

---
+++ tests/e2e/configurations/cfg_constructor/test_cfg_modifiers_attr.py
@@ -0,0 +1,25 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+# pyre-strict
+
+# import json
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.asserts import expect_failure
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test(inplace=False)
+async def test_cfg_modifiers_attr(buck: Buck) -> None:
+    await expect_failure(
+        buck.targets(
+            "root//:test",
+            "--output-attribute=modifiers",
+        ),
+        stderr_regex=r"Found `modifiers` extra named parameter\(s\) for call to test_rule",
+    )
