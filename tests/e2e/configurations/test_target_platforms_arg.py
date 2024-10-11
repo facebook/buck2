172d1d9eceae462e6809907f4a73d4dfa93b6ec5
Jakob Degen <jakobdegen@meta.com>
2024-10-10 21:31:39 -0700

    tests: Isolate `test_target_platform_arg`
    Summary: ^
    
    Reviewed By: IanChilds
    
    Differential Revision: D64161772
    
    fbshipit-source-id: f9e4f5e8b1e2f3ae5daef8410746deda81f24230

--- tests/e2e/configurations/test_target_platforms_arg.py
+++ tests/e2e/configurations/test_target_platforms_arg.py
@@ -9,24 +9,25 @@
 
 
 import re
+from pathlib import Path
 
 from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.buck_workspace import buck_test
 
 
-@buck_test(inplace=True)
+@buck_test(inplace=False)
 async def test_target_platforms_arg(buck: Buck) -> None:
     out = await buck.cquery(
         # Specifying platform without cell to make sure it is resolved against current cell
-        "--target-platforms=//buck2/tests/targets/configurations/target_platforms_arg:p-clouds",
-        "deps(fbcode//buck2/tests/targets/configurations/target_platforms_arg:the-test, 1)",
+        "--target-platforms=//:p-clouds",
+        "deps(//:the-test, 1)",
+        rel_cwd=Path("subcell"),
     )
-    out.check_returncode()
     stdout = re.sub(":p-clouds#[a-f0-9]+\\)", ":p-clouds#HASH)", out.stdout)
     assert (
         stdout
         == """\
-fbcode//buck2/tests/targets/configurations/target_platforms_arg:the-test (fbcode//buck2/tests/targets/configurations/target_platforms_arg:p-clouds#HASH)
-fbcode//buck2/tests/targets/configurations/target_platforms_arg:t-clouds (fbcode//buck2/tests/targets/configurations/target_platforms_arg:p-clouds#HASH)
+subcell//:the-test (subcell//:p-clouds#HASH)
+subcell//:t-clouds (subcell//:p-clouds#HASH)
 """
     )
