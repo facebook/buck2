8cb8e6c081de865b97f1071572599068ea421bfb
Rajneesh Lakkundi <rajneeshl@meta.com>
2024-10-21 00:51:55 -0700

    Add API to compute percentage completion
    Summary: Estimate the completion percentage for BuildKey to show cache warning. The BuildKey doesn't have a 1:1 correspondence with the action count. However, using the DICE context as an indicator of completion progress should serve the purpose.
    
    Reviewed By: ezgicicek
    
    Differential Revision: D64462964
    
    fbshipit-source-id: dbe06e727289defcbd3e08b32841fa1d866943e6

--- app/buck2_event_observer/src/pending_estimate.rs
+++ app/buck2_event_observer/src/pending_estimate.rs
@@ -28,3 +28,152 @@ pub fn pending_estimate<T: SpanTrackable>(roots: &Roots<T>, dice: &DiceState) ->
 
     total
 }
+
+pub fn estimate_completion_percentage<T: SpanTrackable>(roots: &Roots<T>, dice: &DiceState) -> u8 {
+    let from_dice = dice
+        .key_states()
+        .get("BuildKey")
+        .map_or((0, 0), |v| (v.started, v.finished));
+
+    let from_roots = roots.dice_counts().get("BuildKey").copied().unwrap_or(0);
+
+    let started = u64::from(from_dice.0).saturating_sub(from_roots);
+    let finished = u64::from(from_dice.1).saturating_sub(from_roots);
+    if started == 0 {
+        // Avoid divide by zero.
+        return 0;
+    }
+    ((finished as f64 / started as f64) * 100f64) as u8
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use std::sync::Arc;
+    use std::time::Instant;
+    use std::time::UNIX_EPOCH;
+
+    use buck2_data::SpanStartEvent;
+    use buck2_events::span::SpanId;
+    use buck2_events::BuckEvent;
+    use buck2_wrapper_common::invocation_id::TraceId;
+
+    use crate::dice_state::DiceState;
+    use crate::pending_estimate::estimate_completion_percentage;
+    use crate::span_tracker::BuckEventSpanTracker;
+
+    fn setup_roots(tracker: &mut BuckEventSpanTracker) {
+        let t0 = Instant::now();
+        let span = Arc::new(BuckEvent::new(
+            UNIX_EPOCH,
+            TraceId::new(),
+            Some(SpanId::next()),
+            None,
+            SpanStartEvent {
+                data: Some(
+                    buck2_data::ActionExecutionStart {
+                        key: Some(buck2_data::ActionKey {
+                            id: Default::default(),
+                            owner: Some(buck2_data::action_key::Owner::TargetLabel(
+                                buck2_data::ConfiguredTargetLabel {
+                                    label: Some(buck2_data::TargetLabel {
+                                        package: "pkg".into(),
+                                        name: "target".into(),
+                                    }),
+                                    configuration: Some(buck2_data::Configuration {
+                                        full_name: "conf".into(),
+                                    }),
+                                    execution_configuration: None,
+                                },
+                            )),
+                            key: "".to_owned(),
+                        }),
+                        name: Some(buck2_data::ActionName {
+                            category: "category".into(),
+                            identifier: "identifier".into(),
+                        }),
+                        kind: buck2_data::ActionKind::NotSet as i32,
+                    }
+                    .into(),
+                ),
+            }
+            .into(),
+        ));
+        tracker.start_at(&span, t0).unwrap();
+    }
+
+    fn setup_dice_state(dice_state: &mut DiceState, finished: u32, total: u32) {
+        dice_state.update(&buck2_data::DiceStateSnapshot {
+            key_states: {
+                let mut map = HashMap::new();
+                map.insert(
+                    "BuildKey".to_owned(),
+                    buck2_data::DiceKeyState {
+                        started: total,
+                        finished,
+                        check_deps_started: 0,
+                        check_deps_finished: 0,
+                        compute_started: 0,
+                        compute_finished: 0,
+                    },
+                );
+                map
+            },
+        });
+    }
+
+    #[test]
+    fn test_completion_no_progress() -> anyhow::Result<()> {
+        let mut dice = DiceState::new();
+        let mut tracker = BuckEventSpanTracker::new();
+
+        setup_roots(&mut tracker);
+        setup_dice_state(&mut dice, 0, 100);
+        assert_eq!(estimate_completion_percentage(tracker.roots(), &dice), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_completion_percentage_build_complete() -> anyhow::Result<()> {
+        let mut dice = DiceState::new();
+        let mut tracker = BuckEventSpanTracker::new();
+
+        setup_roots(&mut tracker);
+        setup_dice_state(&mut dice, 100, 100);
+        assert_eq!(estimate_completion_percentage(tracker.roots(), &dice), 100);
+        Ok(())
+    }
+
+    #[test]
+    fn test_completion_percentage_intermediate_state() -> anyhow::Result<()> {
+        let mut dice = DiceState::new();
+        let mut tracker = BuckEventSpanTracker::new();
+
+        setup_roots(&mut tracker);
+        // 26/101 -> 25/100 since we have 1 subtracted for the ActionExecutionStart
+        setup_dice_state(&mut dice, 26, 101);
+        assert_eq!(estimate_completion_percentage(tracker.roots(), &dice), 25);
+        Ok(())
+    }
+
+    #[test]
+    fn test_completion_percentage_invalid_dice_state() -> anyhow::Result<()> {
+        let mut dice = DiceState::new();
+        let mut tracker = BuckEventSpanTracker::new();
+
+        setup_roots(&mut tracker);
+        setup_dice_state(&mut dice, 10, 0);
+        assert_eq!(estimate_completion_percentage(tracker.roots(), &dice), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_completion_percentage_empty_span() -> anyhow::Result<()> {
+        let mut dice = DiceState::new();
+        let tracker = BuckEventSpanTracker::new();
+
+        setup_dice_state(&mut dice, 26, 101);
+        assert_eq!(estimate_completion_percentage(tracker.roots(), &dice), 25);
+        Ok(())
+    }
+}
