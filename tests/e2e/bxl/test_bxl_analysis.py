81a3ea53a5d2786f14d6b4abbfda838241ceb0db
Jakob Degen <jakobdegen@meta.com>
2024-10-22 01:35:52 -0700

    tests: Data dir for `test_bxl_analysis`
    Summary: ^
    
    Reviewed By: IanChilds
    
    Differential Revision: D64726818
    
    fbshipit-source-id: 8526797ae9ef51da907c321510556090e1f165ef

--- tests/e2e/bxl/test_bxl_analysis.py
+++ tests/e2e/bxl/test_bxl_analysis.py
@@ -7,61 +7,52 @@
 
 # pyre-strict
 
-
-import re
-
 from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.asserts import expect_failure
 from buck2.tests.e2e_util.buck_workspace import buck_test
 
 
-def _replace_hash(s: str) -> str:
-    return re.sub(r"\b[0-9a-f]{16}\b", "<HASH>", s)
-
-
-@buck_test(inplace=False, data_dir="bxl/simple")
+@buck_test(inplace=False)
 async def test_bxl_analysis(buck: Buck) -> None:
-
     result = await buck.bxl(
-        "//bxl/analysis.bxl:providers_test",
+        "//analysis.bxl:providers_test",
     )
 
     lines = result.stdout.splitlines()
-    assert "the_binary_foo" in lines[0]
-    assert "bin_foo" in lines[1]
-    assert "the_binary_foo" in lines[2]
+    assert "provides_foo_foo" in lines[0]
+    assert "provides_foo_foo" in lines[1]
 
     result = await buck.bxl(
-        "//bxl/analysis.bxl:dependency_test",
+        "//analysis.bxl:dependency_test",
     )
 
-    assert _replace_hash(result.stdout).splitlines() == [
+    assert result.stdout.splitlines() == [
         "dependency",
-        "root//bin:the_binary (root//platforms:platform1#<HASH>)",
+        "root//:stub (<unspecified>)",
     ]
 
 
-@buck_test(inplace=False, data_dir="bxl/simple", allow_soft_errors=True)
+@buck_test(inplace=False, allow_soft_errors=True)
 async def test_bxl_analysis_incompatible_targets_list(buck: Buck) -> None:
     # multiple incompatible targets should be skipped and the analysis should return empty dict
-    result = await buck.bxl("//bxl/analysis.bxl:incompatible_targets")
+    result = await buck.bxl("//analysis.bxl:incompatible_targets")
     assert "Skipping target incompatible node" in result.stderr
-    assert "root//incompatible_targets:incompatible" in result.stderr
+    assert "root//:incompatible_target" in result.stderr
     assert "{}" == result.stdout.strip()
 
 
-@buck_test(inplace=False, data_dir="bxl/simple", allow_soft_errors=True)
+@buck_test(inplace=False, allow_soft_errors=True)
 async def test_bxl_analysis_incompatible_targets_single(buck: Buck) -> None:
     # single incompatible target should be skipped and the analysis should return None
-    result = await buck.bxl("//bxl/analysis.bxl:incompatible_targets_single")
+    result = await buck.bxl("//analysis.bxl:incompatible_targets_single")
     assert "Skipping target incompatible node" in result.stderr
-    assert "root//incompatible_targets:incompatible" in result.stderr
+    assert "root//:incompatible_target" in result.stderr
     assert "None" == result.stdout.strip()
 
 
-@buck_test(inplace=False, data_dir="bxl/simple")
+@buck_test(inplace=False)
 async def test_bxl_analysis_missing_subtarget(buck: Buck) -> None:
     await expect_failure(
-        buck.bxl("//bxl/analysis.bxl:missing_subtarget_test"),
+        buck.bxl("//analysis.bxl:missing_subtarget_test"),
         stderr_regex="requested sub target named `missing_subtarget` .* is not available",
     )
