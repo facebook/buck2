e43e524efe0238df04954a6305592862920b95e9
Jakob Degen <jakobdegen@meta.com>
2024-10-22 01:35:52 -0700

    tests: Isolate `test_bxl_audit`
    Summary:
    Remove the dynamic output test in the process - that is well covered by the standard `audit output` test and does not need a BXL-specific test.
    
    Also switch to doing assertions within bxl
    
    Reviewed By: IanChilds
    
    Differential Revision: D64726824
    
    fbshipit-source-id: ebb56f2b75e8af622c70dc6c0523a933fa3da05a

--- tests/e2e/bxl/test_bxl_audit.py
+++ tests/e2e/bxl/test_bxl_audit.py
@@ -11,33 +11,20 @@ from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.asserts import expect_failure
 from buck2.tests.e2e_util.buck_workspace import buck_test
 
-from buck2.tests.e2e_util.helper.utils import is_running_on_linux
 
-
-@buck_test(inplace=True)
+@buck_test(inplace=False)
 async def test_bxl_audit_output(buck: Buck) -> None:
-    result = await buck.bxl(
-        "//buck2/tests/targets/bxl/simple/bxl/audit.bxl:audit_output_action_exists",
+    await buck.bxl(
+        "//audit.bxl:audit_output_action_exists",
     )
-    assert "fbcode//buck2/tests/targets/rules/shell:gen" in result.stdout
-    assert "ovr_config//platform/linux:x86_64-fbcode-platform" in result.stdout
 
-    if is_running_on_linux():
-        result = await buck.bxl(
-            "//buck2/tests/targets/bxl/simple/bxl/audit.bxl:audit_output_dynamic_action_exists",
-        )
-        assert "fbcode//buck2/tests/targets/rules/ocaml:native" in result.stdout
-        assert "ovr_config//platform/linux:x86_64-fbcode-platform" in result.stdout
-
-    result = await buck.bxl(
-        "//buck2/tests/targets/bxl/simple/bxl/audit.bxl:audit_output_config_not_match",
+    await buck.bxl(
+        "//audit.bxl:audit_output_config_not_match",
     )
-    # we should get the unconfigured target label
-    assert "fbcode//buck2/tests/targets/rules/shell:gen\n" == result.stdout
 
     await expect_failure(
         buck.bxl(
-            "//buck2/tests/targets/bxl/simple/bxl/audit.bxl:audit_output_invalid_path",
+            "//audit.bxl:audit_output_invalid_path",
         ),
         stderr_regex="Malformed buck-out path",
     )
