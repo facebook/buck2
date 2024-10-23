81509feef12610c3fd6e15f983288a9b29d18762
Jakob Degen <jakobdegen@meta.com>
2024-10-22 18:46:36 -0700

    docs: Move markdown tests
    Summary: To a file `markdown.rs`. Makes it clear that we have no tests for starlark code output
    
    Reviewed By: iguridi
    
    Differential Revision: D61778023
    
    fbshipit-source-id: a2b287f1dfa897b7636383f203f5fac84855a665

---
+++ starlark-rust/starlark/src/docs/tests/markdown.rs
@@ -0,0 +1,293 @@
+/*
+ * Copyright 2018 The Starlark in Rust Authors.
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use allocative::Allocative;
+use derive_more::Display;
+use serde::Serialize;
+use starlark_derive::starlark_module;
+use starlark_derive::starlark_value;
+use starlark_derive::NoSerialize;
+use starlark_map::small_map::SmallMap;
+use starlark_syntax::golden_test_template::golden_test_template;
+
+use crate as starlark;
+use crate::any::ProvidesStaticType;
+use crate::assert;
+use crate::docs::markdown::render_doc_item;
+use crate::docs::DocItem;
+use crate::environment::GlobalsBuilder;
+use crate::environment::Methods;
+use crate::environment::MethodsBuilder;
+use crate::environment::MethodsStatic;
+use crate::values::list::UnpackList;
+use crate::values::none::NoneType;
+use crate::values::tuple::UnpackTuple;
+use crate::values::Heap;
+use crate::values::StarlarkValue;
+use crate::values::Value;
+
+fn docs_golden_test(test_file_name: &str, doc: DocItem) -> String {
+    assert!(test_file_name.ends_with(".golden.md"));
+    assert!(!test_file_name.contains('/'));
+
+    let output = render_doc_item("name", &doc);
+
+    golden_test_template(&format!("src/docs/tests/golden/{test_file_name}"), &output);
+
+    output
+}
+
+const STARLARK_CODE: &str = r#"
+"""
+This is the summary of the module's docs
+
+Some extra details can go here,
+    and indentation is kept as expected
+"""
+
+def f1(a, b: str, c: int = 5, *, d: str = "some string", **kwargs) -> list[str]:
+    """
+    Summary line goes here
+
+    Args:
+        a: The docs for a
+        b: The docs for b
+        c: The docs for c, but these
+           go onto two lines
+        **kwargs: Docs for the keyword args
+
+    Returns:
+        A string repr of the args
+    """
+    return [str((a, b, c, d, repr(kwargs)))]
+
+def f2(a, *args: list[str]):
+    """
+    This is a function with *args, and no return type
+
+    Args:
+        *args: Only doc this arg
+    """
+    return None
+
+def f3(a: str) -> str:
+    return a
+
+def f4(a: str) -> str:
+    """ This is a docstring with no 'Args:' section """
+    return a
+
+# Not public, so shouldn't show up
+def _do_not_export():
+    pass
+"#;
+
+#[derive(
+    Debug,
+    derive_more::Display,
+    ProvidesStaticType,
+    Allocative,
+    NoSerialize
+)]
+#[display("magic")]
+struct Magic;
+
+#[starlark_value(type = "magic")]
+impl<'v> StarlarkValue<'v> for Magic {}
+
+/// These are where the module docs go
+#[starlark_module]
+fn module(builder: &mut GlobalsBuilder) {
+    const MAGIC: i32 = 42;
+
+    /// Docs for func1
+    ///
+    /// # Arguments
+    ///     * `foo`: Docs for foo
+    ///
+    /// # Returns
+    /// The string 'func1'
+    fn func1(foo: String) -> anyhow::Result<String> {
+        let _ignore = foo;
+        Ok("func1".to_owned())
+    }
+
+    fn func2() -> anyhow::Result<String> {
+        Ok("func2".to_owned())
+    }
+
+    /// A function with only positional arguments.
+    ///
+    /// And a slightly longer description. With some example code:
+    ///
+    /// ```python
+    /// func3(1)
+    /// ```
+    ///
+    /// And some assertions:
+    ///
+    /// ```rust
+    /// # starlark::assert::all_true(r#"
+    /// 1 == 1
+    /// # "#);
+    /// ```
+    #[starlark(as_type = Magic)]
+    fn func3(
+        #[starlark(require = pos)] a1: i32,
+        #[starlark(require = pos)] a2: Option<i32>,
+        #[starlark(require = pos, default = 1)] step: i32,
+    ) -> anyhow::Result<String> {
+        let _ = (a1, a2, step);
+        Ok("func3".to_owned())
+    }
+
+    fn with_defaults<'v>(
+        #[starlark(default=UnpackList::default())] explicit_default: UnpackList<String>,
+        hidden_default: Option<UnpackList<String>>,
+        #[starlark(default = "my_default")] string_default: &str,
+    ) -> anyhow::Result<NoneType> {
+        let _unused = (explicit_default, hidden_default, string_default);
+        Ok(NoneType)
+    }
+
+    fn pos_either_named(
+        #[starlark(require = pos)] a: i32,
+        b: i32,
+        #[starlark(require = named)] c: i32,
+    ) -> anyhow::Result<NoneType> {
+        let _unused = (a, b, c);
+        Ok(NoneType)
+    }
+
+    fn notypes<'v>(a: Value<'v>) -> anyhow::Result<Value<'v>> {
+        Ok(a)
+    }
+
+    fn starlark_args(#[starlark(args)] args: UnpackTuple<String>) -> anyhow::Result<NoneType> {
+        let _ignore = args;
+        Ok(NoneType)
+    }
+
+    fn starlark_kwargs(
+        #[starlark(kwargs)] kwargs: SmallMap<String, u32>,
+    ) -> anyhow::Result<NoneType> {
+        let _ignore = kwargs;
+        Ok(NoneType)
+    }
+}
+
+#[derive(ProvidesStaticType, Debug, Display, Allocative, Serialize)]
+#[display("obj")]
+struct Obj;
+
+#[starlark_value(type = "obj")]
+impl<'v> StarlarkValue<'v> for Obj {
+    fn get_methods() -> Option<&'static Methods> {
+        static RES: MethodsStatic = MethodsStatic::new();
+        RES.methods(object)
+    }
+}
+
+/// These are where the module docs go
+#[starlark_module]
+fn object(builder: &mut MethodsBuilder) {
+    /// Docs for attr1
+    #[starlark(attribute)]
+    fn attr1<'v>(this: Value<'v>) -> anyhow::Result<String> {
+        Ok("attr1".to_owned())
+    }
+
+    #[starlark(attribute)]
+    fn attr2<'v>(this: Value<'v>) -> anyhow::Result<String> {
+        Ok("attr2".to_owned())
+    }
+
+    /// Docs for func1
+    ///
+    /// # Arguments
+    ///     * `foo`: Docs for foo
+    ///
+    /// # Returns
+    /// The string 'func1'
+    fn func1<'v>(this: Value<'v>, foo: String) -> anyhow::Result<String> {
+        let _ignore = (this, foo);
+        Ok("func1".to_owned())
+    }
+
+    fn func2<'v>(this: Value<'v>) -> anyhow::Result<String> {
+        let _ = this;
+        Ok("func2".to_owned())
+    }
+
+    /// Needs to be escaped when rendered in markdown.
+    fn __exported__<'v>(this: Value<'v>) -> anyhow::Result<NoneType> {
+        let _ = this;
+        Ok(NoneType)
+    }
+}
+
+#[test]
+fn golden_docs_starlark() {
+    let res = docs_golden_test(
+        "starlark.golden.md",
+        DocItem::Module(assert::pass_module(STARLARK_CODE).documentation()),
+    );
+    assert!(!res.contains("_do_not_export"));
+}
+
+#[test]
+fn native_docs_module() {
+    let res = docs_golden_test(
+        "native.golden.md",
+        DocItem::Module(GlobalsBuilder::new().with(module).build().documentation()),
+    );
+    assert!(!res.contains("starlark::assert::all_true"));
+    assert!(res.contains(r#"string_default: str = "my_default"#));
+}
+
+#[test]
+fn golden_docs_object() {
+    let res = docs_golden_test("object.golden.md", Obj.documentation().unwrap());
+    assert!(res.contains(r#"name.\_\_exported\_\_"#));
+}
+
+#[test]
+fn inner_object_functions_have_docs() {
+    let heap = Heap::new();
+    let obj = heap.alloc_simple(Obj);
+    let item = obj
+        .get_attr("func1", &heap)
+        .unwrap()
+        .unwrap()
+        .documentation()
+        .unwrap();
+    let res = render_doc_item("func1", &item);
+    assert!(res.contains("Docs for func1"));
+}
+
+#[test]
+fn inner_module_functions_have_docs() {
+    let item = GlobalsBuilder::new()
+        .with(module)
+        .build()
+        .get("func1")
+        .unwrap()
+        .documentation()
+        .unwrap();
+    let res = render_doc_item("func1", &item);
+    assert!(res.contains("Docs for func1"))
+}
