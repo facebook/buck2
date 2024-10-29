8e900f2499d70ece5e116e1f3f594f1c0e2b6658
Andreas Herrmann <andreas.herrmann@tweag.io>
2024-10-29 16:47:42 +0100

    Add an automated test script

---
+++ examples/persistent_worker/test.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "::group::Local build without persistent worker" >&2
+echo '<file:.buckconfig.no-workers>' > .buckconfig.local
+buck2 clean; buck2 build : -vstderr
+echo "# Verifying Buck2 log" >&2
+buck2 log what-ran --show-std-err --format json \
+  | jq -s '
+      [
+        .[]
+        | select(.identity | startswith("root//:demo-"))
+      ]
+      | if length == 4 then
+          .
+        else
+          error("expected 4 demo targets, got " + (length | tostring))
+        end
+      | .[]
+      | if .reproducer.executor == "Local" and (.std_err | startswith("one-shot.py")) then
+          true
+        else
+          error("expected local without persistent worker, got " + ([.reproducer.executor, .std_err] | tostring))
+        end
+    '
+
+echo "::group::Local build with persistent worker" >&2
+echo '<file:.buckconfig.local-persistent-workers>' > .buckconfig.local
+buck2 clean; buck2 build : -vstderr
+echo "# Verifying Buck2 log" >&2
+buck2 log what-ran --show-std-err --format json \
+  | jq -s '
+      [
+        .[]
+        | select(.identity | startswith("root//:demo-"))
+      ]
+      | if length == 5 then
+          .
+        else
+          error("expected 5 demo targets, got " + (length | tostring))
+        end
+      | .[]
+      | if (.reproducer.executor == "Worker" or .reproducer.executor == "WorkerInit") and (.std_err | startswith("Buck2 persistent worker")) then
+          true
+        else
+          error("expected local without persistent worker, got " + ([.reproducer.executor, .std_err] | tostring))
+        end
+    '
+
+echo "::group::Remote build without persistent worker" >&2
+echo '<file:.buckconfig.buildbuddy>' > .buckconfig.local
+buck2 clean; buck2 build : -vstderr
+echo "# Verifying Buck2 log" >&2
+buck2 log what-ran --show-std-err --format json \
+  | jq -s '
+      [
+        .[]
+        | select(.identity | startswith("root//:demo-"))
+      ]
+      | if length == 4 then
+          .
+        else
+          error("expected 4 demo targets, got " + (length | tostring))
+        end
+      | .[]
+      | if .reproducer.executor == "Re" and (.std_err | startswith("one-shot.py")) then
+          true
+        else
+          error("expected local without persistent worker, got " + ([.reproducer.executor, .std_err] | tostring))
+        end
+    '
+
+echo "::group::Remote build with persistent worker" >&2
+echo '<file:.buckconfig.buildbuddy-persistent-workers>' > .buckconfig.local
+buck2 clean; buck2 build : -vstderr
+echo "# Verifying Buck2 log" >&2
+buck2 log what-ran --show-std-err --format json \
+  | jq -s '
+      [
+        .[]
+        | select(.identity | startswith("root//:demo-"))
+      ]
+      | if length == 4 then
+          .
+        else
+          error("expected 4 demo targets, got " + (length | tostring))
+        end
+      | .[]
+      | if .reproducer.executor == "Re" and (.std_err | startswith("Bazel persistent worker")) then
+          true
+        else
+          error("expected remote persistent worker, got " + ([.reproducer.executor, .std_err] | tostring))
+        end
+    '
