e259d89269f5e8710a3df8fb70136e46d8de2ca0
Andreas Herrmann <andreas.herrmann@tweag.io>
2024-10-30 14:05:34 +0100

    Generate GH actions annotations on missing token

--- examples/persistent_worker/test.sh
+++ examples/persistent_worker/test.sh
@@ -50,49 +50,57 @@ buck2 log what-ran --show-std-err --format json \
 echo "::endgroup::" >&2
 
 echo "::group::Remote build without persistent worker" >&2
-echo '<file:.buckconfig.buildbuddy>' > .buckconfig.local
-buck2 clean; buck2 build : -vstderr
-echo "# Verifying Buck2 log" >&2
-buck2 log what-ran --show-std-err --format json \
-  | jq -s '
-      [
-        .[]
-        | select(.identity | startswith("root//:demo-"))
-      ]
-      | if length == 4 then
-          .
-        else
-          error("expected 4 demo targets, got " + (length | tostring))
-        end
-      | .[]
-      | if .reproducer.executor == "Re" and (.std_err | startswith("one-shot.py")) then
-          true
-        else
-          error("expected local without persistent worker, got " + ([.reproducer.executor, .std_err] | tostring))
-        end
-    '
+if [[ -z ${BUILDBUDDY_API_KEY:+x} ]]; then
+  echo "::notice file=${BASH_SOURCE[0]},line=${LINENO}::SKIPPED Missing BuildBuddy token. See examples/persistent_worker/README.md" >&2
+else
+  echo '<file:.buckconfig.buildbuddy>' > .buckconfig.local
+  buck2 clean; buck2 build : -vstderr
+  echo "# Verifying Buck2 log" >&2
+  buck2 log what-ran --show-std-err --format json \
+    | jq -s '
+        [
+          .[]
+          | select(.identity | startswith("root//:demo-"))
+        ]
+        | if length == 4 then
+            .
+          else
+            error("expected 4 demo targets, got " + (length | tostring))
+          end
+        | .[]
+        | if .reproducer.executor == "Re" and (.std_err | startswith("one-shot.py")) then
+            true
+          else
+            error("expected local without persistent worker, got " + ([.reproducer.executor, .std_err] | tostring))
+          end
+      '
+fi
 echo "::endgroup::" >&2
 
 echo "::group::Remote build with persistent worker" >&2
-echo '<file:.buckconfig.buildbuddy-persistent-workers>' > .buckconfig.local
-buck2 clean; buck2 build : -vstderr
-echo "# Verifying Buck2 log" >&2
-buck2 log what-ran --show-std-err --format json \
-  | jq -s '
-      [
-        .[]
-        | select(.identity | startswith("root//:demo-"))
-      ]
-      | if length == 4 then
-          .
-        else
-          error("expected 4 demo targets, got " + (length | tostring))
-        end
-      | .[]
-      | if .reproducer.executor == "Re" and (.std_err | startswith("Bazel persistent worker")) then
-          true
-        else
-          error("expected remote persistent worker, got " + ([.reproducer.executor, .std_err] | tostring))
-        end
-    '
+if [[ -z ${BUILDBUDDY_API_KEY:+x} ]]; then
+  echo "::notice file=${BASH_SOURCE[0]},line=${LINENO}::SKIPPED Missing BuildBuddy token. See examples/persistent_worker/README.md" >&2
+else
+  echo '<file:.buckconfig.buildbuddy-persistent-workers>' > .buckconfig.local
+  buck2 clean; buck2 build : -vstderr
+  echo "# Verifying Buck2 log" >&2
+  buck2 log what-ran --show-std-err --format json \
+    | jq -s '
+        [
+          .[]
+          | select(.identity | startswith("root//:demo-"))
+        ]
+        | if length == 4 then
+            .
+          else
+            error("expected 4 demo targets, got " + (length | tostring))
+          end
+        | .[]
+        | if .reproducer.executor == "Re" and (.std_err | startswith("Bazel persistent worker")) then
+            true
+          else
+            error("expected remote persistent worker, got " + ([.reproducer.executor, .std_err] | tostring))
+          end
+      '
+fi
 echo "::endgroup::" >&2
