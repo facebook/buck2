81b1763306b18b6ff27f470eb2a0daa8efa0b778
Andreas Herrmann <andreas.herrmann@tweag.io>
2024-11-15 11:53:17 +0100

    Implement RemoteEventSink to publish to Bazel BES
    Repurposes the RemoteEventSink used to connect to Scribe within Meta internal Buck2 and connects
    it to a Bazel Build Event Service over gRPC instead. Adds a translation layer between Buck2
    build events and Bazel build events.

--- app/buck2_events/src/sink/remote.rs
+++ app/buck2_events/src/sink/remote.rs
@@ -334,7 +334,41 @@ mod fbcode {
 
 #[cfg(not(fbcode_build))]
 mod fbcode {
+    use std::collections::HashMap;
     use std::sync::Arc;
+    use std::thread::JoinHandle;
+
+    use anyhow::Context;
+
+    use async_stream::stream;
+
+    use bazel_event_publisher_proto::build_event_stream;
+    use bazel_event_publisher_proto::google::devtools::build::v1;
+    use buck2_data;
+    use buck2_data::BuildCommandStart;
+    use buck2_util::future::try_join_all;
+    use futures::stream;
+
+    use futures::Stream;
+    use futures::StreamExt;
+    use tonic::transport::Channel;
+    use tonic::Request;
+
+    use tokio::runtime::Builder;
+    use tokio::sync::mpsc;
+    use tokio::sync::mpsc::UnboundedReceiver;
+    use tokio::sync::mpsc::UnboundedSender;
+
+    use tokio_stream::wrappers::UnboundedReceiverStream;
+
+    use bazel_event_publisher_proto::google::devtools::build::v1::OrderedBuildEvent;
+    use bazel_event_publisher_proto::google::devtools::build::v1::publish_build_event_client::PublishBuildEventClient;
+    use bazel_event_publisher_proto::google::devtools::build::v1::PublishBuildToolEventStreamRequest;
+    use bazel_event_publisher_proto::google::devtools::build::v1::StreamId;
+
+    use prost;
+    use prost::Message;
+    use prost_types;
 
     use crate::BuckEvent;
     use crate::Event;
@@ -342,15 +376,238 @@ mod fbcode {
     use crate::EventSinkStats;
     use crate::EventSinkWithStats;
 
-    pub enum RemoteEventSink {}
+    pub struct RemoteEventSink {
+        _handler: JoinHandle<()>,
+        send: UnboundedSender<Vec<BuckEvent>>,
+    }
+
+    async fn connect_build_event_server() -> anyhow::Result<PublishBuildEventClient<Channel>> {
+        let uri = std::env::var("BES_URI")?.parse()?;
+        let channel = Channel::builder(uri);
+        // TODO: enable TLS and handle API token
+        // let tls_config = ClientTlsConfig::new();
+        // channel = channel.tls_config(tls_config)?;
+        channel
+            .connect()
+            .await
+            .context("connecting to Bazel event stream gRPC server")?;
+        let client = PublishBuildEventClient::connect(channel)
+            .await
+            .context("creating Bazel event stream gRPC client")?;
+        Ok(client)
+    }
+
+    fn buck_to_bazel_events<S: Stream<Item = BuckEvent>>(events: S) -> impl Stream<Item = v1::BuildEvent> {
+        stream! {
+            for await event in events {
+                println!("EVENT {:?} {:?}", event.event.trace_id, event);
+                match event.data() {
+                    buck2_data::buck_event::Data::SpanStart(start) => {
+                        println!("START {:?}", start);
+                        match start.data.as_ref() {
+                            None => {},
+                            Some(buck2_data::span_start_event::Data::Command(command)) => {
+                                match command.data.as_ref() {
+                                    None => {},
+                                    Some(buck2_data::command_start::Data::Build(BuildCommandStart {})) => {
+                                        let bes_event = build_event_stream::BuildEvent {
+                                            id: Some(build_event_stream::BuildEventId { id: Some(build_event_stream::build_event_id::Id::Started(build_event_stream::build_event_id::BuildStartedId {})) }),
+                                            children: vec![],
+                                            last_message: false,
+                                            payload: Some(build_event_stream::build_event::Payload::Started(build_event_stream::BuildStarted {
+                                                uuid: event.event.trace_id.clone(),
+                                                start_time_millis: 0,
+                                                start_time: Some(event.timestamp().into()),
+                                                build_tool_version: "BUCK2".to_owned(),
+                                                options_description: "UNKNOWN".to_owned(),
+                                                command: "build".to_owned(),
+                                                working_directory: "UNKNOWN".to_owned(),
+                                                workspace_directory: "UNKNOWN".to_owned(),
+                                                server_pid: std::process::id() as i64,
+                                            })),
+                                        };
+                                        let bazel_event = v1::build_event::Event::BazelEvent(prost_types::Any {
+                                            type_url: "type.googleapis.com/build_event_stream.BuildEvent".to_owned(),
+                                            value: bes_event.encode_to_vec(),
+                                        });
+                                        yield v1::BuildEvent {
+                                            event_time: Some(event.timestamp().into()),
+                                            event: Some(bazel_event),
+                                        };
+                                    },
+                                    Some(_) => {},
+                                }
+                            },
+                            Some(_) => {},
+                        }
+                    },
+                    buck2_data::buck_event::Data::SpanEnd(end) => {
+                        println!("END   {:?}", end);
+                        match end.data.as_ref() {
+                            None => {},
+                            Some(buck2_data::span_end_event::Data::Command(command)) => {
+                                match command.data.as_ref() {
+                                    None => {},
+                                    Some(buck2_data::command_end::Data::Build(_build)) => {
+                                        let bes_event = build_event_stream::BuildEvent {
+                                            id: Some(build_event_stream::BuildEventId { id: Some(build_event_stream::build_event_id::Id::BuildFinished(build_event_stream::build_event_id::BuildFinishedId {})) }),
+                                            children: vec![],
+                                            last_message: true,
+                                            payload: Some(build_event_stream::build_event::Payload::Finished(build_event_stream::BuildFinished {
+                                                overall_success: command.is_success,
+                                                exit_code: Some(
+                                                    if command.is_success {
+                                                        build_event_stream::build_finished::ExitCode {
+                                                            name: "SUCCESS".to_owned(),
+                                                            code: 0,
+                                                        }
+                                                    } else {
+                                                        build_event_stream::build_finished::ExitCode {
+                                                            name: "FAILURE".to_owned(),
+                                                            code: 1,
+                                                        }
+                                                    }),
+                                                finish_time_millis: 0,
+                                                finish_time: Some(event.timestamp().into()),
+                                                anomaly_report: None,
+                                                // TODO: convert Buck2 ErrorReport
+                                                failure_detail: None,
+                                            })),
+                                        };
+                                        let bazel_event = v1::build_event::Event::BazelEvent(prost_types::Any {
+                                            type_url: "type.googleapis.com/build_event_stream.BuildEvent".to_owned(),
+                                            value: bes_event.encode_to_vec(),
+                                        });
+                                        yield v1::BuildEvent {
+                                            event_time: Some(event.timestamp().into()),
+                                            event: Some(bazel_event),
+                                        };
+                                        break;
+                                    },
+                                    Some(_) => {},
+                                }
+                            },
+                            Some(_) => {},
+                        }
+                    },
+                    buck2_data::buck_event::Data::Instant(instant) => {
+                        println!("INST  {:?}", instant);
+                    },
+                    buck2_data::buck_event::Data::Record(record) => {
+                        println!("REC   {:?}", record);
+                    },
+                }
+            }
+        }
+    }
+
+    fn stream_build_tool_events<S: Stream<Item = v1::BuildEvent>>(trace_id: String, events: S) -> impl Stream<Item = PublishBuildToolEventStreamRequest> {
+        stream::iter(1..)
+            .zip(events)
+            .map(move |(sequence_number, event)| {
+                PublishBuildToolEventStreamRequest {
+                    check_preceding_lifecycle_events_present: false,
+                    notification_keywords: vec![],
+                    ordered_build_event: Some(OrderedBuildEvent {
+                        stream_id: Some(StreamId {
+                            build_id: trace_id.clone(),
+                            invocation_id: trace_id.clone(),
+                            component: 0,
+                        }),
+                        sequence_number,
+                        event: Some(event),
+                    }),
+                    project_id: "12341234".to_owned(), // TODO: needed
+                }
+            })
+    }
+
+    async fn event_sink_loop(recv: UnboundedReceiver<Vec<BuckEvent>>) -> anyhow::Result<()> {
+        let mut handlers: HashMap<String, (UnboundedSender<BuckEvent>, tokio::task::JoinHandle<anyhow::Result<()>>)> = HashMap::new();
+        let client = connect_build_event_server().await?;
+        let mut recv = UnboundedReceiverStream::new(recv)
+            .flat_map(|v|stream::iter(v));
+        while let Some(event) = recv.next().await {
+            let dbg_trace_id = event.event.trace_id.clone();
+            println!("event_sink_loop event {:?}", &dbg_trace_id);
+            if let Some((send, _)) = handlers.get(&event.event.trace_id) {
+                println!("event_sink_loop redirect {:?}", &dbg_trace_id);
+                send.send(event).unwrap_or_else(|e| println!("build event send failed {:?}", e));
+            } else {
+                println!("event_sink_loop new handler {:?}", event.event.trace_id);
+                let (send, recv) = mpsc::unbounded_channel::<BuckEvent>();
+                let mut client = client.clone();
+                let dbg_trace_id = dbg_trace_id.clone();
+                let trace_id = event.event.trace_id.clone();
+                let handler = tokio::spawn(async move {
+                    let recv = UnboundedReceiverStream::new(recv);
+                    let request = Request::new(stream_build_tool_events(trace_id, buck_to_bazel_events(recv)));
+                    println!("new handler request {:?}", &dbg_trace_id);
+                    let response = client.publish_build_tool_event_stream(request).await?;
+                    println!("new handler response {:?}", &dbg_trace_id);
+                    let mut inbound = response.into_inner();
+                    while let Some(ack) = inbound.message().await? {
+                        // TODO: Handle ACKs properly and add retry.
+                        println!("ACK  {:?}", ack);
+                    }
+                    Ok(())
+                });
+                handlers.insert(event.event.trace_id.to_owned(), (send, handler));
+            }
+        }
+        println!("event_sink_loop recv CLOSED");
+        // TODO: handle closure and retry.
+        // close send handles and await all handlers.
+        let handlers: Vec<tokio::task::JoinHandle<anyhow::Result<()>>> = handlers.into_values().map(|(_, handler)|handler).collect();
+        // TODO: handle retry.
+        try_join_all(handlers).await?.into_iter().collect::<anyhow::Result<Vec<()>>>()?;
+        Ok(())
+    }
 
     impl RemoteEventSink {
-        pub async fn send_now(&self, _event: BuckEvent) {}
-        pub async fn send_messages_now(&self, _events: Vec<BuckEvent>) {}
+        pub fn new() -> anyhow::Result<Self> {
+            let (send, recv) = mpsc::unbounded_channel::<Vec<BuckEvent>>();
+            let handler = std::thread::Builder::new()
+                .name("buck-event-producer".to_owned())
+                .spawn({
+                    move || {
+                        let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+                        runtime.block_on(event_sink_loop(recv)).unwrap();
+                    }
+                }).context("spawning buck-event-producer thread")?;
+            Ok(RemoteEventSink {
+                _handler: handler,
+                send,
+            })
+        }
+        pub async fn send_now(&self, event: BuckEvent) {
+            self.send_messages_now(vec![event]).await;
+        }
+        pub async fn send_messages_now(&self, events: Vec<BuckEvent>) {
+            // TODO: does this make sense for BES? If so, implement send now variant.
+            if let Err(err) = self.send.send(events) {
+                // TODO: proper error handling
+                dbg!(err);
+            }
+        }
+        pub fn offer(&self, event: BuckEvent) {
+            if let Err(err) = self.send.send(vec![event]) {
+                // TODO: proper error handling
+                dbg!(err);
+            }
+        }
     }
 
     impl EventSink for RemoteEventSink {
-        fn send(&self, _event: Event) {}
+        fn send(&self, event: Event) {
+            match event {
+                Event::Buck(event) => {
+                    self.offer(event);
+                }
+                Event::CommandResult(..) => {},
+                Event::PartialResult(..) => {},
+            }
+        }
     }
 
     impl EventSinkWithStats for RemoteEventSink {
@@ -359,7 +616,20 @@ mod fbcode {
         }
 
         fn stats(&self) -> EventSinkStats {
-            match *self {}
+            EventSinkStats {
+                successes: 0,
+                failures_invalid_request: 0,
+                failures_unauthorized: 0,
+                failures_rate_limited: 0,
+                failures_pushed_back: 0,
+                failures_enqueue_failed: 0,
+                failures_internal_error: 0,
+                failures_timed_out: 0,
+                failures_unknown: 0,
+                buffered: 0,
+                dropped: 0,
+                bytes_written: 0,
+            }
         }
     }
 }
@@ -393,7 +663,7 @@ fn new_remote_event_sink_if_fbcode(
             retry_attempts,
             message_batch_size,
         );
-        Ok(None)
+        Ok(Some(RemoteEventSink::new()?))
     }
 }
 
