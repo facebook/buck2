522081e6fc48c51d35af1b12285e05988dd014ba
Roman Perehonchuk <romanp@meta.com>
2024-12-02 03:39:22 -0800

    add deprecated config test Summary:
    Summary:
    context:
    we want to log the soft_error for deprecated configs
    
    Reviewed By: scottcao
    
    Differential Revision: D66012434
    
    fbshipit-source-id: 1a25305c33ab642621bd84c6f685607709543ca6

---
+++ tests/core/interpreter/test_deprecated_config.py
@@ -0,0 +1,46 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+# pyre-strict
+
+
+import pytest
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test()
+@pytest.mark.parametrize("section", ["some", "other"])
+@pytest.mark.parametrize("root", ["true", "false"])
+async def test_deprecated_config(buck: Buck, section: str, root: str) -> None:
+    # TODO(romanp): it must fail with {section}.config1 is no longer used. Please use other.config2
+    _ = (
+        await buck.build(
+            f":test_target_{section}_config1",
+            "-c",
+            f"test.section={section}",
+            "-c",
+            "test.conf=config1",
+            "-c",
+            f"test.root={root}",
+        ),
+    )
+
+
+@buck_test()
+@pytest.mark.parametrize("root", ["true", "false"])
+async def test_not_deprecated_config(buck: Buck, root: str) -> None:
+    section = "other"
+    _ = await buck.build(
+        f":test_target_{section}_config2",
+        "-c",
+        f"test.section={section}",
+        "-c",
+        "test.conf=config2",
+        "-c",
+        f"test.root={root}",
+    )
