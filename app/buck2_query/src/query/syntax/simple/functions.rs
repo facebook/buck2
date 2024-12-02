ff6c8241ec4c32cdcc59f80c0a953b8036f832bf
Roman Perehonchuk <romanp@meta.com>
2024-12-02 05:31:27 -0800

    inputs docs
    Summary: as per title
    
    Reviewed By: blackm00n
    
    Differential Revision: D66601278
    
    fbshipit-source-id: 35fc55f3671163f5a7abe895bff7153f2d6e7ab8

--- app/buck2_query/src/query/syntax/simple/functions.rs
+++ app/buck2_query/src/query/syntax/simple/functions.rs
@@ -379,6 +379,12 @@ impl<Env: QueryEnvironment> DefaultQueryFunctionsModule<Env> {
         }
     }
 
+    /// Returns all inputs non-transitively
+    /// Returns the files that are inputs to the `targets` expression, ignoring all dependencies.
+    /// Returns only the files which are an immediate input to the rule function and thus are needed to go through analysis phase (i.e. produce providers).
+    /// You could consider the `inputs()` and `owner()` operators to be inverses of each other.
+    ///
+    /// `buck2 cquery "inputs(fbcode//buck2/dice/...)"` returns the input files for the `fbcode//buck2/dice/...` targets.
     async fn inputs(&self, targets: TargetSet<Env::Target>) -> QueryFuncResult<Env> {
         Ok(self.implementation.inputs(&targets)?.into())
     }
