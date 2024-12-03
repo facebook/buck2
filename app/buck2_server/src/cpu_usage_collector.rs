b2d28559c67c27cd63694137345093024b27e728
Rajneesh Lakkundi <rajneeshl@meta.com>
2024-12-03 11:31:15 -0800

    Collect CPU usage start asynchronously
    Summary: The get() operation in Linux would need a file read. Considering this in the critical path, spawning new thread to collect CPU usage as discussed [here](https://www.internalfb.com/diff/D66124085?dst_version_fbid=1610391829568839&transaction_fbid=917156633696617).
    
    Reviewed By: stepancheg
    
    Differential Revision: D66656058
    
    fbshipit-source-id: 9e206c11a8a555bd0d6206dc30e39991c2d85d3e

--- app/buck2_server/src/cpu_usage_collector.rs
+++ app/buck2_server/src/cpu_usage_collector.rs
@@ -7,6 +7,9 @@
  * of this source tree.
  */
 
+use std::sync::Arc;
+use std::sync::Mutex;
+
 use buck2_util::os::host_cpu_usage::HostCpuUsage;
 use dupe::Dupe;
 
@@ -19,13 +22,18 @@ pub(crate) struct HostCpuUsageSinceCmdStart {
 /// Collects and emits the delta of CPU usage since the command start (i.e. collector creation).
 #[derive(Clone, Dupe)]
 pub(crate) struct CpuUsageCollector {
-    start: HostCpuUsage,
+    pub start: Arc<Mutex<Option<HostCpuUsage>>>,
 }
 
 impl CpuUsageCollector {
     pub(crate) fn new() -> anyhow::Result<Self> {
-        // Collect the reference point for the CPU usage at the start of the command.
-        let start = HostCpuUsage::get()?;
+        let start = Arc::new(Mutex::new(None));
+        tokio::spawn({
+            let handle = start.dupe();
+            async move {
+                *handle.lock().unwrap() = HostCpuUsage::get().ok();
+            }
+        });
         Ok(Self { start })
     }
 
@@ -33,21 +41,20 @@ impl CpuUsageCollector {
     pub(crate) fn get_usage_since_command_start(
         &self,
     ) -> anyhow::Result<HostCpuUsageSinceCmdStart> {
-        if let Ok(current_usage) = HostCpuUsage::get() {
-            // TODO(rajneeshl): The MacOS low-level API returns u32 for the tick count which overflows
-            // in a couple of weeks. This could lead to situations where the start > current.
-            if let (Some(user_millis), Some(system_millis)) = (
-                current_usage
-                    .user_millis
-                    .checked_sub(self.start.user_millis),
-                current_usage
-                    .system_millis
-                    .checked_sub(self.start.system_millis),
-            ) {
-                return Ok(HostCpuUsageSinceCmdStart {
-                    user_millis,
-                    system_millis,
-                });
+        let start = self.start.lock().expect("Poisoned lock");
+        if let Some(start) = &*start {
+            if let Ok(current_usage) = HostCpuUsage::get() {
+                // TODO(rajneeshl): The MacOS low-level API returns u32 for the tick count which overflows
+                // in a couple of weeks. This could lead to situations where the start > current.
+                if let (Some(user_millis), Some(system_millis)) = (
+                    current_usage.user_millis.checked_sub(start.user_millis),
+                    current_usage.system_millis.checked_sub(start.system_millis),
+                ) {
+                    return Ok(HostCpuUsageSinceCmdStart {
+                        user_millis,
+                        system_millis,
+                    });
+                }
             }
         }
         Err(anyhow::anyhow!("Failed to get CPU usage"))
