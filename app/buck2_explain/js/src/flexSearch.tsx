1c8a8039a933d00bb587f47c1e5dc1ac3917256e
Ignacio Guridi <iguridi@meta.com>
2024-12-06 22:01:27 -0800

    buck2: explain: remove stuff we don't need anymore from flexSearch
    Summary: forgot to remove it with D65634099
    
    Reviewed By: christolliday
    
    Differential Revision: D66775423
    
    fbshipit-source-id: afedfaff266046dd1bdc07b30062af19fa3699a5

--- app/buck2_explain/js/src/flexSearch.tsx
+++ app/buck2_explain/js/src/flexSearch.tsx
@@ -8,7 +8,7 @@
  */
 
 import {Index} from 'flexsearch-ts'
-import {Build, TargetField, TargetValue, TargetValueType} from './fbs/explain'
+import {Build} from './fbs/explain'
 import {formatTargetLabel} from './formatTargetLabel'
 
 export let indexCache: Index | null = null
@@ -16,79 +16,6 @@ export let indexCache: Index | null = null
 function addIfExists(index: Index, key: string, element: string | null | undefined) {
   if (element != null) {
     index.append(key, element)
-  }
-}
-function addList(
-  index: Index,
-  key: string,
-  attr: (i: number) => TargetValue | null | undefined,
-  length: number,
-) {
-  for (let i = 0; i < length; i++) {
-    addTargetValue(index, key, attr(i))
-  }
-}
-function addListOfStrings(index: Index, key: string, attr: (i: number) => string, length: number) {
-  for (let i = 0; i < length; i++) {
-    const value = attr(i)
-    if (value != null) {
-      addIfExists(index, key, value)
-    }
-  }
-}
-function addDict(
-  index: Index,
-  key: string,
-  attr: (i: number) => TargetValue | null | undefined,
-  length: number,
-) {
-  for (let i = 0; i < length; i++) {
-    const value = attr(i)
-    addTargetValue(index, key, value?.key())
-    addTargetValue(index, key, value)
-  }
-}
-function addTargetField(index: Index, key: string, field: TargetField | null) {
-  if (field == null) {
-    return
-  }
-  const name = field?.name()
-  addIfExists(index, key, name)
-  addTargetValue(index, key, field?.value())
-}
-function addTargetValue(index: Index, key: string, value: TargetValue | null | undefined) {
-  if (value == null) {
-    return
-  }
-  const valueType = value?.type()
-  if (valueType == null) {
-    return
-  }
-  switch (valueType) {
-    case TargetValueType.Bool:
-      addIfExists(index, key, value.boolValue()?.toString())
-    case TargetValueType.Int:
-      addIfExists(index, key, value.intValue()?.toString())
-    case TargetValueType.String:
-      addIfExists(index, key, value.stringValue())
-    case TargetValueType.List:
-      addList(index, key, i => value.listValue(i), value.listValueLength())
-    case TargetValueType.Dict:
-      addDict(index, key, i => value.dictValue(i), value.dictValueLength())
-  }
-}
-function addListOfTargetFields(
-  index: Index,
-  key: string,
-  attr: (i: number) => TargetField | null,
-  length: number,
-) {
-  for (let i = 0; i < length; i++) {
-    const value = attr(i)
-    if (value == null) {
-      continue
-    }
-    addTargetField(index, key, value)
   }
 }
 
@@ -108,18 +35,6 @@ export async function indexEverything(build: Build): Promise<void> {
     addIfExists(searchIndex, identifier, label.targetLabel()!)
     addIfExists(searchIndex, identifier, label.cfg()!)
     addIfExists(searchIndex, identifier, label.execCfg()!)
-    addListOfStrings(
-      searchIndex,
-      identifier,
-      i => formatTargetLabel(target.deps(i)!),
-      target.depsLength(),
-    )
-    addListOfTargetFields(
-      searchIndex,
-      identifier,
-      (i: number) => target.attrs(i),
-      target.attrsLength(),
-    )
   }
   indexCache = searchIndex
 }
