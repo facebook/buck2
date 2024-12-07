c8cfff4601fa600f1d63af5f2d414bf828c535df
Ignacio Guridi <iguridi@meta.com>
2024-12-06 22:01:27 -0800

    buck2: explain: better flexSearch code
    Summary: just a refactor
    
    Reviewed By: christolliday
    
    Differential Revision: D66775425
    
    fbshipit-source-id: 8e2787599ff8b853e460d4ed5e015cf6d02349e6

--- app/buck2_explain/js/src/flexSearch.tsx
+++ app/buck2_explain/js/src/flexSearch.tsx
@@ -13,12 +13,6 @@ import {formatTargetLabel} from './formatTargetLabel'
 
 export let indexCache: Index | null = null
 
-function addIfExists(index: Index, key: string, element: string | null | undefined) {
-  if (element != null) {
-    index.append(key, element)
-  }
-}
-
 export async function indexEverything(build: Build): Promise<void> {
   // TODO iguridi: make this in a js worker
   const searchIndex = new Index({tokenize: 'forward', stemmer: false})
@@ -26,15 +20,21 @@ export async function indexEverything(build: Build): Promise<void> {
     let target = build.targets(i)!
     const label = target.label()!
     let identifier = formatTargetLabel(label)
-    addIfExists(searchIndex, identifier, target.name())
-    addIfExists(searchIndex, identifier, target.oncall())
-    addIfExists(searchIndex, identifier, target.executionPlatform())
-    addIfExists(searchIndex, identifier, target.package_())
-    addIfExists(searchIndex, identifier, target.targetConfiguration())
-    addIfExists(searchIndex, identifier, target.type())
-    addIfExists(searchIndex, identifier, label.targetLabel()!)
-    addIfExists(searchIndex, identifier, label.cfg()!)
-    addIfExists(searchIndex, identifier, label.execCfg()!)
+    let data = [
+      target.name(),
+      target.oncall(),
+      target.executionPlatform(),
+      target.package_(),
+      target.targetConfiguration(),
+      target.type(),
+      label.targetLabel()!,
+      label.cfg()!,
+      label.execCfg()!,
+    ].filter(x => x != null)
+
+    for (const field of data) {
+      searchIndex.append(identifier, field)
+    }
   }
   indexCache = searchIndex
 }
