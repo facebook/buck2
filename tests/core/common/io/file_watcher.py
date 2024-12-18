a38cf6b150b408adcf9b09d15aeffffc9dd7634f
John Elliott <jdelliot@meta.com>
2024-12-17 19:09:21 -0800

    Reactor ABA tests to move shared code to common library
    Summary:
    # Context
    
    We are introducing EdenFS notifications to support scalable and ergonomic file system notifications for EdenFS mounts.
    
    # This Diff
    
    Creates a new common library for core tests and lifts shared code from ABA tests to that library.
    
    # Next Steps
    
    * Resolved changes based on commit transitions using Sapling CLI.
    
    Reviewed By: samkevich
    
    Differential Revision: D67247234
    
    fbshipit-source-id: a5f291f6232d0fe8fcd5546f447fa090c79929c0

---
+++ tests/core/common/io/file_watcher.py
@@ -0,0 +1,40 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+# pyre-strict
+
+
+import json
+import subprocess
+
+from buck2.tests.e2e_util.api.buck import Buck
+
+
+async def get_files(buck: Buck) -> list[str]:
+    res = await buck.targets("root//:")
+    for x in res.stderr.splitlines():
+        p = x.split("Files: ", 1)
+        if len(p) > 1:
+            return json.loads(p[1])
+    raise Exception("Missing files output: " + res.stderr)
+
+
+async def run_aba_test(buck: Buck) -> None:
+    # Fails on eden because the repo exists, that's ok
+    subprocess.run(["sl", "init"], cwd=buck.cwd)
+    subprocess.run(["sl", "commit", "--addremove", "-m", "temp"], cwd=buck.cwd)
+    subprocess.run(["sl", "bookmark", "main"], cwd=buck.cwd, check=True)
+
+    assert subprocess.check_output(["sl", "status"], cwd=buck.cwd) == b""
+
+    assert (await get_files(buck)) == ["files/abc", "files/d/empty"]
+
+    subprocess.run(["sl", "mv", "files/abc", "files/d/"], cwd=buck.cwd, check=True)
+    assert (await get_files(buck)) == ["files/d/abc", "files/d/empty"]
+
+    subprocess.run(["sl", "shelve"], cwd=buck.cwd, check=True)
+    assert (await get_files(buck)) == ["files/abc", "files/d/empty"]
--- tests/core/io/test_edenfs_aba.py
+++ tests/core/io/test_edenfs_aba.py
@@ -8,39 +8,12 @@
 # pyre-strict
 
 
-import json
-import subprocess
+from buck2.tests.core.common.io.file_watcher import run_aba_test
 
 from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.buck_workspace import buck_test
 
 
-async def _get_files(buck: Buck) -> list[str]:
-    res = await buck.targets("root//:")
-    for x in res.stderr.splitlines():
-        p = x.split("Files: ", 1)
-        if len(p) > 1:
-            return json.loads(p[1])
-    raise Exception("Missing files output: " + res.stderr)
-
-
-async def _run_test(buck: Buck) -> None:
-    # Fails on eden because the repo exists, that's ok
-    subprocess.run(["sl", "init"], cwd=buck.cwd)
-    subprocess.run(["sl", "commit", "--addremove", "-m", "temp"], cwd=buck.cwd)
-    subprocess.run(["sl", "bookmark", "main"], cwd=buck.cwd, check=True)
-
-    assert subprocess.check_output(["sl", "status"], cwd=buck.cwd) == b""
-
-    assert (await _get_files(buck)) == ["files/abc", "files/d/empty"]
-
-    subprocess.run(["sl", "mv", "files/abc", "files/d/"], cwd=buck.cwd, check=True)
-    assert (await _get_files(buck)) == ["files/d/abc", "files/d/empty"]
-
-    subprocess.run(["sl", "shelve"], cwd=buck.cwd, check=True)
-    assert (await _get_files(buck)) == ["files/abc", "files/d/empty"]
-
-
 @buck_test(setup_eden=True)
 async def test_edenfs_aba_eden(buck: Buck) -> None:
-    await _run_test(buck)
+    await run_aba_test(buck)
