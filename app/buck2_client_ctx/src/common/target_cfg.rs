2763d033e7dc304ce118cd376036b6509fb49309
Ian Childs <ianc@meta.com>
2025-01-20 07:18:30 -0800

    Fix formatting of --target_universe help text
    Summary: ^
    
    Reviewed By: rajneesh
    
    Differential Revision: D68412968
    
    fbshipit-source-id: e8e26ee4257f4207b666d8c5f335e59741c01067

--- app/buck2_client_ctx/src/common/target_cfg.rs
+++ app/buck2_client_ctx/src/common/target_cfg.rs
@@ -71,8 +71,8 @@ pub struct TargetCfgWithUniverseOptions {
     /// universe targets, not the command targets.
     ///
     /// This argument is particularly recommended on most non-trivial cqueries. In the absence of
-    /// this argument, buck2 will use the target literals in your cquery expression as the value for
-    /// this argument, which may not be what you want.
+    /// this argument, buck2 will use the target literals in your cquery expression as the value
+    /// for this argument, which may not be what you want.
     #[clap(long, short = 'u', use_value_delimiter = true, verbatim_doc_comment)]
     pub target_universe: Vec<String>,
 
