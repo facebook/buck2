2df4707d2eb3e302ac3ca28ff52ca9cceea5f80e
Ezgi Çiçek <ezgi@meta.com>
2025-02-04 08:17:37 -0800

    Add test showing that config diff tracking is broken for size/count
    Summary:
    **Context** This diff adds tests to prove that  `CellConfigDiff` tracking is broken. It doesn't track any diff count/size: see respective values are null since D59727035 July: https://fburl.com/scuba/buck2_builds/lsfefrfa
    
    Next diffs, we will get rid of `ConfigDiffTracker`.
    
    **Root-cause**: We never can see previous buckconfigs (because we always start [with fresh userdata](https://www.internalfb.com/code/fbsource/[57acff12b9f6]/fbcode/buck2/app/buck2_server_ctx/src/concurrency.rs?lines=471)) and always return an empty Hashset here: https://www.internalfb.com/code/fbsource/[57acff12b9f6]/fbcode/buck2/app/buck2_common/src/legacy_configs/diffs.rs?lines=63
    
    As a result, since we never have access to buckconfig diff size/count, whenever buckconfigs are changed, we fall into the case of setting
    	new_config_indicator = true  (https://www.internalfb.com/code/fbsource/[57acff12b9f6]/fbcode/buck2/app/buck2_common/src/legacy_configs/diffs.rs?lines=115)
    
    When there is no configs changed,  this code is not invoked since  the cell's buckconfigs are not computed and  `new_config_indicator = 0`  -> `new_configs_used=0`.
    
    Reviewed By: IanChilds, blackm00n
    
    Differential Revision: D68954545
    
    fbshipit-source-id: e75e26d383612e37ce9a2c8b075e79717df17e10

--- tests/core/log/test_diff.py
+++ tests/core/log/test_diff.py
@@ -10,9 +10,11 @@
 import json
 import tempfile
 import typing
+from pathlib import Path
 
 from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.buck_workspace import buck_test
+from buck2.tests.e2e_util.helper.utils import filter_events, read_invocation_record
 
 
 def with_buck2_output(output: str) -> typing.List[str]:
@@ -152,3 +154,55 @@ async def test_config_diff_command_project_relative(buck: Buck) -> None:
     # We only store the path of the modefile
     assert diff[0]["FirstOnly"]["key"] == "my_mode_a.bcfg"
     assert diff[1]["SecondOnly"]["key"] == "my_mode_b.bcfg"
+
+
+@buck_test()
+async def test_config_diff_tracker_modfile_change(buck: Buck, tmp_path: Path) -> None:
+    record_file = tmp_path / "record.json"
+    await buck.build(
+        "//:simple",
+        *with_buck2_output("out"),
+        "@root//mode/my_mode_a",
+    )
+    await buck.build(
+        "//:simple",
+        *with_buck2_output("out"),
+        "@root//mode/my_mode_b",
+        "--unstable-write-invocation-record",
+        str(record_file),
+    )
+    cell_config_diffs = await filter_events(
+        buck, "Event", "data", "Instant", "data", "CellConfigDiff"
+    )
+    cell_config_diff = cell_config_diffs[0]
+    assert cell_config_diff["new_config_indicator_only"]
+    # Config diff count/size tracking is broken as can be seen below.
+    # This will be removed and replaced with external config diffing next.
+    assert (
+        cell_config_diff["config_diff_count"] == 0
+        and cell_config_diff["config_diff_size"] == 0
+    )
+
+    assert read_invocation_record(record_file)["new_configs_used"] == 1
+
+
+@buck_test()
+async def test_config_diff_tracker_no_change(buck: Buck, tmp_path: Path) -> None:
+    record_file = tmp_path / "record.json"
+    await buck.build(
+        "//:simple",
+        *with_buck2_output("out"),
+        "@root//mode/my_mode_a",
+    )
+    await buck.build(
+        "//:simple",
+        *with_buck2_output("out"),
+        "@root//mode/my_mode_a",
+        "--unstable-write-invocation-record",
+        str(record_file),
+    )
+    cell_config_diffs = await filter_events(
+        buck, "Event", "data", "Instant", "data", "CellConfigDiff"
+    )
+    assert len(cell_config_diffs) == 0
+    assert read_invocation_record(record_file)["new_configs_used"] == 0
