2dc016507209286dc3154f033ab5f850009dce65
Chenhao Zuo <nero@meta.com>
2025-02-04 10:50:37 -0800

    Add lazy utils function to try resolve lazy_dict
    Summary:
    Add lazy utils function to try resolve lazy_dict
    
    Will add doc later
    
    Reviewed By: IanChilds
    
    Differential Revision: D68962012
    
    fbshipit-source-id: 9b3444348698cddc3d2bada3d9f7885c65ff9031

---
+++ tests/e2e/bxl/utils/test_lazy_utils.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test(inplace=True)
+async def test_catch_resolve_lazy_dict(buck: Buck) -> None:
+    await buck.bxl(
+        "fbcode//buck2/tests/e2e/bxl/utils/test_lazy_utils.bxl:test_catch_resolve_lazy_dict",
+    )
