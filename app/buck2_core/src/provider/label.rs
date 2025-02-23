0780249b116e8ef1cd4e9517613fa5295586de13
Jakob Degen <jakobdegen@meta.com>
2025-02-23 06:37:35 -0800

    patterns: Remove `check_not_relative_label`
    Summary: In all cases where this is used, the value will subsequently be coerced to a label using the `attr_coercion_context_for_bzl`, which sets `TargetParsingRel::RequireAbsolute` and so will check this at that time
    
    Reviewed By: IanChilds
    
    Differential Revision: D69713839
    
    fbshipit-source-id: bb75b1484fae36e4a672f60ff82c8ffeff183805

--- app/buck2_core/src/provider/label.rs
+++ app/buck2_core/src/provider/label.rs
@@ -230,13 +230,6 @@ impl ProvidersLabel {
     pub fn configure_pair_no_exec(&self, cfg: ConfigurationNoExec) -> ConfiguredProvidersLabel {
         self.configure_pair(cfg.cfg_pair().dupe())
     }
-
-    /// Determines whether a string, **IF IT IS LATER COERCED** would be a relative label.
-    ///
-    /// This function **DOES NOT** validate the entire label string.
-    pub fn maybe_relative_label(raw_label: &str) -> bool {
-        raw_label.starts_with(':')
-    }
 }
 
 impl Serialize for ProvidersLabel {
@@ -346,7 +339,6 @@ pub mod testing {
 #[cfg(test)]
 mod tests {
     use crate::provider::label::ProviderName;
-    use crate::provider::label::ProvidersLabel;
 
     #[test]
     fn providers_name_validation() {
@@ -354,17 +346,5 @@ mod tests {
         ProviderName::new("foo_-,.=+/1".to_owned()).unwrap();
         assert!(ProviderName::new("foo bar".to_owned()).is_err());
         assert!(ProviderName::new("foo@bar".to_owned()).is_err());
-    }
-
-    #[test]
-    fn providers_label_maybe_relative() {
-        assert!(ProvidersLabel::maybe_relative_label(":foo"));
-        assert!(ProvidersLabel::maybe_relative_label(":foo[bar]"));
-        assert!(ProvidersLabel::maybe_relative_label(":invalid@label"));
-        assert!(!ProvidersLabel::maybe_relative_label("root//:bar"));
-        assert!(!ProvidersLabel::maybe_relative_label("root//foo:foo"));
-        assert!(!ProvidersLabel::maybe_relative_label(
-            "root//foo:invalid@label"
-        ));
     }
 }
--- app/buck2_interpreter_for_build_tests/src/attr.rs
+++ app/buck2_interpreter_for_build_tests/src/attr.rs
@@ -246,7 +246,7 @@ fn dep_works() -> buck2_error::Result<()> {
             attrs.dep(default=":reltarget")
         "#
         ),
-        "Use a fully qualified",
+        "Must be absolute",
     );
     Ok(())
 }
@@ -274,7 +274,7 @@ fn source_works() -> buck2_error::Result<()> {
             attrs.source(default=":reltarget")
         "#
         ),
-        "Use a fully qualified",
+        "Must be absolute",
     );
     Ok(())
 }
