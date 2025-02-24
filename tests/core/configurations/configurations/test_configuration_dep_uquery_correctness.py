488d20c3d4e7a2277f21dfc1dc7bc8d4dca2ec70
Jakob Degen <jakobdegen@meta.com>
2025-02-24 10:02:00 -0800

    configuration: Repro `platform_deps` uquery incorrectness
    Summary: TIL about `platform_deps`. They don't work very well
    
    Reviewed By: IanChilds
    
    Differential Revision: D69713855
    
    fbshipit-source-id: fc66e8c720ecf9981123b4c650c82f254bb4f46f

---
+++ tests/core/configurations/configurations/test_configuration_dep_uquery_correctness.py
@@ -0,0 +1,22 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+# pyre-strict
+
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test()
+async def test_default_target_platform(buck: Buck) -> None:
+    result = await buck.uquery(
+        "somepath(:with_custom_dtp, :base)",
+    )
+    path = result.stdout.splitlines()
+    # FIXME(JakobDegen): Bug. This dependency exists.
+    assert len(path) == 0
