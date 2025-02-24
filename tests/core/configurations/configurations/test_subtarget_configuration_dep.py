3cab07e140c85c8c953e3ef4961a691e41433f13
Jakob Degen <jakobdegen@meta.com>
2025-02-24 10:02:00 -0800

    configuration: Repro buggy subtarget handling on `default_target_platform`
    Summary: As I said, doesn't work too well
    
    Reviewed By: IanChilds
    
    Differential Revision: D69713837
    
    fbshipit-source-id: 36c5aae218f36cb265acadf7ec84ce882bc6a39e

---
+++ tests/core/configurations/configurations/test_subtarget_configuration_dep.py
@@ -0,0 +1,22 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+# pyre-strict
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.asserts import expect_failure
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test()
+async def test_default_target_platform_is_subtarget(buck: Buck) -> None:
+    # FIXME(JakobDegen): Bug. The target specifies a subtarget that does have an appropriate
+    # provider.
+    await expect_failure(
+        buck.cquery(":stub"),
+        stderr_regex="Expected `root//:alias_platform` to be a `platform\\(\\)` target",
+    )
