633ce7f2daa12c24f270f6f5acc1f2bab7b85b36
Jakob Degen <jakobdegen@meta.com>
2025-02-24 13:26:58 -0800

    configuration: Repro another bug with subtarget handling
    Summary: I'm not sure if this actually causes any problems, but it's definitely wrong
    
    Reviewed By: IanChilds
    
    Differential Revision: D69713863
    
    fbshipit-source-id: 70ac82e3eacd44ed03153a7072a47c92d13074f4

--- tests/core/configurations/configurations/test_subtarget_configuration_dep.py
+++ tests/core/configurations/configurations/test_subtarget_configuration_dep.py
@@ -7,6 +7,8 @@
 
 # pyre-strict
 
+import json
+
 from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.asserts import expect_failure
 from buck2.tests.e2e_util.buck_workspace import buck_test
@@ -20,3 +22,13 @@ async def test_default_target_platform_is_subtarget(buck: Buck) -> None:
         buck.cquery(":stub"),
         stderr_regex="Expected `root//:alias_platform` to be a `platform\\(\\)` target",
     )
+
+
+@buck_test()
+async def test_subtarget_in_select_key(buck: Buck) -> None:
+    res = await buck.uquery(
+        "root//:with_constraint_key_dep", "-a", "buck.configuration_deps"
+    )
+    res = json.loads(res.stdout)
+    # FIXME(JakobDegen): Bug. `buck.deps`-like attributes do not include subtargets
+    assert list(res.values())[0]["buck.configuration_deps"] == ["root//:cat_alias[sub]"]
