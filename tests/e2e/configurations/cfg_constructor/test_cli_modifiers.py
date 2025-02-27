0e32b28a426ab0fc1943eb0ca46ab7af30aae229
Scott Cao <scottcao@meta.com>
2025-02-27 15:08:28 -0800

    Implement testing allowlist
    Summary:
    For sake of preventing arbitrary usage of CLI modifiers/modefiles containing modifiers, we have an explicit allowlist for CLI modifiers can be used on CI. However, this can only be entirely enabled or disabled, and there are several scenarios that would benefit from more fine-grained allowlisting. One scenario is e2e testing. It's perfectly valid to use most cli modifiers for e2e tests, but those cli modifiers often don't need to be allowlisted for use cases besides testing.
    
    This diff implements a separate allowlist for CLI modifiers on CI when used as part of e2e tests. This only allowlists modifiers for constraints in the list when running under e2e test. We check for whether we are in an e2e test environment by reading the `buildfile.extra_for_test` buckconfig.
    
    Reviewed By: samkevich
    
    Differential Revision: D69970561
    
    fbshipit-source-id: fc1ed7b117763489e18ec1f059c9e97d02405948

--- tests/e2e/configurations/cfg_constructor/test_cli_modifiers.py
+++ tests/e2e/configurations/cfg_constructor/test_cli_modifiers.py
@@ -5,9 +5,6 @@
 # License, Version 2.0 found in the LICENSE-APACHE file in the root directory
 # of this source tree.
 
-# pyre-strict
-
-
 from buck2.tests.e2e.configurations.cfg_constructor.modifiers_util import get_cfg
 from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.asserts import expect_failure
@@ -19,15 +16,23 @@ DATA_DIR = (
 TARGET = f"{DATA_DIR}:test_target"
 CONSTRAINT_A = f"{DATA_DIR}:A_1"
 CONSTRAINT_B = f"{DATA_DIR}:B_1"
+BUCK_TEST_MARKER = buck_test(
+    inplace=True,
+    extra_buck_config={
+        # CLI modifier validation is disabled for users and enabled for CI. To make sure this test case always has CLI modifier validation enabled,
+        # explicitly enable it here.
+        "buck2": {"skip_cli_modifier_validation_DO_NOT_SET_TO_TRUE_ON_CI": ""}
+    },
+)
 
 
-@buck_test(inplace=True)
+@BUCK_TEST_MARKER
 async def test_one_cli_modifier(buck: Buck) -> None:
     # -m A
     assert CONSTRAINT_A in await get_cfg(buck, TARGET, "--modifier", CONSTRAINT_A)
 
 
-@buck_test(inplace=True)
+@BUCK_TEST_MARKER
 async def test_two_cli_modifier(buck: Buck) -> None:
     # -m A,B
     result = await get_cfg(
@@ -37,7 +42,7 @@ async def test_two_cli_modifier(buck: Buck) -> None:
     assert CONSTRAINT_B in result
 
 
-@buck_test(inplace=True)
+@BUCK_TEST_MARKER
 async def test_cli_modifiers_bad_input(buck: Buck) -> None:
     # -m A B (error)
     await expect_failure(
@@ -46,7 +51,7 @@ async def test_cli_modifiers_bad_input(buck: Buck) -> None:
     )
 
 
-@buck_test(inplace=True)
+@BUCK_TEST_MARKER
 async def test_cli_modifier_alias(buck: Buck) -> None:
     assert "ovr_config//os/constraints:linux" in await get_cfg(
         buck, TARGET, "--modifier", "linux"
