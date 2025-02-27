cf7588152137f175d0b3e162a6e2d49866b1962e
Scott Cao <scottcao@meta.com>
2025-02-27 14:43:02 -0800

    impl StrongHash for Hashed
    Summary:
    Implement `StrongHash` for `Hashed<T>` if T also uses `StrongHash`.
    
    This is working up to implementing `StrongHash` for configuration data structures so we can fix hash collision in configuration hashes.
    
    Reviewed By: samkevich
    
    Differential Revision: D68197230
    
    fbshipit-source-id: aa236b070fb20004930e631abab352bd017d8be9

--- starlark-rust/starlark_map/src/hashed.rs
+++ starlark-rust/starlark_map/src/hashed.rs
@@ -24,6 +24,8 @@ use std::ops::Deref;
 use allocative::Allocative;
 use dupe::Dupe;
 use equivalent::Equivalent;
+use strong_hash::StrongHash;
+use strong_hash::StrongHasher;
 
 use crate::hash_value::StarlarkHashValue;
 
@@ -40,6 +42,13 @@ impl<K> Hash for Hashed<K> {
     fn hash<S: Hasher>(&self, state: &mut S) {
         // Only hash the hash, not the key.
         self.hash.hash(state)
+    }
+}
+
+impl<K: StrongHash> StrongHash for Hashed<K> {
+    fn strong_hash<S: StrongHasher>(&self, state: &mut S) {
+        // Only hash the key, not the (weak) hash.
+        self.key.strong_hash(state);
     }
 }
 
