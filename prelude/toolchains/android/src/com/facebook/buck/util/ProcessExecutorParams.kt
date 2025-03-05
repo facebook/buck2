e115b44831111120f9c1f12cb7fa3915a4f5b13b
Joshua Selbo <jselbo@meta.com>
2025-03-05 10:00:49 -0800

    Convert ProcessExecutorParams to Kotlin
    Summary: After D70560988, we can now convert this to a Kotlin data class.
    
    Reviewed By: hick209
    
    Differential Revision: D70506179
    
    fbshipit-source-id: e76e743fa25523a15b94e2b53f849bef9ad280ca

---
+++ prelude/toolchains/android/src/com/facebook/buck/util/ProcessExecutorParams.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+package com.facebook.buck.util
+
+import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableMap
+import java.nio.file.Path
+import java.util.Optional
+
+/** Value type passed to [ProcessExecutor] to launch a process. */
+data class ProcessExecutorParams
+@JvmOverloads
+constructor(
+    /** The command and arguments to launch. */
+    val command: ImmutableList<String>,
+    /** If present, the current working directory for the launched process. */
+    val environment: Optional<ImmutableMap<String, String?>> = Optional.empty(),
+    /**
+     * If present, the map of environment variables used for the launched process. Otherwise,
+     * inherits the current process's environment.
+     */
+    val directory: Optional<Path> = Optional.empty()
+) {
+
+  companion object {
+    @JvmStatic
+    fun ofCommand(vararg args: String): ProcessExecutorParams {
+      return ProcessExecutorParams(ImmutableList.copyOf(args))
+    }
+  }
+}
