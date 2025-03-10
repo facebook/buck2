a9f3dfceda97efa9752dc835a8872c076d1a5391
Nuri Amari <nuriamari@meta.com>
2025-03-09 20:49:36 -0700

    Remove unused parts of Darwin dist_lto_prepare script
    Summary: Distributed thin-lto requires you to pass object files to the linker directly, surrounding them in `--start-lib` `--end-lib` flags if you want archive semantics. This script is used to take static archives and unarchive them such that the contents can then be passed to a linker invocation. It is meant to support both thin archives and regular archives, but in my estimation, it doesn't actually work for thin-archives. For the time being, we won't be needing thin archives support. Any archive we build ourselves we will just pass the object files directly instead of putting them in an archive, then extracting them again. The only archives we still have to extract are vendor provided archives (ie. from the platform SDK), and those up until now are conventional static archives.
    
    Reviewed By: rmaz
    
    Differential Revision: D70174175
    
    fbshipit-source-id: 7d2866e859719d3d1f67ad4bc1096bda786a4831

--- prelude/cxx/dist_lto/tools/dist_lto_prepare_darwin.py
+++ prelude/cxx/dist_lto/tools/dist_lto_prepare_darwin.py
@@ -12,20 +12,13 @@ producing a manifest of the objects contained within.
 """
 
 import argparse
-import enum
 import json
 import os
 import shutil
 import subprocess
 import sys
 import tempfile
-from typing import List, Tuple
-
-
-class ArchiveKind(enum.IntEnum):
-    UNKNOWN = 0
-    ARCHIVE = 1
-    THIN_ARCHIVE = 2
+from typing import List
 
 
 def _gen_path(parent_path: str, filename: str) -> str:
@@ -46,23 +39,6 @@ def _gen_filename(filename: str, num_of_instance: int) -> str:
         return filename
 
 
-def identify_file(path: str) -> Tuple[ArchiveKind, str]:
-    path = os.path.realpath(path)
-    output = subprocess.check_output(["file", "-b", path]).decode()
-    if "ar archive" in output:
-        return (ArchiveKind.ARCHIVE, output)
-    elif "thin archive" in output:
-        return (ArchiveKind.THIN_ARCHIVE, output)
-    else:
-        with open(path, "rb") as infile:
-            head = infile.read(7)
-
-            if head == "!<thin>".encode():
-                return (ArchiveKind.THIN_ARCHIVE, output)
-
-    return (ArchiveKind.UNKNOWN, output)
-
-
 def main(argv: List[str]) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("--manifest-out")
@@ -76,77 +52,64 @@ def main(argv: List[str]) -> int:
     os.makedirs(objects_path, exist_ok=True)
 
     known_objects = []
-    file_type, debug_output = identify_file(args.archive)
-    if file_type == ArchiveKind.ARCHIVE:
-        # Unfortunately, we use llvm-ar and, while binutils ar has had --output for
-        # a long time, llvm-ar does not support --output and the change in llvm-ar
-        # looks like it has stalled for years (https://reviews.llvm.org/D69418)
-        # So, we need to invoke ar in the directory that we want it to extract into, and so
-        # need absolute paths.
-        ar_path = os.path.abspath(args.ar)
-        archive_path = os.path.abspath(args.archive)
-        output = subprocess.check_output(
-            [ar_path, "t", archive_path], cwd=objects_path
-        ).decode()
-        member_list = [member for member in output.split("\n") if member]
+    # Unfortunately, we use llvm-ar and, while binutils ar has had --output for
+    # a long time, llvm-ar does not support --output and the change in llvm-ar
+    # looks like it has stalled for years (https://reviews.llvm.org/D69418)
+    # So, we need to invoke ar in the directory that we want it to extract into, and so
+    # need absolute paths.
+    ar_path = os.path.abspath(args.ar)
+    archive_path = os.path.abspath(args.archive)
+    output = subprocess.check_output(
+        [ar_path, "t", archive_path], cwd=objects_path
+    ).decode()
+    member_list = [member for member in output.split("\n") if member]
 
-        # This will extract all the members of the archive, including duplicates
-        # replacing existing duplicates. That is if first/foo.txt and second/foo.txt
-        # are placed in an archive in that order, this will leave second/foo.txt
-        # in the objects_path.
-        output = subprocess.check_output(
-            [ar_path, "xv", archive_path], cwd=objects_path
-        ).decode()
+    # This will extract all the members of the archive, including duplicates
+    # replacing existing duplicates. That is if first/foo.txt and second/foo.txt
+    # are placed in an archive in that order, this will leave second/foo.txt
+    # in the objects_path.
+    output = subprocess.check_output(
+        [ar_path, "xv", archive_path], cwd=objects_path
+    ).decode()
 
-        # Count all members of the same name.
-        counter = {}
-        for member in member_list:
-            counter.setdefault(member, 0)
-            counter[member] += 1
-            # Insert all objects at most once into the list of known objects
-            if counter[member] == 1:
-                known_objects.append(_gen_path(objects_path, member))
+    # Count all members of the same name.
+    counter = {}
+    for member in member_list:
+        counter.setdefault(member, 0)
+        counter[member] += 1
+        # Insert all objects at most once into the list of known objects
+        if counter[member] == 1:
+            known_objects.append(_gen_path(objects_path, member))
 
-        with tempfile.TemporaryDirectory() as temp_dir:
-            # For each duplicate member, rename and extract duplicates 1 through N
-            # inclusive. While N was already extracted above, we don't want to rely
-            # upon this implementation detail of llvm-ar.
-            for member, count in counter.items():
-                if count <= 1:
-                    continue
-                for current in range(1, count + 1):
-                    # extract the file from archive
-                    output = subprocess.check_output(
-                        [
-                            ar_path,
-                            "xN",
-                            str(current),
-                            archive_path,
-                            member,
-                        ],
-                        cwd=temp_dir,
-                    ).decode()
-                    unique_name = _gen_filename(member, current)
-                    # rename and move the newly extracted file to objects_path
-                    shutil.move(
-                        os.path.join(temp_dir, member),
-                        os.path.join(os.path.abspath(objects_path), unique_name),
-                    )
-                    if current > 1:
-                        known_objects.append(_gen_path(objects_path, unique_name))
-
-    elif file_type == ArchiveKind.THIN_ARCHIVE:
-        output = subprocess.check_output([args.ar, "t", args.archive]).decode()
-        for line in output.splitlines():
-            assert os.path.exists(line)
-            known_objects.append(line)
-    elif file_type == ArchiveKind.UNKNOWN:
-        raise AssertionError(
-            f"unknown archive kind for file {args.archive}: {debug_output}"
-        )
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # For each duplicate member, rename and extract duplicates 1 through N
+        # inclusive. While N was already extracted above, we don't want to rely
+        # upon this implementation detail of llvm-ar.
+        for member, count in counter.items():
+            if count <= 1:
+                continue
+            for current in range(1, count + 1):
+                # extract the file from archive
+                output = subprocess.check_output(
+                    [
+                        ar_path,
+                        "xN",
+                        str(current),
+                        archive_path,
+                        member,
+                    ],
+                    cwd=temp_dir,
+                ).decode()
+                unique_name = _gen_filename(member, current)
+                # rename and move the newly extracted file to objects_path
+                shutil.move(
+                    os.path.join(temp_dir, member),
+                    os.path.join(os.path.abspath(objects_path), unique_name),
+                )
+                if current > 1:
+                    known_objects.append(_gen_path(objects_path, unique_name))
 
     manifest = {
-        "debug": debug_output,
         "objects": known_objects,
     }
     with open(os.path.join(args.manifest_out), "w") as outfile:
