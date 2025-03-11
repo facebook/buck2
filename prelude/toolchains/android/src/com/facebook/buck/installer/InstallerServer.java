0e4e0a5408400b37a56ddb5e47a23be701f47f2d
Chris Tolliday <ctolliday@meta.com>
2025-03-11 14:23:40 -0700

    Stop server after disconnect and connection timeout
    Summary:
    The previous diff is insufficient to prevent zombie processes since there are common scenarios where the installer won't be shut down by buck2.
    
    Buck2 builds and launches the installer in parallel with building the app to install. If the app build fails the installer may have launched without connecting to buck2.
    We could make buck always block on connecting and sending a shutdown if it starts but we can't guarantee that buck cleans up correctly since buck could be force killed at all.
    
    There is code in the installer to shut down if the parent process dies but it's broken on Windows and it doesn't seem ideal to require users to run `buck2 kill` if they hit build errors from this.
    
    Reviewed By: navidqar
    
    Differential Revision: D70910673
    
    fbshipit-source-id: 2a67815873a3d18ed9af952e8e19fd62aab1dad5

--- prelude/toolchains/android/src/com/facebook/buck/installer/InstallerServer.java
+++ prelude/toolchains/android/src/com/facebook/buck/installer/InstallerServer.java
@@ -15,7 +15,9 @@ import com.facebook.buck.util.types.Unit;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.SettableFuture;
+import io.grpc.Attributes;
 import io.grpc.Server;
+import io.grpc.ServerTransportFilter;
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import io.grpc.netty.shaded.io.netty.channel.EventLoopGroup;
 import io.grpc.netty.shaded.io.netty.channel.nio.NioEventLoopGroup;
@@ -23,14 +25,17 @@ import io.grpc.netty.shaded.io.netty.channel.socket.nio.NioServerSocketChannel;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger; // NOPMD
 
-public class InstallerServer {
+public class InstallerServer extends ServerTransportFilter {
   Server grpcServer;
   Logger logger;
+  boolean clientConnected = false;
 
   public InstallerServer(InstallCommand installer, Logger logger, int tcpPort) {
     SettableFuture<Unit> isDone = SettableFuture.create();
@@ -66,6 +71,7 @@ public class InstallerServer {
 
   public void run() throws IOException, InterruptedException {
     this.grpcServer.start();
+    Instant serverStartTime = Instant.now();
 
     do {
       try {
@@ -73,10 +79,32 @@ public class InstallerServer {
           logger.info("Parent process has been killed, shutting down");
           stopServer();
         }
+        long serverUptime = Duration.between(serverStartTime, Instant.now()).toSeconds();
+        if (!this.clientConnected && serverUptime > 20) {
+          logger.info("No client connected for 20 seconds, shutting down");
+          stopServer();
+        }
       } catch (Exception e) {
         logger.log(Level.SEVERE, "Error checking parent process liveness", e);
       }
     } while (!this.grpcServer.awaitTermination(10, TimeUnit.SECONDS));
+  }
+
+  /** Called when buck2 connects */
+  public Attributes transportReady(Attributes transportAttrs) {
+    if (this.clientConnected) {
+      this.logger.log(
+          Level.SEVERE,
+          "Unexpected state, multiple connections detected. Should be 1 connection per server.");
+    }
+    this.clientConnected = true;
+    return transportAttrs;
+  }
+
+  /** Called when buck2 disconnects */
+  public void transportTerminated(Attributes transportAttrs) {
+    this.logger.log(Level.SEVERE, "Client disconnected, shutting down");
+    stopServer();
   }
 
   /** Build an installer server at requested {@code TCP}. */
@@ -88,12 +116,14 @@ public class InstallerServer {
   private Server getTCPServer(InstallerService service, Logger logger, int tcpPort) {
     EventLoopGroup group = new NioEventLoopGroup();
     SocketAddress addr = new InetSocketAddress("127.0.0.1", tcpPort); // NOPMD
+
     Server server =
         NettyServerBuilder.forAddress(addr)
             .channelType(NioServerSocketChannel.class)
             .workerEventLoopGroup(group)
             .bossEventLoopGroup(group)
             .addService(service)
+            .addTransportFilter(this)
             .build();
     logger.info(String.format("Starting server listening on TCP port %s", tcpPort));
     return server;
