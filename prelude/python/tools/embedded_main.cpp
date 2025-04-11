b0b73953c9ca4af0669247d6e6c6693a3136645c
Loren Arthur <lorenarthur@meta.com>
2025-04-11 12:10:51 -0700

    Don't initialize static_extension_finder in embedded_main
    Summary:
    We add `static_extension_finder:_initialize` as a startup function which get's called in `sitecustomize.py`
    https://www.internalfb.com/code/fbsource/[d376626e7c1a]/tools/build_defs/fbcode_macros/build_defs/lib/python_common.bzl?lines=1836%3Fcommit%3D5b81c1e9fec0
    So we do not need to do it here. This keeps us from getting errors when using `sys.executable` without setting the PYTHONPATH.
    
    Reviewed By: itamaro, AishwaryaSivaraman
    
    Differential Revision: D72184536
    
    fbshipit-source-id: 1d140e31706fb89468c87d858f697861a87cfef4

--- prelude/python/tools/embedded_main.cpp
+++ prelude/python/tools/embedded_main.cpp
@@ -165,35 +165,6 @@ int main(int argc, char* argv[]) {
         Py_DECREF(result);
       });
     };
-
-    // Call static_extension_finder._initialize()
-    PyObject* pmodule = PyImport_ImportModule("static_extension_finder");
-    if (pmodule == nullptr) {
-      PyErr_Print();
-      fprintf(
-          stderr, "Error: could not import module 'static_extension_finder'\n");
-      // return 1;
-    } else {
-      PyObject* pinitialize = PyObject_GetAttrString(pmodule, "_initialize");
-      Py_DECREF(pmodule);
-      if (pinitialize == nullptr || !PyCallable_Check(pinitialize)) {
-        PyErr_Print();
-        fprintf(
-            stderr,
-            "Error: could not find '_initialize' in module 'static_extension_finder'\n");
-        // return 1;
-      }
-      PyObject* retvalue = PyObject_CallObject(pinitialize, nullptr);
-      Py_DECREF(pinitialize);
-      if (retvalue == nullptr) {
-        PyErr_Print();
-        fprintf(
-            stderr,
-            "Error: could not call 'static_extension_finder._initialize()'\n");
-        // return 1;
-      }
-      Py_DECREF(retvalue);
-    }
   }
 
   PyConfig_Clear(&config);
