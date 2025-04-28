510a0c9921756766f6f27dc85bd721ec901c7e1b
Rebecca Turner <rbt@sent.as>
2025-04-28 12:19:41 -0700

    `//A/B/C` is a valid name for `//A/B/C:C`
    > Prefer using the short name when referring to an eponymous target (`//x` instead of `//x:x`). If
    > you are in the same package, prefer the local reference (`:x` instead of `//x`).
    
    See: https://bazel.build/build/style-guide#target-naming
    
    This syntax is supported on the command-line, but not in BUCK files. Unfortunately, the only(?)
    Starlark formatter (buildifier: https://github.com/bazelbuild/buildtools) automatically abbreviates
    targets on the assumption that this syntax is valid, which causes errors when using Buck2:
    
        ...
        4: Error coercing "//src/Foo"
        5: Invalid absolute target pattern `//src/Foo` is not allowed
        6: Expected a `:`, a trailing `/...` or the literal `...`.
    
    We can adjust the parsing rules to allow this syntax in more places.

--- app/buck2_core/src/pattern/pattern.rs
+++ app/buck2_core/src/pattern/pattern.rs
@@ -302,7 +302,7 @@ impl<T: PatternType> ParsedPattern<T> {
             cell_alias_resolver,
             TargetParsingOptions {
                 relative,
-                infer_target: false,
+                infer_target: true,
                 strip_package_trailing_slash: false,
             },
             pattern,
@@ -1511,7 +1511,8 @@ mod tests {
             CellRelativePath::unchecked_new("package").to_owned(),
         );
 
-        assert_matches!(
+        assert_eq!(
+            mk_target("root", "package/path", "path"),
             ParsedPattern::<TargetPatternExtra>::parse_not_relaxed(
                 "path",
                 TargetParsingRel::AllowRelative(
@@ -1520,12 +1521,7 @@ mod tests {
                 ),
                 &resolver(),
                 &alias_resolver(),
-            ),
-            Err(e) => {
-                assert!(
-                    format!("{:?}", e).contains(&format!("{}", TargetPatternParseError::UnexpectedFormat))
-                );
-            }
+            )?
         );
 
         assert_eq!(
@@ -1693,7 +1689,7 @@ mod tests {
             ),
             Err(e) => {
                 assert!(
-                    format!("{:?}", e).contains(&format!("{}", TargetPatternParseError::UnexpectedFormat))
+                    format!("{:?}", e).contains(&format!("{}", TargetPatternParseError::AbsoluteRequired))
                 );
             }
         );
