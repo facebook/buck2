de0762ecafbb9656ce4cd07a6eca39ab83b520a9
Chris Hopman <cjhopman@meta.com>
2025-05-02 16:38:14 -0700

    Add utility for aggregating metrics
    Summary: Just simple utilities to allow processing an event at a time into these aggregated metrics.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D73392170
    
    fbshipit-source-id: 63730310ad27e3a5ad91b05a41e87552f8e622ea

--- app/buck2_build_api/src/build/detailed_aggregated_metrics/types.rs
+++ app/buck2_build_api/src/build/detailed_aggregated_metrics/types.rs
@@ -11,7 +11,10 @@ use std::sync::Arc;
 
 use buck2_artifact::actions::key::ActionKey;
 use buck2_core::provider::label::ConfiguredProvidersLabel;
+use buck2_core::provider::label::ProvidersName;
+use buck2_data::ToProtoMessage;
 use buck2_node::nodes::configured::ConfiguredTargetNode;
+use dupe::Dupe;
 
 use crate::artifact_groups::ArtifactGroup;
 use crate::build::BuildProviderType;
@@ -24,6 +27,11 @@ pub struct ActionExecutionMetrics {
     pub output_size_bytes: u64,
 }
 
+pub struct AnalysisMetrics {
+    pub actions: usize,
+    pub retained_memory: usize,
+}
+
 pub struct TopLevelTargetSpec {
     pub label: Arc<ConfiguredProvidersLabel>,
     pub target: ConfiguredTargetNode,
@@ -34,4 +42,155 @@ pub struct TopLevelTargetSpec {
 pub struct PerBuildEvents {
     pub executed_actions: fxhash::FxHashSet<ActionKey>,
     pub top_level_targets: Vec<TopLevelTargetSpec>,
+}
+
+pub struct TopLevelTargetAggregatedData {
+    proto: buck2_data::TopLevelTargetMetrics,
+}
+
+#[derive(Clone, Copy, Dupe)]
+pub enum BuiltWhen {
+    ThisBuild,
+    Previously,
+}
+
+impl TopLevelTargetAggregatedData {
+    pub fn new(target: &ConfiguredProvidersLabel, action_graph_size: Option<usize>) -> Self {
+        Self {
+            proto: buck2_data::TopLevelTargetMetrics {
+                target: Some(target.target().as_proto()),
+                provider: match target.name() {
+                    ProvidersName::Default => None,
+                    v => Some(v.to_string()),
+                },
+                action_graph_size: action_graph_size.map(|v| v as u64),
+                metrics: Some(buck2_data::AggregatedBuildMetrics::default()),
+                amortized_metrics: Some(buck2_data::AggregatedBuildMetrics::default()),
+            },
+        }
+    }
+
+    pub fn aggregate_execution_event(
+        &mut self,
+        factor: usize,
+        ev: &ActionExecutionMetrics,
+        when: BuiltWhen,
+    ) {
+        let factor = 1.0 / (factor as f64);
+        self.proto
+            .metrics
+            .as_mut()
+            .unwrap()
+            .aggregate_execution(1.0, ev, when);
+        self.proto
+            .amortized_metrics
+            .as_mut()
+            .unwrap()
+            .aggregate_execution(factor, ev, when);
+    }
+
+    pub fn aggregate_analysis_event(&mut self, factor: usize, ev: &AnalysisMetrics) {
+        let factor = 1.0 / (factor as f64);
+        self.proto
+            .metrics
+            .as_mut()
+            .unwrap()
+            .aggregate_analysis(1.0, ev);
+        self.proto
+            .amortized_metrics
+            .as_mut()
+            .unwrap()
+            .aggregate_analysis(factor, ev);
+    }
+
+    pub fn into_proto(self) -> buck2_data::TopLevelTargetMetrics {
+        self.proto
+    }
+}
+
+trait AggregatedBuildMetricsExt {
+    fn aggregate_execution(&mut self, factor: f64, ev: &ActionExecutionMetrics, when: BuiltWhen);
+    fn aggregate_analysis(&mut self, factor: f64, ev: &AnalysisMetrics);
+}
+
+impl AggregatedBuildMetricsExt for buck2_data::AggregatedBuildMetrics {
+    fn aggregate_execution(&mut self, factor: f64, ev: &ActionExecutionMetrics, when: BuiltWhen) {
+        self.full_graph_execution_time_ms += factor * (ev.execution_time_ms as f64);
+        self.full_graph_output_size_bytes += factor * (ev.output_size_bytes as f64);
+
+        if let BuiltWhen::ThisBuild = when {
+            match ev.execution_kind {
+                buck2_data::ActionExecutionKind::Local
+                | buck2_data::ActionExecutionKind::LocalWorker => {
+                    self.local_execution_time_ms += factor * (ev.execution_time_ms as f64);
+                    self.local_executions += factor;
+                }
+                buck2_data::ActionExecutionKind::Remote => {
+                    self.remote_execution_time_ms += factor * (ev.execution_time_ms as f64);
+                    self.remote_executions += factor;
+                }
+                buck2_data::ActionExecutionKind::ActionCache
+                | buck2_data::ActionExecutionKind::RemoteDepFileCache => {
+                    self.remote_cache_hits += factor;
+                }
+                buck2_data::ActionExecutionKind::NotSet
+                | buck2_data::ActionExecutionKind::Simple
+                | buck2_data::ActionExecutionKind::Deferred
+                | buck2_data::ActionExecutionKind::LocalDepFile
+                | buck2_data::ActionExecutionKind::LocalActionCache => {
+                    // ignored.
+                }
+            }
+        }
+    }
+
+    fn aggregate_analysis(&mut self, factor: f64, ev: &AnalysisMetrics) {
+        self.analysis_retained_memory += factor * (ev.retained_memory as f64);
+        self.declared_actions += factor * (ev.actions as f64);
+    }
+}
+
+pub struct AllTargetsAggregatedData {
+    pub proto: buck2_data::AllTargetsBuildMetrics,
+}
+
+#[allow(unused)]
+impl AllTargetsAggregatedData {
+    pub fn new(action_graph_size: Option<usize>) -> Self {
+        Self {
+            proto: buck2_data::AllTargetsBuildMetrics {
+                action_graph_size: action_graph_size.map(|v| v as u64),
+                metrics: Some(buck2_data::AggregatedBuildMetrics::default()),
+                compute_time_ms: None,
+            },
+        }
+    }
+
+    pub(crate) fn aggregate_execution_event(
+        &mut self,
+        ev: &ActionExecutionMetrics,
+        built_when: BuiltWhen,
+    ) {
+        self.proto
+            .metrics
+            .as_mut()
+            .unwrap()
+            .aggregate_execution(1.0, ev, built_when);
+    }
+
+    pub fn into_proto(self) -> buck2_data::AllTargetsBuildMetrics {
+        self.proto
+    }
+
+    pub(crate) fn aggregate_analysis_event(&mut self, ev: &AnalysisMetrics) {
+        self.proto
+            .metrics
+            .as_mut()
+            .unwrap()
+            .aggregate_analysis(1.0, ev);
+    }
+
+    pub(crate) fn set_compute_time(&mut self, elapsed: std::time::Duration) {
+        self.proto.compute_time_ms = Some(elapsed.as_millis() as u64);
+    }
 }
