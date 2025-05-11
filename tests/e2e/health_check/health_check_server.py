3c4e158609c40d629bb08e007e8e9bbf72eb80f0
Rajneesh Lakkundi <rajneeshl@meta.com>
2025-05-11 05:25:56 -0700

    Add e2e tests
    Summary:
    Add test to ensure that hung health check server does not impact build. This is presently a failing test that is passed in the next diff.
    
    This diff adds the most basic test to unblock the release. In further diffs, will add other tests to ensure regression testing.
    
    Reviewed By: blackm00n
    
    Differential Revision: D73997897
    
    fbshipit-source-id: 5ad8d9145a79a75de0b1b3de865c5d21b83126bb

---
+++ tests/e2e/health_check/health_check_server.py
@@ -0,0 +1,125 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+# pyre-unsafe
+
+import argparse
+import logging
+import os
+import signal
+import sys
+import threading
+import time
+from concurrent import futures
+
+import grpc
+from buck2.app.buck2_health_check_proto import health_check_pb2, health_check_pb2_grpc
+
+
+class HealthChecker(health_check_pb2_grpc.HealthCheckServicer):
+    def __init__(self, stop_event, argsparse, *args, **kwargs):
+        self.args = argsparse
+        self.delay = argsparse.delay
+        self.with_request_hang = argsparse.with_request_hang
+        self.stats_file_handle = open(
+            os.environ["HEALTH_CHECK_SERVER_STATS_OUTPUT"], "w+"
+        )
+
+    def UpdateContext(self, request, context):
+        self.stats_file_handle.write("UpdateContext Requested\n")
+        self.stats_file_handle.flush()
+        self.handle_request_delays()
+        self.stats_file_handle.write("UpdateContext Completed\n")
+        self.stats_file_handle.flush()
+        return health_check_pb2.Empty()
+
+    def RunChecks(self, request, context):
+        self.stats_file_handle.write("RunChecks Requested\n")
+        self.stats_file_handle.flush()
+        self.handle_request_delays()
+        self.stats_file_handle.write("RunChecks Completed\n")
+        self.stats_file_handle.flush()
+        return health_check_pb2.HealthCheckResult(
+            reports=[health_check_pb2.Report(tag="test_tag")]
+        )
+
+    def handle_request_delays(self):
+        if self.with_request_hang:
+            while True:
+                time.sleep(1)
+        if self.delay:
+            time.sleep(int(self.delay))
+
+
+def shutdown(stop_event):
+    stop_event.set()
+
+
+def serve(args):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    stop_event = threading.Event()
+    health_checker = HealthChecker(stop_event, args)
+    health_check_pb2_grpc.add_HealthCheckServicer_to_server(health_checker, server)
+    listen_addr = server.add_insecure_port("127.0.0.1:0")
+
+    with open(args.state_info_file, "w+") as f:
+        f.write(str(listen_addr))
+
+    # This server is started by a shell scrip from the buck2 binary.
+    # Since we don't have the process reference in tests, we need to send the SIGTERM instead of SIGINT.
+    signal.signal(signal.SIGTERM, lambda x, y: shutdown(stop_event))
+    signal.signal(signal.SIGINT, lambda x, y: shutdown(stop_event))
+
+    server.start()
+    try:
+        stop_event.wait()
+        print("Stopped RPC server, Waiting for RPCs to complete...")
+        server.stop(1).wait()
+    finally:
+        print("Exiting health check server")
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(description="Parse args for health check server")
+    parser.add_argument(
+        "--state-info-file",
+        help="File name to which the server port is written",
+        default="",
+    )
+    parser.add_argument(
+        "--with-init-hang",
+        required=False,
+        action="store_true",
+        help="flag that if set will hang on init",
+    )
+    parser.add_argument(
+        "--with-request-hang",
+        required=False,
+        action="store_true",
+        help="flag that if set will hang the request",
+    )
+    parser.add_argument(
+        "--delay",
+        type=int,
+        help="response delay in seconds",
+        default=0,
+        required=False,
+    )
+    # no need to parse --tcp-port and other not related params
+    args, _ = parser.parse_known_args(args)
+    return args
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    args = parse_args(sys.argv[1:])
+    serve(args)
+
+
+if __name__ == "__main__":
+    # Do not add code here, it won't be run. Add them to the function called below.
+    main()  # pragma: no cover
---
+++ tests/e2e/health_check/test_health_check.py
@@ -0,0 +1,114 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+# pyre-strict
+
+
+import asyncio
+
+import tempfile
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test(
+    inplace=True,
+    extra_buck_config={
+        "buck2_health_check": {
+            "enable_health_checks": "false",
+        }
+    },
+)
+async def test_health_check_disabled_with_request_hang(buck: Buck) -> None:
+    await buck.kill()
+    (
+        health_check_state_file,
+        server_output,
+        server_process,
+    ) = await start_health_check_server(buck, "--with-request-hang")
+
+    env = {
+        # Set the CLI_PATH to `echo` making it effectively a no-op since we want buck to use the test server and not spawn a new one.
+        "BUCK2_HEALTH_CHECK_CLI_PATH": "echo",
+        "BUCK2_HEALTH_CHECK_STATE_INFO_PATH": health_check_state_file,
+    }
+    await buck.build(
+        "fbcode//buck2/tests/targets/rules/rust/hello_world:welcome",
+        env=env,
+    )
+    with open(server_output, "r") as f:
+        # Until the health check server is enabled via buckconfig, the server will not receive any requests.
+        assert f.read().strip() == ""
+
+    server_process.kill()
+
+
+@buck_test(
+    inplace=True,
+    extra_buck_config={
+        "buck2_health_check": {
+            "enable_health_checks": "true",
+        }
+    },
+)
+async def test_health_check_enabled_with_request_hang(buck: Buck) -> None:
+    await buck.kill()
+    (
+        health_check_state_file,
+        server_output,
+        server_process,
+    ) = await start_health_check_server(buck, "--with-request-hang")
+
+    env = {
+        # Set the CLI_PATH to `echo` making it effectively a no-op since we want buck to use the test server and not spawn a new one.
+        "BUCK2_HEALTH_CHECK_CLI_PATH": "echo",
+        "BUCK2_HEALTH_CHECK_STATE_INFO_PATH": health_check_state_file,
+    }
+    await buck.build(
+        "fbcode//buck2/tests/targets/rules/rust/hello_world:welcome",
+        env=env,
+    )
+    with open(server_output, "r") as f:
+        lines = f.readlines()
+        # Only the first call to update is logged. The server is stuck processing that request.
+        assert lines.count("UpdateContext Requested\n") == 1
+        assert lines.count("RunChecks Requested\n") == 0
+        assert lines.count("UpdateContext Completed\n") == 0
+        assert lines.count("RunChecks Completed\n") == 0
+
+    server_process.kill()
+
+
+async def start_health_check_server(
+    buck: Buck, request_setup: str
+) -> tuple[str, str, asyncio.subprocess.Process]:
+    server_output = tempfile.NamedTemporaryFile("w", delete=False).name
+    health_check_state_file = tempfile.NamedTemporaryFile("w", delete=False).name
+
+    # Start the server before the build begins to ensure that the server is ready to accept requests.
+    # This is necessary since the run_request_hang_server.sh script may need to build the server target.
+    cmd = buck.run(
+        "fbcode//buck2/tests/e2e/health_check:health_check_server_bin",
+        "--",
+        "--isolation-dir",  # Avoids interference with the build of test target
+        "health_check_server",
+        "--state-info-file",
+        health_check_state_file,
+        request_setup,
+        env={"HEALTH_CHECK_SERVER_STATS_OUTPUT": server_output},
+    )
+    process = await cmd.start()
+
+    # Loop until the tcp_port is written which implies that the health check server has started
+    while True:
+        with open(health_check_state_file, "r") as f:
+            if f.read().strip():
+                break
+        await asyncio.sleep(0.1)
+
+    return (health_check_state_file, server_output, process)
