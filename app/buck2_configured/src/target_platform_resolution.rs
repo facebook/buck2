6fbd2cf82693a2f0d4d11655a0b8cc6204744b22
Scott Cao <scottcao@meta.com>
2025-05-15 17:11:05 -0700

    Add context when error is from resolving modifiers
    Summary: Otherwise, we may just get some analysis error without any indication of where the error is coming from.
    
    Reviewed By: cjhopman
    
    Differential Revision: D74827675
    
    fbshipit-source-id: 1ae4b77ad08f23f308775e60451a5c5209c17d24

--- app/buck2_configured/src/target_platform_resolution.rs
+++ app/buck2_configured/src/target_platform_resolution.rs
@@ -19,6 +19,7 @@ use buck2_core::global_cfg_options::GlobalCfgOptions;
 use buck2_core::target::configured_target_label::ConfiguredTargetLabel;
 use buck2_core::target::label::label::TargetLabel;
 use buck2_core::target::target_configured_target_label::TargetConfiguredTargetLabel;
+use buck2_error::BuckErrorContext;
 use buck2_futures::cancellation::CancellationContext;
 use buck2_node::cfg_constructor::CFG_CONSTRUCTOR_CALCULATION_IMPL;
 use buck2_node::configuration::target_platform_detector::TargetPlatformDetector;
@@ -150,6 +151,7 @@ impl ConfiguredTargetCalculationImpl for ConfiguredTargetCalculationInstance {
                     node.rule_type(),
                 )
                 .await
+                .with_buck_error_context(|| format!("Resolving modifiers for target `{}`", target))
         }
 
         match node.rule_kind() {
