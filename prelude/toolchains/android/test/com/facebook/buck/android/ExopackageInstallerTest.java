b6ea2495f53ad5415de815f34f3a8bda48788f22
Navid Qaragozlou <navidq@meta.com>
2025-06-02 08:53:36 -0700

    Add unit tests for AndroidDeviceImpl
    Summary: Adding unit tests for AndroidDeviceImpl also removing some test cases that are not needed anymore.
    
    Reviewed By: ywett02
    
    Differential Revision: D75723462
    
    fbshipit-source-id: ef81591834c999025748a4add388c29fd6f1e578

--- prelude/toolchains/android/test/com/facebook/buck/android/ExopackageInstallerTest.java
+++ prelude/toolchains/android/test/com/facebook/buck/android/ExopackageInstallerTest.java
@@ -10,13 +10,9 @@
 package com.facebook.buck.android;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import com.facebook.buck.android.exopackage.ExopackageInstaller;
 import com.facebook.buck.android.exopackage.NativeExoHelper;
-import com.facebook.buck.android.exopackage.PackageInfo;
-import com.facebook.buck.android.exopackage.RealAndroidDevice;
 import com.facebook.buck.core.filesystems.AbsPath;
 import com.facebook.buck.core.filesystems.RelPath;
 import com.facebook.buck.io.filesystem.impl.ProjectFilesystemUtils;
@@ -28,7 +24,6 @@ import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -39,113 +34,6 @@ public class ExopackageInstallerTest {
   @Rule public ExpectedException thrown = ExpectedException.none();
 
   @Rule public final TemporaryPaths tmpDir = new TemporaryPaths();
-
-  @Test
-  public void testParsePathAndPackageInfo() {
-    String lines =
-        "package:/data/app/com.facebook.katana-1.apk\r\n"
-            + "  Package [com.facebook.katana] (4229ce68):\r\n"
-            + "    userId=10145 gids=[1028, 1015, 3003]\r\n"
-            + "    pkg=Package{42690b80 com.facebook.katana}\r\n"
-            + "    codePath=/data/app/com.facebook.katana-1.apk\r\n"
-            + "    resourcePath=/data/app/com.facebook.katana-1.apk\r\n"
-            + "    nativeLibraryPath=/data/app-lib/com.facebook.katana-1\r\n"
-            + "    versionCode=1640376 targetSdk=14\r\n"
-            + "    versionName=8.0.0.0.23\r\n"
-            + "";
-    Optional<PackageInfo> optionalInfo =
-        RealAndroidDevice.parsePathAndPackageInfo("com.facebook.katana", lines);
-
-    assertTrue(optionalInfo.isPresent());
-    PackageInfo info = optionalInfo.get();
-
-    assertEquals("/data/app/com.facebook.katana-1.apk", info.apkPath);
-    assertEquals("/data/app-lib/com.facebook.katana-1", info.nativeLibPath);
-    assertEquals("1640376", info.versionCode);
-  }
-
-  @Test
-  public void testParsePathAndPackageInfoOnLollipop() {
-    String lines =
-        "package:/data/app/com.facebook.buck.android.agent-1.apk\r\n"
-            + "  Package [com.facebook.buck.android.agent] (3f784d07):\r\n"
-            + "    userId=10062 gids=[]\r\n"
-            + "    pkg=Package{81b1e34 com.facebook.buck.android.agent}\r\n"
-            + "    codePath=/data/app/com.facebook.buck.android.agent-1\r\n"
-            + "    resourcePath=/data/app/com.facebook.buck.android.agent-1\r\n"
-            + "    legacyNativeLibraryDir=/data/app/com.facebook.buck.android.agent-1/lib\r\n"
-            + "    primaryCpuAbi=armeabi-v7a\r\n"
-            + "    secondaryCpuAbi=null\r\n"
-            + "    versionCode=3 targetSdk=19\r\n"
-            + "    versionName=3\r\n"
-            + "";
-    Optional<PackageInfo> optionalInfo =
-        RealAndroidDevice.parsePathAndPackageInfo("com.facebook.buck.android.agent", lines);
-
-    assertTrue(optionalInfo.isPresent());
-    PackageInfo info = optionalInfo.get();
-
-    assertEquals("/data/app/com.facebook.buck.android.agent-1/base.apk", info.apkPath);
-    assertEquals("/data/app/com.facebook.buck.android.agent-1/lib", info.nativeLibPath);
-    assertEquals("3", info.versionCode);
-  }
-
-  @Test
-  public void testParseOnlyPackageInfo() {
-    String lines =
-        "1\r\n"
-            + "  Package [com.facebook.katana] (4229ce68):\r\n"
-            + "    userId=10145 gids=[1028, 1015, 3003]\r\n"
-            + "    pkg=Package{42690b80 com.facebook.katana}\r\n"
-            + "    codePath=/data/app/com.facebook.katana-1.apk\r\n"
-            + "    resourcePath=/data/app/com.facebook.katana-1.apk\r\n"
-            + "    nativeLibraryPath=/data/app-lib/com.facebook.katana-1\r\n"
-            + "    versionCode=1640376 targetSdk=14\r\n"
-            + "    versionName=8.0.0.0.23\r\n"
-            + "";
-    Optional<PackageInfo> optionalInfo =
-        RealAndroidDevice.parsePathAndPackageInfo("com.facebook.katana", lines);
-
-    assertFalse(optionalInfo.isPresent());
-  }
-
-  @Test
-  public void testParsePathAndPackageInfoHandlesErrorLines() {
-    String lines =
-        "/system/csc/customer.xml can't open file\n"
-            + "package:/data/app/com.facebook.buck.android.agent-1/base.apk\n"
-            + "Key Set Manager:\n"
-            + "  [com.facebook.buck.android.agent]\n"
-            + "      Signing KeySets: 148\n"
-            + "\n"
-            + "/thislineisahunkofjunk\n"
-            + "Packages:\n"
-            + "  Package [com.facebook.buck.android.agent] (1d1d165b):\n"
-            + "    pkg=Package{bdd64f0 com.facebook.buck.android.agent}\n"
-            + "    codePath=/data/app/com.facebook.buck.android.agent-1\n"
-            + "    resourcePath=/data/app/com.facebook.buck.android.agent-1\n"
-            + "    legacyNativeLibraryDir=/data/app/com.facebook.buck.android.agent-1/lib\n"
-            + "    nativeLibraryRootDir=/data/app/com.facebook.buck.android.agent-1/lib\n"
-            + "    nativeLibraryDir=/data/app/com.facebook.buck.android.agent-1/lib/arm\n"
-            + "    versionCode=9 targetSdk=19\n"
-            + "    versionName=9\n";
-    Optional<PackageInfo> optionalPackageInfo =
-        RealAndroidDevice.parsePathAndPackageInfo("com.facebook.buck.android.agent", lines);
-    assertTrue(optionalPackageInfo.isPresent());
-  }
-
-  @Test
-  public void testChunkArgs() {
-    assertEquals(ImmutableList.of(), RealAndroidDevice.chunkArgs(ImmutableList.of(), 8));
-
-    assertEquals(
-        ImmutableList.of(ImmutableList.of("abcd", "efg")),
-        RealAndroidDevice.chunkArgs(ImmutableList.of("abcd", "efg"), 8));
-
-    assertEquals(
-        ImmutableList.of(ImmutableList.of("abcd", "efg"), ImmutableList.of("hijkl")),
-        RealAndroidDevice.chunkArgs(ImmutableList.of("abcd", "efg", "hijkl"), 8));
-  }
 
   @Test
   public void testFilterLibrariesForAbi() {
---
+++ prelude/toolchains/android/test/com/facebook/buck/android/exopackage/AndroidDeviceImplTest.kt
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+package com.facebook.buck.android.exopackage
+
+import java.io.File
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+class AndroidDeviceImplTest {
+
+  private lateinit var mockAdbUtils: AdbUtils
+  private lateinit var androidDevice: AndroidDeviceImpl
+  private val serialNumber = "test-serial"
+
+  @Before
+  fun setUp() {
+    mockAdbUtils = mock()
+    androidDevice = AndroidDeviceImpl(serialNumber, mockAdbUtils)
+  }
+
+  @Test
+  fun testInstallApkOnDevice() {
+    val apkFile = mock<File>()
+    whenever(apkFile.absolutePath).thenReturn("/path/to/test.apk")
+    whenever(apkFile.name).thenReturn("test.apk")
+    whenever(apkFile.length()).thenReturn(1024L)
+
+    // Test with verifyTempWritable = true
+    val result = androidDevice.installApkOnDevice(apkFile, false, false, true, false)
+
+    verify(mockAdbUtils)
+        .executeAdbShellCommand("echo exo > /data/local/tmp/buck-experiment", serialNumber)
+    verify(mockAdbUtils).executeAdbShellCommand("rm /data/local/tmp/buck-experiment", serialNumber)
+    verify(mockAdbUtils).executeAdbCommand("install -r -d ${apkFile.absolutePath}", serialNumber)
+    assertTrue(result)
+  }
+
+  @Test
+  fun testInstallApexOnDevice() {
+    val apexFile = mock<File>()
+    whenever(apexFile.absolutePath).thenReturn("/path/to/test.apex")
+    whenever(apexFile.name).thenReturn("test.apex")
+    whenever(apexFile.length()).thenReturn(1024L)
+
+    whenever(mockAdbUtils.executeAdbCommand("root", serialNumber)).thenReturn("")
+    whenever(mockAdbUtils.executeAdbShellCommand("whoami", serialNumber)).thenReturn("root")
+    whenever(mockAdbUtils.executeAdbShellCommand("pm", serialNumber, true))
+        .thenReturn("force-non-staged")
+
+    val result = androidDevice.installApexOnDevice(apexFile, false)
+
+    verify(mockAdbUtils).executeAdbCommand("root", serialNumber)
+    verify(mockAdbUtils)
+        .executeAdbCommand(
+            "install --apex --force-non-staged ${apexFile.absolutePath}", serialNumber)
+    verify(mockAdbUtils).executeAdbShellCommand("stop", serialNumber)
+    verify(mockAdbUtils).executeAdbShellCommand("start", serialNumber)
+    assertTrue(result)
+  }
+
+  @Test
+  fun testStopPackage() {
+    androidDevice.stopPackage("com.test.app")
+
+    verify(mockAdbUtils).executeAdbShellCommand("am force-stop com.test.app", serialNumber)
+  }
+
+  @Test
+  fun testGetPackageInfo() {
+    whenever(mockAdbUtils.executeAdbShellCommand("pm path com.test.app", serialNumber))
+        .thenReturn("package:/data/app/com.test.app-1/base.apk")
+
+    val result = androidDevice.getPackageInfo("com.test.app")
+
+    assertTrue(result.isPresent)
+    assertEquals("/data/app/com.test.app-1/base.apk", result.get().apkPath)
+  }
+
+  @Test
+  fun testUninstallPackage() {
+    androidDevice.uninstallPackage("com.test.app")
+
+    verify(mockAdbUtils).executeAdbCommand("uninstall com.test.app", serialNumber)
+  }
+
+  @Test
+  fun testGetSignature() {
+    val packagePath = "/data/app/com.test.app-1/base.apk"
+    whenever(
+            mockAdbUtils.executeAdbShellCommand(
+                "unzip -l $packagePath | grep -E -o 'META-INF/[A-Z]+\\.SF'", serialNumber))
+        .thenReturn("META-INF/CERT.SF")
+    whenever(
+            mockAdbUtils.executeAdbShellCommand(
+                "unzip -p $packagePath META-INF/CERT.SF | grep -E 'SHA1-Digest-Manifest:|SHA-256-Digest-Manifest:'",
+                serialNumber))
+        .thenReturn("SHA1-Digest-Manifest: abcdef1234567890")
+
+    val result = androidDevice.getSignature(packagePath)
+
+    assertEquals("abcdef1234567890", result)
+  }
+
+  @Test
+  fun testGetSerialNumber() {
+    assertEquals(serialNumber, androidDevice.getSerialNumber())
+  }
+
+  @Test
+  fun testIsEmulator() {
+    // Setup for non-emulator
+    whenever(mockAdbUtils.executeAdbShellCommand("getprop ro.kernel.qemu", serialNumber))
+        .thenReturn("0")
+
+    assertFalse(androidDevice.isEmulator())
+
+    // Setup for emulator
+    whenever(mockAdbUtils.executeAdbShellCommand("getprop ro.kernel.qemu", serialNumber))
+        .thenReturn("1")
+
+    assertTrue(androidDevice.isEmulator())
+  }
+
+  @Test
+  fun testGetDeviceAbis() {
+    whenever(mockAdbUtils.executeAdbShellCommand("getprop ro.product.cpu.abilist", serialNumber))
+        .thenReturn("arm64-v8a,armeabi-v7a,armeabi")
+
+    val result = androidDevice.getDeviceAbis()
+
+    assertEquals(listOf("arm64-v8a", "armeabi-v7a", "armeabi"), result)
+  }
+
+  @Test
+  fun testGetDiskSpace() {
+    whenever(
+            mockAdbUtils.executeAdbShellCommand(
+                "df -h /data | awk '{print $2, $3, $4}'", serialNumber))
+        .thenReturn("Size Used Available\n64G 32G 32G")
+
+    val result = androidDevice.getDiskSpace()
+
+    assertEquals(listOf("64G", "32G", "32G"), result)
+  }
+}
