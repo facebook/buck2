053f772174ea024c9746fe5c576c740db47b2f39
Austin Seipp <aseipp@pobox.com>
2025-06-03 20:54:11 -0700

    buck2_daemon: automatically max out fd limits on startup (#986)
    Summary:
    This automatically increases the fd limit of the buck2d process up to whatever the existing user hard limit is, or a reasonable limit (0x80000) if the soft limit is already good enough. This is not configurable and it always happens without question.
    
    The logic behind this is simple: there is never, ever, ever a scenario where I am thinking to myself: "You know what would be great? If my build system ran out of file descriptors and the build failed". Yet this happens all the time with on buck2 on macOS and (apparently?) my Fedora Asahi laptop, where the default soft limits are very, very low.
    
    When I run `buck2 build`, I am asking it to build things. Buck is good at building things, and often it builds lots of things. The soft limit is a simple guardrail, but only that. And if too many files are open due to something like an excessive number of actions, the reality is that there is very little chance the file descriptor table is actually the limiting resource, versus the underlying machine resources (net/disk/cpu/memory).
    
    Just always set the rlimit to the hard cap. This will be inherited by children including the forkserver, which is completely fine.
    
    Fixes https://github.com/facebook/buck2/issues/419. Closes https://github.com/facebook/buck2/issues/928. Closes https://github.com/facebook/buck2/issues/981.
    
    Pull Request resolved: https://github.com/facebook/buck2/pull/986
    
    Reviewed By: iguridi
    
    Differential Revision: D75642106
    
    Pulled By: JakobDegen
    
    fbshipit-source-id: bcd4203fbbf11f4841e49535093fa7209b724d7b

---
+++ app/buck2_common/src/rlimits.rs
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+#[cfg(unix)]
+pub fn raise_file_descriptor_limits() -> buck2_error::Result<()> {
+    use buck2_error::BuckErrorContext;
+    use nix::sys::resource;
+    use nix::sys::resource::Resource;
+
+    let (soft_limit, hard_limit) = resource::getrlimit(Resource::RLIMIT_NOFILE)?;
+
+    // If the soft limit is already maxxed out or "big enough", don't mess with it.
+    //
+    // The "big enough" value is the default value on Linux.
+    if soft_limit == hard_limit || soft_limit >= 0x80_000 {
+        return Ok(());
+    }
+
+    // Set the maximum number of open file descriptors to the hard limit.
+    //
+    // The `systemd.exec` man page says this about setting the limits with `ulimit -n` directly:
+    //
+    // > Don't use. Be careful when raising the soft limit above 1024, since `select(2)` cannot
+    // > function with file descriptors above 1023 on Linux. Nowadays, the hard limit defaults to
+    // > 524288, a very high value compared to historical defaults. Typically applications should
+    // > increase their soft limit to the hard limit on their own, if they are OK with working with
+    // > file descriptors above 1023, i.e. do not use `select(2)`. Note that file descriptors are
+    // > nowadays accounted like any other form of memory, thus there should not be any need to
+    // > lower the hard limit.
+    //
+    // However, this is practically not an issue for Buck for a number of reasons, most importantly
+    // it is in fact a program that needs a lot of open files. But also, it doesn't matter what the
+    // systemd man page says in reality, because if the user hits the soft limit, they're just going
+    // to run `ulimit -n 10000000` and move on, so we might as well just do effectively that for
+    // them. Therefore, we don't reset rlimits anywhere and just let all subprocesses inherit this.
+    //
+    // See: https://www.freedesktop.org/software/systemd/man/devel/systemd.exec.html
+    //
+    // And: https://github.com/facebook/buck2/pull/986
+    resource::setrlimit(Resource::RLIMIT_NOFILE, hard_limit, hard_limit)
+        .with_buck_error_context(|| format!(
+            "Open file descriptor limit is lower than 80,000. Buck needs a lot of FDs. Raising open FD limit from {} to hard limit {}",
+            soft_limit,
+            hard_limit,
+        ))?;
+
+    Ok(())
+}
+
+#[cfg(not(unix))]
+pub fn raise_file_descriptor_limits() -> buck2_error::Result<()> {
+    Ok(())
+}
