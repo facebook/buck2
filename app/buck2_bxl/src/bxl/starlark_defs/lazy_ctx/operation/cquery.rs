40955c8b217bf84a25dce74be6957ba6936e7d80
Chenhao Zuo <nero@meta.com>
2025-06-05 10:02:49 -0700

    Refactor cquery eval result
    Summary:
    Refactor cquery eval result
    For cquery eval result we can just return `QueryEvaluationResult<ConfiguredTargetNode>`
    
    So we don't need `SingleOrMappedQueryEvaluationValue` anymore
    
    Reviewed By: IanChilds
    
    Differential Revision: D75942214
    
    fbshipit-source-id: a9405db9d8f6d1dea0d369b617c957bb6d0a50ff

--- app/buck2_bxl/src/bxl/starlark_defs/lazy_ctx/operation/cquery.rs
+++ app/buck2_bxl/src/bxl/starlark_defs/lazy_ctx/operation/cquery.rs
@@ -11,21 +11,14 @@ use allocative::Allocative;
 use async_recursion::async_recursion;
 use buck2_build_api::query::oneshot::QUERY_FRONTEND;
 use buck2_core::global_cfg_options::GlobalCfgOptions;
-use buck2_error::starlark_error::from_starlark_with_options;
 use buck2_node::nodes::configured::ConfiguredTargetNode;
-use buck2_query::query::environment::QueryTarget;
 use buck2_query::query::syntax::simple::eval::values::QueryEvaluationResult;
-use buck2_query::query::syntax::simple::eval::values::QueryEvaluationValue;
 use dice::DiceComputations;
-use indexmap::IndexMap;
 use starlark::values::Heap;
 use starlark::values::Value;
-use starlark::values::dict::Dict;
 
 use crate::bxl::starlark_defs::context::BxlContextCoreData;
-use crate::bxl::starlark_defs::file_set::StarlarkFileSet;
-use crate::bxl::starlark_defs::targetset::NodeLike;
-use crate::bxl::starlark_defs::targetset::StarlarkTargetSet;
+use crate::bxl::starlark_defs::query_util::parse_query_evaluation_result;
 
 #[derive(Debug, Allocative)]
 pub(crate) struct LazyCqueryOperation {
@@ -60,64 +53,8 @@ impl LazyCqueryOperation {
     }
 }
 
-pub(crate) enum SingleOrMappedQueryEvaluationValue<T: QueryTarget> {
-    Single(QueryEvaluationValue<T>),
-    Map(IndexMap<String, QueryEvaluationValue<T>>),
-}
-
-impl<T: NodeLike> SingleOrMappedQueryEvaluationValue<T> {
-    pub(crate) fn from_query_evaluation_result(
-        result: QueryEvaluationResult<T>,
-    ) -> buck2_error::Result<Self> {
-        match result {
-            QueryEvaluationResult::Single(v) => Ok(Self::Single(v)),
-            QueryEvaluationResult::Multiple(multi) => Ok(Self::Map(
-                multi
-                    .0
-                    .into_iter()
-                    .map(|(key, res)| res.map(|val| (key, val)))
-                    .collect::<buck2_error::Result<IndexMap<_, _>>>()?,
-            )),
-        }
-    }
-
-    pub(crate) fn into_value<'v>(self, heap: &'v Heap) -> buck2_error::Result<Value<'v>> {
-        Ok(match self {
-            SingleOrMappedQueryEvaluationValue::Single(res) => match res {
-                QueryEvaluationValue::TargetSet(targets) => {
-                    heap.alloc(StarlarkTargetSet::from(targets))
-                }
-                QueryEvaluationValue::FileSet(files) => heap.alloc(StarlarkFileSet::from(files)),
-            },
-            SingleOrMappedQueryEvaluationValue::Map(map) => heap.alloc(Dict::new(
-                map.into_iter()
-                    .map(|(q, val)| {
-                        Ok((
-                            heap.alloc(q).get_hashed().map_err(|e| {
-                                from_starlark_with_options(
-                                    e,
-                                    buck2_error::starlark_error::NativeErrorHandling::Unknown,
-                                    false,
-                                )
-                            })?,
-                            match val {
-                                QueryEvaluationValue::TargetSet(targets) => {
-                                    heap.alloc(StarlarkTargetSet::from(targets))
-                                }
-                                QueryEvaluationValue::FileSet(files) => {
-                                    heap.alloc(StarlarkFileSet::from(files))
-                                }
-                            },
-                        ))
-                    })
-                    .collect::<buck2_error::Result<_>>()?,
-            )),
-        })
-    }
-}
-
 pub(crate) enum LazyCqueryResult {
-    Eval(SingleOrMappedQueryEvaluationValue<ConfiguredTargetNode>),
+    Eval(QueryEvaluationResult<ConfiguredTargetNode>),
 }
 
 impl LazyCqueryOperation {
@@ -146,9 +83,7 @@ impl LazyCqueryOperation {
                     )
                     .await?
                     .0;
-                Ok(LazyCqueryResult::Eval(
-                    SingleOrMappedQueryEvaluationValue::from_query_evaluation_result(res)?,
-                ))
+                Ok(LazyCqueryResult::Eval(res))
             }
         }
     }
@@ -157,7 +92,7 @@ impl LazyCqueryOperation {
 impl LazyCqueryResult {
     pub(crate) fn into_value<'v>(self, heap: &'v Heap) -> buck2_error::Result<Value<'v>> {
         match self {
-            LazyCqueryResult::Eval(result) => Ok(result.into_value(heap)?),
+            LazyCqueryResult::Eval(result) => parse_query_evaluation_result(result, heap),
         }
     }
 }
