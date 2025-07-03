96c03899828144179e51783b9a91fa4eafa89f2d
Nuri Amari <nuriamari@meta.com>
2025-07-03 13:37:43 -0700

    Use filelist for compiler stats merger
    Summary: For large applications, the command line becomes too long without a filelist.
    
    Reviewed By: rmaz
    
    Differential Revision: D77736824
    
    fbshipit-source-id: 7db32ed8c2520bdd1160357e8ffecad3f9308b5c

--- prelude/cxx/dist_lto/tools/dist_lto_compiler_stats_merger.py
+++ prelude/cxx/dist_lto/tools/dist_lto_compiler_stats_merger.py
@@ -21,23 +21,23 @@ A helper script designed to merge multiple JSON files storing LLVM stats produce
 def main(argv: List[str]) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--input-stats-files",
-        nargs="+",
-        help="A list of JSON static files produced by LLVM's -stats -stats-json",
+        "--input-stats-filelist",
+        help="A filelist of JSON static files produced by LLVM's -stats -stats-json",
     )
     parser.add_argument(
         "--output-stats-file", help="A path to write the merged JSON static file to."
     )
     args = parser.parse_args(argv[1:])
     merged_stats: Dict[int, str] = {}
-    for stats_file in args.input_stats_files:
-        with open(stats_file, "r") as f:
-            stats = json.load(f)
-            for key, value in stats.items():
-                if key in merged_stats:
-                    merged_stats[key] += value
-                else:
-                    merged_stats[key] = value
+    with open(args.input_stats_filelist, "r") as stats_filelist:
+        for stats_file in stats_filelist:
+            with open(stats_file.strip(), "r") as f:
+                stats = json.load(f)
+                for key, value in stats.items():
+                    if key in merged_stats:
+                        merged_stats[key] += value
+                    else:
+                        merged_stats[key] = value
 
     with open(args.output_stats_file, "w") as f:
         json.dump(merged_stats, f, sort_keys=True, indent=4)
