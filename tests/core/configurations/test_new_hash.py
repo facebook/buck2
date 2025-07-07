c10e2031c1f77d82e1f74562e4476d94765b711b
Scott Cao <scottcao@meta.com>
2025-07-07 14:39:01 -0700

    Add repro test case for bug in new_platform_hash_rollout
    Summary:
    `buck2.new_platform_hash_rollout` buckconfig is used to turn on strong hashing for buck-out hashes, which would eliminate the issue of hash collisions. Unfortunately, I realize there is a bug in which the rollout would cause a graph divergence problem - Because `new_platform_hash_rollout` is only read by core but only on daemon startup, a buck daemon used after landing changes to `buck2.new_platform_hash_rollout` could still be using previous weak hashes, but if this daemon gets killed and restarted, then it would use strong hashes on the same revision, diverging the build graph on the same revision and creating a build speed/capacity problem.
    
    This diff adds a test case to repro this bug.
    
    Reviewed By: JakobDegen
    
    Differential Revision: D77637075
    
    fbshipit-source-id: 82699b911863d13d73fbd0abdd737c4e988351d6

---
+++ tests/core/configurations/test_new_hash.py
@@ -0,0 +1,24 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under both the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree and the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree.
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test()
+async def test_hash_changes_after_buckconfig_rollout(buck: Buck) -> None:
+    result1 = await buck.cquery("//:target")
+    with open(buck.cwd / ".buckconfig.local", "w") as f:
+        print("[buck2]", file=f)
+        print("new_platform_hash_rollout = 1", file=f)
+    result2 = await buck.cquery("//:target")
+    # FIXME(scottcao): This is a bug. They should not be equal.
+    assert result1.stdout == result2.stdout
+
+    await buck.kill()
+    result3 = await buck.cquery("//:target")
+    assert result1.stdout != result3.stdout
