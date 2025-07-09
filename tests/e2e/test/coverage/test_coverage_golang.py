e5b47647bb75385b94cf36722986f0e08b0ce920
Johnny Souza <johnnysouza@meta.com>
2025-07-09 03:54:24 -0700

    Make sure coverage tmpdir and output paths are absolute
    Summary:
    Go tests with coverage enabled **in RE** have `COVERAGE_COLLECTION_TMPDIR` and `COVERAGE_COLLECTION_OUTPUT_FILE` paths relative to current working directory (these paths are absolute in local).
    
    Then the coverage wrapper ends up passing a relative path to `-test.coverprofile`. And the test binary treats `-test.coverprofile` as relative to its own path:
    
    ```
    $ buck-out/v2/gen/fbcode/311cb2c14e8f88ba/libfb/go/ods/__ods_test__/ods_test -test.v '-test.run=''^TestSetGetCounterNoTags$' -test.coverprofile=foo.profile
    === RUN   TestSetGetCounterNoTags
     --- PASS: TestSetGetCounterNoTags (0.00s)
    PASS
    coverage: 12.9% of statements in libfb/go/ods
    
    $ find . -name foo.profile
    ./buck-out/v2/gen/fbcode/311cb2c14e8f88ba/libfb/go/ods/__ods_test__/foo.profile
    ```
    
    BUT the coverage wrapper treats it as relative to the current working directory and fails because it can't find the profiler data.
    
    This diff resolves both `COVERAGE_COLLECTION_TMPDIR` to absolute path before using it to create the value passed to `-test.coverprofile`.
    
    It does the same to `COVERAGE_COLLECTION_OUTPUT_FILE` for good measure.
    
    Reviewed By: echistyakov
    
    Differential Revision: D77998464
    
    fbshipit-source-id: 50db660bb3685450c1fc3f45344e8be4dda21914

--- tests/e2e/test/coverage/test_coverage_golang.py
+++ tests/e2e/test/coverage/test_coverage_golang.py
@@ -35,3 +35,23 @@ async def test_go_test_dbgo_cov(buck: Buck, tmp_path: Path) -> None:
     assert "fbcode/test_frameworks/gotest/playground/simple_add_test.go" in paths, str(
         paths
     )
+
+
+@buck_test(inplace=True)
+async def test_go_test_dbgo_cov_on_remote_execution(buck: Buck, tmp_path: Path) -> None:
+    coverage_file = tmp_path / "coverage.txt"
+    await buck.test(
+        "@fbcode//mode/dbgo-cov",
+        "fbcode//test_frameworks/gotest/playground:simple_add_test_re",
+        "--",
+        f"--coverage-output={coverage_file}",
+    )
+    paths = []
+    with open(coverage_file) as results:
+        for line in results:
+            paths.append(json.loads(line)["filepath"])
+
+    assert "fbcode/test_frameworks/gotest/playground/simple.go" in paths, str(paths)
+    assert "fbcode/test_frameworks/gotest/playground/simple_add_test.go" in paths, str(
+        paths
+    )
