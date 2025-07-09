414f86460fd813e9d1084c4020a8a0f3cfb31e68
Johnny Souza <johnnysouza@meta.com>
2025-07-09 03:54:24 -0700

    Buck2 E2E test for go coverage using mode/dbgo-cov
    Summary: tests
    
    Reviewed By: echistyakov
    
    Differential Revision: D77998465
    
    fbshipit-source-id: ce558685b39e2afb395217b50bfee542501ae6ed

---
+++ tests/e2e/test/coverage/test_coverage_golang.py
@@ -0,0 +1,37 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is dual-licensed under either the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree or the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree. You may select, at your option, one of the
+# above-listed licenses.
+
+# pyre-strict
+
+
+import json
+from pathlib import Path
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.asserts import expect_failure
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test(inplace=True)
+async def test_go_test_dbgo_cov(buck: Buck, tmp_path: Path) -> None:
+    coverage_file = tmp_path / "coverage.txt"
+    await buck.test(
+        "@fbcode//mode/dbgo-cov",
+        "fbcode//test_frameworks/gotest/playground:simple_add_test",
+        "--",
+        f"--coverage-output={coverage_file}",
+    )
+    paths = []
+    with open(coverage_file) as results:
+        for line in results:
+            paths.append(json.loads(line)["filepath"])
+
+    assert "fbcode/test_frameworks/gotest/playground/simple.go" in paths, str(paths)
+    assert "fbcode/test_frameworks/gotest/playground/simple_add_test.go" in paths, str(
+        paths
+    )
