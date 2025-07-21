4b175cf0748cea8cb7301a2ba219e9aa3d42bd66
Andrew Zhang <azhang2542@meta.com>
2025-07-21 09:59:19 -0700

    Add tests for ?modifier syntax in audit subtargets
    Summary: Add test for `?modifier` syntax for `audit subtargets` command`.
    
    Reviewed By: christolliday
    
    Differential Revision: D78532257
    
    fbshipit-source-id: 84ec0533505504c723df86a4915e0b13a8a094b9

--- tests/core/audit/test_audit_subtargets.py
+++ tests/core/audit/test_audit_subtargets.py
@@ -7,9 +7,11 @@
 # above-listed licenses.
 
 # pyre-strict
-
+import re
+from typing import List
 
 from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.asserts import expect_failure
 from buck2.tests.e2e_util.buck_workspace import buck_test
 from buck2.tests.e2e_util.helper.golden import golden
 
@@ -85,3 +87,131 @@ async def test_audit_subtargets_json(buck: Buck) -> None:
 
     result = await buck.audit("subtargets", "//:deeply_nested[sub2]", "--json")
     golden(output=result.stdout, rel_path="json/golden.deeply_nested_subs_of_sub.json")
+
+
+def _extract_configuration(s: str) -> List[str]:
+    return re.findall(r"\((.*?)\)", s)
+
+
+@buck_test()
+async def test_audit_subtarget_modifiers(buck: Buck) -> None:
+    result = await buck.audit(
+        "subtargets",
+        "//:deeply_nested?root//:linux",
+    )
+
+    cfgs = _extract_configuration(result.stdout)
+    for cfg in cfgs:
+        cfg = await buck.audit_configurations(cfg)
+        assert "root//:linux" in cfg.stdout
+
+    result = await buck.audit(
+        "subtargets",
+        "//:nested?root//:macos",
+        "//:deeply_nested?root//:linux",
+        "--json",
+    )
+
+    [linux_cfg, macos_cfg] = _extract_configuration(result.stdout)
+
+    linux_cfg = await buck.audit_configurations(linux_cfg)
+    assert "root//:linux" in linux_cfg.stdout
+
+    macos_cfg = await buck.audit_configurations(macos_cfg)
+    assert "root//:macos" in macos_cfg.stdout
+
+    result = await buck.audit(
+        "subtargets",
+        "//:foo?root//:macos+root//:arm",
+        "//:foo?root//:arm+root//:macos",
+        "--json",
+    )
+
+    [cfg] = _extract_configuration(result.stdout)
+
+    cfg = await buck.audit_configurations(cfg)
+    assert "root//:macos" in cfg.stdout
+
+
+@buck_test()
+async def test_audit_subtarget_modifiers_target_universe(buck: Buck) -> None:
+    result = await buck.audit(
+        "subtargets",
+        "//:deeply_nested",
+        "--target-universe",
+        "//:deeply_nested?root//:linux",
+    )
+
+    cfgs = _extract_configuration(result.stdout)
+    for cfg in cfgs:
+        cfg = await buck.audit_configurations(cfg)
+        assert "root//:linux" in cfg.stdout
+
+    result = await buck.audit(
+        "subtargets",
+        "//:deeply_nested",
+        "--target-universe",
+        "//:deeply_nested?root//:linux,//:deeply_nested?root//:macos",
+        "--json",
+    )
+
+    [linux_cfg, macos_cfg] = _extract_configuration(result.stdout)
+
+    linux_cfg = await buck.audit_configurations(linux_cfg)
+    assert "root//:linux" in linux_cfg.stdout
+
+    macos_cfg = await buck.audit_configurations(macos_cfg)
+    assert "root//:macos" in macos_cfg.stdout
+
+    result = await buck.audit(
+        "subtargets",
+        "//:foo",
+        "--target-universe",
+        "//:foo?root//:macos+root//:arm,//:foo?root//:arm+root//:macos",
+        "--json",
+    )
+
+    [cfg] = _extract_configuration(result.stdout)
+
+    cfg = await buck.audit_configurations(cfg)
+    assert "root//:macos" in cfg.stdout
+
+
+@buck_test()
+async def test_audit_subtarget_fails_with_global_modifiers(buck: Buck) -> None:
+    await expect_failure(
+        buck.audit(
+            "subtargets",
+            "--modifier",
+            "root//:linux",
+            "//:deeply_nested?root//:macos",
+        ),
+        stderr_regex=r"Cannot specify modifiers with \?modifier syntax when global CLI modifiers are set with --modifier flag",
+    )
+
+    await expect_failure(
+        buck.audit(
+            "subtargets",
+            "--modifier",
+            "root//:linux",
+            "//:deeply_nested",
+            "--target-universe",
+            "//:deeply_nested?root//:macos",
+        ),
+        stderr_regex=r"Cannot specify modifiers with \?modifier syntax when global CLI modifiers are set with --modifier flag",
+    )
+
+
+@buck_test()
+async def test_audit_subtarget_fails_with_pattern_modifier_and_target_universe_modifier(
+    buck: Buck,
+) -> None:
+    await expect_failure(
+        buck.audit(
+            "subtargets",
+            "//:deeply_nested?root//:linux",
+            "--target-universe",
+            "//:deeply_nested?root//:macos",
+        ),
+        stderr_regex=r"Cannot use \?modifier syntax in target pattern expression with --target-universe flag",
+    )
