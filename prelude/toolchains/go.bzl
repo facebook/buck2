facebook/buck2

commit ff865194a85b093d09b1e51fd0707522697100ee

Move system_go_* toolchains to toolchains/go
Summary: Move system toolchains to the same place where hermetic are

Reviewed By: echistyakov

Differential Revision: D79265796

fbshipit-source-id: 474ad691a397a240bbfc89c733dcd8a8210972fd

prelude/toolchains/go.bzl
@@ -6,105 +6,11 @@
 # of this source tree. You may select, at your option, one of the
 # above-listed licenses.
 
-load("@prelude//go:toolchain.bzl", "GoToolchainInfo")
-load("@prelude//go_bootstrap:go_bootstrap.bzl", "GoBootstrapToolchainInfo")
-load("@prelude//os_lookup:defs.bzl", "ScriptLanguage")
-load("@prelude//utils:cmd_script.bzl", "cmd_script")
+load("@prelude//fbcode/buck2/prelude/toolchains/go:system_go_bootstrap_toolchain.bzl", _system_go_bootstrap_toolchain = "system_go_bootstrap_toolchain")
+load("@prelude//fbcode/buck2/prelude/toolchains/go:system_go_toolchain.bzl", _system_go_toolchain = "system_go_toolchain")
 
-def go_platform() -> (str, str):
-    arch = host_info().arch
-    if arch.is_aarch64:
-        go_arch = "arm64"
-    elif arch.is_x86_64:
-        go_arch = "amd64"
-    else:
-        fail("Unsupported go arch: {}".format(arch))
+# deprecated: use `@prelude//fbcode/buck2/prelude/toolchains/go:system_go_bootstrap_toolchain.bzl` instead
+system_go_bootstrap_toolchain = _system_go_bootstrap_toolchain
 
-    os = host_info().os
-    if os.is_macos:
-        go_os = "darwin"
-    elif os.is_linux:
-        go_os = "linux"
-    elif os.is_windows:
-        go_os = "windows"
-    else:
-        fail("Unsupported go os: {}".format(os))
-
-    return go_os, go_arch
-
-def _system_go_bootstrap_toolchain_impl(ctx):
-    go_os, go_arch = go_platform()
-
-    script_language = ScriptLanguage("bat" if go_os == "windows" else "sh")
-    go = "go.exe" if go_os == "windows" else "go"
-
-    go_cmd = cmd_script(ctx, "go", cmd_args(go), script_language)
-
-    return [
-        DefaultInfo(),
-        GoBootstrapToolchainInfo(
-            env_go_arch = go_arch,
-            env_go_os = go_os,
-            go = RunInfo(go_cmd),
-            go_wrapper = ctx.attrs.go_wrapper[RunInfo],
-        ),
-    ]
-
-system_go_bootstrap_toolchain = rule(
-    impl = _system_go_bootstrap_toolchain_impl,
-    doc = """Example system go toolchain rules (WIP). Usage:
-  system_go_bootstrap_toolchain(
-      name = "go_bootstrap",
-      visibility = ["PUBLIC"],
-  )""",
-    attrs = {
-        "go_wrapper": attrs.default_only(attrs.dep(providers = [RunInfo], default = "prelude//go/tools:go_wrapper_py")),
-    },
-    is_toolchain_rule = True,
-)
-
-def _system_go_toolchain_impl(ctx):
-    go_os, go_arch = go_platform()
-
-    script_language = ScriptLanguage("bat" if go_os == "windows" else "sh")
-    go = "go.exe" if go_os == "windows" else "go"
-
-    return [
-        DefaultInfo(sub_targets = {"go": [
-            RunInfo(go),
-        ]}),
-        GoToolchainInfo(
-            assembler = RunInfo(cmd_script(ctx, "asm", cmd_args(go, "tool", "asm"), script_language)),
-            cgo = RunInfo(cmd_script(ctx, "cgo", cmd_args(go, "tool", "cgo"), script_language)),
-            concat_files = ctx.attrs.concat_files[RunInfo],
-            compiler = RunInfo(cmd_script(ctx, "compile", cmd_args(go, "tool", "compile"), script_language)),
-            cover = RunInfo(cmd_script(ctx, "cover", cmd_args(go, "tool", "cover"), script_language)),
-            env_go_arch = go_arch,
-            env_go_os = go_os,
-            external_linker_flags = [],
-            gen_stdlib_importcfg = ctx.attrs.gen_stdlib_importcfg[RunInfo],
-            go = RunInfo(cmd_script(ctx, "go", cmd_args(go), script_language)),
-            go_wrapper = ctx.attrs.go_wrapper[RunInfo],
-            linker = RunInfo(cmd_script(ctx, "link", cmd_args(go, "tool", "link"), script_language)),
-            packer = RunInfo(cmd_script(ctx, "pack", cmd_args(go, "tool", "pack"), script_language)),
-            build_tags = [],
-            linker_flags = [],
-            assembler_flags = [],
-            compiler_flags = [],
-        ),
-    ]
-
-system_go_toolchain = rule(
-    impl = _system_go_toolchain_impl,
-    doc = """Example system go toolchain rules (WIP). Usage:
-  system_go_toolchain(
-      name = "go",
-      visibility = ["PUBLIC"],
-  )""",
-    attrs = {
-        "concat_files": attrs.default_only(attrs.dep(providers = [RunInfo], default = "prelude//go_bootstrap/tools:go_concat_files")),
-        "gen_stdlib_importcfg": attrs.default_only(attrs.dep(providers = [RunInfo], default = "prelude//go/tools:gen_stdlib_importcfg")),
-        "go_wrapper": attrs.default_only(attrs.dep(providers = [RunInfo], default = "prelude//go_bootstrap/tools:go_go_wrapper")),
-    },
-    is_toolchain_rule = True,
-)
+# deprecated: use `@prelude//fbcode/buck2/prelude/toolchains/go:system_go_toolchain.bzl` instead
+system_go_toolchain = _system_go_toolchain

prelude/toolchains/demo.bzl
@@ -7,12 +7,13 @@
 # above-listed licenses.
 
 load("@prelude//android/tools:jdk_system_image.bzl", "jdk_system_image")
+load("@prelude//fbcode/buck2/prelude/toolchains/go:system_go_bootstrap_toolchain.bzl", "system_go_bootstrap_toolchain")
+load("@prelude//fbcode/buck2/prelude/toolchains/go:system_go_toolchain.bzl", "system_go_toolchain")
 load("@prelude//tests:test_toolchain.bzl", "noop_test_toolchain")
 load("@prelude//toolchains:android.bzl", "android_sdk_tools", "system_android_toolchain")
 load("@prelude//toolchains:cxx.bzl", "system_cxx_toolchain")
 load("@prelude//toolchains:dex.bzl", "system_dex_toolchain", "system_noop_dex_toolchain")
 load("@prelude//toolchains:genrule.bzl", "system_genrule_toolchain")
-load("@prelude//toolchains:go.bzl", "system_go_bootstrap_toolchain", "system_go_toolchain")
 load("@prelude//toolchains:haskell.bzl", "system_haskell_toolchain")
 load("@prelude//toolchains:java.bzl", "java_test_toolchain", "javacd_toolchain", "system_java_bootstrap_toolchain", "system_java_lib", "system_java_tool", "system_prebuilt_jar_bootstrap_toolchain")
 load("@prelude//toolchains:kotlin.bzl", "kotlincd_toolchain", "system_kotlin_bootstrap_toolchain")

prelude/toolchains/go/system_go_bootstrap_toolchain.bzl
@@ -1,0 +1,63 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is dual-licensed under either the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree or the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree. You may select, at your option, one of the
+# above-listed licenses.
+
+load("@prelude//go_bootstrap:go_bootstrap.bzl", "GoBootstrapToolchainInfo")
+load("@prelude//os_lookup:defs.bzl", "ScriptLanguage")
+load("@prelude//utils:cmd_script.bzl", "cmd_script")
+
+def go_platform() -> (str, str):
+    arch = host_info().arch
+    if arch.is_aarch64:
+        go_arch = "arm64"
+    elif arch.is_x86_64:
+        go_arch = "amd64"
+    else:
+        fail("Unsupported go arch: {}".format(arch))
+
+    os = host_info().os
+    if os.is_macos:
+        go_os = "darwin"
+    elif os.is_linux:
+        go_os = "linux"
+    elif os.is_windows:
+        go_os = "windows"
+    else:
+        fail("Unsupported go os: {}".format(os))
+
+    return go_os, go_arch
+
+def _system_go_bootstrap_toolchain_impl(ctx):
+    go_os, go_arch = go_platform()
+
+    script_language = ScriptLanguage("bat" if go_os == "windows" else "sh")
+    go = "go.exe" if go_os == "windows" else "go"
+
+    go_cmd = cmd_script(ctx, "go", cmd_args(go), script_language)
+
+    return [
+        DefaultInfo(),
+        GoBootstrapToolchainInfo(
+            env_go_arch = go_arch,
+            env_go_os = go_os,
+            go = RunInfo(go_cmd),
+            go_wrapper = ctx.attrs.go_wrapper[RunInfo],
+        ),
+    ]
+
+system_go_bootstrap_toolchain = rule(
+    impl = _system_go_bootstrap_toolchain_impl,
+    doc = """Example system go toolchain rules (WIP). Usage:
+  system_go_bootstrap_toolchain(
+      name = "go_bootstrap",
+      visibility = ["PUBLIC"],
+  )""",
+    attrs = {
+        "go_wrapper": attrs.default_only(attrs.dep(providers = [RunInfo], default = "prelude//go/tools:go_wrapper_py")),
+    },
+    is_toolchain_rule = True,
+)

prelude/toolchains/go/system_go_toolchain.bzl
@@ -1,0 +1,78 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is dual-licensed under either the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree or the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree. You may select, at your option, one of the
+# above-listed licenses.
+
+load("@prelude//go:toolchain.bzl", "GoToolchainInfo")
+load("@prelude//os_lookup:defs.bzl", "ScriptLanguage")
+load("@prelude//utils:cmd_script.bzl", "cmd_script")
+
+def go_platform() -> (str, str):
+    arch = host_info().arch
+    if arch.is_aarch64:
+        go_arch = "arm64"
+    elif arch.is_x86_64:
+        go_arch = "amd64"
+    else:
+        fail("Unsupported go arch: {}".format(arch))
+
+    os = host_info().os
+    if os.is_macos:
+        go_os = "darwin"
+    elif os.is_linux:
+        go_os = "linux"
+    elif os.is_windows:
+        go_os = "windows"
+    else:
+        fail("Unsupported go os: {}".format(os))
+
+    return go_os, go_arch
+
+def _system_go_toolchain_impl(ctx):
+    go_os, go_arch = go_platform()
+
+    script_language = ScriptLanguage("bat" if go_os == "windows" else "sh")
+    go = "go.exe" if go_os == "windows" else "go"
+
+    return [
+        DefaultInfo(sub_targets = {"go": [
+            RunInfo(go),
+        ]}),
+        GoToolchainInfo(
+            assembler = RunInfo(cmd_script(ctx, "asm", cmd_args(go, "tool", "asm"), script_language)),
+            cgo = RunInfo(cmd_script(ctx, "cgo", cmd_args(go, "tool", "cgo"), script_language)),
+            concat_files = ctx.attrs.concat_files[RunInfo],
+            compiler = RunInfo(cmd_script(ctx, "compile", cmd_args(go, "tool", "compile"), script_language)),
+            cover = RunInfo(cmd_script(ctx, "cover", cmd_args(go, "tool", "cover"), script_language)),
+            env_go_arch = go_arch,
+            env_go_os = go_os,
+            external_linker_flags = [],
+            gen_stdlib_importcfg = ctx.attrs.gen_stdlib_importcfg[RunInfo],
+            go = RunInfo(cmd_script(ctx, "go", cmd_args(go), script_language)),
+            go_wrapper = ctx.attrs.go_wrapper[RunInfo],
+            linker = RunInfo(cmd_script(ctx, "link", cmd_args(go, "tool", "link"), script_language)),
+            packer = RunInfo(cmd_script(ctx, "pack", cmd_args(go, "tool", "pack"), script_language)),
+            build_tags = [],
+            linker_flags = [],
+            assembler_flags = [],
+            compiler_flags = [],
+        ),
+    ]
+
+system_go_toolchain = rule(
+    impl = _system_go_toolchain_impl,
+    doc = """Example system go toolchain rules (WIP). Usage:
+  system_go_toolchain(
+      name = "go",
+      visibility = ["PUBLIC"],
+  )""",
+    attrs = {
+        "concat_files": attrs.default_only(attrs.dep(providers = [RunInfo], default = "prelude//go_bootstrap/tools:go_concat_files")),
+        "gen_stdlib_importcfg": attrs.default_only(attrs.dep(providers = [RunInfo], default = "prelude//go/tools:gen_stdlib_importcfg")),
+        "go_wrapper": attrs.default_only(attrs.dep(providers = [RunInfo], default = "prelude//go_bootstrap/tools:go_go_wrapper")),
+    },
+    is_toolchain_rule = True,
+)

commit 53f2fab3045b572b124a1a9da8f2059f83ef8f02

prelude/go: Fix wrong starlark load paths for OSS (#1059)
Summary:
The issue was introduced in
Commit: ff865194a85b093d09b1e51fd0707522697100ee

Pull Request resolved: https://github.com/facebook/buck2/pull/1059

Reviewed By: podtserkovskiy

Differential Revision: D79638814

fbshipit-source-id: 0bc373a397542d347e1917c6481fb0d405e2a42f

prelude/toolchains/go.bzl
@@ -6,11 +6,11 @@
 # of this source tree. You may select, at your option, one of the
 # above-listed licenses.
 
-load("@prelude//fbcode/buck2/prelude/toolchains/go:system_go_bootstrap_toolchain.bzl", _system_go_bootstrap_toolchain = "system_go_bootstrap_toolchain")
-load("@prelude//fbcode/buck2/prelude/toolchains/go:system_go_toolchain.bzl", _system_go_toolchain = "system_go_toolchain")
+load("@prelude//toolchains/go:system_go_bootstrap_toolchain.bzl", _system_go_bootstrap_toolchain = "system_go_bootstrap_toolchain")
+load("@prelude//toolchains/go:system_go_toolchain.bzl", _system_go_toolchain = "system_go_toolchain")
 
-# deprecated: use `@prelude//fbcode/buck2/prelude/toolchains/go:system_go_bootstrap_toolchain.bzl` instead
+# deprecated: use `@prelude///toolchains/go:system_go_bootstrap_toolchain.bzl` instead
 system_go_bootstrap_toolchain = _system_go_bootstrap_toolchain
 
-# deprecated: use `@prelude//fbcode/buck2/prelude/toolchains/go:system_go_toolchain.bzl` instead
+# deprecated: use `@prelude///toolchains/go:system_go_toolchain.bzl` instead
 system_go_toolchain = _system_go_toolchain