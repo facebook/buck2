290702cad05bd4ba87616eff9684733d8714452d
Alexander Kampmann <akampmann@meta.com>
2025-08-08 08:58:55 -0700

    do not lose screen recording on timeouts
    Summary:
    There are some cases were we do not save the screen recording, because we assume that there is an error when there is not. E.g.
    
    https://www.internalfb.com/intern/testinfra/diagnostics/9007199354991152.281475143834126.1753962113/
    
    What happens here is that screenrecord terminates on its own. Then, when we try to terminate it, we cannot. We handle this as a failure, and do not save the screen recording.
    With this diff, we detect such cases, and we store the screen recording.
    
    Reviewed By: tagantroy
    
    Differential Revision: D79345725
    
    fbshipit-source-id: f6e07f3f9c04a8fe173c6407b2f52ce3e6f88768

--- prelude/toolchains/android/src/com/facebook/buck/testrunner/reportlayer/VideoRecordingReportLayer.java
+++ prelude/toolchains/android/src/com/facebook/buck/testrunner/reportlayer/VideoRecordingReportLayer.java
@@ -12,9 +12,14 @@ package com.facebook.buck.testrunner.reportlayer;
 
 import com.android.ddmlib.IDevice;
 import com.facebook.buck.testrunner.InstrumentationTestRunner;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ProcessBuilder.Redirect;
 import java.nio.file.Path;
+import java.util.regex.Pattern;
 
 /**
  * report layer to start a process to record video of tests before they run and collect the video as
@@ -74,7 +79,8 @@ public class VideoRecordingReportLayer extends ReportLayer {
           String.format("%s/%s", this.getVideoRecordingStoragePath(), VIDEO_RECORDING_SCRIPT_NAME);
       this.runner.pushFileWithSyncService(device, scriptFile.getAbsolutePath(), remoteScriptPath);
       // start recording process
-      return this.runner.exec(String.format("adb shell sh %s", remoteScriptPath));
+      String[] command = new String[] {"adb", "shell", "sh", remoteScriptPath};
+      return new ProcessBuilder(command).redirectOutput(Redirect.PIPE).start();
     } catch (Exception e) {
       System.err.printf("Failed to start video recording process with error: %s\n", e);
       return null;
@@ -87,17 +93,33 @@ public class VideoRecordingReportLayer extends ReportLayer {
       videoRecordingProcess.getOutputStream().write("STOP".getBytes());
       videoRecordingProcess.getOutputStream().write(System.lineSeparator().getBytes());
       videoRecordingProcess.getOutputStream().flush();
+      StringBuilder output = new StringBuilder();
+      try (InputStream is = videoRecordingProcess.getInputStream()) {
+        byte[] allOutput = readAllBytes(is);
+        output.append(new String(allOutput));
+      } catch (IOException e) {
+        output.append("IOError: ");
+        output.append(e.getMessage());
+        output.append("\n");
+      }
       int exitCode = videoRecordingProcess.waitFor();
       if (exitCode != 0) {
-        throw new Exception(
-            String.format("Failed to stop video recording process with exit code %d", exitCode));
+        // check whether screenrecord terminated before we tried to kill it. We don't want to throw
+        // an error in that case.
+        Pattern noProcessKilled = Pattern.compile("kill: \\d+: No such process");
+        if (!noProcessKilled.matcher(output).find()) {
+          throw new Exception(
+              String.format(
+                  "Failed to stop video recording process with exit code %d, Output: %s",
+                  exitCode, output.toString()));
+        }
       }
       // pull file to TRA
       Path video_artifact =
           this.runner.createTRA(
               "video_recording_test_artifact", "video recording", VIDEO_RECORDING_FILE_NAME);
       if (null == video_artifact) {
-        throw new Exception("Failed to create TRA for vide recording");
+        throw new Exception("Failed to create TRA for video recording");
       }
       this.runner.pullFileWithSyncService(
           device,
@@ -106,5 +128,19 @@ public class VideoRecordingReportLayer extends ReportLayer {
     } catch (Exception e) {
       System.err.printf("Failed to collect video recording process with error: %s", e);
     }
+  }
+
+  private static byte[] readAllBytes(InputStream inputStream) throws IOException {
+    final int bufLen = 1024;
+    byte[] buf = new byte[bufLen];
+    int readLen;
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    while ((readLen = inputStream.read(buf, 0, bufLen)) != -1) {
+      outputStream.write(buf, 0, readLen);
+    }
+
+    return outputStream.toByteArray();
   }
 }
