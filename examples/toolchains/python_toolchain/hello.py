635637837cea512a70f40ebc1444f6a4432bbfb8
zsol <zsol@meta.com>
2025-08-08 11:18:14 -0700

    remote_python_toolchain: demonstrate custom cpython version
    Differential Revision: D79885612

---
+++ examples/toolchains/python_toolchain/hello.py
@@ -0,0 +1,18 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is dual-licensed under either the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree or the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree. You may select, at your option, one of the
+# above-listed licenses.
+
+# pyre-strict
+import sys
+
+
+def main() -> None:
+    print(sys.version)
+
+
+if __name__ == "__main__":
+    main()
