fb34ae3ebfb99e7047f4ba3d06611c820cf99165
Milen Dzhumerov <mgd@meta.com>
2025-08-11 08:48:23 -0700

    Codesigning: simplify codesign manifest tree
    Summary:
    **Problem**
    
    The codesign manifest tree is a relatively complex output - a tree of JSON files, containing signing commands. This means that all clients need to understand the correct ordering of processing codesigning commands (inside out) and implement the required parsing.
    
     The goal is to make misusing the codesigning manifest much harder and simplify clients.
    
    **Solution**
    
    Postprocess the output and produce a simple list of codesigning commands. This flattens the nested structure of JSON files and provides an explicit ordering of the codesigning commands, removing the need for clients to implement this logic.
    
    Reviewed By: manicaesar
    
    Differential Revision: D79993153
    
    fbshipit-source-id: ce5e3a06353cdf34e452c3064197e725d6c2de9c

--- prelude/apple/tools/codesign_manifest_tree_postprocessor/main.py
+++ prelude/apple/tools/codesign_manifest_tree_postprocessor/main.py
@@ -7,9 +7,44 @@
 # above-listed licenses.
 
 import argparse
-import shutil
+import json
+import os
 
 from pathlib import Path
+
+
+def _extract_codesign_invocations_from_codesign_manifest_tree(
+    current_path, codesign_manifest_tree, invocations
+):
+    inner_manifests = codesign_manifest_tree.get("inner_codesign_manifests", {})
+    for inner_rel_path, inner_codesign_manifest_tree in inner_manifests.items():
+        inner_path = os.path.join(current_path, inner_rel_path)
+        # Process inner bundles first (i.e., inside out), as signing outer bundles
+        # requires inner bundles to have already been signed (otherwise the outer
+        # signature would be invalidated if the inner bundle is signed afterwards).
+        _extract_codesign_invocations_from_codesign_manifest_tree(
+            current_path=inner_path,
+            codesign_manifest_tree=inner_codesign_manifest_tree,
+            invocations=invocations,
+        )
+
+    codesign_manifest_path = codesign_manifest_tree.get("codesign_manifest")
+    if codesign_manifest_path:
+        with open(codesign_manifest_path, "r") as codesign_manifest_file:
+            codesign_manifest = json.load(codesign_manifest_file)
+            codesign_manifest_invocations = codesign_manifest.get("invocations", [])
+            for original_manifest_invocation in codesign_manifest_invocations:
+                original_invocation_path = original_manifest_invocation["path"]
+                updated_invocation_path = (
+                    os.path.join(current_path, original_invocation_path)
+                    if original_invocation_path != "."
+                    else current_path
+                )
+
+                updated_manifest_invocation = dict(original_manifest_invocation)
+                updated_manifest_invocation["path"] = updated_invocation_path
+
+                invocations.append(updated_manifest_invocation)
 
 
 def _args_parser() -> argparse.ArgumentParser:
@@ -37,7 +72,26 @@ def _args_parser() -> argparse.ArgumentParser:
 def _main() -> None:
     args_parser = _args_parser()
     args = args_parser.parse_args()
-    shutil.copy2(args.codesign_manifest_tree, args.output)
+
+    with open(args.codesign_manifest_tree, "r") as codesign_manifest_tree_file:
+        codesign_manifest_tree = json.load(codesign_manifest_tree_file)
+
+        invocations = []
+        _extract_codesign_invocations_from_codesign_manifest_tree(
+            current_path="",
+            codesign_manifest_tree=codesign_manifest_tree,
+            invocations=invocations,
+        )
+
+        with open(args.output, "w") as output_file:
+            json.dump(
+                {
+                    "version": 1,
+                    "invocations": invocations,
+                },
+                output_file,
+                indent=4,
+            )
 
 
 if __name__ == "__main__":
