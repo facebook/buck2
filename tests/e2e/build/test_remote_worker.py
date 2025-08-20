3430a47d6300dcb955bc17935bce44c7170055c4
Ian Childs <ianc@meta.com>
2025-08-20 06:44:18 -0700

    Add some tests for persistent remote workers
    Summary:
    For now, just use `fbcode//remote_execution/rust/worker_tools:dummy_worker_tool_server`, which will set a `DUMMY_WORKER_TOOL_ENV_VAR` in https://www.internalfb.com/code/fbsource/[37089cac46f5]/fbcode/remote_execution/lib/if/worker_tool/worker_tool.thrift?lines=7-11, and which is then passed as an env var to any `WorkerToolExecuteRequest`: https://www.internalfb.com/code/fbsource/[37089cac46f5]/fbcode/remote_execution/lib/if/worker_tool/worker_tool.thrift?lines=13-22. The tool itself will just run the command that you pass to it.
    
    We have two actions that use the same worker and run them sequentially, so that they get the same `DUMMY_WORKER_TOOL_ENV_VAR`, and then a third action prints those. In the test we verify that they are the same.
    
    Reviewed By: christolliday
    
    Differential Revision: D80094136
    
    fbshipit-source-id: 31ada9268b0fa16f2170550734e8d8027cbc763c

---
+++ tests/e2e/build/test_remote_worker.py
@@ -0,0 +1,50 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is dual-licensed under either the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree or the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree. You may select, at your option, one of the
+# above-listed licenses.
+
+# pyre-strict
+
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.buck_workspace import buck_test
+from buck2.tests.e2e_util.helper.utils import expect_exec_count, random_string
+
+
+@buck_test(inplace=True)
+async def test_remote_worker(buck: Buck) -> None:
+    target = "fbcode//buck2/tests/targets/rules/remote_worker:run_two_worker_rules"
+    result = await buck.build(
+        target,
+        "-c",
+        "buck2.fat_mac_linux_enable_remote_persistent_workers=True",
+        "-c",
+        f"test.cache_buster={random_string()}",
+    )
+
+    output = result.get_build_report().output_for_target(target)
+
+    output_path = buck.cwd / output
+    with open(output_path, "r") as f:
+        output_lines = f.readlines()
+        assert len(output_lines) == 2
+        assert output_lines[0].strip() == output_lines[1].strip()
+
+
+@buck_test(inplace=True)
+async def test_remote_worker_caches(buck: Buck) -> None:
+    target = "fbcode//buck2/tests/targets/rules/remote_worker:run_remote_worker_1"
+    args = [
+        target,
+        "-c",
+        "buck2.fat_mac_linux_enable_remote_persistent_workers=True",
+    ]
+    await buck.build(*args)
+
+    buck.kill()
+
+    await buck.build(*args)
+    await expect_exec_count(buck, 0)
