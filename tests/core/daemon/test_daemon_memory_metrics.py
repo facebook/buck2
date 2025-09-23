3e51f220cd9ef6f1107b086be4166c5db5893b9a
Thomas Orozco <torozco@meta.com>
2025-09-23 03:22:08 -0700

    add cgroup memory stats tests
    Summary: Test e2e that this works.
    
    Reviewed By: Will-MingLun-Li
    
    Differential Revision: D82557869
    
    fbshipit-source-id: b7fd4e1c7a68b57fc928f4af4b30041657149133

---
+++ tests/core/daemon/test_daemon_memory_metrics.py
@@ -0,0 +1,69 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is dual-licensed under either the MIT license found in the
+# LICENSE-MIT file in the root directory of this source tree or the Apache
+# License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+# of this source tree. You may select, at your option, one of the
+# above-listed licenses.
+
+# pyre-strict
+
+import json
+import typing
+
+from buck2.tests.e2e_util.api.buck import Buck
+from buck2.tests.e2e_util.buck_workspace import buck_test
+
+
+@buck_test(skip_for_os=["darwin", "windows"])
+async def test_metrics_cgroup_neither(buck: Buck) -> None:
+    write_config(buck, resource_control=False, action_cgroup_pool=False)
+    snapshot = await start_daemon_and_get_snapshot(buck)
+    assert snapshot["daemon_cgroup"] is None
+    assert snapshot["forkserver_cgroup"] is None
+
+
+@buck_test(skip_for_os=["darwin", "windows"])
+async def test_metrics_cgroup_daemon_only(buck: Buck) -> None:
+    write_config(buck, resource_control=True, action_cgroup_pool=False)
+    snapshot = await start_daemon_and_get_snapshot(buck)
+    assert snapshot["daemon_cgroup"]["anon"] > 0
+    assert snapshot["forkserver_cgroup"] is None
+
+
+@buck_test(skip_for_os=["darwin", "windows"])
+async def test_metrics_cgroup_both(buck: Buck) -> None:
+    write_config(buck, resource_control=True, action_cgroup_pool=True)
+    snapshot = await start_daemon_and_get_snapshot(buck)
+    assert snapshot["daemon_cgroup"]["anon"] >= snapshot["forkserver_cgroup"]["anon"]
+    assert snapshot["daemon_cgroup"]["file"] >= snapshot["forkserver_cgroup"]["file"]
+    assert (
+        snapshot["daemon_cgroup"]["kernel"] >= snapshot["forkserver_cgroup"]["kernel"]
+    )
+
+
+# Placeholder for tests to be listed successfully on non-Linux platforms.
+async def test_noop() -> None:
+    pass
+
+
+def write_config(
+    buck: Buck, *, resource_control: bool, action_cgroup_pool: bool
+) -> None:
+    with open(buck.cwd / ".buckconfig", "a") as buckconfig:
+        buckconfig.write("[buck2_resource_control]\n")
+        buckconfig.write(f"status = {"required" if resource_control else "off"}\n")
+        buckconfig.write(
+            f"enable_action_cgroup_pool = {"true" if action_cgroup_pool else "false"}\n"
+        )
+
+
+async def start_daemon_and_get_snapshot(buck: Buck) -> dict[str, typing.Any]:
+    # Start the daemon
+    await buck.targets(":")
+
+    # Get the snapshot
+    status_result = await buck.status("--snapshot")
+    status_data = json.loads(status_result.stdout)
+    snapshot = status_data["snapshot"]
+    return snapshot
