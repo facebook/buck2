b44600c5d941e2f45429e575ef807992ebe40dd6
Milen Dzhumerov <mgd@meta.com>
2025-10-13 23:42:00 -0700

    Code Signing: extract args for signing context
    Summary: Extracts args that will be used for provisioning metadata extraction.
    
    Reviewed By: benb
    
    Differential Revision: D84349139
    
    fbshipit-source-id: 596847a984ecb70bdfe8b831b74a21614e9795ca

--- prelude/apple/tools/bundling/signing_context.py
+++ prelude/apple/tools/bundling/signing_context.py
@@ -8,8 +8,11 @@
 
 import argparse
 import shlex
+from pathlib import Path
 
 from typing import Optional, Union
+
+from apple.tools.code_signing.apple_platform import ApplePlatform
 
 from apple.tools.code_signing.codesign_bundle import (
     AdhocSigningContext,
@@ -20,6 +23,97 @@ from apple.tools.code_signing.list_codesign_identities import (
     AdHocListCodesignIdentities,
     ListCodesignIdentities,
 )
+
+
+def add_args_for_signing_context(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        "--codesign",
+        action="store_true",
+        help="Should the final bundle be codesigned.",
+    )
+    parser.add_argument(
+        "--ad-hoc",
+        action="store_true",
+        help="Perform ad-hoc signing if set.",
+    )
+    parser.add_argument(
+        "--ad-hoc-codesign-identity",
+        metavar="<identity>",
+        type=str,
+        required=False,
+        help="Codesign identity to use when ad-hoc signing is performed. Should be present when selection of provisioining profile is requested for ad-hoc signing.",
+    )
+    parser.add_argument(
+        "--codesign-identities-command",
+        metavar='<"/signing/identities --available">',
+        type=str,
+        required=False,
+        help="Command listing available code signing identities. If it's not provided `security` utility is assumed to be available and is used.",
+    )
+    parser.add_argument(
+        "--profiles-dir",
+        metavar="</provisioning/profiles/directory>",
+        type=Path,
+        required=False,
+        help="Required if non-ad-hoc code signing is requested. Path to directory with provisioning profile files.",
+    )
+    parser.add_argument(
+        "--embed-provisioning-profile-when-signing-ad-hoc",
+        action="store_true",
+        help="Perform selection of provisioining profile and embed it into final bundle when ad-hoc signing if set.",
+    )
+    parser.add_argument(
+        "--fast-provisioning-profile-parsing",
+        action="store_true",
+        help="Uses experimental faster provisioning profile parsing.",
+    )
+    parser.add_argument(
+        "--strict-provisioning-profile-search",
+        action="store_true",
+        required=False,
+        help="Fail code signing if more than one matching profile found.",
+    )
+    parser.add_argument(
+        "--provisioning-profile-filter",
+        metavar="<regex>",
+        type=str,
+        required=False,
+        help="Regex to disambiguate multiple matching profiles, evaluated against provisioning profile filename.",
+    )
+    parser.add_argument(
+        "--entitlements",
+        metavar="<Entitlements.plist>",
+        type=Path,
+        required=False,
+        help="Path to file with entitlements to be used during code signing. If it's not provided the minimal entitlements are going to be generated.",
+    )
+    parser.add_argument(
+        "--info-plist-source",
+        metavar="</prepared/Info.plist>",
+        type=Path,
+        required=False,
+        help="Path to Info.plist source file which is used only to make code signing decisions (to be bundled `Info.plist` should be present in spec parameter). Required if code signing is requested.",
+    )
+    parser.add_argument(
+        "--info-plist-destination",
+        metavar="<Info.plist>",
+        type=Path,
+        required=False,
+        help="Required if code signing is requested. Bundle relative destination path to Info.plist file if it is present in bundle.",
+    )
+    parser.add_argument(
+        "--platform",
+        metavar="<apple platform>",
+        type=ApplePlatform,
+        required=False,
+        help="Required if code signing or Swift support is requested. Apple platform for which the bundle is built.",
+    )
+    parser.add_argument(
+        "--log-file",
+        type=Path,
+        required=False,
+        help="Path to a log file. If present logging will be directed to this file in addition to stderr.",
+    )
 
 
 def signing_context_and_selected_identity_from_args(
