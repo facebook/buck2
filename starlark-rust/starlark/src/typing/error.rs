a9ad23b315cab90435d5bb5239c252d9117f2fc1
Cormac Relf <web@cormacrelf.net>
2025-11-19 21:50:14 +1100

    Add TypingOrInternalError::into_eval_exception

--- starlark-rust/starlark/src/typing/error.rs
+++ starlark-rust/starlark/src/typing/error.rs
@@ -136,6 +136,16 @@ impl From<InternalError> for TypingOrInternalError {
     }
 }
 
+impl TypingOrInternalError {
+    #[cold]
+    pub(crate) fn into_eval_exception(self) -> EvalException {
+        match self {
+            Self::Typing(e) => e.into_eval_exception(),
+            Self::Internal(e) => e.into_eval_exception(),
+        }
+    }
+}
+
 pub enum TypingNoContextOrInternalError {
     Typing,
     Internal(InternalError),
