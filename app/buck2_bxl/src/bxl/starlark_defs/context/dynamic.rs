7700f9caa447754a581370a7dbfdeeba66ca9acf
Chenhao Zuo <nero@meta.com>
2025-11-21 12:26:38 -0800

    Fix broken link
    Summary: ^
    
    Reviewed By: Will-MingLun-Li
    
    Differential Revision: D87589911
    
    fbshipit-source-id: a5d3517b32173e76e0f08715fa88bd65a09a7112

--- app/buck2_bxl/src/bxl/starlark_defs/context/dynamic.rs
+++ app/buck2_bxl/src/bxl/starlark_defs/context/dynamic.rs
@@ -284,9 +284,9 @@ pub(crate) fn register_dynamic_actions(globals: &mut GlobalsBuilder) {
     /// and the result of calling it can be passed to `ctx.actions.dynamic_output_new`.
     ///
     /// Be aware that the context argument of the called impl function differs between
-    /// [`dynamic_actions`](../#dynamic_actions) where it is [`actions: AnalysisActions`](../AnalysisActions)
+    /// [`dynamic_actions`](../#dynamic_actions) where it is [`actions: AnalysisActions`](../build/AnalysisActions)
     /// and [`bxl.dynamic_actions`](../../bxl/#dynamic_actions)
-    /// where it is [`bxl_ctx: bxl.Context`](../../bxl/Context).
+    /// where it is [`bxl_ctx: bxl.Context`](../bxl/Context).
     fn dynamic_actions<'v>(
         #[starlark(require = named)] r#impl: StarlarkCallableChecked<
             'v,
