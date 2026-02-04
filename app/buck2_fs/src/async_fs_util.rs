7a8f035f1915d8dd4105c30d379c37b265803f07
David Tolnay <dtolnay@meta.com>
2026-02-04 12:42:51 -0800

    Resolve clippy::needless_question_mark lints
    Summary:
    ```lang=log
    warning: enclosing `Ok` and `?` operator are unneeded
      --> fbcode/buck2/app/buck2_fs/src/async_fs_util.rs:50:5
       |
    50 |     Ok(tokio::task::spawn_blocking(move || fs_util::write(path, content)).await??)
       |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.91.0/index.html#needless_question_mark
       = note: `-W clippy::needless-question-mark` implied by `-W clippy::all`
       = help: to override `-W clippy::all` add `#[allow(clippy::needless_question_mark)]`
    help: remove the enclosing `Ok` and `?` operator
       |
    50 -     Ok(tokio::task::spawn_blocking(move || fs_util::write(path, content)).await??)
    50 +     tokio::task::spawn_blocking(move || fs_util::write(path, content)).await?
       |
    
    warning: enclosing `Ok` and `?` operator are unneeded
      --> fbcode/buck2/app/buck2_fs/src/async_fs_util.rs:55:5
       |
    55 |     Ok(tokio::task::spawn_blocking(move || fs_util::read_to_string(path)).await??)
       |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.91.0/index.html#needless_question_mark
    help: remove the enclosing `Ok` and `?` operator
       |
    55 -     Ok(tokio::task::spawn_blocking(move || fs_util::read_to_string(path)).await??)
    55 +     tokio::task::spawn_blocking(move || fs_util::read_to_string(path)).await?
       |
    
    warning: enclosing `Ok` and `?` operator are unneeded
      --> fbcode/buck2/app/buck2_fs/src/async_fs_util.rs:62:5
       |
    62 |     Ok(tokio::task::spawn_blocking(move || fs_util::read_to_string_if_exists(path)).await??)
       |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.91.0/index.html#needless_question_mark
    help: remove the enclosing `Ok` and `?` operator
       |
    62 -     Ok(tokio::task::spawn_blocking(move || fs_util::read_to_string_if_exists(path)).await??)
    62 +     tokio::task::spawn_blocking(move || fs_util::read_to_string_if_exists(path)).await?
       |
    
    warning: enclosing `Ok` and `?` operator are unneeded
      --> fbcode/buck2/app/buck2_fs/src/async_fs_util.rs:67:5
       |
    67 |     Ok(tokio::task::spawn_blocking(move || fs_util::create_dir_all(dir)).await??)
       |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.91.0/index.html#needless_question_mark
    help: remove the enclosing `Ok` and `?` operator
       |
    67 -     Ok(tokio::task::spawn_blocking(move || fs_util::create_dir_all(dir)).await??)
    67 +     tokio::task::spawn_blocking(move || fs_util::create_dir_all(dir)).await?
       |
    
    warning: enclosing `Ok` and `?` operator are unneeded
      --> fbcode/buck2/app/buck2_fs/src/async_fs_util.rs:72:5
       |
    72 |     Ok(tokio::task::spawn_blocking(move || fs_util::metadata(path)).await??)
       |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.91.0/index.html#needless_question_mark
    help: remove the enclosing `Ok` and `?` operator
       |
    72 -     Ok(tokio::task::spawn_blocking(move || fs_util::metadata(path)).await??)
    72 +     tokio::task::spawn_blocking(move || fs_util::metadata(path)).await?
       |
    ```
    
    Reviewed By: diliop
    
    Differential Revision: D92296923
    
    fbshipit-source-id: e8a6ee6c98122dda0ccfcf32b334418aac92bf4d

--- app/buck2_fs/src/async_fs_util.rs
+++ app/buck2_fs/src/async_fs_util.rs
@@ -47,27 +47,27 @@ pub async fn write<P: AsRef<AbsPath>>(
 ) -> buck2_error::Result<()> {
     let path = path.as_ref().to_owned();
     let content = content.as_ref().to_owned();
-    Ok(tokio::task::spawn_blocking(move || fs_util::write(path, content)).await??)
+    tokio::task::spawn_blocking(move || fs_util::write(path, content)).await?
 }
 
 pub async fn read_to_string<P: AsRef<AbsPath>>(path: P) -> buck2_error::Result<String> {
     let path = path.as_ref().to_owned();
-    Ok(tokio::task::spawn_blocking(move || fs_util::read_to_string(path)).await??)
+    tokio::task::spawn_blocking(move || fs_util::read_to_string(path)).await?
 }
 
 pub async fn read_to_string_if_exists<P: AsRef<AbsPath>>(
     path: P,
 ) -> buck2_error::Result<Option<String>> {
     let path = path.as_ref().to_owned();
-    Ok(tokio::task::spawn_blocking(move || fs_util::read_to_string_if_exists(path)).await??)
+    tokio::task::spawn_blocking(move || fs_util::read_to_string_if_exists(path)).await?
 }
 
 pub async fn create_dir_all<P: AsRef<AbsPath>>(dir: P) -> buck2_error::Result<()> {
     let dir = dir.as_ref().to_owned();
-    Ok(tokio::task::spawn_blocking(move || fs_util::create_dir_all(dir)).await??)
+    tokio::task::spawn_blocking(move || fs_util::create_dir_all(dir)).await?
 }
 
 pub async fn metadata<P: AsRef<AbsPath>>(path: P) -> buck2_error::Result<Metadata> {
     let path = path.as_ref().to_owned();
-    Ok(tokio::task::spawn_blocking(move || fs_util::metadata(path)).await??)
+    tokio::task::spawn_blocking(move || fs_util::metadata(path)).await?
 }
