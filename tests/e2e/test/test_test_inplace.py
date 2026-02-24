9ea89affe0d32f3faea7a5521547ed8c9e7d8ba0
Ian Childs <ianc@meta.com>
2026-02-24 15:37:36 -0800

    Add stress run test for C++ tests
    Summary:
    Unfortunately, `test_stress_runs` only checks for the failing case: https://www.internalfb.com/code/fbsource/[0bbc8c3652b0def746280a88e64f87b504d3f583]/fbcode/buck2/tests/e2e/test/test_test_inplace.py?lines=397-410
    
    This adds a successful stress test.
    
    Differential Revision: D94200812
    
    fbshipit-source-id: 70b0f4280a4d4b29d47e9263b491123b5b5693f1

--- tests/e2e/test/test_test_inplace.py
+++ tests/e2e/test/test_test_inplace.py
@@ -117,6 +117,19 @@ async def test_cpp_test(buck: Buck) -> None:
     )
 
 
+@buck_test(inplace=True, skip_for_os=["windows"])
+async def test_cpp_stress_runs(buck: Buck) -> None:
+    mode = get_mode_from_platform()
+    res = await buck.test(
+        "fbcode//buck2/tests/targets/rules/cxx:cpp_test_pass",
+        mode,
+        "--",
+        "--stress-runs=10",
+    )
+
+    assert "Pass 10" in res.stderr, "Expected stress runs to be run"
+
+
 @buck_test(inplace=True, skip_for_os=["darwin"])
 async def test_cpp_test_fdb_message(buck: Buck) -> None:
     await expect_failure(
