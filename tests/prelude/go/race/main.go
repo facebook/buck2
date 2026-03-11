81532e3f6c311e31c7d035806dc7038f9617ebad
Michael Podtserkovskii <michaelpo@meta.com>
2026-03-11 09:44:46 -0700

    Add tests for race detector
    Summary: Data Race detrctor tests, should work fine on all platforms we support in CI
    
    Reviewed By: echistyakov
    
    Differential Revision: D95846097
    
    fbshipit-source-id: ae5a11d563f5707f0b9223409f234c390b800767

---
+++ tests/prelude/go/race/main.go
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is dual-licensed under either the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree or the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree. You may select, at your option, one of the
+ * above-listed licenses.
+ */
+
+package main
+
+import "fmt"
+
+// Example from https://go.dev/doc/articles/race_detector
+func main() {
+	c := make(chan bool)
+	m := make(map[string]string)
+	go func() {
+		m["1"] = "a" // First conflicting access.
+		c <- true
+	}()
+	m["2"] = "b" // Second conflicting access.
+	<-c
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+}
