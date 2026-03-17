8a23524ab6ca26e8853ec8ed49342029cb02a1f6
Michael Podtserkovskii <michaelpo@meta.com>
2026-03-17 14:20:51 -0700

    Remove dead code from go_wrapper
    Summary: We no longer need it as we now don't use `go build`
    
    Reviewed By: siutsin
    
    Differential Revision: D96763770
    
    fbshipit-source-id: 0c88cea7361b468d7ce0d45dc4260bec73138054

--- prelude/go/tools/go_wrapper.go
+++ prelude/go/tools/go_wrapper.go
@@ -21,107 +21,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"unicode"
 )
-
-// GPT-4 implementation of Python's shlex.split
-func shellSplit(input string) ([]string, error) {
-	var result []string
-	reader := strings.NewReader(input)
-	scanner := bufio.NewScanner(reader)
-	scanner.Split(bufio.ScanWords)
-	var token strings.Builder
-	inQuotes := false
-	var quoteChar rune
-	appendToken := func() {
-		if token.Len() > 0 {
-			result = append(result, token.String())
-			token.Reset()
-		}
-	}
-	for scanner.Scan() {
-		word := scanner.Text()
-		for _, r := range word {
-			switch {
-			case r == '\'' || r == '"':
-				if inQuotes {
-					if r == quoteChar {
-						inQuotes = false
-						quoteChar = 0
-						appendToken()
-						continue
-					}
-				} else {
-					inQuotes = true
-					quoteChar = r
-					continue
-				}
-			case unicode.IsSpace(r):
-				if !inQuotes {
-					appendToken()
-					continue
-				}
-			}
-			token.WriteRune(r)
-		}
-		if !inQuotes {
-			appendToken()
-		}
-	}
-	if inQuotes {
-		return nil, fmt.Errorf("unclosed quote in input: %s", input)
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-// A copy of https://github.com/golang/go/blob/go1.23.0/src/cmd/internal/quoted/quoted.go#L65
-func join(args []string) (string, error) {
-	var buf []byte
-	for i, arg := range args {
-		if i > 0 {
-			buf = append(buf, ' ')
-		}
-		var sawSpace, sawSingleQuote, sawDoubleQuote bool
-		for _, c := range arg {
-			switch {
-			case c > unicode.MaxASCII:
-				continue
-			case isSpaceByte(byte(c)):
-				sawSpace = true
-			case c == '\'':
-				sawSingleQuote = true
-			case c == '"':
-				sawDoubleQuote = true
-			}
-		}
-		switch {
-		case !sawSpace && !sawSingleQuote && !sawDoubleQuote:
-			buf = append(buf, arg...)
-
-		case !sawSingleQuote:
-			buf = append(buf, '\'')
-			buf = append(buf, arg...)
-			buf = append(buf, '\'')
-
-		case !sawDoubleQuote:
-			buf = append(buf, '"')
-			buf = append(buf, arg...)
-			buf = append(buf, '"')
-
-		default:
-			return "", fmt.Errorf("argument %q contains both single and double quotes and cannot be quoted", arg)
-		}
-	}
-	return string(buf), nil
-}
-
-// A copy of https://github.com/golang/go/blob/go1.23.0/src/cmd/internal/quoted/quoted.go#L15
-func isSpaceByte(c byte) bool {
-	return c == ' ' || c == '\t' || c == '\n' || c == '\r'
-}
 
 func loadArgs(args []string) []string {
 	newArgs := make([]string, 0, 0)
@@ -163,9 +63,6 @@ func main() {
 	var defaultGoArch = flag.String("default-goarch", "", "default GOARCH (if not set by env)")
 	var outputFile = flag.String("output", "", "file to redirect stdout to")
 	var convertJsonStream = flag.Bool("convert-json-stream", false, "convert json stream to array")
-	var workdir = flag.String("workdir", "", "directory to run the command in")
-	var useFakeGoroot = flag.Bool("use-fake-goroot", false, "use a fake GOROOT")
-	var useTmpWorkdir = flag.Bool("use-tmp-workdir", false, "use BUCK_SCRATCH_PATH as workdir")
 	flag.Parse()
 	unknownArgs := flag.Args()
 
@@ -211,14 +108,6 @@ func main() {
 		}
 		envs["GOCACHE"] = absBuckScratchPath
 		envs["TMPDIR"] = absBuckScratchPath
-
-		if *useFakeGoroot {
-			envs["GOROOT"] = absBuckScratchPath
-		}
-
-		if *useTmpWorkdir {
-			*workdir = absBuckScratchPath
-		}
 	}
 
 	cwd, err := os.Getwd()
@@ -226,20 +115,6 @@ func main() {
 		log.Fatal("Failed to get current working directory: %s", err)
 	}
 
-	for _, envVar := range []string{"CC", "CGO_CFLAGS", "CGO_CPPFLAGS", "CGO_LDFLAGS"} {
-		if value, ok := envs[envVar]; ok {
-			//  HACK: Split the value into a list of arguments then join them back.
-			//  This is because buck encodes quoted args in a way `go` doesn't like,
-			//  but `join` does it in a way that `go` expects.
-			splitValue := strings.Split(value, "\t")
-			joinedValue, err := join(splitValue)
-			if err != nil {
-				log.Fatal("Failed to join %q: %s", envVar, err)
-			}
-			// HACK: Replace %cwd% with the current working directory to make it work when `go` does `cd` to a tmp-dir.
-			envs[envVar] = strings.ReplaceAll(joinedValue, "%cwd%", cwd)
-		}
-	}
 	for i, arg := range unknownArgs {
 		unknownArgs[i] = strings.ReplaceAll(arg, "%cwd%", cwd)
 	}
@@ -261,10 +136,6 @@ func main() {
 	cmd.Env = make([]string, 0, len(envs)/2)
 	for k, v := range envs {
 		cmd.Env = append(cmd.Env, k+"="+v)
-	}
-
-	if *workdir != "" {
-		cmd.Dir = *workdir
 	}
 
 	stdout, err := cmd.StdoutPipe()
