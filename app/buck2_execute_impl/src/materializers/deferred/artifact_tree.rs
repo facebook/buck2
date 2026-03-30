93917d731986f4e3fce40800f8d874f3f3c285a3
Deepak Kumar <deepakkumar@meta.com>
2026-03-29 21:19:13 -0700

    Make soft_error! return Ok in OSS builds and remove .unwrap() at call sites

--- app/buck2_execute_impl/src/materializers/deferred/artifact_tree.rs
+++ app/buck2_execute_impl/src/materializers/deferred/artifact_tree.rs
@@ -392,7 +392,7 @@ impl ArtifactTree {
             }
             Err(e) => {
                 // NOTE: This shouldn't normally happen?
-                soft_error!("cleanup_finished_vacant", e, quiet: true).unwrap();
+                let _unused = soft_error!("cleanup_finished_vacant", e, quiet: true);
             }
         }
     }
