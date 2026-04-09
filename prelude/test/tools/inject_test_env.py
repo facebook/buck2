e95cb7250437c644277f23b132faf9dbd645336a
Keeran Rothenfusser <keeranr@meta.com>
2026-04-09 05:29:32 -0700

    Dry run for inject_test_env.py
    Summary:
    In case just one part of a buck build needs to be rebuilt, we can see a command line that would have the same effect as running the `inject_test_env.py` script. Without the dry run, the user would have to read the environment from a file, and load that into the current bash session.
    
    We make sure that the command works on unix and windows systems, where on windows, we use double escaping around a call to bash, which means that we can reuse the bash syntax that is valid on linux, with extra escaping for double quotes.
    
    Reviewed By: scottcao
    
    Differential Revision: D99990364
    
    fbshipit-source-id: 01ceb5c9a1247423211a480ad097091b5679d712

--- prelude/test/tools/inject_test_env.py
+++ prelude/test/tools/inject_test_env.py
@@ -10,12 +10,20 @@
 import argparse
 import json
 import os
+import shlex
+import subprocess
+import sys
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "env_file", help="A JSON file containing the environment to inject"
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Print a command line that can be used to run this command in the current environment. Useful for debugging",
     )
     parser.add_argument("executable")
     parser.add_argument("args", nargs="*")
@@ -26,6 +34,20 @@ def main():
         env_from_file = json.load(env_file)
         env = {**os.environ, **env_from_file}
 
+    if args.dry_run:
+        env_str = (
+            "; ".join(f"export {k}={shlex.quote(v)}" for k, v in env_from_file.items())
+            + "; "
+            if env_from_file
+            else ""
+        )
+        inner_cmd = env_str + shlex.join([args.executable, *args.args])
+        if sys.platform == "win32":
+            print_str = subprocess.list2cmdline(["bash", "-c", inner_cmd])
+        else:
+            print_str = "(" + inner_cmd + ")"
+        print(print_str)
+        return
     os.execve(args.executable, [args.executable, *args.args], env)
 
 
