8d8caa3e9c5838043218af816bc85a9eeb65c32e
Daniel Gorin <danielgo@meta.com>
2026-05-18 10:08:38 -0700

    Replace epp_dodger with erl_scan in dependency_analyzer
    Summary:
    # Context
    The Erlang buck2 rules invoke the `dependency_analyzer` script on every `.erl` and `.hrl` file to extract its direct dependencies (`-include`, `-include_lib`, `-behaviour`, etc)., to track finer-grained dependencies and avoid unnecessary rebuilds when dependencies change. Currently, the `dependency-analyzer` rellies on `epp_dodger:parse_file/1`, which essentially parses the module, but ignoring pre-processir directives (so -include` directives are preserved, etc.
    
    # Problem
    Using `epp_dodger` essentially means that the full module needs to be parsed, which can be expensive as modules grow in size (worst case is at least cubic). The Erlang grammar is such that forms can identified  directly from the scanner output (find the next `'dot`), and given a form, deciding if we are in a relevant directive is trivial match. This means that we can drop the `epp_dodger` and use a linear, scan-based algorithm.
    
    Dropping `epp_dodger` and `erl_syntax` let us use the dependency-analyzer also in contexts where we don't have a full OTP toolchain (like when bootstrapping OTP itself).
    
    # This diff
    Rewrites `dependency_analyzer.erl` against `erl_scan`, with `erl_parse:parse_term/1` reserved for the inner term of the relevant directives. The token stream is processed form-by-form via `erl_scan:tokens/3`; and we read one line at a time to reduce memory usage.
    
    Reviewed By: TheGeorge, michalmuskala
    
    Differential Revision: D105181045
    
    fbshipit-source-id: 1481420152dc7c91e46df4d43eb963a9e3bad015

--- prelude/erlang/toolchain/dependency_analyzer.erl
+++ prelude/erlang/toolchain/dependency_analyzer.erl
@@ -1,3 +1,4 @@
+%% @format
 %% Copyright (c) Meta Platforms, Inc. and affiliates.
 %%
 %% This source code is licensed under both the MIT license found in the
@@ -22,7 +23,8 @@
             | include_lib
             | behaviour
             | parse_transform
-            | manual_dependency,
+            | manual_dependency
+            | doc_file,
   file  := <<"header_or_source_file.(h|e)rl">>,
   app   => application %% only for include_lib
   },
@@ -33,60 +35,14 @@
 
 -export([main/1]).
 
-%%% AST nodes to parse
-%%%
-%%% NOTE: This implementation parses the raw syntax tree structures from erl_syntax
-%%%       instead of using the accessors, to use pattern-matching during the processing.
+-type entry() ::
+    #{type := include, file := binary()}
+    | #{type := include_lib, file := binary(), app := binary()}
+    | #{type := behaviour, file := binary()}
+    | #{type := parse_transform, file := binary()}
+    | #{type := manual_dependency, file := binary()}
+    | #{type := doc_file, file := binary()}.
 
-%% -include("header.hrl").
--define(MATCH_INCLUDE(Include),
-    {tree, attribute, _, {attribute, {atom, _, include}, [{string, _, Include}]}}
-).
-
-%% -include_lib("app/include/header.hrl").
--define(MATCH_INCLUDE_LIB(Include),
-    {tree, attribute, _, {attribute, {atom, _, include_lib}, [{string, _, Include}]}}
-).
-
-%% -behavior(Module).
--define(MATCH_BEHAVIOR(Module),
-    {tree, attribute, _, {attribute, {tree, atom, _, behavior}, [{tree, atom, _, Module}]}}
-).
-
-%% -behaviour(Module).
--define(MATCH_BEHAVIOUR(Module),
-    {tree, attribute, _, {attribute, {tree, atom, _, behaviour}, [{tree, atom, _, Module}]}}
-).
-
-%% -compile({parse_transform, Module}).
--define(MATCH_PARSETRANSFORM(Module),
-    {tree, attribute, _,
-        {attribute, {tree, atom, _, compile}, [
-            {tree, tuple, _, [
-                {tree, atom, _, parse_transform},
-                {tree, atom, _, Module}
-            ]}
-        ]}}
-).
-
-%% -build_dependencies(Modules)
--define(MATCH_MANUAL_DEPENDENCIES(Modules),
-    {tree, attribute, _, {attribute, {tree, atom, _, build_dependencies}, [{tree, list, _, {list, Modules, none}}]}}
-).
-
-%% -moduledoc({file, "path"}).
--define(MATCH_MODULEDOC_FILE(Path),
-    {tree, attribute, _,
-        {attribute, {tree, atom, _, moduledoc}, [{tree, tuple, _, [{tree, atom, _, file}, {tree, string, _, Path}]}]}}
-).
-
-%% -doc({file, "path"}).
--define(MATCH_DOC_FILE(Path),
-    {tree, attribute, _,
-        {attribute, {tree, atom, _, doc}, [{tree, tuple, _, [{tree, atom, _, file}, {tree, string, _, Path}]}]}}
-).
-
-%% entry point
 -spec main([string()]) -> ok.
 main([InFile]) ->
     do(InFile, stdout);
@@ -100,63 +56,133 @@ main(_) ->
 usage() ->
     io:format("dependency_analyzer.escript some_file.(h|e)rl [out.term]").
 
--spec do(file:filename(), {file, file:filename()} | stdout) -> ok.
-do(InFile0, Outspec) ->
+-spec do(InFile, OutSpec) -> ok when
+    InFile :: file:filename(),
+    OutSpec :: {file, file:filename()} | stdout.
+do(InFile0, OutSpec) ->
     InFile = dependency_utils:chars_to_binary(InFile0),
-    {ok, Forms} = epp_dodger:parse_file(InFile),
-    Dependencies = lists:sort(process_forms(Forms, [])),
-    case Outspec of
-        {file, OutFile0} ->
-            OutData = erlang:term_to_binary(Dependencies, [deterministic]),
-            OutFile = dependency_utils:chars_to_binary(OutFile0),
-            ok = prim_file:write_file(OutFile, OutData);
-        stdout ->
-            io:format("~tp~n", [Dependencies])
+    {ok, File} = file:open(InFile, [read, raw, binary, read_ahead]),
+    try
+        Dependencies = lists:sort(scan_file(File, [], [], 1, [])),
+        case OutSpec of
+            {file, OutFile0} ->
+                OutData = erlang:term_to_binary(Dependencies, [deterministic]),
+                OutFile = dependency_utils:chars_to_binary(OutFile0),
+                ok = prim_file:write_file(OutFile, OutData);
+            stdout ->
+                io:format("~tp~n", [Dependencies])
+        end
+    after
+        ok = file:close(File)
     end.
 
--spec process_forms(erl_syntax:forms(), [file:filename()]) -> [#{string() => string()}].
-process_forms([], Acc) ->
-    Acc;
-process_forms([?MATCH_INCLUDE(Include) | Rest], Acc) ->
-    Dependency = #{file => list_to_binary(filename:basename(Include)), type => include},
-    process_forms(Rest, [Dependency | Acc]);
-process_forms([?MATCH_INCLUDE_LIB(IncludeLib) | Rest], Acc) ->
-    Dependency =
-        case filename:split(IncludeLib) of
-            [App, "include", Include] ->
-                #{
-                    app => list_to_atom(App),
-                    file => list_to_binary(Include),
-                    type => include_lib
-                };
-            _ ->
-                error(malformed_header_include_lib)
-        end,
-    process_forms(Rest, [Dependency | Acc]);
-process_forms([?MATCH_BEHAVIOR(Module) | Rest], Acc) ->
-    Dependency = #{file => module_to_erl(Module), type => behaviour},
-    process_forms(Rest, [Dependency | Acc]);
-process_forms([?MATCH_BEHAVIOUR(Module) | Rest], Acc) ->
-    Dependency = #{file => module_to_erl(Module), type => behaviour},
-    process_forms(Rest, [Dependency | Acc]);
-process_forms([?MATCH_PARSETRANSFORM(Module) | Rest], Acc) ->
-    Dependency = #{file => module_to_erl(Module), type => parse_transform},
-    process_forms(Rest, [Dependency | Acc]);
-process_forms([?MATCH_MANUAL_DEPENDENCIES(Modules) | Rest], Acc) ->
-    Dependencies = [
-        #{file => module_to_erl(Module), type => manual_dependency}
-     || {tree, atom, _, Module} <- Modules
-    ],
-    process_forms(Rest, Dependencies ++ Acc);
-process_forms([?MATCH_MODULEDOC_FILE(Path) | Rest], Acc) ->
-    Dependency = #{file => list_to_binary(Path), type => doc_file},
-    process_forms(Rest, [Dependency | Acc]);
-process_forms([?MATCH_DOC_FILE(Path) | Rest], Acc) ->
-    Dependency = #{file => list_to_binary(Path), type => doc_file},
-    process_forms(Rest, [Dependency | Acc]);
-process_forms([_ | Rest], Acc) ->
-    process_forms(Rest, Acc).
+-spec scan_file(File, Cont, Chars, Loc, Acc) -> [entry()] when
+    File :: file:io_device(),
+    Cont :: erl_scan:return_cont(),
+    Chars :: string(),
+    Loc :: erl_anno:location(),
+    Acc :: [entry()].
+scan_file(File, Cont, Chars, Loc, Acc) ->
+    case erl_scan:tokens(Cont, Chars, Loc) of
+        {done, {ok, Tokens, NextLoc}, Rest} ->
+            scan_file(File, [], Rest, NextLoc, extract_dependencies(Tokens) ++ Acc);
+        {done, {error, _ErrInfo, NextLoc}, Rest} ->
+            scan_file(File, [], Rest, NextLoc, Acc);
+        {done, {eof, _NextLoc}, _Rest} ->
+            Acc;
+        {more, NewCont} ->
+            case file:read_line(File) of
+                {ok, Bin} ->
+                    scan_file(File, NewCont, to_charlist(Bin), Loc, Acc);
+                eof ->
+                    case erl_scan:tokens(NewCont, eof, Loc) of
+                        {done, {ok, Tokens, _}, _} -> extract_dependencies(Tokens) ++ Acc;
+                        _ -> Acc
+                    end
+            end
+    end.
 
--spec module_to_erl(module()) -> file:filename().
+-spec extract_dependencies(Tokens) -> [entry()] when
+    Tokens :: [erl_scan:token()].
+extract_dependencies([{'-', _}, {atom, _, Name} | Rest]) when
+    Name =:= include orelse
+        Name =:= include_lib orelse
+        Name =:= behaviour orelse
+        Name =:= behavior orelse
+        Name =:= doc orelse
+        Name =:= moduledoc orelse
+        Name =:= compile orelse
+        Name =:= build_dependencies
+->
+    case parse_attribute_value(Name, Rest) of
+        {include, MaybePath} when is_list(MaybePath) ->
+            [#{type => include, file => filename:basename(F)} || {ok, F} <- [try_characters_to_binary(MaybePath)]];
+        {include_lib, MaybePath} when is_list(MaybePath) ->
+            case try_characters_to_binary(MaybePath) of
+                {ok, Path} ->
+                    case filename:split(Path) of
+                        [App, <<"include">>, Include] -> [#{type => include_lib, app => App, file => Include}];
+                        _ -> []
+                    end;
+                error ->
+                    []
+            end;
+        {behaviour, M} when is_atom(M) ->
+            [#{type => behaviour, file => module_to_erl(M)}];
+        {behavior, M} when is_atom(M) ->
+            [#{type => behaviour, file => module_to_erl(M)}];
+        {doc, {file, MaybePath}} when is_list(MaybePath) ->
+            [#{type => doc_file, file => F} || {ok, F} <- [try_characters_to_binary(MaybePath)]];
+        {moduledoc, {file, MaybePath}} when is_list(MaybePath) ->
+            [#{type => doc_file, file => F} || {ok, F} <- [try_characters_to_binary(MaybePath)]];
+        {compile, Term} ->
+            [#{type => parse_transform, file => module_to_erl(M)} || M <- collect_parse_transforms(Term)];
+        {build_dependencies, Modules} when is_list(Modules) ->
+            [#{type => manual_dependency, file => module_to_erl(M)} || M <- Modules, is_atom(M)];
+        _ ->
+            []
+    end;
+extract_dependencies(_) ->
+    [].
+
+-spec parse_attribute_value(Name, Tokens) -> {Name, Value} | error when
+    Name :: atom(),
+    Tokens :: [erl_scan:token()],
+    Value :: term().
+parse_attribute_value(Name, Tokens) ->
+    case erl_parse:parse_term(Tokens) of
+        {ok, Term} -> {Name, Term};
+        {error, _} -> error
+    end.
+
+-spec module_to_erl(Module) -> binary() when
+    Module :: module().
 module_to_erl(Module) ->
     <<(atom_to_binary(Module))/binary, ".erl"/utf8>>.
+
+-spec to_charlist(Bin) -> string() when
+    Bin :: binary().
+to_charlist(Bin) ->
+    case unicode:characters_to_list(Bin) of
+        L when is_list(L) -> L;
+        _ -> binary_to_list(Bin)
+    end.
+
+-spec try_characters_to_binary(Value) -> {ok, binary()} | error when
+    Value :: term().
+try_characters_to_binary(Value) ->
+    try unicode:characters_to_binary(Value) of
+        B when is_binary(B) -> {ok, B};
+        _ -> error
+    catch
+        _:_ -> error
+    end.
+
+-spec collect_parse_transforms(Term) -> [module()] when
+    Term :: term().
+collect_parse_transforms({parse_transform, M}) when is_atom(M) ->
+    [M];
+collect_parse_transforms(L) when is_list(L) ->
+    [M || {parse_transform, M} <- L, is_atom(M)];
+collect_parse_transforms(_) ->
+    [].
