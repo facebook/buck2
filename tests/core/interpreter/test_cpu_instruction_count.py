a08b89a1ef66c1dac7ab9e46f57e3e0133d9106a
Scott Cao <scottcao@meta.com>
2026-08-29 18:44:00 -0700

    Fix flakey cpu instruction count test
    Summary:
    Skipping is counted as flakey by our test infra, so we need it to just pass if we want a test case to "skip".
    
    ___
    
    Differential Revision: D118016127
    
    fbshipit-source-id: d7b0e715463b84e4a1c8a8ef5f553ed379d991f8

--- tests/core/interpreter/test_cpu_instruction_count.py
+++ tests/core/interpreter/test_cpu_instruction_count.py
@@ -8,10 +8,9 @@
 
 # pyre-strict
 
-
+import logging
 import platform
 
-import pytest
 from buck2.tests.e2e_util.api.buck import Buck
 from buck2.tests.e2e_util.buck_workspace import buck_test
 from buck2.tests.e2e_util.helper.utils import filter_events
@@ -35,8 +34,7 @@ async def test_cpu_instruction_count(buck: Buck) -> None:
     # We only populate counters on Linux
     if platform.system() == "Linux":
         if cpu_instruction_count is None:
-            # warnings.warn(
-            pytest.xfail(
+            logging.warning(
                 "cpu_instruction_count is None, but we expect it to be populated on Linux most of the time. This is not a failure."
             )
         else:
